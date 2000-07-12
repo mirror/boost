5db0bd68099525a73d83a8c8e7701ee22519a40c
mark_rodgers <mark_rodgers@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2000-07-12 08:31:46 +0000

    Added missing BOOST_NO_USING_TEMPLATE docs and moved this since it infects BCB4 as well and BCC5.5. Added BOOST_NO_POINTER_TO_MEMBER_CONST at the suggestion of Fabrice Truillot.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@7624 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config.hpp
+++ boost/boost/config.hpp
@@ -63,6 +63,10 @@
 //  friend functions to be defined at namespace scope, then using'ed to boost.
 //  Probably GCC specific.  See boost/operators.hpp for example.
 
+//  BOOST_NO_POINTER_TO_MEMBER_CONST: The compiler does not correctly handle
+//  pointers to const member functions, preventing use of these in overloaded 
+//  function templates.  See boost/functional.hpp for example. 
+
 //  BOOST_NO_STD_ITERATOR: The C++ implementation fails to provide the
 //  std::iterator class.
 
@@ -80,7 +84,9 @@
 //  BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION. Class template partial
 //  specialization (14.5.4 [temp.class.spec]) not supported.
 
-//  BOOST_NO_USING_TEMPLATE: This needs some docs!
+//  BOOST_NO_USING_TEMPLATE: The compiler will not accept a using declaration
+//  that imports a template from the global namespace into a named namespace.
+//  Probably Borland specific.
 
 //  Compiler Control or Information Macros  ----------------------------------//
 //
@@ -142,10 +148,10 @@
 #   if __BORLANDC__ <= 0x0550
 // Borland C++ Builder 4 and 5:
 #   define BOOST_NO_MEMBER_TEMPLATE_FRIENDS
+#   define BOOST_NO_USING_TEMPLATE
 #     if __BORLANDC__ == 0x0550
 // Borland C++ Builder 5, command-line compiler 5.5:
 #       define BOOST_NO_OPERATORS_IN_NAMESPACE
-#       define BOOST_NO_USING_TEMPLATE
 #     endif
 #   endif
 #   if defined BOOST_DECL_EXPORTS
@@ -177,6 +183,7 @@
 # elif defined  __MWERKS__
 #   if __MWERKS__ <= 0x2301
 #     define BOOST_NO_MEMBER_TEMPLATE_FRIENDS
+#     define BOOST_NO_POINTER_TO_MEMBER_CONST
 #   endif
 #   if __MWERKS__ >= 0x2300
 #     define BOOST_SYSTEM_HAS_STDINT_H
