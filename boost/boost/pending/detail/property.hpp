6f953bf8aacbd8e8e122689fe809a46aa0df9501
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2000-09-25 17:58:52 +0000

    plugin -> property name change
    git-svn-id: http://svn.boost.org/svn/boost/trunk@7831 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/pending/detail/property.hpp
+++ boost/boost/pending/detail/property.hpp
@@ -1,5 +1,5 @@
-#ifndef BOOST_DETAIL_PLUGIN_HPP
-#define BOOST_DETAIL_PLUGIN_HPP
+#ifndef BOOST_DETAIL_PROPERTY_HPP
+#define BOOST_DETAIL_PROPERTY_HPP
 
 #include <utility> // for std::pair
 
@@ -8,60 +8,60 @@ namespace boost {
   namespace detail {
 
     template <int TagMatched>
-    struct plugin_value_dispatch { };
+    struct property_value_dispatch { };
 
     template <>
-    struct plugin_value_dispatch<1> {
-      template <class Plugin, class T, class Tag>
-      static T& get_value(Plugin& p, T, Tag) {
+    struct property_value_dispatch<1> {
+      template <class Property, class T, class Tag>
+      static T& get_value(Property& p, T, Tag) {
 	return p.m_value; 
       }
-      template <class Plugin, class T, class Tag>
-      static const T& const_get_value(const Plugin& p, T, Tag) {
+      template <class Property, class T, class Tag>
+      static const T& const_get_value(const Property& p, T, Tag) {
 	return p.m_value; 
       }
     };
     template <>
-    struct plugin_value_dispatch<0> {
-      template <class Plugin, class T, class Tag>
-      static T& get_value(Plugin& p, T t, Tag tag) {
-	typedef typename Plugin::next_type Next;
+    struct property_value_dispatch<0> {
+      template <class Property, class T, class Tag>
+      static T& get_value(Property& p, T t, Tag tag) {
+	typedef typename Property::next_type Next;
 	typedef typename Next::tag_type Next_tag;
 	enum { match = int(Next_tag::num) == int(Tag::num) };
-	return plugin_value_dispatch<match>
+	return property_value_dispatch<match>
 	  ::get_value(static_cast<Next&>(p), t, tag);
       }
-      template <class Plugin, class T, class Tag>
-      static const T& const_get_value(const Plugin& p, T t, Tag tag) {
-	typedef typename Plugin::next_type Next;
+      template <class Property, class T, class Tag>
+      static const T& const_get_value(const Property& p, T t, Tag tag) {
+	typedef typename Property::next_type Next;
 	typedef typename Next::tag_type Next_tag;
 	enum { match = int(Next_tag::num) == int(Tag::num) };
-	return plugin_value_dispatch<match>
+	return property_value_dispatch<match>
 	  ::const_get_value(static_cast<const Next&>(p), t, tag);
       }
     };
 
-    template <class Plugin>
-    struct build_plugin_tag_value_alist
+    template <class Property>
+    struct build_property_tag_value_alist
     {
-      typedef typename Plugin::next_type NextPlugin;
-      typedef typename Plugin::value_type Value;
-      typedef typename Plugin::tag_type Tag;
-      typedef typename build_plugin_tag_value_alist<NextPlugin>::type Next;
+      typedef typename Property::next_type NextProperty;
+      typedef typename Property::value_type Value;
+      typedef typename Property::tag_type Tag;
+      typedef typename build_property_tag_value_alist<NextProperty>::type Next;
       typedef std::pair< std::pair<Tag,Value>, Next> type;
     };
     template <>
-    struct build_plugin_tag_value_alist<no_plugin>
+    struct build_property_tag_value_alist<no_property>
     {
-      typedef no_plugin type;
+      typedef no_property type;
     };
 
-    struct error_plugin_not_found { };
+    struct error_property_not_found { };
 
 #if !defined BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
     template <class TagValueAList, class Tag>
     struct extract_value {
-      typedef error_plugin_not_found type;
+      typedef error_property_not_found type;
     };
     template <class Value, class Tag1, class Tag2, class Rest>
     struct extract_value< std::pair<std::pair<Tag1,Value>,Rest>, Tag2> {
@@ -85,7 +85,7 @@ namespace boost {
     template <class TagValueAList>
     struct ev_selector { typedef recursive_extract type; };
     template <>
-    struct ev_selector<no_plugin> { typedef end_extract type; };
+    struct ev_selector<no_property> { typedef end_extract type; };
 
     struct recursive_extract {
       template <class TagValueAList, class Tag1>
@@ -105,7 +105,7 @@ namespace boost {
     struct end_extract {
       template <class AList, class Tag1>
       struct bind {
-	typedef error_plugin_not_found type;
+	typedef error_property_not_found type;
       };
     };
 #endif //!defined BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
@@ -113,4 +113,4 @@ namespace boost {
   } // namespace detail 
 } // namespace boost
 
-#endif // BOOST_DETAIL_PLUGIN_HPP
+#endif // BOOST_DETAIL_PROPERTY_HPP
