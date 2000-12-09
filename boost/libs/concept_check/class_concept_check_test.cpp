e71c16fd5eceacce5d881091cd340a064f6f86f3
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2000-12-09 15:24:02 +0000

    changing directory name, and reorganizing docs
    git-svn-id: http://svn.boost.org/svn/boost/trunk@8410 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/concept_check/class_concept_check_test.cpp
@@ -0,0 +1,37 @@
+// (C) Copyright Jeremy Siek 2000. Permission to copy, use, modify,
+// sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+
+#include <boost/pending/concept_checks.hpp>
+
+/*
+
+  This file verifies that the BOOST_CLASS_REQUIRES macro of the Boost
+  Concept Checking Library does not cause errors when it is not suppose
+  to.
+
+*/
+
+struct foo { bool operator()(int) { return true; } };
+struct bar { bool operator()(int, char) { return true; } };
+
+using namespace boost;
+
+class class_requires_test
+{
+  BOOST_CLASS_REQUIRES(int, EqualityComparableConcept);
+  typedef int* int_ptr; typedef const int* const_int_ptr;
+  BOOST_CLASS_REQUIRES2(int_ptr, const_int_ptr, Comparable2Concept);
+  BOOST_CLASS_REQUIRES3(foo, bool, int, UnaryFunctionConcept);
+  BOOST_CLASS_REQUIRES4(bar, bool, int, char, BinaryFunctionConcept);
+};
+
+int
+main()
+{
+  class_requires_test x;
+  ignore_unused_variable_warning(x);
+  return 0;
+}
