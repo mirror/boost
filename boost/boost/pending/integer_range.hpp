e03f41570657fc242360f8c8c782b625d69ebcf1
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-02-04 23:37:01 +0000

    Factored counting_iterator stuff into boost/counting_iterator.hpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@8935 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/pending/integer_range.hpp
+++ boost/boost/pending/integer_range.hpp
@@ -1,63 +1,27 @@
-// (C) Copyright David Abrahams 2000. Permission to copy, use,
-// modify, sell and distribute this software is granted provided this
-// copyright notice appears in all copies. This software is provided
-// "as is" without express or implied warranty, and with no claim as
-// to its suitability for any purpose.
+// (C) Copyright David Abrahams and Jeremy Siek 2000-2001. Permission to copy,
+// use, modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided "as is"
+// without express or implied warranty, and with no claim as to its suitability
+// for any purpose.
 //
-// (C) Copyright Jeremy Siek 2000. Permission to copy, use, modify,
-// sell and distribute this software is granted provided this
-// copyright notice appears in all copies. This software is provided
-// "as is" without express or implied warranty, and with no claim as
-// to its suitability for any purpose.
+// Revision History:
+// 04 Jan 2001  Factored counting_iterator stuff into
+//              boost/counting_iterator.hpp (David Abrahams)
 
 #ifndef BOOST_INTEGER_RANGE_HPP_
 #define BOOST_INTEGER_RANGE_HPP_
 
 #include <boost/config.hpp>
-
-#if 1
-// Still evaluating whether VC++ can handle this.
-#define BOOST_USE_ITERATOR_ADAPTORS
-#endif
-
-#ifdef BOOST_USE_ITERATOR_ADAPTORS
-#include <boost/pending/iterator_adaptors.hpp>
-#else
-#include <boost/pending/detail/int_iterator.hpp>
-#endif
+#include <boost/counting_iterator.hpp>
 
 namespace boost {
 
 //=============================================================================
 // Counting Iterator and Integer Range Class
 
-#ifdef BOOST_USE_ITERATOR_ADAPTORS
-template <class IntegerType>
-struct counting_iterator_policies : public default_iterator_policies
-{
-    IntegerType
-    dereference(type<IntegerType>, const IntegerType& i) const
-        { return i; }
-};
-template <class IntegerType>
-struct counting_iterator_traits {
-    typedef IntegerType value_type;
-    typedef IntegerType reference;
-    typedef value_type* pointer;
-    typedef std::ptrdiff_t difference_type;
-    typedef std::random_access_iterator_tag iterator_category;
-};
-#endif
-
 template <class IntegerType>
 struct integer_range {
-#ifdef BOOST_USE_ITERATOR_ADAPTORS
-    typedef iterator_adaptor<IntegerType,
-      counting_iterator_policies<IntegerType>,
-      counting_iterator_traits<IntegerType> > iterator;
-#else
-    typedef int_iterator<IntegerType> iterator;
-#endif
+    typedef typename counting_iterator_generator<IntegerType>::type iterator;
 
     typedef iterator const_iterator;
     typedef IntegerType value_type;
@@ -91,9 +55,5 @@ make_integer_range(IntegerType first, IntegerType last)
 }
 
 } // namespace boost
-
-#ifdef BOOST_USE_ITERATOR_ADAPTORS
-#undef BOOST_USE_ITERATOR_ADAPTORS
-#endif
 
 #endif // BOOST_INTEGER_RANGE_HPP_
