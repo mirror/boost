dea0c04c4c3eaee2fb9f5ecb3a4f4ba3fa345571
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-02-09 15:23:55 +0000

    Applied John Maddock's Borland patch Moving <true> specialization to unspecialized template
    git-svn-id: http://svn.boost.org/svn/boost/trunk@9058 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/select_type.hpp
+++ boost/boost/detail/select_type.hpp
@@ -7,7 +7,9 @@
 // See http://www.boost.org for most recent version including documentation.
 
 // Revision History
-// 06 Feb 2001 - Created (David Abrahams)
+// 09 Feb 01  Applied John Maddock's Borland patch Moving <true>
+//            specialization to unspecialized template (David Abrahams)
+// 06 Feb 01  Created (David Abrahams)
 
 #ifndef SELECT_TYPE_DWA20010206_HPP
 # define SELECT_TYPE_DWA20010206_HPP
@@ -17,10 +19,8 @@ namespace boost { namespace detail {
   // Template class if_true -- select among 2 types based on a bool constant expression
   // Usage:
   //   typename if_true<(bool_const_expression)>::template then<true_type, false_type>::type
-  template <bool> struct if_true;
-
-  template <>
-  struct if_true<true>
+  template <bool>
+  struct if_true
   {
       template <class T, class F>
       struct then { typedef T type; };
