f87b6e5635031976ca7e5271095cdd839f38662a
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-02-12 01:50:50 +0000

    finished 1st draft
    git-svn-id: http://svn.boost.org/svn/boost/trunk@9143 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/utility/indirect_iterator_example.cpp
+++ boost/libs/utility/indirect_iterator_example.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <functional>
 #include <boost/iterator_adaptors.hpp>
 
 int main(int, char*[])
@@ -16,11 +17,44 @@ int main(int, char*[])
   char* pointers_to_chars[N];                        // at the end.
   for (int i = 0; i < N; ++i)
     pointers_to_chars[i] = &characters[i];
+
+  // Example of using indirect_iterator_generator
   
   boost::indirect_iterator_generator<char**, char>::type 
     indirect_first(pointers_to_chars), indirect_last(pointers_to_chars + N);
 
   std::copy(indirect_first, indirect_last, std::ostream_iterator<char>(std::cout, ","));
+  std::cout << std::endl;
+  
+
+  // Example of using indirect_iterator_pair_generator
+
+  typedef boost::indirect_iterator_pair_generator<char**,
+    char, char*, char&, const char*, const char&> PairGen;
+
+  char mutable_characters[N];
+  char* pointers_to_mutable_chars[N];
+  for (int i = 0; i < N; ++i)
+    pointers_to_mutable_chars[i] = &mutable_characters[i];
+
+  PairGen::iterator mutable_indirect_first(pointers_to_mutable_chars),
+    mutable_indirect_last(pointers_to_mutable_chars + N);
+  PairGen::const_iterator const_indirect_first(pointers_to_chars),
+    const_indirect_last(pointers_to_chars + N);
+
+  std::transform(const_indirect_first, const_indirect_last,
+		 mutable_indirect_first, std::bind1st(std::plus<char>(), 1));
+
+  std::copy(mutable_indirect_first, mutable_indirect_last,
+	    std::ostream_iterator<char>(std::cout, ","));
+  std::cout << std::endl;
+
+  
+  // Example of using make_indirect_iterator()
+
+  std::copy(boost::make_indirect_iterator(pointers_to_chars), 
+	    boost::make_indirect_iterator(pointers_to_chars + N),
+	    std::ostream_iterator<char>(std::cout, ","));
   std::cout << std::endl;
   
   return 0;
