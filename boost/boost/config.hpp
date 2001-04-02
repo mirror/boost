1348dd971532753e474fb2b8476310c371705d87
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-04-02 11:35:49 +0000

    fixed config.hpp to detect missing <limits> header
    git-svn-id: http://svn.boost.org/svn/boost/trunk@9692 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config.hpp
+++ boost/boost/config.hpp
@@ -236,8 +236,10 @@
 #      define BOOST_NO_AUTO_PTR
 #   endif
 #   if __GNUC__ == 2 && __GNUC_MINOR__ <= 97
-#     include <iterator>  // not sure this is the right way to do this -JGS
-#     if defined(__STL_CONFIG_H) && !defined(__SGI_STL_INTERNAL_ITERATOR_H) && !defined(__GLIBCPP__) && !defined(_CXXRT_STD) && !defined(__SGI_STL) // need to ask Dietmar about this -JGS
+#     include <string>  // not sure this is the right way to do this -JGS
+#     if defined(__BASTRING__) && !defined(__GLIBCPP__) && !defined(_CXXRT_STD) && !defined(__SGI_STL) // need to ask Dietmar about this -JGS
+        // this should only detect the stdlibc++ that ships with gcc, and
+        // not any replacements that may be installed...
 #       define BOOST_NO_STD_ITERATOR
 #       define BOOST_NO_LIMITS
 #     endif
