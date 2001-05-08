fb5772775e834d91e41e06800ec20c27c730db6e
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-05-08 11:11:30 +0000

    Fixed more wchar_t problems
    git-svn-id: http://svn.boost.org/svn/boost/trunk@10058 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/integer_traits.hpp
+++ boost/boost/integer_traits.hpp
@@ -85,8 +85,8 @@ class integer_traits<unsigned char>
 template<>
 class integer_traits<wchar_t>
   : public std::numeric_limits<wchar_t>,
-#if defined(__BORLANDC__)
-    // Borland C++ does not have WCHAR_MIN and WCHAR_MAX
+#if defined(__BORLANDC__) || defined(__CYGWIN__) || defined(__MINGW32__) || (defined(__BEOS__) && defined(__GNUC__))
+    // No WCHAR_MIN and WCHAR_MAX, whar_t is short and unsigned:
     public detail::integer_traits_base<wchar_t, 0, 0xffff>
 #elif defined(__sgi) && (!defined(__SGI_STL_PORT) || __SGI_STL_PORT < 0x400)
     // SGI MIPSpro with native library doesn't have them, either
@@ -171,3 +171,4 @@ class integer_traits<unsigned long long>
 } // namespace boost
 
 #endif /* BOOST_INTEGER_TRAITS_HPP */
+
