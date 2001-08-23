c9917cce68e945e0c5ddfbb9d76bdb504e94432b
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-08-23 19:05:21 +0000

    Tabs converted to spaces.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@10916 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/ref.hpp
+++ boost/boost/ref.hpp
@@ -6,18 +6,18 @@
 #endif
 
 //
-//	ref.hpp - ref/cref, useful helper functions
+//  ref.hpp - ref/cref, useful helper functions
 //
-//	Version 1.00.0003 (2001-08-22)
+//  Version 1.00.0003 (2001-08-22)
 //
-//	Copyright (C) 1999, 2000 Jaakko Järvi (jaakko.jarvi@cs.utu.fi)
+//  Copyright (C) 1999, 2000 Jaakko Järvi (jaakko.jarvi@cs.utu.fi)
 //
-//	Permission to copy, use, modify, sell and distribute this software
-//	is granted provided this copyright notice appears in all copies.
-//	This software is provided "as is" without express or implied
-//	warranty, and with no claim as to its suitability for any purpose.
+//  Permission to copy, use, modify, sell and distribute this software
+//  is granted provided this copyright notice appears in all copies.
+//  This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
 //
-//	See http://www.boost.org/libs/bind/ref.html for documentation.
+//  See http://www.boost.org/libs/bind/ref.html for documentation.
 //
 
 namespace boost
@@ -27,27 +27,27 @@ template<class T> class reference_wrapper
 { 
 public:
 
-	explicit reference_wrapper(T & t): t_(t) {}
+    explicit reference_wrapper(T & t): t_(t) {}
 
-	operator T & () const { return t_; }
+    operator T & () const { return t_; }
 
-	T & get() const { return t_; }
+    T & get() const { return t_; }
 
 private:
 
-	  T & t_;
+    T & t_;
 
-	  reference_wrapper & operator= (reference_wrapper const &);
+    reference_wrapper & operator= (reference_wrapper const &);
 };
 
 template<class T> inline reference_wrapper<T> ref(T & t)
 { 
-	return reference_wrapper<T>(t);
+    return reference_wrapper<T>(t);
 }
 
 template<class T> inline reference_wrapper<T const> cref(T const & t)
 {
-	return reference_wrapper<T const>(t);
+    return reference_wrapper<T const>(t);
 }
 
 } // namespace boost
