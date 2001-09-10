00718f332943a8b8b895fd3df67bb64b17a601c7
beman_dawes <beman_dawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-09-10 14:04:28 +0000

    Initial base_from_member commit
    git-svn-id: http://svn.boost.org/svn/boost/trunk@11086 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/utility.hpp
+++ boost/boost/utility.hpp
@@ -10,23 +10,15 @@
 
 //  Classes appear in alphabetical order
 
-//  Revision History
-//  21 May 01  checked_delete() and checked_array_delete() added (Beman Dawes,
-//             suggested by Dave Abrahams, generalizing idea from Vladimir Prus)
-//  21 May 01  made next() and prior() inline (Beman Dawes)  
-//  26 Jan 00  protected noncopyable destructor added (Miki Jovanovic)
-//  10 Dec 99  next() and prior() templates added (Dave Abrahams)
-//  30 Aug 99  moved cast templates to cast.hpp (Beman Dawes)
-//   3 Aug 99  cast templates added
-//  20 Jul 99  name changed to utility.hpp 
-//   9 Jun 99  protected noncopyable default ctor
-//   2 Jun 99  Initial Version. Class noncopyable only contents (Dave Abrahams)
-
 #ifndef BOOST_UTILITY_HPP
 #define BOOST_UTILITY_HPP
 
-#include <boost/config.hpp>        // broken compiler workarounds 
-#include <boost/static_assert.hpp> 
+#include <boost/config.hpp>        // broken compiler workarounds
+#include <boost/static_assert.hpp>
+
+// certain headers are part of the <utility.hpp> interface
+#include <boost/utility/base_from_member.hpp>  
+ 
 #include <cstddef>                 // for size_t
 #include <utility>                 // for std::pair
 
