96904a941e4ceb65550ef2add7d9c3af2963a6e0
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-10-08 11:28:39 +0000

    Added sunpro 5.3 options
    git-svn-id: http://svn.boost.org/svn/boost/trunk@11351 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/sunpro_cc.hpp
+++ boost/boost/config/compiler/sunpro_cc.hpp
@@ -7,28 +7,31 @@
 
 //  Sun C++ compiler setup:
 
-#    if __SUNPRO_CC >= 0x520
+#    if __SUNPRO_CC <= 0x500
+#      define BOOST_NO_MEMBER_TEMPLATES
+#      define BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+#    endif
+
+#    if (__SUNPRO_CC <= 0x520)
        //
-       // Sunpro 5.1 and later:
+       // Sunpro 5.3 and later:
        //
-       // although sunpro 5.1 supports the syntax for
+       // although sunpro 5.3 supports the syntax for
        // inline initialization it often gets the value
        // wrong, especially where the value is computed
        // from other constants (J Maddock 6th May 2001)
 #      define BOOST_NO_INCLASS_MEMBER_INITIALIZATION
-       // although sunpro 5.1 supports the syntax for
+       // although sunpro 5.3 supports the syntax for
        // partial specialization, it often seems to
        // bind to the wrong specialization.  Better
        // to disable it until suppport becomes more stable
        // (J Maddock 6th May 2001).
 #      define BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+#    endif
 
+#    if (__SUNPRO_CC <= 0x530) || !defined(BOOST_STRICT_CONFIG)
        // integral constant expressions with 64 bit numbers fail
 #      define BOOST_NO_INTEGRAL_INT64_T
-#    endif
-#    if __SUNPRO_CC <= 0x500
-#      define BOOST_NO_MEMBER_TEMPLATES
-#      define BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 #    endif
 
 #define BOOST_COMPILER "Sun compiler version " BOOST_STRINGIZE(__SUNPRO_CC)
@@ -40,11 +43,12 @@
 #error "Compiler not supported or configured - please reconfigure"
 #endif
 //
-// last known and checked version is 0x520:
-#if (__SUNPRO_CC > 0x520)
+// last known and checked version is 0x530:
+#if (__SUNPRO_CC > 0x530)
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
 #  endif
 #endif
+
 
 
