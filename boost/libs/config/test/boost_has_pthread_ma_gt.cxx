1d2c66e926b094a35be84e111c5819d4fbc224aa
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-10-11 12:16:21 +0000

    Added thread lib config options
    git-svn-id: http://svn.boost.org/svn/boost/trunk@11374 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/config/test/boost_has_pthread_ma_gt.cxx
@@ -0,0 +1,33 @@
+//  (C) Copyright John Maddock 2001. Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
+//  MACRO:         BOOST_HAS_PTHREAD_MUTEXATTR_SETTYPE
+//  TITLE:         pthread_mutexattr_gettype
+//  DESCRIPTION:   The platform supports POSIX API pthread_mutexattr_gettype.
+
+#include <pthread.h>
+
+
+namespace boost_has_pthread_mutexattr_gettype{
+
+void f()
+{
+    // this is never called, it just has to compile:
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    int type;
+    pthread_mutexattr_gettype(&attr, &type);
+}
+
+int test()
+{
+   return 0;
+}
+
+}
+
+
+
+
