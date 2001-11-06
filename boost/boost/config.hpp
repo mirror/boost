b3807e9c310843316f7454ad03efa3aad19449d1
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-11-06 16:56:22 +0000

    changed ordering to get VC++ pragma's (truncation) before <utility> gets included (in select_stdlib_config.hpp)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@11615 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config.hpp
+++ boost/boost/config.hpp
@@ -30,23 +30,23 @@
 #if !defined(BOOST_COMPILER_CONFIG) && !defined(BOOST_NO_COMPILER_CONFIG) && !defined(BOOST_NO_CONFIG)
 #  include <boost/config/select_compiler_config.hpp>
 #endif
-// if we don't have a std library config set, try and find one:
-#if !defined(BOOST_STDLIB_CONFIG) && !defined(BOOST_NO_STDLIB_CONFIG) && !defined(BOOST_NO_CONFIG)
-#  include <boost/config/select_stdlib_config.hpp>
-#endif
-// if we don't have a platform config set, try and find one:
-#if !defined(BOOST_PLATFORM_CONFIG) && !defined(BOOST_NO_PLATFORM_CONFIG) && !defined(BOOST_NO_CONFIG)
-#  include <boost/config/select_platform_config.hpp>
-#endif
-
-
 // if we have a compiler config, include it now:
 #ifdef BOOST_COMPILER_CONFIG
 #  include BOOST_COMPILER_CONFIG
 #endif
+
+// if we don't have a std library config set, try and find one:
+#if !defined(BOOST_STDLIB_CONFIG) && !defined(BOOST_NO_STDLIB_CONFIG) && !defined(BOOST_NO_CONFIG)
+#  include <boost/config/select_stdlib_config.hpp>
+#endif
 // if we have a std library config, include it now:
 #ifdef BOOST_STDLIB_CONFIG
 #  include BOOST_STDLIB_CONFIG
+#endif
+
+// if we don't have a platform config set, try and find one:
+#if !defined(BOOST_PLATFORM_CONFIG) && !defined(BOOST_NO_PLATFORM_CONFIG) && !defined(BOOST_NO_CONFIG)
+#  include <boost/config/select_platform_config.hpp>
 #endif
 // if we have a platform config, include it now:
 #ifdef BOOST_PLATFORM_CONFIG
