6e241dc0c78cf815bcc33a90ce5b22c833712fad
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-11-25 18:39:44 +0000

    initial checkin
    git-svn-id: http://svn.boost.org/svn/boost/trunk@11781 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/preprocessor.hpp
@@ -0,0 +1,97 @@
+#ifndef BOOST_PREPROCESSOR_HPP
+#define BOOST_PREPROCESSOR_HPP
+
+//  Copyright (C) 2001
+//  Housemarque, Inc.
+//  http://www.housemarque.com
+//  
+//  Permission to copy, use, modify, sell and distribute this software is
+//  granted provided this copyright notice appears in all copies. This
+//  software is provided "as is" without express or implied warranty, and
+//  with no claim as to its suitability for any purpose.
+
+//  See http://www.boost.org for most recent version.
+
+/*! \file
+
+<a href="../../../../boost/preprocessor.hpp">Click here to see the header.</a>
+
+Includes all PREPROCESSOR library headers.
+*/
+
+#ifndef BOOST_PREPROCESSOR_ARITHMETIC_HPP
+#  include <boost/preprocessor/arithmetic.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_ASSERT_MSG_HPP
+#  include <boost/preprocessor/assert_msg.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_CAT_HPP
+#  include <boost/preprocessor/cat.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_COMMA_HPP
+#  include <boost/preprocessor/comma.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_COMMA_IF_HPP
+#  include <boost/preprocessor/comma_if.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_COMPARISON_HPP
+#  include <boost/preprocessor/comparison.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_DEC_HPP
+#  include <boost/preprocessor/dec.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_EMPTY_HPP
+#  include <boost/preprocessor/empty.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_ENUM_PARAMS_HPP
+#  include <boost/preprocessor/enum_params.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_ENUM_PARAMS_WITH_A_DEFAULT_HPP
+#  include <boost/preprocessor/enum_params_with_a_default.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_ENUM_PARAMS_WITH_DEFAULTS_HPP
+#  include <boost/preprocessor/enum_params_with_defaults.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_ENUM_SHIFTED_PARAMS_HPP
+#  include <boost/preprocessor/enum_shifted_params.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_IDENTITY_HPP
+#  include <boost/preprocessor/identity.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_IF_HPP
+#  include <boost/preprocessor/if.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_INC_HPP
+#  include <boost/preprocessor/inc.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_LIMITS_HPP
+#  include <boost/preprocessor/limits.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_LOGICAL_HPP
+#  include <boost/preprocessor/logical.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_MAX_HPP
+#  include <boost/preprocessor/max.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_MIN_HPP
+#  include <boost/preprocessor/min.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_REPEAT_HPP
+#  include <boost/preprocessor/repeat.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_REPEAT_2ND_HPP
+#  include <boost/preprocessor/repeat_2nd.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_REPEAT_3RD_HPP
+#  include <boost/preprocessor/repeat_3rd.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_STRINGIZE_HPP
+#  include <boost/preprocessor/stringize.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_TUPLE_HPP
+#  include <boost/preprocessor/tuple.hpp>
+#endif
+#ifndef BOOST_PREPROCESSOR_WHILE_HPP
+#  include <boost/preprocessor/while.hpp>
+#endif
+#endif
