61f19f90009090277d1066b3458ada5bc6ac4b76
jmaurer <jmaurer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2001-12-17 19:56:05 +0000

    improve documentation (request by David Mechner)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@12090 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/random/random_demo.cpp
+++ boost/libs/random/random_demo.cpp
@@ -34,17 +34,21 @@ namespace std {
 }
 #endif
 
-// try boost::mt19937 or boost::ecuyer1988 instead of boost::minstd_rand
+// This is a typedef for a random number generator.
+// Try boost::mt19937 or boost::ecuyer1988 instead of boost::minstd_rand
 typedef boost::minstd_rand base_generator_type;
 
-// This is a reproducible simulation experiment.
+// This is a reproducible simulation experiment.  See main().
 void experiment(base_generator_type & generator)
 {
+  // Define a uniform random number distribution of integer values between
+  // 1 and 6 inclusive.  The random numbers come from "generator".
   typedef boost::uniform_smallint<base_generator_type> generator_type;
   generator_type die_gen(generator, 1, 6);
 
 #if !defined(__SUNPRO_CC) || (__SUNPRO_CC > 0x530)
-  // For an STL iterator interface, use iterator_adaptors.hpp
+  // If you want to use an STL iterator interface, use iterator_adaptors.hpp.
+  // Unfortunately, this doesn't work on SunCC yet.
   boost::generator_iterator_generator<generator_type>::type
     die = boost::make_generator_iterator(die_gen);
   for(int i = 0; i < 10; i++)
@@ -55,23 +59,27 @@ void experiment(base_generator_type & generator)
 
 int main()
 {
-  // initialize by reproducible seed
-  // Make sure it's unsigned, otherwise the wrong overload may be selected
-  // with mt19937.
+  // Define a random number generator and initialize it with a reproducible
+  // seed.
+  // (The seed is unsigned, otherwise the wrong overload may be selected
+  // when using mt19937 as the base_generator_type.)
   base_generator_type generator(42u);
 
   std::cout << "10 samples of a uniform distribution in [0..1):\n";
-  boost::uniform_01<base_generator_type> uni(generator);
-  std::cout.setf(std::ios::fixed);
 
-  // Random number generators have an STL Generator interface
+  // Define a uniform random number distribution which produces "double"
+  // values between 0 and 1 (0 inclusive, 1 exclusive).
+  boost::uniform_01<base_generator_type> uni(generator);
+
+  std::cout.setf(std::ios::fixed);
+  // You can now retrieve random numbers from that distribution by means
+  // of a STL Generator interface, i.e. calling the generator as a zero-
+  // argument function.
   for(int i = 0; i < 10; i++)
     std::cout << uni() << '\n';
 
   /*
-   * Change seed to something else
-   * Make sure the seed is unsigned, otherwise the wrong overload may be
-   * selected with mt19937.
+   * Change seed to something else.
    *
    * Caveat: std::time(0) is not a very good truly-random seed.  When
    * called in rapid succession, it could return the same values, and
@@ -87,19 +95,27 @@ int main()
   generator.seed(static_cast<unsigned int>(std::time(0)));
 
   std::cout << "\nexperiment: roll a die 10 times:\n";
+
+  // You can save a generator's state by copy construction.
   base_generator_type saved_generator = generator;
+
+  // When calling other functions which take a generator or distribution
+  // as a parameter, make sure to always call by reference (or pointer).
+  // Calling by value invokes the copy constructor, which means that the
+  // sequence of random numbers at the caller is disconnected from the
+  // sequence at the callee.
   experiment(generator);
 
   std::cout << "redo the experiment to verify it:\n";
   experiment(saved_generator);
 
-  // after that, both generators are equivalent
+  // After that, both generators are equivalent
   assert(generator == saved_generator);
 
 #ifndef BOOST_NO_OPERATORS_IN_NAMESPACE
   {
-    // save the generator state for future use,
-    // can be read again at any time via operator>>
+    // You can save the generator state for future use.  You can read the
+    // state back in at any later time using operator>>.
     std::ofstream file("rng.saved", std::ofstream::trunc);
     file << generator;
   }
