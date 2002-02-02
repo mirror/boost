452885202fb7e1c202ec95c82360bd94724bbcd2
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-02-02 12:16:57 +0000

    Placeholders are now static only on MSVC.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@12630 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/bind/placeholders.hpp
+++ boost/boost/bind/placeholders.hpp
@@ -19,20 +19,31 @@
 //
 
 #include <boost/bind/arg.hpp>
+#include <boost/config.hpp>
+
+// With MSVC precompiled headers, unnamed namespaces are not unique
+
+#ifdef BOOST_MSVC
+# define BOOST_BIND_STATIC static
+#else
+# define BOOST_BIND_STATIC
+#endif
 
 namespace
 {
 
-boost::arg<1> _1;
-boost::arg<2> _2;
-boost::arg<3> _3;
-boost::arg<4> _4;
-boost::arg<5> _5;
-boost::arg<6> _6;
-boost::arg<7> _7;
-boost::arg<8> _8;
-boost::arg<9> _9;
+BOOST_BIND_STATIC boost::arg<1> _1;
+BOOST_BIND_STATIC boost::arg<2> _2;
+BOOST_BIND_STATIC boost::arg<3> _3;
+BOOST_BIND_STATIC boost::arg<4> _4;
+BOOST_BIND_STATIC boost::arg<5> _5;
+BOOST_BIND_STATIC boost::arg<6> _6;
+BOOST_BIND_STATIC boost::arg<7> _7;
+BOOST_BIND_STATIC boost::arg<8> _8;
+BOOST_BIND_STATIC boost::arg<9> _9;
 
 } // unnamed namespace
+
+#undef BOOST_BIND_STATIC
 
 #endif // #ifndef BOOST_BIND_PLACEHOLDERS_HPP_INCLUDED
