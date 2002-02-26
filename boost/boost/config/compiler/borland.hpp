6f5a457229a318c3a12629466fd93a96344e8e79
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-02-26 13:10:28 +0000

    Applying Patch from "Ed James-Beckham" <edjboost@yahoo.com>
    git-svn-id: http://svn.boost.org/svn/boost/trunk@12940 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/borland.hpp
+++ boost/boost/config/compiler/borland.hpp
@@ -8,30 +8,38 @@
 //  Borland C++ compiler setup:
 
 #   if __BORLANDC__ <= 0x0550
-// Borland C++ Builder 4 and 5:
+// Borland C++Builder 4 and 5:
 #     define BOOST_NO_MEMBER_TEMPLATE_FRIENDS
 #     if __BORLANDC__ == 0x0550
-// Borland C++ Builder 5, command-line compiler 5.5:
+// Borland C++Builder 5, command-line compiler 5.5:
 #       define BOOST_NO_OPERATORS_IN_NAMESPACE
 #     endif
 #   endif
 
-#if (__BORLANDC__ >= 0x550)
-// <climits> is partly broken, some macos define symbols that are really in
+// Borland C++Builder 6 defaults to using STLPort.  If _USE_OLD_RW_STL is
+// defined, then we have 0x560 or greater with the Rogue Wave implementation
+// which presumably has the std::DBL_MAX bug.
+#if ((__BORLANDC__ >= 0x550) && (__BORLANDC__ < 0x560)) || defined(_USE_OLD_RW_STL)
+// <climits> is partly broken, some macros define symbols that are really in
 // namespace std, so you end up having to use illegal constructs like
 // std::DBL_MAX, as a fix we'll just include float.h and have done with:
 #include <float.h>
 #endif
 
-// Version 5.51:
+// Version 6.0 and below:
+#if (__BORLANDC__ <= 0X560) || !defined(BOOST_STRICT_CONFIG)
+#  define BOOST_NO_DEPENDENT_NESTED_DERIVATIONS
+#  define BOOST_NO_INTEGRAL_INT64_T
+#  define BOOST_NO_PRIVATE_IN_AGGREGATE
+#  define BOOST_NO_SWPRINTF
+#  define BOOST_NO_USING_TEMPLATE
+#endif
+
+// Version 5.51 and below:
 #if (__BORLANDC__ <= 0x551) || !defined(BOOST_STRICT_CONFIG)
 #  define BOOST_NO_CV_SPECIALIZATIONS
 #  define BOOST_NO_CV_VOID_SPECIALIZATIONS
-#  define BOOST_NO_INTEGRAL_INT64_T
-#  define BOOST_NO_PRIVATE_IN_AGGREGATE
-#  define BOOST_NO_DEPENDENT_NESTED_DERIVATIONS
-#  define BOOST_NO_SWPRINTF
-#  define BOOST_NO_USING_TEMPLATE
+#  define BOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS
 #endif
 
 //
@@ -50,14 +58,10 @@
 #endif
 //
 // last known and checked version is 5.51:
-#if (__BORLANDC__ > 0x551)
+#if (__BORLANDC__ > 0x560)
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
 #  else
 #     pragma message( "Unknown compiler version - please run the configure tests and report the results")
 #  endif
 #endif
-
-
-
-
