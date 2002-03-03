502796074a8e801c00140d33c86a15dd3be1892d
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-03-03 11:33:14 +0000

    Fixed selection of BOOST_NO_STDC_NAMESPACE on bcb6
    git-svn-id: http://svn.boost.org/svn/boost/trunk@13046 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/stdlib/stlport.hpp
+++ boost/boost/config/stdlib/stlport.hpp
@@ -80,10 +80,17 @@
 // but doesn't always get them all, define BOOST_NO_STDC_NAMESPACE, since our
 // workaround does not conflict with STLports:
 //
-#if defined(__STL_IMPORT_VENDOR_CSTD) || defined(__STL_USE_OWN_NAMESPACE) || defined(_STLP_IMPORT_VENDOR_CSTD) || defined(_STLP_USE_OWN_NAMESPACE)
-#  define BOOST_NO_STDC_NAMESPACE
+//
+// Harold Howe says:
+// Borland switched to STLport in BCB6. Defining BOOST_NO_STDC_NAMESPACE with
+// BCB6 does cause problems. If we detect BCB6, then don't define 
+// BOOST_NO_STDC_NAMESPACE
+//
+#if !defined(__BORLANDC__) || (__BORLANDC__ < 0x560)
+#  if defined(__STL_IMPORT_VENDOR_CSTD) || defined(__STL_USE_OWN_NAMESPACE) || defined(_STLP_IMPORT_VENDOR_CSTD) || defined(_STLP_USE_OWN_NAMESPACE)
+#     define BOOST_NO_STDC_NAMESPACE
+#  endif
 #endif
-
 //
 // std::reverse_iterate behaves like VC6's under some circumstances:
 //
@@ -114,6 +121,7 @@
 
 
 #define BOOST_STDLIB "STLPort standard library version " BOOST_STRINGIZE(__SGI_STL_PORT)
+
 
 
 
