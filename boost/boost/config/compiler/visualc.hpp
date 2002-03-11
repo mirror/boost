e07c271d99d7f09fa79831a128e98d67d330e542
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-03-11 01:23:15 +0000

    fixed incorrect #warning directive
    git-svn-id: http://svn.boost.org/svn/boost/trunk@13171 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/visualc.hpp
+++ boost/boost/config/compiler/visualc.hpp
@@ -79,7 +79,7 @@
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
 #  else
-#     warning "Unknown compiler version - please run the configure tests and report the results"
+#     pragma message("Unknown compiler version - please run the configure tests and report the results")
 #  endif
 #endif
 
