de1261afb9b1041f4c8b74d980afc153d7dd4877
beman_dawes <beman_dawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-03-13 01:34:11 +0000

    Fix copyright message
    git-svn-id: http://svn.boost.org/svn/boost/trunk@13185 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/user.hpp
+++ boost/boost/config/user.hpp
@@ -1,4 +1,10 @@
-//  (C) Copyright Boost.org 2001.
+//  boost/config/user.hpp  ---------------------------------------------------//
+
+//  (C) Copyright Boost.org 2001. Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
 //  Do not check in modified versions of this file,
 //  This file may be customized by the end user, but not by boost.
 
