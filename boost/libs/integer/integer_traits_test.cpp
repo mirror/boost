423adaaf67ca10752589bea228507f6a4580d399
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-05-05 11:00:28 +0000

    Patched so that the code now compilers with VC6
    git-svn-id: http://svn.boost.org/svn/boost/trunk@13669 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/integer/integer_traits_test.cpp
+++ boost/libs/integer/integer_traits_test.cpp
@@ -35,10 +35,20 @@
  * Therefore, avoid explicit function template instantiations.
  */
 
+#if defined(BOOST_MSVC) && (BOOST_MSVC <= 1200)
+template<typename T> inline T make_char_numeric_for_streaming(T x) { return x; }
+namespace fix{
+inline int make_char_numeric_for_streaming(char c) { return c; }
+inline int make_char_numeric_for_streaming(signed char c) { return c; }
+inline int make_char_numeric_for_streaming(unsigned char c) { return c; }
+}
+using namespace fix;
+#else
 template<typename T> inline T make_char_numeric_for_streaming(T x) { return x; }
 inline int make_char_numeric_for_streaming(char c) { return c; }
 inline int make_char_numeric_for_streaming(signed char c) { return c; }
 inline int make_char_numeric_for_streaming(unsigned char c) { return c; }
+#endif
 
 template<class T>
 void runtest(const char * type, T)
