bc44a74e53f0d1a6f76299d0c8c5405814fe5383
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-06-17 11:03:00 +0000

    Committed fixes from Paul Mensonides
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14161 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/preprocessor/detail/cat.hpp
+++ boost/boost/preprocessor/detail/cat.hpp
@@ -2,24 +2,29 @@
 #define BOOST_PREPROCESSOR_DETAIL_CAT_HPP
 
 /* Copyright (C) 2002 Vesa Karvonen
- *
- * Permission to copy, use, modify, sell and distribute this software is
- * granted provided this copyright notice appears in all copies. This
- * software is provided "as is" without express or implied warranty, and
- * with no claim as to its suitability for any purpose.
- *
- * See http://www.boost.org for most recent version.
- */
+*
+* Permission to copy, use, modify, sell and distribute this software is
+* granted provided this copyright notice appears in all copies. This
+* software is provided "as is" without express or implied warranty, and
+* with no claim as to its suitability for any purpose.
+*
+* See http://www.boost.org for most recent version.
+*/
 
-#define BOOST_PP_DETAIL_CAT2(A,B) BOOST_PP_DETAIL_DO_CAT2(A,B)
+#define BOOST_PP_DETAIL_CAT2(A,B) BOOST_PP_DETAIL_CAT2_DELAY(A,B)
+#define BOOST_PP_DETAIL_CAT2_DELAY(A, B) BOOST_PP_DETAIL_DO_CAT2(A, B)
 #define BOOST_PP_DETAIL_DO_CAT2(A,B) A##B
 
-#define BOOST_PP_DETAIL_CAT3(A,B,C) BOOST_PP_DETAIL_DO_CAT3(A,B,C)
+#define BOOST_PP_DETAIL_CAT3(A,B,C) BOOST_PP_DETAIL_CAT3_DELAY(A,B,C)
+#define BOOST_PP_DETAIL_CAT3_DELAY(A,B,C) BOOST_PP_DETAIL_DO_CAT3(A,B,C)
 #define BOOST_PP_DETAIL_DO_CAT3(A,B,C) A##B##C
 
-#define BOOST_PP_DETAIL_CAT4(A,B,C,D) BOOST_PP_DETAIL_DO_CAT4(A,B,C,D)
+#define BOOST_PP_DETAIL_CAT4(A,B,C,D) BOOST_PP_DETAIL_CAT4_DELAY(A,B,C,D)
+#define BOOST_PP_DETAIL_CAT4_DELAY(A,B,C,D) BOOST_PP_DETAIL_DO_CAT4(A,B,C,D)
 #define BOOST_PP_DETAIL_DO_CAT4(A,B,C,D) A##B##C##D
 
-#define BOOST_PP_DETAIL_CAT5(A,B,C,D,E) BOOST_PP_DETAIL_DO_CAT5(A,B,C,D,E)
+#define BOOST_PP_DETAIL_CAT5(A,B,C,D,E) BOOST_PP_DETAIL_CAT5_DELAY(A,B,C,D,E)
+#define BOOST_PP_DETAIL_CAT5_DELAY(A,B,C,D,E) BOOST_PP_DETAIL_DO_CAT5(A,B,C,D,E)
 #define BOOST_PP_DETAIL_DO_CAT5(A,B,C,D,E) A##B##C##D##E
+
 #endif
