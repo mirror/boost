5b09c7cbddedccbec5fa80f99aa10340e6a6f095
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-06-19 20:11:17 +0000

    Update from Daryle
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14187 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/integer/static_log2.hpp
+++ boost/boost/integer/static_log2.hpp
@@ -12,8 +12,12 @@
 
 #include <boost/integer_fwd.hpp>  // self include
 
-#include <boost/config.hpp>  // for BOOST_STATIC_CONSTANT
+#include <boost/config.hpp>  // for BOOST_STATIC_CONSTANT, etc.
 #include <boost/limits.hpp>  // for std::numeric_limits
+
+#ifdef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+#include <boost/pending/ct_if.hpp>  // for boost::ct_if<>
+#endif
 
 
 namespace boost
@@ -30,8 +34,21 @@ template < unsigned long Val, int Place = 0, int Index
  = std::numeric_limits<unsigned long>::digits >
     struct static_log2_helper_t;
 
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+
 template < unsigned long Val, int Place >
     struct static_log2_helper_t< Val, Place, 1 >;
+
+#else
+
+template < int Place >
+    struct static_log2_helper_final_step;
+
+template < unsigned long Val, int Place = 0, int Index
+ = std::numeric_limits<unsigned long>::digits >
+    struct static_log2_helper_nopts_t;
+
+#endif
 
 // Recursively build the logarithm by examining the upper bits
 template < unsigned long Val, int Place, int Index >
@@ -50,7 +67,11 @@ private:
      : Place );
     BOOST_STATIC_CONSTANT( int, new_index = Index - half_place );
 
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
     typedef static_log2_helper_t<new_val, new_place, new_index>  next_step_type;
+#else
+    typedef static_log2_helper_nopts_t<new_val, new_place, new_index>  next_step_type;
+#endif
 
 public:
     BOOST_STATIC_CONSTANT( int, value = next_step_type::value );
@@ -58,6 +79,8 @@ public:
 };  // boost::detail::static_log2_helper_t
 
 // Non-recursive case
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+
 template < unsigned long Val, int Place >
 struct static_log2_helper_t< Val, Place, 1 >
 {
@@ -65,6 +88,33 @@ public:
     BOOST_STATIC_CONSTANT( int, value = Place );
 
 };  // boost::detail::static_log2_helper_t
+
+#else
+
+template < int Place >
+struct static_log2_helper_final_step
+{
+public:
+    BOOST_STATIC_CONSTANT( int, value = Place );
+
+};  // boost::detail::static_log2_helper_final_step
+
+template < unsigned long Val, int Place, int Index >
+struct static_log2_helper_nopts_t
+{
+private:
+    typedef static_log2_helper_t<Val, Place, Index>  recursive_step_type;
+    typedef static_log2_helper_final_step<Place>     final_step_type;
+
+    typedef typename ct_if<( Index != 1 ), recursive_step_type,
+     final_step_type>::type  next_step_type;
+
+public:
+    BOOST_STATIC_CONSTANT( int, value = next_step_type::value );
+
+};  // boost::detail::static_log2_helper_nopts_t
+
+#endif
 
 }  // namespace detail
 
