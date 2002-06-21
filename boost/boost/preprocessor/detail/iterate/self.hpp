a9ac63e1186b69527855e78a03b3c15e7aee1445
pmenso57 <pmenso57@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-06-21 23:08:36 +0000

    indirect file inclusion
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14222 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/preprocessor/detail/iterate/self.hpp
@@ -0,0 +1,18 @@
+# /* **************************************************************************
+#  *                                                                          *
+#  *     (C) Copyright Paul Mensonides 2002.  Permission to copy, use,        *
+#  *     modify, sell, and distribute this software is granted provided       *
+#  *     this copyright notice appears in all copies.  This software is       *
+#  *     provided "as is" without express or implied warranty, and with       *
+#  *     no claim at to its suitability for any purpose.                      *
+#  *                                                                          *
+#  ************************************************************************** */
+#
+# line 1 ""
+#
+# if !defined(BOOST_PP_INDIRECT_FILE)
+#   error BOOST_PP:  no file to indirect file to include defined
+# else
+#   include BOOST_PP_INDIRECT_FILE
+#   undef BOOST_PP_INDIRECT_FILE
+# endif
