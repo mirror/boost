3c2a6cdcdaf92113cff9769224d6ade876dffbe6
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-06-22 15:55:01 +0000

    Sleep(0) changed to Sleep(1) to (hopefully) avoid livelocks.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14226 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/lwm_win32.hpp
+++ boost/boost/detail/lwm_win32.hpp
@@ -57,7 +57,12 @@ public:
         {
             while( winapi::InterlockedExchange(&m_.l_, 1) )
             {
-                winapi::Sleep(0);
+                // Note: changed to Sleep(1) from Sleep(0).
+                // According to MSDN, Sleep(0) will never yield
+                // to a lower-priority thread, whereas Sleep(1)
+                // will. Performance seems not to be affected.
+
+                winapi::Sleep(1);
             }
         }
 
@@ -65,8 +70,8 @@ public:
         {
             winapi::InterlockedExchange(&m_.l_, 0);
 
-            // Note: adding a Sleep(0) here will make
-            // the mutex more fair and will increase the overall
+            // Note: adding a yield here will make
+            // the spinlock more fair and will increase the overall
             // performance of some applications substantially in
             // high contention situations, but will penalize the
             // low contention / single thread case up to 5x
