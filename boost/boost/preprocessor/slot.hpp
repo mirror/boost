a4853dc7f3abe914a1e2944f5f56543de834233c
pmenso57 <pmenso57@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-07-04 05:43:11 +0000

    evaluated slots moved to separate file
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14295 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/preprocessor/slot.hpp
@@ -0,0 +1,21 @@
+# ifndef BOOST_PREPROCESSOR_SLOT_HPP
+# define BOOST_PREPROCESSOR_SLOT_HPP
+#
+# /* **************************************************************************
+#  *                                                                          *
+#  *     (C) Copyright Paul Mensonides 2002.  Permission to copy, use,        *
+#  *     modify, sell, and distribute this software is granted provided       *
+#  *     this copyright notice appears in all copies.  This software is       *
+#  *     provided "as is" without express or implied warranty, and with       *
+#  *     no claim at to its suitability for any purpose.                      *
+#  *                                                                          *
+#  ************************************************************************** */
+#
+# include <boost/preprocessor/detail/cat.hpp>
+#
+# define BOOST_PP_SLOT_LIMIT 5
+#
+# define BOOST_PP_ASSIGN_SLOT(i) BOOST_PP_DETAIL_CAT4(<boost/preprocessor/detail/slot/slot, i, ., hpp>)
+# define BOOST_PP_SLOT(i) BOOST_PP_DETAIL_CAT2(BOOST_PP_SLOT_, i)
+#
+# endif
