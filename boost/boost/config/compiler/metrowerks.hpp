064e25ac1b3b06b44a1d58f4b636309758d25df6
beman_dawes <beman_dawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-07-12 19:58:26 +0000

    8.1 increment
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14429 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/metrowerks.hpp
+++ boost/boost/config/compiler/metrowerks.hpp
@@ -42,8 +42,8 @@
 #  error "Compiler not supported or configured - please reconfigure"
 #endif
 //
-// last known and checked version is 0x3000:
-#if (__MWERKS__ > 0x3000)
+// last known and checked version is 0x3001:
+#if (__MWERKS__ > 0x3001)
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
 #  endif
