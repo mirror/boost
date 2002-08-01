4e6d57135d66eae4ab61441d9624756700b4f9d3
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-08-01 15:56:38 +0000

    new file from Gennaro Prota
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14658 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/dynamic_bitset/timing_tests.cpp
@@ -0,0 +1,76 @@
+
+#include "boost/timer.hpp"
+#include "dynamic_bitset.hpp"
+
+#include <iostream>
+#include <typeinfo>
+#include <iomanip>
+
+
+template <typename T> void timing_test();
+void prologue();
+
+
+
+int main()
+{
+
+    prologue();
+
+    timing_test<unsigned short>();
+    timing_test<unsigned int>();
+    timing_test<unsigned long>();
+# ifdef BOOST_HAS_LONG_LONG
+    timing_test<unsigned long long>();
+# endif
+    
+
+    return 0;
+}
+
+
+
+void prologue() {
+    
+    std::cout << "Compiler: " << BOOST_COMPILER << '\n';
+    std::cout << "STLPort used? ";
+#  ifdef _STLPORT_VERSION
+    std::cout << "Yes, v." << std::hex << _STLPORT_VERSION;
+#  else
+    std::cout << "No.";
+#  endif
+    std::cout << std::dec << "\n";
+        
+}
+
+
+template <typename T>
+void timing_test() {
+
+    
+    const int num = 1000000;
+    
+    std::size_t dummy = 0; // this is printed at the end of the test,
+                           // to prevent the optimizer to eliminate
+                           // the call to count() in the loop below :-)
+
+
+    std::cout << "\n\nTimings for dynamic_bitset<" << typeid(T).name()
+              << ">  [" << num << " iterations]\n";
+    std::cout << "--------------------------------------------------\n";
+
+    { // new implementation
+        boost::timer time;
+        
+        for (unsigned long i=0; i<num; ++i) {
+            boost::dynamic_bitset<T> bs(std::size_t(5000), i);
+            dummy += bs.count();
+        }
+        const double elaps = time.elapsed();
+        std::cout << "Elapsed: " << elaps << '\n';
+    }
+
+
+    std::cout << "(total count: " << dummy << " )\n";
+
+}
