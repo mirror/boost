070ec5b28856a5b6f19adbd77e3d9d8a775e4c14
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-08-05 01:59:53 +0000

    function_traits.hpp:   - Added function_traits template that returns arity, result type, and     argument types for a function type.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@14671 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/type_traits/function_traits.hpp
+++ boost/boost/type_traits/function_traits.hpp
@@ -291,6 +291,7 @@ struct function_traits_helper<R (*)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)>
   typedef T9 arg9_type;
   typedef T10 arg10_type;
 };
+
 } // end namespace detail
 
 template<typename Function>
