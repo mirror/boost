19ac7f039efc53a848b944e7bb17a0664dd1093f
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-09-03 11:34:12 +0000

    Second try at fixing Intel errors, without messing up Intel+STLport config.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15141 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/array.hpp
+++ boost/boost/array.hpp
@@ -56,7 +56,7 @@ namespace boost {
 #if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !defined(BOOST_MSVC_STD_ITERATOR) && !(defined(__ICL) && defined(_CPPLIB_VER) && (_CPPLIB_VER <= 310))
         typedef std::reverse_iterator<iterator> reverse_iterator;
         typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-#elif defined(BOOST_MSVC) && (BOOST_MSVC == 1300) || (defined(__ICL) && defined(_CPPLIB_VER) && (_CPPLIB_VER == 310))
+#elif defined(BOOST_MSVC) && (BOOST_MSVC == 1300) || (defined(__ICL) && defined(_CPPLIB_VER) && (_CPPLIB_VER == 310) && defined(_ITERATOR_))
         // workaround for broken reverse_iterator in VC7
         typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, iterator,
                                       reference, iterator, reference> > reverse_iterator;
@@ -163,4 +163,5 @@ namespace boost {
 } /* namespace boost */
 
 #endif /*BOOST_ARRAY_HPP*/
+
 
