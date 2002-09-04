56eddf7fb7a52842fb6e841190b9e3547117147f
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-09-04 23:19:36 +0000

    Fix to avoid sleep for non-posix platforms
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15163 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/test/posix_time/testclock.cpp
+++ boost/libs/date_time/test/posix_time/testclock.cpp
@@ -13,11 +13,13 @@ main()
   ptime tu = second_clock::universal_time();
   std::cout << to_simple_string(tu) << std::endl;
 
+#if (defined(_POSIX_TIMERS))
   for (int i=0; i < 3; ++i) {
     ptime t2 = second_clock::local_time();
     std::cout << to_simple_string(t2) << std::endl;
     sleep(1);
   }
+#endif
 
 //   for (int i=0; i < 10; ++i) {
 //     ptime t2 = high_precision_clock::local_time();
