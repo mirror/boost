982eec1ccc922529947c651b91e553723875d3f9
pmenso57 <pmenso57@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-09-08 21:15:12 +0000

    not supposed to be here!
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15211 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/preprocessor/regression/test.cpp
+++
@@ -1,8 +0,0 @@
-const int default_param_0 = 0 , default_param_1 = 1 , default_param_2 = 2 , default_param_3 = 3 , default_param_4 = 4 , default_param_5 = 5 , default_param_6 =
-6 , default_param_7 = 7 , default_param_8 = 8 , default_param_9 = 9 , default_param_10 = 10 , default_param_11 = 11 , default_param_12 = 12 , default_param_13 =
- 13 , default_param_14 = 14 , default_param_15 = 15 , default_param_16 = 16 , default_param_17 = 17 , default_param_18 = 18 , default_param_19 = 19 , default_param_20 = 20 , default_param_21 = 21 , default_param_22 = 22 , default_param_23 = 23 , default_param_24 = 24 , default_param_25 = 25 , default_param_26 = 26 , default_param_27 = 27 , default_param_28 = 28 , default_param_29 = 29 , default_param_30 = 30 , default_param_31 = 31 , default_param_32 = 32 , default_param_33 =
- 33 , default_param_34 = 34 , default_param_35 = 35 , default_param_36 = 36 , default_param_37 = 37 , default_param_38 = 38 , default_param_39 = 39 , default_param_40 = 40 , default_param_41 = 41 , default_param_42 = 42 , default_param_43 = 43 , default_param_44 = 44 , default_param_45 = 45 , default_param_46 = 46 , default_param_47 = 47 , default_param_48 = 48 , default_param_49 = 49;
-
-int main(void) {
-	return 0;
-}
