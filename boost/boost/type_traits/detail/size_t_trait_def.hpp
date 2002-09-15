a84d7e2de4a4ed98684763a6c22e935d8bf34ba9
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-09-15 23:50:33 +0000

    some cleanup
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15349 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/type_traits/detail/size_t_trait_def.hpp
+++ boost/boost/type_traits/detail/size_t_trait_def.hpp
@@ -16,33 +16,45 @@
 
 // no include guards, the header is intended for multiple inclusion!
 
-#include "boost/type_traits/detail/size_t_trait.hpp"
 #include "boost/type_traits/detail/template_arity_spec.hpp"
+#include "boost/mpl/size_t_c.hpp"
 #include "boost/mpl/aux_/lambda_support.hpp"
 
-#define BOOST_TT_AUX_SIZE_T_TRAIT_DEF1(trait,T,value) \
+#include <cstddef>
+
+#if !defined(BOOST_MSVC) || BOOST_MSVC > 1200
+#   define BOOST_TT_AUX_SIZE_T_TRAIT_VALUE_DECL(C) /**/
+#else
+#   define BOOST_TT_AUX_SIZE_T_TRAIT_VALUE_DECL(C) \
+    typedef mpl::size_t_c< C > base_; \
+    using base_::value; \
+    /**/
+#endif
+
+
+#define BOOST_TT_AUX_SIZE_T_TRAIT_DEF1(trait,T,C) \
 template< typename T > struct trait \
-    : ::boost::type_traits::size_t_trait< value > \
+    : mpl::size_t_c< C > \
 { \
+    BOOST_TT_AUX_SIZE_T_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(1,trait,(T)) \
 }; \
 \
 BOOST_TT_AUX_TEMPLATE_ARITY_SPEC(1,trait) \
 /**/
 
-#define BOOST_TT_AUX_SIZE_T_TRAIT_SPEC1(trait,spec,value) \
+#define BOOST_TT_AUX_SIZE_T_TRAIT_SPEC1(trait,spec,C) \
 template<> struct trait<spec> \
-    : ::boost::type_traits::size_t_trait< value > \
+    : mpl::size_t_c< C > \
 { \
+    BOOST_TT_AUX_SIZE_T_TRAIT_VALUE_DECL(C) \
     BOOST_MPL_AUX_LAMBDA_SUPPORT(1,trait,(spec)) \
 }; \
-/**/ 
+/**/
 
-// BOOST_TT_AUX_TEMPLATE_ARITY_SPEC(1,trait)
-
-#define BOOST_TT_AUX_SIZE_T_TRAIT_PARTIAL_SPEC1_1(param,trait,spec,value) \
+#define BOOST_TT_AUX_SIZE_T_TRAIT_PARTIAL_SPEC1_1(param,trait,spec,C) \
 template< param > struct trait<spec> \
-    : ::boost::type_traits::size_t_trait< value > \
+    : mpl::size_t_c< C > \
 { \
 }; \
 /**/
