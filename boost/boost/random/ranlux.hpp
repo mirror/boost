c0106d2642f0d60b4561115ad0d95ba52ed18c35
jmaurer <jmaurer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-01 19:35:43 +0000

    move ranlux bases here (from subtract_with_carry.hpp)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15630 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/random/ranlux.hpp
+++ boost/boost/random/ranlux.hpp
@@ -27,11 +27,19 @@
 
 namespace boost {
 
+namespace random {
+  typedef subtract_with_carry<int, (1<<24), 10, 24, 0> ranlux_base;
+  typedef subtract_with_carry_01<float, int, (1<<24), 10, 24, 0> ranlux_base_flt;
+}
+
 typedef random::discard_block<random::ranlux_base, 218, 24, 0> ranlux_4;
 typedef random::discard_block<random::ranlux_base, 404, 24, 0> ranlux_7;
 typedef random::discard_block<random::ranlux_base, 794, 24, 0> ranlux_14;
 
 #if !defined(BOOST_NO_INT64_T) && !defined(BOOST_NO_INTEGRAL_INT64_T)
+namespace random {
+  typedef random::subtract_with_carry<int64_t, (int64_t(1)<<48), 10, 24, 0> ranlux64_base;
+}
 typedef random::discard_block<random::ranlux64_base, 218, 24, 0> ranlux64_4;
 typedef random::discard_block<random::ranlux64_base, 404, 24, 0> ranlux64_7;
 typedef random::discard_block<random::ranlux64_base, 794, 24, 0> ranlux64_14;
