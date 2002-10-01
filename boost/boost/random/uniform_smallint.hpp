0beb3ece2ef648876cde45233d42abbe61f5c0e7
jmaurer <jmaurer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-01 22:17:37 +0000

    make the distribution usable with underlying floating-point generators
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15637 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/random/uniform_smallint.hpp
+++ boost/boost/random/uniform_smallint.hpp
@@ -30,22 +30,34 @@
 namespace boost {
 
 // uniform integer distribution on a small range [min, max]
-template<class UniformRandomNumberGenerator, class IntType = int>
-class uniform_smallint
+template<class UniformRandomNumberGenerator, class IntType = int,
+         bool is_integer = std::numeric_limits<typename UniformRandomNumberGenerator::result_type>::is_integer>
+class uniform_smallint;
+
+
+template<class UniformRandomNumberGenerator, class IntType>
+class uniform_smallint<UniformRandomNumberGenerator, IntType, true>
 {
 public:
   typedef UniformRandomNumberGenerator base_type;
   typedef IntType result_type;
   BOOST_STATIC_CONSTANT(bool, has_fixed_range = false);
 
+  BOOST_STATIC_ASSERT(std::numeric_limits<IntType>::is_integer);
+
   uniform_smallint(base_type & rng, IntType min, IntType max);
+
+  result_type min() const { return _min; }
+  result_type max() const { return _max; }
+  base_type& base() const { return _rng; }
+  void reset() { }
+
   result_type operator()()
   {
     // we must not use the low bits here, because LCGs get very bad then
     return ((_rng() - _rng.min()) / _factor) % _range + _min;
   }
-  result_type min() const { return _min; }
-  result_type max() const { return _max; }
+
 #ifndef BOOST_NO_OPERATORS_IN_NAMESPACE
   friend bool operator==(const uniform_smallint& x, const uniform_smallint& y)
   { return x._min == y._min && x._max == y._max && x._rng == y._rng; }
@@ -65,18 +77,15 @@ private:
 #ifndef BOOST_NO_INCLASS_MEMBER_INITIALIZATION
 //  A definition is required even for integral static constants
 template<class UniformRandomNumberGenerator, class IntType>
-const bool uniform_smallint<UniformRandomNumberGenerator, IntType>::has_fixed_range;
+const bool uniform_smallint<UniformRandomNumberGenerator, IntType, true>::has_fixed_range;
 #endif
 
 template<class UniformRandomNumberGenerator, class IntType>
-uniform_smallint<UniformRandomNumberGenerator, IntType>::
+uniform_smallint<UniformRandomNumberGenerator, IntType, true>::
 uniform_smallint(base_type & rng, IntType min, IntType max) 
   : _rng(rng), _min(min), _max(max),
     _range(static_cast<base_result>(_max-_min)+1), _factor(1)
 {
-#ifndef BOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS
-  BOOST_STATIC_ASSERT(std::numeric_limits<IntType>::is_integer);
-#endif
   assert(min < max);
   
   // LCGs get bad when only taking the low bits.
@@ -97,6 +106,60 @@ uniform_smallint(base_type & rng, IntType min, IntType max)
     for( ; r_base/_range/32 >= _range; _factor *= 2)
       r_base /= 2;
   }
+}
+
+
+// Implementation for floating-point generator base
+template<class UniformRandomNumberGenerator, class IntType>
+class uniform_smallint<UniformRandomNumberGenerator, IntType, false>
+{
+public:
+  typedef UniformRandomNumberGenerator base_type;
+  typedef IntType result_type;
+  BOOST_STATIC_CONSTANT(bool, has_fixed_range = false);
+
+  BOOST_STATIC_ASSERT(std::numeric_limits<IntType>::is_integer);
+
+  uniform_smallint(base_type & rng, IntType min, IntType max);
+
+  result_type min() const { return _min; }
+  result_type max() const { return _max; }
+  base_type& base() const { return _rng.base(); }
+  void reset() { }
+
+  result_type operator()()
+  {
+    return static_cast<IntType>(_rng() * _range) + _min;
+  }
+
+#ifndef BOOST_NO_OPERATORS_IN_NAMESPACE
+  friend bool operator==(const uniform_smallint& x, const uniform_smallint& y)
+  { return x._min == y._min && x._max == y._max && x._rng == y._rng; }
+#else
+  // Use a member function
+  bool operator==(const uniform_smallint& rhs) const
+  { return _min == rhs._min && _max == rhs._max && _rng == rhs._rng;  }
+#endif
+private:
+  typedef typename base_type::result_type base_result;
+  uniform_01<base_type> _rng;
+  IntType _min, _max;
+  base_result _range;
+};
+
+#ifndef BOOST_NO_INCLASS_MEMBER_INITIALIZATION
+//  A definition is required even for integral static constants
+template<class UniformRandomNumberGenerator, class IntType>
+const bool uniform_smallint<UniformRandomNumberGenerator, IntType, false>::has_fixed_range;
+#endif
+
+template<class UniformRandomNumberGenerator, class IntType>
+uniform_smallint<UniformRandomNumberGenerator, IntType, false>::
+uniform_smallint(base_type & rng, IntType min, IntType max) 
+  : _rng(rng), _min(min), _max(max),
+    _range(static_cast<base_result>(_max-_min)+1)
+{
+  assert(min < max);
 }
 
 } // namespace boost
