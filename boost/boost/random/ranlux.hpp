9240be67c29dd807711554f193217dc3833d4f81
jmaurer <jmaurer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-03 22:06:48 +0000

    focus on luxury levels for naming
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15691 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/random/ranlux.hpp
+++ boost/boost/random/ranlux.hpp
@@ -30,28 +30,24 @@ namespace boost {
 namespace random {
   typedef subtract_with_carry<int, (1<<24), 10, 24, 0> ranlux_base;
   typedef subtract_with_carry_01<float, 24, 10, 24> ranlux_base_01;
-  typedef subtract_with_carry_01<float, 48, 10, 24> ranlux64_base_01;
+  typedef subtract_with_carry_01<double, 48, 10, 24> ranlux64_base_01;
 }
 
-typedef random::discard_block<random::ranlux_base, 218, 24> ranlux_4;
-typedef random::discard_block<random::ranlux_base, 404, 24> ranlux_7;
-typedef random::discard_block<random::ranlux_base, 794, 24> ranlux_14;
+typedef random::discard_block<random::ranlux_base, 223, 24> ranlux3;
+typedef random::discard_block<random::ranlux_base, 389, 24> ranlux4;
 
-typedef random::discard_block<random::ranlux_base_01, 218, 24> ranlux_4_01;
-typedef random::discard_block<random::ranlux_base_01, 404, 24> ranlux_7_01;
-typedef random::discard_block<random::ranlux_base_01, 794, 24> ranlux_14_01;
+typedef random::discard_block<random::ranlux_base_01, 223, 24> ranlux3_01;
+typedef random::discard_block<random::ranlux_base_01, 389, 24> ranlux4_01;
 
-typedef random::discard_block<random::ranlux64_base_01, 218, 24> ranlux64_4_01;
-typedef random::discard_block<random::ranlux64_base_01, 404, 24> ranlux64_7_01;
-typedef random::discard_block<random::ranlux64_base_01, 794, 24> ranlux64_14_01;
+typedef random::discard_block<random::ranlux64_base_01, 223, 24> ranlux64_3_01;
+typedef random::discard_block<random::ranlux64_base_01, 389, 24> ranlux64_4_01;
 
 #if !defined(BOOST_NO_INT64_T) && !defined(BOOST_NO_INTEGRAL_INT64_T)
 namespace random {
   typedef random::subtract_with_carry<int64_t, (int64_t(1)<<48), 10, 24, 0> ranlux64_base;
 }
-typedef random::discard_block<random::ranlux64_base, 218, 24> ranlux64_4;
-typedef random::discard_block<random::ranlux64_base, 404, 24> ranlux64_7;
-typedef random::discard_block<random::ranlux64_base, 794, 24> ranlux64_14;
+typedef random::discard_block<random::ranlux64_base, 223, 24> ranlux64_3;
+typedef random::discard_block<random::ranlux64_base, 389, 24> ranlux64_4;
 #endif /* !BOOST_NO_INT64_T && !BOOST_NO_INTEGRAL_INT64_T */
 
 } // namespace boost
