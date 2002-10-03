453a187bb3359a72982e67dcc1ca224dac5bafd5
jmaurer <jmaurer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-03 00:07:13 +0000

    add new engines and new distributions
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15662 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/random.hpp
+++ boost/boost/random.hpp
@@ -44,6 +44,15 @@
 #include <boost/random/shuffle_output.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/lagged_fibonacci.hpp>
+#include <boost/random/ranlux.hpp>
+#include <boost/random/linear_feedback_shift.hpp>
+#include <boost/random/xor_combine.hpp>
+
+namespace boost {
+  typedef random::xor_combine<random::xor_combine<random::linear_feedback_shift<uint32_t, 32, 31, 13, 12, 0>, 0,
+    random::linear_feedback_shift<uint32_t, 32, 29, 2, 4, 0>, 0, 0>, 0,
+                      random::linear_feedback_shift<uint32_t, 32, 28, 3, 17, 0>, 0, 0> taus88;
+} // namespace  boost
 
 // misc
 #include <boost/random/random_number_generator.hpp>
@@ -60,6 +69,9 @@
 #include <boost/random/geometric_distribution.hpp>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/lognormal_distribution.hpp>
+#include <boost/random/poisson_distribution.hpp>
+#include <boost/random/gamma_distribution.hpp>
+#include <boost/random/binomial_distribution.hpp>
 #include <boost/random/uniform_on_sphere.hpp>
 
 #endif // BOOST_RANDOM_HPP
