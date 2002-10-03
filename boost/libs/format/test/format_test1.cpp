5144ec1ce1118bda93ce7419536c31b8a0e44c19
samuel_k <samuel_k@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-03 22:16:53 +0000

    removed unneeded "using wstring".
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15694 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/format/test/format_test1.cpp
+++ boost/libs/format/test/format_test1.cpp
@@ -6,9 +6,8 @@
 
 int test_main(int, char* [])
 {
-  using std::wstring;
+
   using boost::format;
-  using boost::wformat;
   using boost::io::str;
 
   if(str( format("  %%  ") ) != "  %  ")
@@ -30,9 +29,13 @@ int test_main(int, char* [])
       BOOST_ERROR("Basic parsing Failed");
   if(str( format("%%##%#x ##%%1 %s00") % 20 % "Escaped OK" ) != "%##0x14 ##%1 Escaped OK00")
       BOOST_ERROR("Basic p-parsing Failed") ;
+
+
+  using boost::wformat;
   if(str( wformat(L"%%##%%##%%1 %1%00") % L"Escaped OK" ) != L"%##%##%1 Escaped OK00")
       BOOST_ERROR("Basic w-parsing Failed");
   if(str( wformat(L"%%##%#x ##%%1 %s00") % 20 % L"Escaped OK" ) != L"%##0x14 ##%1 Escaped OK00")
       BOOST_ERROR("Basic wp-parsing Failed") ;
+
   return 0;
 }
