fa57de26ce82e00cd727c5ce22ce09cc5a5a6b24
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-10-14 16:29:29 +0000

    Fix for gcc 2.95.3 compile constant issue
    git-svn-id: http://svn.boost.org/svn/boost/trunk@15922 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/test/testtime_resolution_traits.cpp
+++ boost/libs/date_time/test/testtime_resolution_traits.cpp
@@ -42,8 +42,13 @@ main()
         nano_res::to_tick_count(0,0,0,1) == 1);
   check("nano tick calculations",  
         nano_res::to_tick_count(0,0,1,1) == 1000000001);
+#if (defined(__GNUC__) && (__GNUC__ < 3))
+  check("nano tick calculations",  
+        nano_res::to_tick_count(0,1,0,0) == 60000000000LL);
+#else
   check("nano tick calculations",  
         nano_res::to_tick_count(0,1,0,0) == 60000000000);
+#endif
 
   //skip io on VC6 b/c of lack of operator<< for int64
 #if (defined(BOOST_MSVC) && (_MSC_VER <= 1200))  // 1200 == VC++ 6.0
