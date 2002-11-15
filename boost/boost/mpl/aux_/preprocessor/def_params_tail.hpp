5766a28da6af93b3276e24d56677ea6ac8b55c87
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-11-15 11:54:48 +0000

    preprocessor fixes
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16254 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/aux_/preprocessor/def_params_tail.hpp
+++ boost/boost/mpl/aux_/preprocessor/def_params_tail.hpp
@@ -21,6 +21,8 @@
 #include "boost/mpl/aux_/config/dtp.hpp"
 #include "boost/mpl/aux_/config/preprocessor.hpp"
 
+#include "boost/preprocessor/comma_if.hpp"
+#include "boost/preprocessor/logical/and.hpp"
 #include "boost/preprocessor/identity.hpp"
 #include "boost/preprocessor/empty.hpp"
 
@@ -47,9 +49,11 @@
     /**/
 
 #   define BOOST_MPL_PP_DEF_PARAMS_TAIL_DELAY_2(i, n, param, value_func) \
-    , BOOST_MPL_PP_DEF_PARAMS_TAIL_##i(n,param,value_func) \
+    BOOST_PP_COMMA_IF(BOOST_PP_AND(i,n)) \
+    BOOST_MPL_PP_DEF_PARAMS_TAIL_##i(n,param,value_func) \
     /**/
 
+#   define BOOST_MPL_PP_DEF_PARAMS_TAIL_0(i,p,v) BOOST_MPL_PP_FILTER_PARAMS_##i(p##1 v(),p##2 v(),p##3 v(),p##4 v(),p##5 v(),p##6 v(),p##7 v(),p##8 v(),p##9 v())
 #   define BOOST_MPL_PP_DEF_PARAMS_TAIL_1(i,p,v) BOOST_MPL_PP_FILTER_PARAMS_##i(p##2 v(),p##3 v(),p##4 v(),p##5 v(),p##6 v(),p##7 v(),p##8 v(),p##9 v(),p1)
 #   define BOOST_MPL_PP_DEF_PARAMS_TAIL_2(i,p,v) BOOST_MPL_PP_FILTER_PARAMS_##i(p##3 v(),p##4 v(),p##5 v(),p##6 v(),p##7 v(),p##8 v(),p##9 v(),p1,p2)
 #   define BOOST_MPL_PP_DEF_PARAMS_TAIL_3(i,p,v) BOOST_MPL_PP_FILTER_PARAMS_##i(p##4 v(),p##5 v(),p##6 v(),p##7 v(),p##8 v(),p##9 v(),p1,p2,p3)
@@ -70,7 +74,8 @@
 #   include "boost/preprocessor/cat.hpp"
 
 #   define BOOST_MPL_PP_AUX_TAIL_PARAM_FUNC(unused, i, op) \
-    , BOOST_PP_CAT( \
+    BOOST_PP_COMMA_IF(i) \
+    BOOST_PP_CAT( \
           BOOST_PP_TUPLE_ELEM(3, 1, op) \
         , BOOST_PP_ADD_D(1, i, BOOST_PP_INC(BOOST_PP_TUPLE_ELEM(3, 0, op))) \
         ) BOOST_PP_TUPLE_ELEM(3, 2, op)() \
