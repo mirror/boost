91563b4b2f401deaf9c0abbc370638fd48e7477b
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-11-19 16:23:01 +0000

    License added.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16326 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/smart_ptr.hpp
+++ boost/boost/smart_ptr.hpp
@@ -1,7 +1,18 @@
-//  Boost smart_ptr.hpp header file  -----------------------------------------//
-
-// For compatibility, this header includes the header for the four "classic"
-// smart pointer class templates.
+//
+//  smart_ptr.hpp
+//
+//  For compatibility, this header includes the headers for the four "classic"
+//  smart pointer class templates.
+//
+//  Copyright (c) 1998-2002 boost.org
+//
+//  Permission to copy, use, modify, sell and distribute this software
+//  is granted provided this copyright notice appears in all copies.
+//  This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+//
+//  http://www.boost.org/libs/smart_ptr/smart_ptr.htm
+//
 
 #include <boost/scoped_ptr.hpp> 
 #include <boost/scoped_array.hpp>
