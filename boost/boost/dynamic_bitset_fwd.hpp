be2ef419731e144df0be9213a8fcf13ba475b840
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-11-20 10:26:29 +0000

    Added copyright statement
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16338 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/dynamic_bitset_fwd.hpp
+++ boost/boost/dynamic_bitset_fwd.hpp
@@ -1,3 +1,11 @@
+// (C) Copyright Chuck Allison and Jeremy Siek 2001, 2002. 
+//
+// Permission to copy, use, modify, sell and distribute this software
+// is granted provided this copyright notice appears in all
+// copies. This software is provided "as is" without express or
+// implied warranty, and with no claim as to its suitability for any
+// purpose.
+
 #ifndef BOOST_DYNAMIC_BITSET_FWD_HPP
 #define BOOST_DYNAMIC_BITSET_FWD_HPP
 
--- boost/boost/format/format_config.hpp
+++ boost/boost/format/format_config.hpp
@@ -1,3 +1,17 @@
+// -*- C++ -*-
+//  Boost general library 'format'   ---------------------------
+//  See http://www.boost.org for updates, documentation, and revision history.
+
+//  (C) Samuel Krempp 2001
+//                  krempp@crans.ens-cachan.fr
+//  Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
+// ideas taken from Rüdiger Loos's format class
+// and Karl Nelson's ofstream (also took its parsing code as basis for printf parsing)
+
 // ------------------------------------------------------------------------------
 // format_config.hpp : configuration for the format library
 // The contents of this file should be integrated into the boost config system.
