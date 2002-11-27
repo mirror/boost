6a795aa2e9b7eaa782e3668babeee3f8ee508bda
samuel_k <samuel_k@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-11-27 18:03:53 +0000

    refactored feed_args(..) code
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16445 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/format/feed_args.hpp
+++ boost/boost/format/feed_args.hpp
@@ -118,98 +118,100 @@ namespace  {
 
 
   
-  template< class Ch, class Tr, class T> 
-  void put( T x, 
-            const format_item<Ch, Tr>& specs, 
-            std::basic_string<Ch, Tr> & res, 
-            BOOST_IO_STD basic_ostringstream<Ch, Tr>& oss_ )
-  {
-    // does the actual conversion of x, with given params, into a string
-    // using the *supplied* strinstream. (the stream state is important)
+template< class Ch, class Tr, class T> 
+void put( T x, 
+          const format_item<Ch, Tr>& specs, 
+          std::basic_string<Ch, Tr> & res, 
+          BOOST_IO_STD basic_ostringstream<Ch, Tr>& oss_ )
+{
+  // does the actual conversion of x, with given params, into a string
+  // using the *supplied* strinstream. (the stream state is important)
 
-    typedef std::basic_string<Ch, Tr> string_t;
-    typedef format_item<Ch, Tr>  format_item_t;
+  typedef std::basic_string<Ch, Tr> string_t;
+  typedef format_item<Ch, Tr>  format_item_t;
 
-    stream_format_state<Ch, Tr>   prev_state(oss_);
+  stream_format_state<Ch, Tr>   prev_state(oss_);
     
-    specs.state_.apply_on(oss_);
+  specs.state_.apply_on(oss_);
 
-    // in case x is a group, apply the manip part of it, 
-    // in order to find width
-    put_head( oss_, x );
-    empty_buf( oss_);
+  // in case x is a group, apply the manip part of it, 
+  // in order to find width
+  put_head( oss_, x );
+  empty_buf( oss_);
 
-    const std::streamsize w=oss_.width();
-    const std::ios_base::fmtflags fl=oss_.flags();
-    const bool internal = (fl & std::ios_base::internal) != 0;
-    const bool two_stepped_padding = internal
-                                        &&  ! ( specs.pad_scheme_ & format_item_t::spacepad ) 
-                                        && specs.truncate_ < 0 ;
+  const std::streamsize w=oss_.width();
+  const std::ios_base::fmtflags fl=oss_.flags();
+  const bool internal = (fl & std::ios_base::internal) != 0;
+  const bool two_stepped_padding = internal
+    &&  ! ( specs.pad_scheme_ & format_item_t::spacepad ) 
+    && specs.truncate_ < 0 ;
+      
 
-    if( w > 0)
-      {
-        if(two_stepped_padding)
-          { } // in this case, let the stream do the padding, and later on, we'll do our hack..
-        else {
-            oss_.width(0);
+  if(! two_stepped_padding) 
+    {
+      if(w>0) // handle simple padding via do_pad, not natively in stream 
+        oss_.width(0);
+      put_last( oss_, x);
+      res = oss_.str();
+
+      if (specs.truncate_ >= 0)
+        res.erase(specs.truncate_);
+
+      // complex pads :
+      if(specs.pad_scheme_ & format_item_t::spacepad)
+        {
+          if( res.size()==0 ||   ( res[0]!='+' && res[0]!='-'  ))
+            {
+              res.insert(res.begin(), 1, ' '); // insert 1 space at  pos 0
+            }
         }
-      }
+      if(w > 0) // need do_pad
+        {
+          do_pad(res,w,oss_.fill(), fl, (specs.pad_scheme_ & format_item_t::centered) !=0 );
+        }
+    } 
+  else  // 2-stepped padding
+    {
+      put_last( oss_, x); // oss_.width() may result in padding.
+      res = oss_.str();
+      
+      if (specs.truncate_ >= 0)
+        res.erase(specs.truncate_);
 
-    put_last( oss_, x);
-    res = oss_.str();
+      if( res.size() - w > 0)
+        { //   length w exceeded
+          // either it was multi-output with first output padding up all width..
+          // either it was one big arg and we are fine.
+          empty_buf( oss_);
+          oss_.width(0);
+          put_last(oss_, x );
+          string_t tmp = oss_.str();  // minimal-length output
+          std::streamsize d;
+          if( (d=w - tmp.size()) <=0 ) 
+            {
+              // minimal length is already >= w, so no padding  (cool!)
+              std::swap(res, tmp ); 
+            }
+          else
+            { // hum..  we need to pad (it was necessarily multi-output)
+              typedef typename string_t::size_type size_type;
+              size_type i = 0;
+              while( i<tmp.size() && tmp[i] == res[i] ) // find where we should pad.
+                ++i; 
+              tmp.insert(i, static_cast<size_type>( d ), oss_.fill());
+              std::swap(res, tmp );
+            }
+        }
+      else 
+        { // okay, only one thing was printed and padded, so res is fine.
+        }
+    }
 
-    if (specs.truncate_ >= 0)
-      res.erase(specs.truncate_);
+  prev_state.apply_on(oss_);
+  empty_buf( oss_);
+  oss_.clear();
+} // end- put(..)
 
-    // complex pads :
-
-    if(specs.pad_scheme_ & format_item_t::spacepad)
-      {
-        if( res.size()==0 ||   ( res[0]!='+' && res[0]!='-'  ))
-          {
-            res.insert(res.begin(), 1, ' '); // insert 1 space at  pos 0
-          }
-      }
-
-    if(two_stepped_padding)
-      {
-        if( res.size() - w > 0)
-          { 
-            // either it was one big arg and we have nothing to do about it,
-            // either it was multi-output with first output padding up all width..
-            empty_buf( oss_);
-            oss_.width(0);
-            put_last(oss_, x );
-            string_t tmp = oss_.str();  // minimal-length output
-            std::streamsize d;
-            if( (d=w - tmp.size()) >0 ) // hum.. then we need to pad.
-              {
-                typedef typename string_t::size_type size_type;
-                size_type i = 0;
-                while( i<tmp.size() && tmp[i] == res[i] ) // find where we should pad.
-                  ++i; 
-                tmp.insert(i, static_cast<size_type>( d ), oss_.fill());
-                std::swap(res, tmp );
-              }
-            else
-              {
-                // size is already >= w, so no padding  (cool!)
-                std::swap(res, tmp ); 
-              }
-          }
-        else 
-          { // okay, only one thing was printed and padded, so res is fine.
-          }
-      }
-    else if(w > 0) // other cases that need do_pad
-      {
-        do_pad(res,w,oss_.fill(), fl, (specs.pad_scheme_ & format_item_t::centered) !=0 );
-      }
-
-    prev_state.apply_on(oss_);
-    empty_buf( oss_);
-    oss_.clear();
-  }
 
 }  // local namespace
 
@@ -221,21 +223,20 @@ template< class Ch, class Tr, class T>
 void distribute(basic_format<Ch,Tr>& self, T x) 
   // call put(x, ..) on every occurence of the current argument :
 {
-    if(self.cur_arg_ >= self.num_args_)
-      {
-        if( self.exceptions() & too_many_args_bit )
-          boost::throw_exception(too_many_args()); // too many variables have been supplied !
-        else return;
-      }
-    for(unsigned long i=0; i < self.items_.size(); ++i)
+  if(self.cur_arg_ >= self.num_args_)
+    {
+      if( self.exceptions() & too_many_args_bit )
+        boost::throw_exception(too_many_args()); // too many variables have been supplied !
+      else return;
+    }
+  for(unsigned long i=0; i < self.items_.size(); ++i)
     {
       if(self.items_[i].argN_ == self.cur_arg_)
-      {
+        {
           put<Ch, Tr, T> (x, self.items_[i], self.items_[i].res_, self.oss_ );
-      }
-  }
+        }
+    }
 }
-
 
 template<class Ch, class Tr, class T> 
 basic_format<Ch, Tr>&  feed(basic_format<Ch,Tr>& self, T x) 
