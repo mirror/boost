0f2a7970592950e54f0c4dcb8f5cba5ea45ca91d
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2002-12-04 15:52:33 +0000

    Workarounds for broken RogueWave lib that comes with Sun
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16509 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/iterator.hpp
+++ boost/boost/detail/iterator.hpp
@@ -98,6 +98,42 @@ struct iterator_traits
     : std::iterator_traits<Iterator>
 {};
 using std::distance;
+# elif  !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) \
+     && !defined(BOOST_MSVC_STD_ITERATOR)
+
+// Rogue Wave Standard Library fools itself into thinking partial
+// specialization is missing on some platforms (e.g. Sun), so fails to
+// supply iterator_traits!
+template <class Iterator>
+struct iterator_traits
+{
+    typedef typename Iterator::value_type value_type;
+    typedef typename Iterator::reference reference;
+    typedef typename Iterator::pointer pointer;
+    typedef typename Iterator::difference_type difference_type;
+    typedef typename Iterator::iterator_category iterator_category;
+};
+
+template <class T>
+struct iterator_traits<T*>
+{
+    typedef T value_type;
+    typedef T& reference;
+    typedef T* pointer;
+    typedef std::ptrdiff_t difference_type;
+    typedef std::random_access_iterator_tag iterator_category;
+};
+
+template <class T>
+struct iterator_traits<T const*>
+{
+    typedef T value_type;
+    typedef T const& reference;
+    typedef T const* pointer;
+    typedef std::ptrdiff_t difference_type;
+    typedef std::random_access_iterator_tag iterator_category;
+};
+
 # else
 
 // is_mutable_iterator --
