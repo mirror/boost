4074d78f2b74ce28ce1777cac73264e42f58c69d
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-01-08 17:21:10 +0000

    Workaround VC7 bug which strips const from nested classes
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16797 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/utility/addressof.hpp
+++ boost/boost/utility/addressof.hpp
@@ -14,13 +14,25 @@
 // For more information, see http://www.boost.org
 
 #ifndef BOOST_UTILITY_ADDRESSOF_HPP
-#define BOOST_UTILITY_ADDRESSOF_HPP
+# define BOOST_UTILITY_ADDRESSOF_HPP
+
+# include <boost/config.hpp>
+# include <boost/detail/workaround.hpp>
+# if BOOST_WORKAROUND(BOOST_MSVC, == 1300)
+#  include <boost/type_traits/add_pointer.hpp>
+# endif
 
 namespace boost {
 
 // Do not make addressof() inline. Breaks MSVC 7. (Peter Dimov)
 
-template <typename T> T* addressof(T& v)
+// VC7 strips const from nested classes unless we add indirection here
+# if BOOST_WORKAROUND(BOOST_MSVC, == 1300)
+template <typename T> typename add_pointer<T>::type
+# else
+template <typename T> T*
+# endif
+addressof(T& v)
 {
   return reinterpret_cast<T*>(
        &const_cast<char&>(reinterpret_cast<const volatile char &>(v)));
