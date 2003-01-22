49595686f98a5caf9e5f11a4d78059baa10c50d2
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-01-22 12:12:14 +0000

    Added Cray support
    git-svn-id: http://svn.boost.org/svn/boost/trunk@16989 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/cstdint.hpp
+++ boost/boost/cstdint.hpp
@@ -167,6 +167,12 @@ namespace boost
      typedef unsigned short  uint16_t;
      typedef unsigned short  uint_least16_t;
      typedef unsigned short  uint_fast16_t;
+# elif (USHRT_MAX == 0xffffffff) && defined(CRAY)
+     // no 16-bit types on Cray:
+     typedef short           int_least16_t;
+     typedef short           int_fast16_t;
+     typedef unsigned short  uint_least16_t;
+     typedef unsigned short  uint_fast16_t;
 # else
 #    error defaults not correct; you must hand modify boost/cstdint.hpp
 # endif
@@ -372,5 +378,6 @@ BOOST_HAS_STDINT_H is defined (John Maddock).
 # undef UINTMAX_C
 
 #endif // __STDC_CONSTANT_MACROS_DEFINED etc.
+
 
 
