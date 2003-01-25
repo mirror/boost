97c5b5d1e299f5941c923a72c51dfa47f6271c9b
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-01-25 21:41:45 +0000

    fix for metrowerks compile problem
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17045 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/compiler_config.hpp
+++ boost/boost/date_time/compiler_config.hpp
@@ -18,6 +18,10 @@
 #define INT64_C(value)  long long(value)
 #endif
 
+#if (defined(__MWERKS__))
+#include <cstdlib> //for abs()
+#endif
+
 //Work around compilers that don't have std::abs
 #if (defined(__GNUC__) && (__GNUC__ <= 3)) || (defined(BOOST_MSVC) && _MSC_VER <= 1200) || (defined(__BORLANDC__) && (__BORLANDC__ >= 0x561))
 #  define BOOST_NO_LONG_LONG_ABS
