570ee574938ede3496552933d834479e52f248d9
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-01-30 14:22:28 +0000

    Delete nothrow, new throw
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17088 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/function/test/stateless_test.cpp
+++ boost/libs/function/test/stateless_test.cpp
@@ -21,13 +21,12 @@
 struct stateless_integer_add {
   int operator()(int x, int y) const { return x+y; }
 
-  void* operator new(std::size_t, stateless_integer_add*) throw()
+  void* operator new(std::size_t, stateless_integer_add*)
   {
     throw std::runtime_error("Cannot allocate a stateless_integer_add");
-    return 0; // suppress warnings
   }
 
-  void operator delete(void*, stateless_integer_add*)
+  void operator delete(void*, stateless_integer_add*) throw()
   {
   }
 };
