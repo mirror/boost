334c769bd56f72df01d83a49ecd0a4a92230eb4a
beman_dawes <beman_dawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-01-31 21:26:53 +0000

    fix wrong dir name
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17136 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/preprocessor/test/iteration.h
+++ boost/libs/preprocessor/test/iteration.h
@@ -18,15 +18,15 @@
 #
 # define NO_FLAGS
 #
-# define BOOST_PP_ITERATION_PARAMS_1 (3, (1, 10, <libs/preprocessor/regression/iteration.h>))
+# define BOOST_PP_ITERATION_PARAMS_1 (3, (1, 10, <libs/preprocessor/test/iteration.h>))
 # include BOOST_PP_ITERATE()
 #
 # undef NO_FLAGS
 #
-# define BOOST_PP_ITERATION_PARAMS_1 (4, (1, 5, <libs/preprocessor/regression/iteration.h>, 0x0001))
+# define BOOST_PP_ITERATION_PARAMS_1 (4, (1, 5, <libs/preprocessor/test/iteration.h>, 0x0001))
 # include BOOST_PP_ITERATE()
 #
-# define BOOST_PP_ITERATION_PARAMS_1 (4, (1, 5, <libs/preprocessor/regression/iteration.h>, 0x0002))
+# define BOOST_PP_ITERATION_PARAMS_1 (4, (1, 5, <libs/preprocessor/test/iteration.h>, 0x0002))
 # include BOOST_PP_ITERATE()
 #
 # elif defined NO_FLAGS
@@ -46,7 +46,7 @@ struct BOOST_PP_CAT(Y, BOOST_PP_ITERATION()) { };
 # elif BOOST_PP_ITERATION_DEPTH() == 1 \
 	&& BOOST_PP_ITERATION_FLAGS() == 0x0002
 
-# define BOOST_PP_ITERATION_PARAMS_2 (3, (1, BOOST_PP_ITERATION(), <libs/preprocessor/regression/iteration.h>))
+# define BOOST_PP_ITERATION_PARAMS_2 (3, (1, BOOST_PP_ITERATION(), <libs/preprocessor/test/iteration.h>))
 # include BOOST_PP_ITERATE()
 
 # elif BOOST_PP_ITERATION_DEPTH() == 2 \
