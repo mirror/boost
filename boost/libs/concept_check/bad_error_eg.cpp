12b6d7eb403f51bef895e200595f6bc2b894afc1
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-04 08:41:54 +0000

    Added copyright statement
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17187 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/concept_check/bad_error_eg.cpp
+++ boost/libs/concept_check/bad_error_eg.cpp
@@ -1,3 +1,9 @@
+// (C) Copyright Jeremy Siek 2000. Permission to copy, use, modify,
+// sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+
 #include <list>
 #include <algorithm>
 
