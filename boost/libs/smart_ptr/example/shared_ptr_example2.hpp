73d67d12b55d4d282388f38ab14c56299a0384a0
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-04 13:35:06 +0000

    Added copyright.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17197 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/smart_ptr/example/shared_ptr_example2.hpp
+++ boost/libs/smart_ptr/example/shared_ptr_example2.hpp
@@ -1,5 +1,13 @@
 //  Boost shared_ptr_example2 header file  -----------------------------------//
 
+//  (C) Copyright Beman Dawes 2001. Permission to copy,
+//  use, modify, sell and distribute this software is granted provided this
+//  copyright notice appears in all copies. This software is provided "as is"
+//  without express or implied warranty, and with no claim as to its
+//  suitability for any purpose.
+
+//  See http://www.boost.org for most recent version including documentation.
+
 #include <boost/shared_ptr.hpp>
 
 //  This example demonstrates the handle/body idiom (also called pimpl and
