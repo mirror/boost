f5d08e1c057f4ccf4d172e9d68a1791988d6f639
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-04 11:32:55 +0000

    Added copyright statement and untabified
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17192 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/format/test/format_test2.cpp
+++ boost/libs/format/test/format_test2.cpp
@@ -1,3 +1,14 @@
+// -*- C++ -*-
+//  Boost general library 'format'   ---------------------------
+//  See http://www.boost.org for updates, documentation, and revision history.
+
+//  (C) Samuel Krempp 2001
+//                  krempp@crans.ens-cachan.fr
+//  Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
 #include "boost/format.hpp"
 
 #include <iostream> 
