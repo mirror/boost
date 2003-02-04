f8de033083602161788b695979ba076eaad7f576
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-04 12:20:06 +0000

    Untabified
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17195 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/signals/trackable.hpp
+++ boost/boost/signals/trackable.hpp
@@ -145,39 +145,39 @@ namespace BOOST_SIGNALS_NAMESPACE {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5>
+               typename T5>
       inline void add_if_trackable(R (*)(T1, T2, T3, T4, T5)) const
       {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5, typename T6>
+               typename T5, typename T6>
       inline void add_if_trackable(R (*)(T1, T2, T3, T4, T5, T6)) const
       {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5, typename T6, typename T7>
+               typename T5, typename T6, typename T7>
       inline void add_if_trackable(R (*)(T1, T2, T3, T4, T5, T6, T7)) const
       {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5, typename T6, typename T7, typename T8>
+               typename T5, typename T6, typename T7, typename T8>
       inline void add_if_trackable(R (*)(T1, T2, T3, T4, T5, T6, T7, T8)) const
       {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5, typename T6, typename T7, typename T8, typename T9>
+               typename T5, typename T6, typename T7, typename T8, typename T9>
       inline void 
       add_if_trackable(R (*)(T1, T2, T3, T4, T5, T6, T7, T8, T9)) const
       {
       }
 
       template<typename R, typename T1, typename T2, typename T3, typename T4,
-     	       typename T5, typename T6, typename T7, typename T8, typename T9,
-	       typename T10>
+               typename T5, typename T6, typename T7, typename T8, typename T9,
+               typename T10>
       inline void 
       add_if_trackable(R (*)(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)) const
       {
