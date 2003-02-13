bdb07ea68d361c22aab494386561b440288d4b7b
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-13 18:39:48 +0000

    add c includes for high precision clock types
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17377 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/c_time.hpp
+++ boost/boost/date_time/c_time.hpp
@@ -10,15 +10,19 @@
   Provide workarounds related to the ctime header
 */
 
-
+#include "boost/date_time/compiler_config.hpp"
 #include <ctime>
 //Work around libraries that don't put time_t and time in namespace std
-#include "boost/config.hpp"
+
 #ifdef BOOST_NO_STDC_NAMESPACE
 namespace std { using ::time_t; using ::time; using ::localtime;
                 using ::tm;  using ::gmtime; }
 #endif
 
+//The following is used to support high precision time clocks
+#ifdef BOOST_HAS_GETTIMEOFDAY
+#include <sys/time.h>
+#endif
 
 /* Copyright (c) 2002
  * CrystalClear Software, Inc.
