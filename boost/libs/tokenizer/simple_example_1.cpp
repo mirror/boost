5780be7a8787131c520f171d93d9bd11131b3280
bjorn_karlsson <bjorn_karlsson@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-13 14:06:40 +0000

    Added copyright
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17361 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/tokenizer/simple_example_1.cpp
+++ boost/libs/tokenizer/simple_example_1.cpp
@@ -1,3 +1,14 @@
+// © Copyright John R. Bandela 2001. 
+
+// Permission to copy, use, modify, sell and distribute this software
+// is granted provided this copyright notice appears in all
+// copies. This software is provided "as is" without express or
+// implied warranty, and with no claim as to its suitability for any
+// purpose.
+
+// See http://www.boost.org/libs/tokenizer for documenation
+
+
 // simple_example_1.cpp
 #include<iostream>
 #include<boost/tokenizer.hpp>
--- boost/libs/tokenizer/simple_example_5.cpp
+++ boost/libs/tokenizer/simple_example_5.cpp
@@ -1,3 +1,13 @@
+// © Copyright John R. Bandela 2001. 
+
+// Permission to copy, use, modify, sell and distribute this software
+// is granted provided this copyright notice appears in all
+// copies. This software is provided "as is" without express or
+// implied warranty, and with no claim as to its suitability for any
+// purpose.
+
+// See http://www.boost.org/libs/tokenizer for documenation
+
 /// simple_example_5.cpp
 #include<iostream>
 #include<boost/token_iterator.hpp>
