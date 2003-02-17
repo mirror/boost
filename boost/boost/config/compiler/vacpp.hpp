9c6a7c4c03e9c3eb0f3336e3b57571ed1aab8dc1
schoepflin <schoepflin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-17 12:34:08 +0000

    Define BOOST_NO_MEMBER_TEMPLATE_KEYWORD only for compiler version <= 502.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17484 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/vacpp.hpp
+++ boost/boost/config/compiler/vacpp.hpp
@@ -12,13 +12,15 @@
 #  define BOOST_NO_MEMBER_FUNCTION_SPECIALIZATIONS
 #endif
 
+#if (__IBMCPP__ <= 502)
+#  define BOOST_NO_MEMBER_TEMPLATE_KEYWORD
+#endif
+
 #if (__IBMCPP__ <= 502) || !defined(BOOST_STRICT_CONFIG)
 // Actually the compiler supports inclass member initialization but it
 // requires a definition for the class member and it doesn't recognize
 // it as an integral constant expression when used as a template argument.
 #  define BOOST_NO_INCLASS_MEMBER_INITIALIZATION
-
-#  define BOOST_NO_MEMBER_TEMPLATE_KEYWORD
 #  define BOOST_NO_INTEGRAL_INT64_T
 #endif
 
