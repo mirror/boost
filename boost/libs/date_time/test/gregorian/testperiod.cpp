2c1c08569b7340b3d00bdd1956f73d600dddce90
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-23 21:12:18 +0000

    added test for adjacent, is_before, and is_after
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17609 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/test/gregorian/testperiod.cpp
+++ boost/libs/date_time/test/gregorian/testperiod.cpp
@@ -82,6 +82,54 @@ int main()
   date_period r4(date(2000,Jan,5), date(2000,Jan,10));
   check("[5 -10) merge [6-9)  -> [5-10)", i1.merge(i5) == r4);
   
+
+  date bf_start(2000,Jan,5);
+  date bf_end(2000,Jan,10);
+  date bf_before(2000,Jan,4); //is before the period
+  date bf_after(2000,Jan,11);  //is really after
+  date bf_during(2000, Jan, 7);
+  date_period bfp1(bf_start, bf_end);  //[2000-Jan-5 - 2000-Jan10)
+  
+  check("is before -- start boundary", !bfp1.is_before(bf_start));
+  check("is before -- end boundary", bfp1.is_before(bf_end));
+  check("is before -- last boundary", !bfp1.is_before(bfp1.last()));
+  check("is before -- false", !bfp1.is_before(bf_before));
+  check("is before -- false", !bfp1.is_before(bf_during));
+  check("is before -- true",  bfp1.is_before(bf_after));
+
+  check("is after -- start boundary", !bfp1.is_after(bf_start));
+  check("is after -- end boundary", !bfp1.is_after(bf_end));
+  check("is after -- last boundary", !bfp1.is_after(bfp1.last()));
+  check("is after -- true",   bfp1.is_after(bf_before));
+  check("is after -- false", !bfp1.is_after(bf_during));
+  check("is after -- false", !bfp1.is_after(bf_after));
+
+  //adjacent tests
+  /*
+          [5-----10)            adj1
+     [1----5)                   adj2
+             [7-----12)         adj3
+                     [12----15) adj4
+     [1-3)                      adj5
+             [7-9)              adj6
+  */  
+  date_period adj1(date(2000,Jan,5), date(2000,Jan,10));
+  date_period adj2(date(2000,Jan,1), date(2000,Jan,5));
+  date_period adj3(date(2000,Jan,7), date(2000,Jan,12));
+  date_period adj4(date(2000,Jan,12), date(2000,Jan,15));
+  date_period adj5(date(2000,Jan,1),  date(2000,Jan,3));
+  date_period adj6(date(2000,Jan,7),  date(2000,Jan,9));
+  
+  check("is adjacent -- adj1-->adj2", adj1.is_adjacent(adj2));
+  check("is adjacent -- adj2-->adj1", adj2.is_adjacent(adj1));
+  check("is adjacent -- adj1-->adj3", !adj1.is_adjacent(adj3));
+  check("is adjacent -- adj3-->adj1", !adj3.is_adjacent(adj1));
+  check("is adjacent -- adj1-->adj4", !adj1.is_adjacent(adj4));
+  check("is adjacent -- adj4-->adj1", !adj4.is_adjacent(adj1));
+  check("is adjacent -- adj1-->adj5", !adj1.is_adjacent(adj5));
+  check("is adjacent -- adj5-->adj1", !adj5.is_adjacent(adj1));
+  check("is adjacent -- adj1-->adj6", !adj1.is_adjacent(adj6));
+  check("is adjacent -- adj6-->adj1", !adj6.is_adjacent(adj1));
   
   printTestStats();
 
