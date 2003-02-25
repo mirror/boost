fcfc398d6cc325f0d1450728139c0232e57d4202
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-02-25 11:51:56 +0000

    implement 'switch' construct
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17633 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/deref.hpp
+++ boost/boost/mpl/deref.hpp
@@ -1,9 +1,9 @@
 //-----------------------------------------------------------------------------
-// boost mpl/select2nd.hpp header file
+// boost mpl/deref.hpp header file
 // See http://www.boost.org for updates, documentation, and revision history.
 //-----------------------------------------------------------------------------
 //
-// Copyright (c) 2000-03
+// Copyright (c) 2002-03
 // Aleksey Gurtovoy
 //
 // Permission to use, copy, modify, distribute and sell this software
@@ -14,8 +14,8 @@
 // suitability of this software for any purpose. It is provided "as is" 
 // without express or implied warranty.
 
-#ifndef BOOST_MPL_SELECT2ND_HPP_INCLUDED
-#define BOOST_MPL_SELECT2ND_HPP_INCLUDED
+#ifndef BOOST_MPL_DEREF_HPP_INCLUDED
+#define BOOST_MPL_DEREF_HPP_INCLUDED
 
 #include "boost/mpl/aux_/void_spec.hpp"
 #include "boost/mpl/aux_/lambda_support.hpp"
@@ -25,24 +25,24 @@ namespace boost {
 namespace mpl {
 
 template<
-      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Pair)
+      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Iterator)
     >
-struct select2nd
+struct deref
 {
-    typedef typename Pair::second type;
-    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,select2nd,(Pair))
+    typedef typename Iterator::type type;
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,deref,(Iterator))
 };
 
-#if defined(BOOST_MPL_MSVC_60_ETI_BUG)
-template<> struct select2nd<int>
+#if defined(BOOST_MPL_MSVC_ETI_BUG)
+template<> struct deref<int>
 {
     typedef int type;
 };
 #endif
 
-BOOST_MPL_AUX_VOID_SPEC(1, select2nd)
+BOOST_MPL_AUX_VOID_SPEC(1, deref)
 
 } // namespace mpl
 } // namespace boost
 
-#endif // BOOST_MPL_SELECT2ND_HPP_INCLUDED
+#endif // BOOST_MPL_DEREF_HPP_INCLUDED
--- boost/boost/mpl/switch.hpp
+++ boost/boost/mpl/switch.hpp
@@ -1,9 +1,9 @@
 //-----------------------------------------------------------------------------
-// boost mpl/select2nd.hpp header file
+// boost mpl/switch.hpp header file
 // See http://www.boost.org for updates, documentation, and revision history.
 //-----------------------------------------------------------------------------
 //
-// Copyright (c) 2000-03
+// Copyright (c) 2003
 // Aleksey Gurtovoy
 //
 // Permission to use, copy, modify, distribute and sell this software
@@ -14,35 +14,42 @@
 // suitability of this software for any purpose. It is provided "as is" 
 // without express or implied warranty.
 
-#ifndef BOOST_MPL_SELECT2ND_HPP_INCLUDED
-#define BOOST_MPL_SELECT2ND_HPP_INCLUDED
+#ifndef BOOST_MPL_SWITCH_HPP_INCLUDED
+#define BOOST_MPL_SWITCH_HPP_INCLUDED
 
+#include "boost/mpl/find_if.hpp"
+#include "boost/mpl/select1st.hpp"
+#include "boost/mpl/select2nd.hpp"
+#include "boost/mpl/deref.hpp"
+#include "boost/mpl/lambda.hpp"
+#include "boost/mpl/apply.hpp"
 #include "boost/mpl/aux_/void_spec.hpp"
 #include "boost/mpl/aux_/lambda_support.hpp"
-#include "boost/mpl/aux_/config/eti.hpp"
 
 namespace boost {
 namespace mpl {
 
-template<
-      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Pair)
+template< 
+      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Body)
+    , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(T)
     >
-struct select2nd
+struct switch_
 {
-    typedef typename Pair::second type;
-    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,select2nd,(Pair))
+    typedef typename find_if<
+          Body
+        , apply1< lambda< select1st<_1> >, T >
+        >::type iter_;
+        
+    typedef typename deref<iter_>::type pair_;
+    typedef typename lambda< typename select2nd<pair_>::type >::type f_;
+    typedef typename apply1<f_,T>::type type;
+
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(2,switch_,(Body,T))
 };
 
-#if defined(BOOST_MPL_MSVC_60_ETI_BUG)
-template<> struct select2nd<int>
-{
-    typedef int type;
-};
-#endif
-
-BOOST_MPL_AUX_VOID_SPEC(1, select2nd)
+BOOST_MPL_AUX_VOID_SPEC(2, switch_)
 
 } // namespace mpl
 } // namespace boost
 
-#endif // BOOST_MPL_SELECT2ND_HPP_INCLUDED
+#endif // BOOST_MPL_SWITCH_HPP_INCLUDED
