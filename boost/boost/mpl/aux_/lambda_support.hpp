e8046248d531d9d7b78c014c2e5faffb8364e44c
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-03-07 11:38:59 +0000

    restore an old revision for SGI MIPSpro C++
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17757 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/aux_/lambda_support.hpp
+++ boost/boost/mpl/aux_/lambda_support.hpp
@@ -26,6 +26,7 @@
 
 #else
 
+#   include "boost/mpl/aux_/config/workaround.hpp"
 #   include "boost/mpl/aux_/preprocessor/params.hpp"
 #   include "boost/preprocessor/tuple/to_list.hpp"
 #   include "boost/preprocessor/list/for_each_i.hpp"
@@ -36,10 +37,34 @@
     typedef_ param BOOST_PP_CAT(arg,BOOST_PP_INC(i)); \
     /**/
 
+// agurt, 07/mar/03: restore an old revision for the sake of SGI MIPSpro C++
+#if BOOST_WORKAROUND(__EDG_VERSION__, <= 238) 
+
+#   define BOOST_MPL_AUX_LAMBDA_SUPPORT(i, name, params) \
+    BOOST_STATIC_CONSTANT(int, arity = i); \
+    BOOST_PP_LIST_FOR_EACH_I_R( \
+          1 \
+        , BOOST_MPL_AUX_LAMBDA_SUPPORT_ARG_TYPEDEF_FUNC \
+        , typedef \
+        , BOOST_PP_TUPLE_TO_LIST(i,params) \
+        ) \
+    struct rebind \
+    { \
+        template< BOOST_MPL_PP_PARAMS(i,typename U) > struct apply \
+            : name< BOOST_MPL_PP_PARAMS(i,U) > \
+        { \
+        }; \
+    }; \
+    /**/
+
+#   define BOOST_MPL_AUX_LAMBDA_SUPPORT_SPEC(i, name, params) \
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(i, name, params) \
+    /**/
+
+#elif BOOST_WORKAROUND(__EDG_VERSION__, <= 244) && !defined(BOOST_INTEL_CXX_VERSION)
 // agurt, 18/jan/03: old EDG-based compilers actually enforce 11.4 para 9
 // (in strict mode), so we have to provide an alternative to the 
 // MSVC-optimized implementation
-#if defined(__EDG_VERSION__) && !defined(BOOST_INTEL_CXX_VERSION)
 
 #   define BOOST_MPL_AUX_LAMBDA_SUPPORT_SPEC(i, name, params) \
     BOOST_STATIC_CONSTANT(int, arity = i); \
