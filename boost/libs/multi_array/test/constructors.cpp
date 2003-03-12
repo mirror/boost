9db020ae7af0894bbbea888f1b49fd914eacfb9a
rgarcia <rgarcia@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-03-12 15:39:25 +0000

    Fixed a broken patch job....grr!
    git-svn-id: http://svn.boost.org/svn/boost/trunk@17853 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/multi_array/test/constructors.cpp
+++ boost/libs/multi_array/test/constructors.cpp
@@ -10,12 +10,12 @@
 #include <algorithm>
 #include <list>
 
-void check_shape(const double&, size_t*, int*, unsigned int)
+void check_shape(const double&, std::size_t*, int*, unsigned int)
 {}
 
 template <class Array>
 void check_shape(const Array& A, 
-                 size_t* sizes, 
+                 std::size_t* sizes, 
                  int* strides,
                  unsigned int num_elements)
 {
