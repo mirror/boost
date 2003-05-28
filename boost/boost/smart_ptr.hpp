439cda06131896db9adef25b472a308c88d2b588
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-05-28 13:51:35 +0000

    smart_ptr.hpp now includes all smart pointer headers for convenience.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@18588 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/smart_ptr.hpp
+++ boost/boost/smart_ptr.hpp
@@ -1,10 +1,11 @@
 //
 //  smart_ptr.hpp
 //
-//  For compatibility, this header includes the headers for the four "classic"
-//  smart pointer class templates.
+//  For convenience, this header includes the rest of the smart
+//  pointer library headers.
 //
 //  Copyright (c) 1998-2002 boost.org
+//  Copyright (c) 2003 Peter Dimov
 //
 //  Permission to copy, use, modify, sell and distribute this software
 //  is granted provided this copyright notice appears in all copies.
@@ -14,7 +15,15 @@
 //  http://www.boost.org/libs/smart_ptr/smart_ptr.htm
 //
 
-#include <boost/scoped_ptr.hpp> 
+#include <boost/config.hpp>
+
+#include <boost/scoped_ptr.hpp>
 #include <boost/scoped_array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/shared_array.hpp>
+
+#if !defined(BOOST_NO_MEMBER_TEMPLATES) || defined(BOOST_MSVC6_MEMBER_TEMPLATES)
+# include <boost/weak_ptr.hpp>
+# include <boost/intrusive_ptr.hpp>
+# include <boost/enable_shared_from_this.hpp>
+#endif
