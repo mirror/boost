7cc81252f88e4b81472c58e80347639e47a35ffc
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-05-30 00:37:20 +0000

    fix config for Borland
    git-svn-id: http://svn.boost.org/svn/boost/trunk@18601 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/aux_/config/overload_resolution.hpp
+++ boost/boost/mpl/aux_/config/overload_resolution.hpp
@@ -1,9 +1,11 @@
-//-----------------------------------------------------------------------------
-// boost mpl/aux_/config/overload_resolution.hpp header file
-// See http://www.boost.org for updates, documentation, and revision history.
-//-----------------------------------------------------------------------------
-//
-// Copyright (c) 2002
+
+#ifndef BOOST_MPL_AUX_CONFIG_OVERLOAD_RESOLUTION_HPP_INCLUDED
+#define BOOST_MPL_AUX_CONFIG_OVERLOAD_RESOLUTION_HPP_INCLUDED
+
+// + file: boost/mpl/aux_/config/overload_resolution.hpp
+// + last modified: 02/may/03
+
+// Copyright (c) 2002-03
 // Aleksey Gurtovoy
 //
 // Permission to use, copy, modify, distribute and sell this software
@@ -13,20 +15,20 @@
 // supporting documentation. No representations are made about the 
 // suitability of this software for any purpose. It is provided "as is" 
 // without express or implied warranty.
+//
+// See http://www.boost.org/libs/mpl for documentation.
 
-#ifndef BOOST_MPL_AUX_CONFIG_OVERLOAD_RESOLUTION_HPP_INCLUDED
-#define BOOST_MPL_AUX_CONFIG_OVERLOAD_RESOLUTION_HPP_INCLUDED
-
-#include "boost/config.hpp"
-#include "boost/detail/workaround.hpp"
+#include "boost/mpl/aux_/config/workaround.hpp"
 
 #if !defined(BOOST_MPL_BROKEN_OVERLOAD_RESOLUTION)
 
-#   if BOOST_WORKAROUND(__BORLANDC__, <= 0x561) || BOOST_WORKAROUND(__MWERKS__, < 0x3001)
-                            
+#   if BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x561)) \
+    || BOOST_WORKAROUND(__MWERKS__, < 0x3001)
+
 #      define BOOST_MPL_BROKEN_OVERLOAD_RESOLUTION
 
 #   endif
+
 #endif
 
 #endif // BOOST_MPL_AUX_CONFIG_OVERLOAD_RESOLUTION_HPP_INCLUDED
