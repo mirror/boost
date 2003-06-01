6a4518048611c51398e621c32b2b6d6fba2097d9
beman_dawes <beman_dawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-06-01 18:06:27 +0000

    BOOST_NO_INTRINSIC_WCHAR_T now correct for 7.0 on Win32, based on config_info report
    git-svn-id: http://svn.boost.org/svn/boost/trunk@18640 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/intel.hpp
+++ boost/boost/config/compiler/intel.hpp
@@ -56,7 +56,7 @@
 #endif
 
 #if _MSC_VER+0 >= 1000
-#  ifndef _NATIVE_WCHAR_T_DEFINED
+#  if !defined(_NATIVE_WCHAR_T_DEFINED) && !defined(_WCHAR_T_DEFINED)
 #     define BOOST_NO_INTRINSIC_WCHAR_T
 #  endif
 #  if _MSC_VER >= 1200
