1b8fa8acea6695f1f2a951df50403faacfe5848e
imaman <imaman@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-07-28 08:41:23 +0000

    Copyright notice added
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19327 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/variant/test/test8.cpp
+++ boost/libs/variant/test/test8.cpp
@@ -1,3 +1,19 @@
+//-----------------------------------------------------------------------------
+// boost variant/get.hpp header file
+// See http://www.boost.org for updates, documentation, and revision history.
+//-----------------------------------------------------------------------------
+//
+// Copyright (c) 2003
+// Eric Friedman, Itay Maman
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee, 
+// provided that the above copyright notice appears in all copies and 
+// that both the copyright notice and this permission notice appear in 
+// supporting documentation. No representations are made about the 
+// suitability of this software for any purpose. It is provided "as is" 
+// without express or implied warranty.
+
 // File: test8.cpp
 
 #include "boost/test/minimal.hpp"
