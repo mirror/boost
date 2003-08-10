f80e727568ee94ab8e4f6bafaf6fee387b69efe7
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-10 14:12:58 +0000

    add time_t conversion function
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19504 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/posix_time/conversion.hpp
+++ boost/boost/date_time/posix_time/conversion.hpp
@@ -1,19 +1,33 @@
-#ifndef POSIX_TIME_HPP___
-#define POSIX_TIME_HPP___
-/* Copyright (c) 2002 CrystalClear Software, Inc.
+#ifndef POSIX_TIME_CONVERSION_HPP___
+#define POSIX_TIME_CONVERSION_HPP___
+/* Copyright (c) 2003 CrystalClear Software, Inc.
  * Disclaimer & Full Copyright at end of file
  * Author: Jeff Garland 
  */
-//!@file Global header file to get all of posix time types
 
 #include "boost/date_time/posix_time/ptime.hpp"
-#include "boost/date_time/posix_time/time_formatters.hpp"
-#include "boost/date_time/posix_time/time_parsers.hpp"
-#include "boost/date_time/posix_time/conversion.hpp"
- 
+#include "boost/date_time/posix_time/posix_time_duration.hpp"
+#include "boost/date_time/c_time.hpp"
+
+namespace boost {
+
+namespace posix_time {
 
 
-/* Copyright (c) 2002
+  //! Function that converts a time_t into a ptime.
+  inline
+  ptime from_time_t(time_t t) 
+  {
+    ptime start(gregorian::date(1970,1,1));
+    return start + seconds(t);
+  }
+
+
+} } //namespace boost::posix_time
+
+
+
+/* Copyright (c) 2003
  * CrystalClear Software, Inc.
  *
  * Permission to use, copy, modify, distribute and sell this software
