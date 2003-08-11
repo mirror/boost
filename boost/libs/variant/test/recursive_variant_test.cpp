16997f0b57d9740adba32db55e57ba7b7721db71
ebf <ebf@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-11 20:42:02 +0000

    Added copyright and file info.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19538 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/variant/test/recursive_variant_test.cpp
+++ boost/libs/variant/test/recursive_variant_test.cpp
@@ -1,5 +1,21 @@
-#include "boost/variant.hpp"
+//-----------------------------------------------------------------------------
+// boost-libs variant/test/recursive_variant_test.cpp source file
+// See http://www.boost.org for updates, documentation, and revision history.
+//-----------------------------------------------------------------------------
+//
+// Copyright (c) 2003
+// Eric Friedman, Itay Maman
+//
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee, 
+// provided that the above copyright notice appears in all copies and 
+// that both the copyright notice and this permission notice appear in 
+// supporting documentation. No representations are made about the 
+// suitability of this software for any purpose. It is provided "as is" 
+// without express or implied warranty.
+
 #include "boost/test/minimal.hpp"
+#include "boost/variant.hpp"
 
 #include <iostream>
 #include <sstream>
