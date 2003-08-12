97846417f0cc0a7c66c33c506b2eaa7fd3b61015
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-12 14:43:07 +0000

    add methods for date generator streaming
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19555 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/date_time/src/gregorian/date_generators.cpp
@@ -0,0 +1,43 @@
+
+
+/* Copyright (c) 2003 CrystalClear Software, Inc.
+ * Disclaimer & Full Copyright at end of file
+ * Author: Jeff Garland, Bart Garst 
+ */
+
+#include "boost/date_time/date_generators.hpp"
+
+namespace boost {
+namespace date_time {
+
+  const char* const _nth_as_str[] = {"out of range", "first", "second",
+    "third", "fourth", "fifth"};
+    
+  //! Returns nth arg as string. 1 -> "first", 2 -> "second", max is 5.
+  const char*
+  nth_as_str(const int ele)
+  {
+    if(ele >= 1 || ele <= 5) {
+      return _nth_as_str[ele];
+    } 
+    else {
+      return _nth_as_str[0];
+    }
+  }
+
+} } //namespace date_time 
+
+
+
+
+/* Copyright (c) 2003
+ * CrystalClear Software, Inc.
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  CrystalClear Software makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ */
