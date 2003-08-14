23ffd683b3e544933f4bac7d7fb9052780546455
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-14 14:48:16 +0000

    check in docs re-generation instructions/resources
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19596 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/mpl/doc/msxsl_build.bat
+++ boost/libs/mpl/doc/msxsl_build.bat
@@ -1,11 +1,14 @@
 @echo off
 
-if "%4" == ""  goto build
-f:\msxsl\msxsl.exe f:\home\depot\xml\stylesheets\docbook-xsl-1.50.0\html\titlepage.templates.xml f:\home\depot\xml\stylesheets\docbook-xsl-1.52.2\template\titlepage.xsl | f:\msxsl\msxsl.exe - f:\msxsl\to_utf8.xsl -o f:\home\depot\xml\stylesheets\docbook-1.50.0\html\titlepage.templates.xsl
+if "%3" == ""  goto usage
 
-:build
-sx -x lower -x empty %2 >%3.xml
-f:\msxsl\msxsl.exe %3.xml f:\home\depot\xml\stylesheets\docbook-xsl-1.50.0\html\my\%1 -o %3
+c:\home\sx\bin\sx -x lower -x empty %2 >%3.xml
+c:\home\msxsl\msxsl.exe %3.xml ..\..\..\..\..\docbook-xsl-1.50.0\html\my\%1 -o %3
 del %3.xml
-f:\tidy\tidy.exe -config f:\tidy\config.txt -m %3
-f:\tidy\tidy_attr.py %3
+c:\home\tidy\tidy.exe -config tidy_config -m %3
+python tidy_attr.py %3
+exit
+
+:usage
+echo Usage example: 
+echo    msxsl_build.bat docbook_msxsl.xsl src\body.sgml index.html
