ccc63b0bd2eef04f5c94d45d88f73c70e26cbef4
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-20 03:32:01 +0000

    serious beef up of int_adpater tests
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19707 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/test/testint_adapter.cpp
+++ boost/libs/date_time/test/testint_adapter.cpp
@@ -1,12 +1,13 @@
 /* Copyright (c) 2001 CrystalClear Software, Inc.
  * Disclaimer & Full Copyright at end of file
- * Author: Jeff Garland 
+ * Author: Jeff Garland, Bart Garst
  */
 
 #include "boost/date_time/int_adapter.hpp"
 #include "boost/date_time/testfrmwk.hpp"
 #include "boost/cstdint.hpp"
 #include <iostream>
+#include <sstream>
 
 template<typename int_type> 
 void print()
@@ -24,6 +25,30 @@ void print()
     int_type::pos_infinity().as_number() << std::endl;
   std::cout << "not a number:  " << 
     int_type::not_a_number().as_number() << std::endl;
+  std::stringstream ss("");
+  std::string s("");
+  int_type i = int_type::neg_infinity();
+  ss << i;
+  s = "-infinity";
+  check("streaming -infinity", ss.str() == s);
+  
+  i = int_type::pos_infinity();
+  ss.str("");
+  ss << i;
+  s = "+infinity";
+  check("streaming +infinity", ss.str() == s);
+  
+  i = int_type::not_a_number();
+  ss.str("");
+  ss << i;
+  s = "not-a-number";
+  check("streaming nan", ss.str() == s);
+
+  i = 12;
+  ss.str("");
+  ss << i;
+  s = "12";
+  check("streaming digits", ss.str() == s);
 #endif
 }
 
@@ -34,11 +59,21 @@ void test_int()
   int_type i = int_type::neg_infinity();
 
   check("is infinity", i.is_infinity());
+  check("is special_value (neg_inf)", i.is_special());
   check("as_special convert", boost::date_time::neg_infin == i.as_special() );
   check("as_special convert", boost::date_time::neg_infin == int_type::to_special(i.as_number()) );
+  int_type h = int_type::neg_infinity();
   i = int_type::pos_infinity();
   check("is infinity", i.is_infinity());
   check("as_special convert", boost::date_time::pos_infin == i.as_special() );
+  check("infinity less",     h < i);
+  check("infinity less",     h < 0);
+  check("infinity greater",  i > h);
+  check("infinity greater",  i > 0);
+  h = int_type::not_a_number();
+  check("nan less",  !(h < 0));
+  check("nan greater",  !(h > 0));
+  check("nan equal",  h == int_type::not_a_number());
   i = 1;
   check("is infinity", !i.is_infinity());
   int_type j = int_type::neg_infinity();
@@ -47,6 +82,8 @@ void test_int()
   check("infinity greater",  (i > j));
   check("infinity equal",    !(j == i));
   check("infinity equal",    j == j);
+  check("infinity equal",    !(j == 0));
+  check("infinity not equal",    j != 0);
 
   int_type k = 1;
   check("as_special convert", boost::date_time::not_special == k.as_special() );
@@ -54,16 +91,27 @@ void test_int()
   check("infinity not equal",    i != int_type::neg_infinity());
   check("infinity not equal",    i != int_type::pos_infinity());
   int_type l = i + int_type::pos_infinity();
+  check("is special_value (pos_inf)", l.is_special());
   check("add infinity" ,         l == int_type::pos_infinity());
+  check("inf - inf = nan",       (l - l) == int_type::not_a_number());
+  check("-inf + inf = nan",       (j + l) == int_type::not_a_number());
   check("add 2",                 (i + 2) == 3);
   i = int_type::not_a_number();
+  check("+inf * integer", (l * 2) == l);
+  check("+inf / integer", (l / 2) == l);
+  check("+inf / -integer", (l / -2) == j);
+  check("+inf * -integer", (l * -2) == j);
+  check("+inf * -inf", (l * j) == j);
+  check("+inf / +inf", (l / l) == i);
+  check("+inf * zero", (l * 0) == i);
+  check("is special_value (nan)", i.is_special());
   check("as_special convert", boost::date_time::not_a_date_time == i.as_special() );
   check("add not a number",      (i + 2) == int_type::not_a_number());
   check("sub not a number",      (i - 2) == int_type::not_a_number());
   check("sub from infin",        (l - 2) == int_type::pos_infinity());
   i = 5;
   check("add zero ",             (i + 0) == 5);
-  check("sub from 5-2 ",         (5 - 2) == 3);
+  check("sub from 5-2 ",         (i - 2) == 3);
   //  std::cout << i.as_number() << std::endl;
   check("from special ", 
         int_type::from_special(boost::date_time::pos_infin) == int_type::pos_infinity());
