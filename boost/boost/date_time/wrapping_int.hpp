d74eada964d9a1667a4200f0a35b66612c2ab285
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-08-28 13:22:27 +0000

    add subtract functions to wrapping int
    git-svn-id: http://svn.boost.org/svn/boost/trunk@19831 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/wrapping_int.hpp
+++ boost/boost/date_time/wrapping_int.hpp
@@ -2,7 +2,7 @@
 #define _DATE_TIME_WRAPPING_INT_HPP__
 /* Copyright (c) 2001 CrystalClear Software, Inc.
  * Disclaimer & Full Copyright at end of file
- * Author: Jeff Garland 
+ * Author: Jeff Garland, Bart Garst
  */
 
 
@@ -80,30 +80,46 @@ public:
   typedef int_type_ int_type;
   static unsigned long wrap_value() {return wrap_max;}
   static unsigned long min_value()  {return wrap_min;}
-  //!Add, return true if wrapped
-  wrapping_int2(int_type v) : value_(v) {};
+  /*! If initializing value is out of range of [wrap_min, wrap_max],
+   * value will be initialized to closest of min or max */
+  wrapping_int2(int_type v) : value_(v) {
+    if(value_ < wrap_min)
+    {
+      value_ = wrap_min;
+    }
+    if(value_ > wrap_max)
+    {
+      value_ = wrap_max;
+    }
+  }
   //! Explicit converion method
   int_type as_int()   const   {return value_;}
   operator int_type() const {return value_;}
+  //!Add, return number of wraps performed 
   int_type add(int_type v) 
   {
-    //take the mod here and assign it....
-    int_type remainder = v % wrap_max;
-    int_type overflow = v / wrap_max;
-//     std::cout << "wi2: " << value_ << "|"
-// 	      << v << "|"
-// 	      << remainder << "|" 
-// 	      << wrap_max  << "|"
-//               << wrap_min  << std::endl;
+    int_type remainder = v % (wrap_max - wrap_min + 1);
+    int_type overflow = v / (wrap_max - wrap_min + 1);
     value_ += remainder;
-    if ((value_) > wrap_max) {
+    if ((value_) > wrap_max) 
+    {
       overflow++;
       value_ -= (wrap_max - wrap_min + 1);
     }
-//     std::cout << "wi2 final: " 
-// 	      << overflow << "|" 
-// 	      << value_ << std::endl;
     return overflow;
+  }
+  //! Subtract will return '-d' if wrapping took place ('d' is the number of wraps)
+  int_type subtract(int_type v) 
+  {
+    int_type remainder = v % (wrap_max - wrap_min + 1);
+    int_type underflow = -(v / (wrap_max - wrap_min + 1));
+    value_ -= remainder;
+    if ((value_) < wrap_min) 
+    {
+      underflow--;
+      value_ += (wrap_max - wrap_min + 1);
+    }
+    return underflow;
   }
 		    
 private:
@@ -135,3 +151,4 @@ private:
 
 
 #endif
+
