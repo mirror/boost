9a0a3963cf4b9f9ee085abceef9f5067a74f081b
ebf <ebf@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-09-15 06:25:22 +0000

    Added base-derived test.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20065 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/variant/test/variant_reference_test.cpp
+++ boost/libs/variant/test/variant_reference_test.cpp
@@ -17,6 +17,48 @@
 #include "boost/variant.hpp"
 #include "boost/test/minimal.hpp"
 
+#include "boost/mpl/bool.hpp"
+#include "boost/type_traits/is_pointer.hpp"
+
+/////
+// support types and functions
+
+struct base_t { };
+struct derived_t : base_t { };
+
+template <typename Base, typename Derived>
+bool check_base_derived(Base* b, Derived* d, long)
+{
+    return b == d;
+}
+
+template <typename Base, typename Derived>
+bool check_base_derived(Base& b, Derived& d, int)
+{
+    return &b == &d;
+}
+
+template <typename T>
+T* wknd_get(boost::variant<T*>& var, long)
+{
+    return boost::get<T*>(var);
+}
+
+template <typename T>
+T& wknd_get(boost::variant<T&>& var, long)
+{
+    return boost::get<T>(var);
+}
+
+template <typename T>
+T& wknd_get(boost::variant<T>& var, int)
+{
+    return boost::get<T>(var);
+}
+
+/////
+// test functions
+
 template <typename T>
 void test_reference_content(T& t, const T& value1, const T& value2)
 {
@@ -39,10 +81,45 @@ void test_reference_content(T& t, const T& value1, const T& value2)
     BOOST_CHECK(( boost::get<T>(var2) == value1 ));
 }
 
+template <typename Base, typename Derived>
+void base_derived_test(Derived d)
+{
+    typedef typename boost::is_pointer<Base>::type is_ptr;
+
+    Base b(d);
+    BOOST_CHECK((check_base_derived(
+          b
+        , d
+        , 1L
+        )));
+
+    boost::variant<Base> base_var(d);
+    BOOST_CHECK((check_base_derived(
+          wknd_get(base_var, 1L)
+        , d
+        , 1L
+        )));
+
+    boost::variant<Derived> derived_var(d);
+    boost::variant<Base> base_from_derived_var(derived_var);
+    BOOST_CHECK((check_base_derived(
+          wknd_get(base_from_derived_var, 1L)
+        , wknd_get(derived_var, 1L)
+        , 1L
+        )));
+}
+
 int test_main(int , char* [])
 {
     int i = 0;
     test_reference_content(i, 1, 2);
+
+    /////
+
+    derived_t d;
+    base_derived_test< int&,int >(i);
+    base_derived_test< base_t*,derived_t* >(&d);
+    base_derived_test< base_t&,derived_t& >(d);
 
     return boost::exit_success;
 }
