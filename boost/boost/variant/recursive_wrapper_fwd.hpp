c957bd44716e7f54d2367bd1bf2bb76ea82b9e30
ebf <ebf@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-09-15 04:53:47 +0000

    Added is_recursive_wrapper and unwrap_recursive.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20060 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/variant/recursive_wrapper_fwd.hpp
+++ boost/boost/variant/recursive_wrapper_fwd.hpp
@@ -6,6 +6,8 @@
 // Copyright (c) 2002
 // Eric Friedman, Itay Maman
 //
+// Portions Copyright (C) 2002 David Abrahams
+//
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee, 
 // provided that the above copyright notice appears in all copies and 
@@ -16,6 +18,17 @@
 
 #ifndef BOOST_VARIANT_RECURSIVE_WRAPPER_FWD_HPP
 #define BOOST_VARIANT_RECURSIVE_WRAPPER_FWD_HPP
+
+#include "boost/config.hpp"
+#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+#   include "boost/mpl/apply_if.hpp"
+#   include "boost/mpl/bool.hpp"
+#   include "boost/mpl/identity.hpp"
+#   include "boost/type.hpp"
+#   include "boost/type_traits/detail/bool_trait_def.hpp"
+#endif
+
+#include "boost/mpl/aux_/lambda_support.hpp"
 
 namespace boost {
 
@@ -36,6 +49,100 @@ namespace boost {
 //   };
 //
 template <typename T> class recursive_wrapper;
+
+///////////////////////////////////////////////////////////////////////////////
+// metafunction is_recursive_wrapper (modeled on code by David Abrahams)
+//
+// True iff specified type matches recursive_wrapper<T>.
+//
+
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+
+template <typename T>
+struct is_recursive_wrapper
+    : mpl::false_
+{
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,is_recursive_wrapper,(T))
+};
+
+template <typename T>
+struct is_recursive_wrapper< recursive_wrapper<T> >
+    : mpl::true_
+{
+    BOOST_MPL_AUX_LAMBDA_SUPPORT_SPEC(1,is_recursive_wrapper,(T))
+};
+
+#else // defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+
+namespace detail {
+
+typedef char (&yes_recursive_wrapper_t)[1];
+typedef char (&no_recursive_wrapper_t)[2];
+
+no_recursive_wrapper_t is_recursive_wrapper_test(...);
+
+template<typename T>
+yes_recursive_wrapper_t is_recursive_wrapper_test(
+      type< ::boost::recursive_wrapper<T> >
+    );
+
+template<typename T>
+struct is_recursive_wrapper_impl
+{
+    BOOST_STATIC_CONSTANT(bool, value = (
+          sizeof(is_recursive_wrapper_test(type<T>()))
+          == sizeof(yes_recursive_wrapper_t)
+        ));
+};
+
+} // namespace detail
+
+BOOST_TT_AUX_BOOL_TRAIT_DEF1(
+      is_recursive_wrapper
+    , T
+    , (::boost::detail::is_recursive_wrapper_impl<T>::value)
+    )
+
+#endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION workaround
+
+///////////////////////////////////////////////////////////////////////////////
+// metafunction unwrap_recursive
+//
+// If specified type T matches recursive_wrapper<U>, then U; else T.
+//
+
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+
+template <typename T>
+struct unwrap_recursive
+{
+    typedef T type;
+
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,unwrap_recursive,(T))
+};
+
+template <typename T>
+struct unwrap_recursive< recursive_wrapper<T> >
+{
+    typedef T type;
+
+    BOOST_MPL_AUX_LAMBDA_SUPPORT_SPEC(1,unwrap_recursive,(T))
+};
+
+#else // defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
+
+template <typename T>
+struct unwrap_recursive
+    : mpl::apply_if<
+          is_recursive_wrapper<T>
+        , T
+        , mpl::identity< T >
+        >
+{
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,unwrap_recursive,(T))
+};
+
+#endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION workaround
 
 } // namespace boost
 
