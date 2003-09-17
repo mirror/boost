eeead1da5cbbe1c3cbdd8ba20c38a2cf4dbddc6d
ebf <ebf@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-09-17 06:27:09 +0000

    Now tests for const-correctness.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20087 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/variant/test/variant_visit_test.cpp
+++ boost/libs/variant/test/variant_visit_test.cpp
@@ -40,7 +40,7 @@ public:
     // not recommended design, but simplifies workarounds:
 
     template <typename U>
-    bool operator()(const U&) const
+    bool operator()(U&) const
     {
         return ::boost::is_same<T,U>::value;
     }
@@ -56,7 +56,7 @@ public:
     // not recommended design, but simplifies workarounds:
 
     template <typename U1, typename U2>
-    bool operator()(const U1&, const U2&) const
+    bool operator()(U1&, U2&) const
     {
         return ::boost::mpl::and_<
               boost::is_same<T1,U1>
@@ -66,6 +66,42 @@ public:
 
 };
 
+template <typename Checker, typename Variant>
+inline void unary_test(Variant& var, Checker* = 0)
+{
+    Checker checker;
+    const Checker& const_checker = checker;
+
+    // standard tests
+
+    BOOST_CHECK( boost::apply_visitor(checker, var) );
+    BOOST_CHECK( boost::apply_visitor(const_checker, var) );
+    BOOST_CHECK( boost::apply_visitor(Checker(), var) );
+
+    // delayed tests
+
+    BOOST_CHECK( boost::apply_visitor(checker)(var) );
+    BOOST_CHECK( boost::apply_visitor(const_checker)(var) );
+}
+
+template <typename Checker, typename Variant1, typename Variant2>
+inline void binary_test(Variant1& var1, Variant2& var2, Checker* = 0)
+{
+    Checker checker;
+    const Checker& const_checker = checker;
+
+    // standard tests
+
+    BOOST_CHECK( boost::apply_visitor(checker, var1, var2) );
+    BOOST_CHECK( boost::apply_visitor(const_checker, var1, var2) );
+    BOOST_CHECK( boost::apply_visitor(Checker(), var1, var2) );
+
+    // delayed tests
+
+    BOOST_CHECK( boost::apply_visitor(checker)(var1, var2) );
+    BOOST_CHECK( boost::apply_visitor(const_checker)(var1, var2) );
+}
+
 int test_main(int , char* [])
 {
     typedef boost::variant<udt1,udt2> var_t;
@@ -74,59 +110,38 @@ int test_main(int , char* [])
     udt2 u2;
     var_t var2(u2);
 
+    const var_t& cvar1 = var1;
+    const var_t& cvar2 = var2;
+
     //
     // unary tests
     //
 
     typedef unary_check_content_type<udt1> check1_t;
+    typedef unary_check_content_type<const udt1> check1_const_t;
     typedef unary_check_content_type<udt2> check2_t;
+    typedef unary_check_content_type<const udt2> check2_const_t;
 
-    check1_t check1;
-    const check1_t& check1_const = check1;
-    BOOST_CHECK( boost::apply_visitor(check1, var1) );
-    BOOST_CHECK( boost::apply_visitor(check1_const, var1) );
-    BOOST_CHECK( boost::apply_visitor(check1_t(), var1) );
+    unary_test< check1_t       >(var1);
+    unary_test< check1_const_t >(cvar1);
 
-    check2_t check2;
-    const check2_t& check2_const = check2;
-    BOOST_CHECK( boost::apply_visitor(check2, var2) );
-    BOOST_CHECK( boost::apply_visitor(check2_const, var2) );
-    BOOST_CHECK( boost::apply_visitor(check2_t(), var2) );
+    unary_test< check2_t       >(var2);
+    unary_test< check2_const_t >(cvar2);
 
     //
     // binary tests
     //
 
     typedef binary_check_content_type<udt1,udt2> check12_t;
+    typedef binary_check_content_type<const udt1, const udt2> check12_const_t;
     typedef binary_check_content_type<udt2,udt1> check21_t;
+    typedef binary_check_content_type<const udt2, const udt1> check21_const_t;
 
-    check12_t check12;
-    const check12_t& check12_const = check12;
-    BOOST_CHECK( boost::apply_visitor(check12, var1, var2) );
-    BOOST_CHECK( boost::apply_visitor(check12_const, var1, var2) );
-    BOOST_CHECK( boost::apply_visitor(check12_t(), var1, var2) );
+    binary_test< check12_t       >(var1,var2);
+    binary_test< check12_const_t >(cvar1,cvar2);
 
-    check21_t check21;
-    const check21_t& check21_const = check21;
-    BOOST_CHECK( boost::apply_visitor(check21, var2, var1) );
-    BOOST_CHECK( boost::apply_visitor(check21_const, var2, var1) );
-    BOOST_CHECK( boost::apply_visitor(check21_t(), var2, var1) );
-
-    //
-    // delayed tests
-    //
-
-    BOOST_CHECK( boost::apply_visitor(check1)(var1) );
-    BOOST_CHECK( boost::apply_visitor(check1_const)(var1) );
-
-    BOOST_CHECK( boost::apply_visitor(check2)(var2) );
-    BOOST_CHECK( boost::apply_visitor(check2_const)(var2) );
-
-    BOOST_CHECK( boost::apply_visitor(check12)(var1, var2) );
-    BOOST_CHECK( boost::apply_visitor(check12_const)(var1, var2) );
-
-    BOOST_CHECK( boost::apply_visitor(check21)(var2, var1) );
-    BOOST_CHECK( boost::apply_visitor(check21_const)(var2, var1) );
+    binary_test< check21_t       >(var2,var1);
+    binary_test< check21_const_t >(cvar2,cvar1);
 
     return boost::exit_success;
 }
