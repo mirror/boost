66b5218fe3f1eaaa60c13de4a8844580c9fb9a7a
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-10-01 10:28:17 +0000

    Updated licence
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20237 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/static_assert.hpp
+++ boost/boost/static_assert.hpp
@@ -1,8 +1,7 @@
 //  (C) Copyright John Maddock 2000.
-//  Permission to copy, use, modify, sell and
-//  distribute this software is granted provided this copyright notice appears
-//  in all copies. This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
+//  Use, modification and distribution is subject to the 
+//  Boost Software License, Version 1.0. (See accompanying file 
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 //  See http://www.boost.org/libs/static_assert for documentation.
 
@@ -103,3 +102,4 @@ template<int x> struct static_assert_test{};
 
 
 #endif // BOOST_STATIC_ASSERT_HPP
+
