0f516c862962e59191fb622014cfe1dc9aacfe48
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-10-02 11:53:35 +0000

    Changed licence to new Boost licnece
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20241 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/static_assert/static_assert_test_fail_5.cpp
+++ boost/libs/static_assert/static_assert_test_fail_5.cpp
@@ -1,8 +1,7 @@
 //  (C) Copyright Steve Cleary & John Maddock 2000.
-//  Permission to copy, use, modify, sell and
-//  distribute this software is granted provided this copyright notice appears
-//  in all copies. This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
+//  Use, modification and distribution is subject to the 
+//  Boost Software License, Version 1.0. (See accompanying file 
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 //  See http://www.boost.org for most recent version including documentation.
 
@@ -34,6 +33,7 @@ struct Bill
 };
 
 Bill<int, char> b;
+
 
 
 
