3e9a34e93c7f3f4671ef71d9fbb238b28ee7f9e1
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-10-19 19:56:26 +0000

    add month map functionality to provide for parsing of date strings with chars -- eg: Feb, March, april, etc
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20411 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/src/gregorian/greg_month.cpp
+++ boost/libs/date_time/src/gregorian/greg_month.cpp
@@ -18,6 +18,45 @@ namespace gregorian {
   unsigned short days_in_month[NumMonths] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
 
+  /*! Returns a shared pointer to a map of Month strings & numbers.
+   * Strings are both full names and abbreviations.</br>
+   * Ex. ("jan",1), ("february",2), etc...</br>
+   * Note: All characters are lowercase - for case insensitivity</br> 
+   * Note: Case-insensitivity does not function with MSVC 6.0. In this case
+   * the first letter is capitalized only. */
+  greg_month::month_map_ptr_type greg_month::get_month_map_ptr()
+  {
+    static month_map_ptr_type month_map_ptr(new greg_month::month_map_type())
+;
+
+    if(month_map_ptr->empty()) {
+      std::string s("");
+      for(int i = 1; i <= 12; ++i) {
+        greg_month m(static_cast<month_enum>(i));
+        s = m.as_long_string();
+#if defined(BOOST_DATE_TIME_NO_STD_TRANSFORM)
+#else
+        std::transform(s.begin(), s.end(),
+                       s.begin(),
+                       tolower);
+#endif
+        month_map_ptr->insert(std::make_pair(s, i));
+        
+        s = m.as_short_string();
+#if defined(BOOST_DATE_TIME_NO_STD_TRANSFORM)
+#else
+        std::transform(s.begin(), s.end(),
+                       s.begin(),
+                       tolower);
+#endif
+        month_map_ptr->insert(std::make_pair(s, i));
+      }
+    }
+    return month_map_ptr;
+  }
+ 
+
+
   //! Returns 3 char english string for the month ex: Jan, Feb, Mar, Apr
   const char*
   greg_month::as_short_string() const 
@@ -48,3 +87,4 @@ namespace gregorian {
  * representations about the suitability of this software for any
  * purpose.  It is provided "as is" without express or implied warranty.
  */
+
