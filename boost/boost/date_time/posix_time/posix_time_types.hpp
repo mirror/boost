1261043a507bd96c4b4f82c2d8795198ff43517b
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-11-23 02:37:57 +0000

    license update
    git-svn-id: http://svn.boost.org/svn/boost/trunk@20912 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/posix_time/posix_time_types.hpp
+++ boost/boost/date_time/posix_time/posix_time_types.hpp
@@ -1,9 +1,11 @@
-#ifndef POSIX_TIME_TYPES_HPP___
-#define POSIX_TIME_TYPES_HPP___
-/* Copyright (c) 2002 CrystalClear Software, Inc.
- * Disclaimer & Full Copyright at end of file
+/* Copyright (c) 2002,2003 CrystalClear Software, Inc.
+ * Use, modification and distribution is subject to the 
+ * Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
  * Author: Jeff Garland 
  */
+#ifndef POSIX_TIME_TYPES_HPP___
+#define POSIX_TIME_TYPES_HPP___
 
 #include "boost/date_time/time_clock.hpp"
 #include "boost/date_time/microsec_time_clock.hpp"
@@ -45,18 +47,6 @@ namespace posix_time {
 
 
 
-/* Copyright (c) 2002
- * CrystalClear Software, Inc.
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  CrystalClear Software makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- *
- */
 
 #endif
 
