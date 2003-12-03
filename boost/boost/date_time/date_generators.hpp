9cdedc84af44b0c74c4e86d4b7f7fdd09eead3e2
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-12-03 02:01:43 +0000

    fix tabs / formatting in source
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21102 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/date_generators.hpp
+++ boost/boost/date_time/date_generators.hpp
@@ -22,12 +22,13 @@ namespace date_time {
 
   //! Base class for all generators that take a year and produce a date.
   /*! This class is a base class for polymorphic function objects that take
-      a year and produce a concrete date.
-      @param date_type The type representing a date.  This type must
-       export a calender_type which defines a year_type.
-   */
+    a year and produce a concrete date.
+    @param date_type The type representing a date.  This type must
+    export a calender_type which defines a year_type.
+  */
   template<class date_type>
-  class year_based_generator {
+  class year_based_generator
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::year_type        year_type;
@@ -35,105 +36,103 @@ namespace date_time {
     virtual ~year_based_generator() {};
     virtual date_type get_date(year_type y) const = 0;
   };
-
+  
   //! Generates a date by applying the year to the given month and day.
   /*!
     Example usage: 
     @code
-     partial_date pd(1, Jan);
-     partial_date pd2(70);
-     date d = pd.get_date(2002); //2002-Jan-01
-     date d2 = pd2.get_date(2002); //2002-Mar-10
+    partial_date pd(1, Jan);
+    partial_date pd2(70);
+    date d = pd.get_date(2002); //2002-Jan-01
+    date d2 = pd2.get_date(2002); //2002-Mar-10
     @endcode
     \ingroup date_alg
-   */
+  */
   template<class date_type>
-  class partial_date : public year_based_generator<date_type> {
-  public:
-    typedef typename date_type::calendar_type calendar_type;
-    typedef typename calendar_type::day_type         day_type;
-    typedef typename calendar_type::month_type       month_type;
-    typedef typename calendar_type::year_type        year_type;
-    typedef typename date_type::duration_type        duration_type;
-    typedef typename duration_type::duration_rep     duration_rep;
-    partial_date(day_type d, month_type m) :
-      day_(d),
-      month_(m)
-    {}
-    //! Partial date created from number of days into year. Range 1-366
-    /*! Allowable values range from 1 to 366. 1=Jan1, 366=Dec31. If argument
-     * exceeds range, partial_date will be created with closest in-range value.
-     * 60 will always be Feb29, if get_date() is called with a non-leap year
-     * an exception will be thrown */
-    partial_date(duration_rep days) :
-      day_(1), // default values
-      month_(1)
-    {
-      date_type d1(2000,1,1);
-      if(days > 1)
-      {
-	if(days > 366) // prevents wrapping
-	{
-	  days = 366;
-	}
-	days = days - 1;
-        duration_type dd(days);
-	d1 = d1 + dd;
-      }
-      day_ = d1.day();
-      month_ = d1.month();
-    }
-    //! Return a concrete date when provided with a year specific year.
-    /*! Will throw an 'invalid_argument' exception if a partial_date object,
-     * instantiated with Feb-29, has get_date called with a non-leap year.
-     * Example:
-     * @code
-     * partial_date pd(29, Feb);
-     * pd.get_date(2003); // throws invalid_argument exception
-     * pg.get_date(2000); // returns 2000-2-29
-     * @endcode
-     */
-    date_type get_date(year_type y) const
-    {
-      if((day_ == 29) && (month_ == 2) && !(calendar_type::is_leap_year(y)))
-      {
-	std::stringstream ss("");
-	ss << "No Feb 29th in given year of " << y << ".";
-	throw std::invalid_argument(ss.str());
-	//return date_type(1,1,1); // should never reach
-      }
-      else{
-        return date_type(y, month_, day_);
-      }
-    }
-    date_type operator()(year_type y) const
-    {
-      return get_date(y);
-      //return date_type(y, month_, day_);
-    }
-    bool operator==(const partial_date& rhs) const
-    {
-      return (month_ == rhs.month_) && (day_ == rhs.day_);
-    }
-    bool operator<(const partial_date& rhs) const
-    {
-      if (month_ < rhs.month_) return true;
-      if (month_ > rhs.month_) return false;
-      //months are equal
-      return (day_ < rhs.day_);
-    }
-
-    // added for streaming purposes 
-    month_type month()const{return month_;}
-    day_type day()const{return day_;}
-    
-  private:
-    day_type day_;
-    month_type month_;
-  };
+ class partial_date : public year_based_generator<date_type>
+ {
+ public:
+   typedef typename date_type::calendar_type calendar_type;
+   typedef typename calendar_type::day_type         day_type;
+   typedef typename calendar_type::month_type       month_type;
+   typedef typename calendar_type::year_type        year_type;
+   typedef typename date_type::duration_type        duration_type;
+   typedef typename duration_type::duration_rep     duration_rep;
+   partial_date(day_type d, month_type m) :
+     day_(d),
+     month_(m)
+   {}
+   //! Partial date created from number of days into year. Range 1-366
+   /*! Allowable values range from 1 to 366. 1=Jan1, 366=Dec31. If argument
+    * exceeds range, partial_date will be created with closest in-range value.
+    * 60 will always be Feb29, if get_date() is called with a non-leap year
+    * an exception will be thrown */
+   partial_date(duration_rep days) :
+     day_(1), // default values
+     month_(1)
+   {
+     date_type d1(2000,1,1);
+     if(days > 1) {
+       if(days > 366) // prevents wrapping
+       {
+         days = 366;
+       }
+       days = days - 1;
+       duration_type dd(days);
+       d1 = d1 + dd;
+     }
+     day_ = d1.day();
+     month_ = d1.month();
+   }
+   //! Return a concrete date when provided with a year specific year.
+   /*! Will throw an 'invalid_argument' exception if a partial_date object,
+    * instantiated with Feb-29, has get_date called with a non-leap year.
+    * Example:
+    * @code
+    * partial_date pd(29, Feb);
+    * pd.get_date(2003); // throws invalid_argument exception
+    * pg.get_date(2000); // returns 2000-2-29
+    * @endcode
+         */
+   date_type get_date(year_type y) const
+   {
+     if((day_ == 29) && (month_ == 2) && !(calendar_type::is_leap_year(y))) {
+       std::stringstream ss("");
+       ss << "No Feb 29th in given year of " << y << ".";
+       throw std::invalid_argument(ss.str());
+       //return date_type(1,1,1); // should never reach
+     } else {
+       return date_type(y, month_, day_);
+     }
+   }
+   date_type operator()(year_type y) const
+   {
+     return get_date(y);
+     //return date_type(y, month_, day_);
+   }
+   bool operator==(const partial_date& rhs) const
+   {
+     return (month_ == rhs.month_) && (day_ == rhs.day_);
+   }
+   bool operator<(const partial_date& rhs) const
+   {
+     if (month_ < rhs.month_) return true;
+     if (month_ > rhs.month_) return false;
+     //months are equal
+     return (day_ < rhs.day_);
+   }
+   
+   // added for streaming purposes
+   month_type month()const{return month_;}
+   day_type day()const{return day_;}
+   
+ private:
+   day_type day_;
+   month_type month_;
+ };
 
 
-  //! Useful generator functor for finding holidays 
+  //! Useful generator functor for finding holidays
   /*! Based on the idea in Cal. Calc. for finding holidays that are
    *  the 'first Monday of September'. When instantiated with
    *  'fifth' kday of month, the result will be the last kday of month
@@ -151,7 +150,8 @@ namespace date_time {
    *  \ingroup date_alg
    */
   template<class date_type>
-  class nth_kday_of_month : public year_based_generator<date_type> {
+  class nth_kday_of_month : public year_based_generator<date_type>
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::day_of_week_type  day_of_week_type;
@@ -181,13 +181,13 @@ namespace date_time {
         week++;
       }
       // remove wrapping to next month behavior
-      if(d.month() != month_) { 
-	d = d - one_week;
+      if(d.month() != month_) {
+        d = d - one_week;
       }
       return d;
     }
     // added for streaming
-    month_type month() const 
+    month_type month() const
     {
       return month_;
     }
@@ -196,9 +196,9 @@ namespace date_time {
       return wn_;
     }
     day_of_week_type day_of_week() const
-    { 
+    {
       return dow_;
-    } 
+    }
     const char* nth_week_as_str() const
     {
       return nth_as_str(wn_);
@@ -208,16 +208,17 @@ namespace date_time {
     week_num wn_;
     day_of_week_type dow_;
   };
-
+  
   //! Returns nth arg as string. 1 -> "first", 2 -> "second", max is 5.
   BOOST_DATE_TIME_DECL const char* nth_as_str(int n);
-  
-  //! Useful generator functor for finding holidays and daylight savings 
+
+  //! Useful generator functor for finding holidays and daylight savings
   /*! Similar to nth_kday_of_month, but requires less paramters
    *  \ingroup date_alg
    */
   template<class date_type>
-  class first_kday_of_month : public year_based_generator<date_type> {
+  class first_kday_of_month : public year_based_generator<date_type>
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::day_of_week_type  day_of_week_type;
@@ -227,7 +228,7 @@ namespace date_time {
     //!Specify the first 'Sunday' in 'April' spec
     /*!@param dow The day of week, eg: Sunday, Monday, etc
      * @param m The month of the year, eg: Jan, Feb, Mar, etc
-     */ 
+     */
     first_kday_of_month(day_of_week_type dow, month_type m) :
       month_(m),
       dow_(dow)
@@ -241,7 +242,7 @@ namespace date_time {
         d = d + one_day;
       }
       return d;
-    }
+        }
     // added for streaming
     month_type month() const
     {
@@ -255,18 +256,19 @@ namespace date_time {
     month_type month_;
     day_of_week_type dow_;
   };
-
-
-
+  
+  
+  
   //! Calculate something like Last Sunday of January
-  /*! Useful generator functor for finding holidays and daylight savings  
+  /*! Useful generator functor for finding holidays and daylight savings
    *  Get the last day of the month and then calculate the difference
    *  to the last previous day.
    *  @param date_type A date class that exports day_of_week, month_type, etc.
    *  \ingroup date_alg
    */
   template<class date_type>
-  class last_kday_of_month : public year_based_generator<date_type> {
+  class last_kday_of_month : public year_based_generator<date_type>
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::day_of_week_type  day_of_week_type;
@@ -276,7 +278,7 @@ namespace date_time {
     //!Specify the date spec like last 'Sunday' in 'April' spec
     /*!@param dow The day of week, eg: Sunday, Monday, etc
      * @param m The month of the year, eg: Jan, Feb, Mar, etc
-     */ 
+     */
     last_kday_of_month(day_of_week_type dow, month_type m) :
       month_(m),
       dow_(dow)
@@ -292,7 +294,7 @@ namespace date_time {
       return d;
     }
     // added for streaming
-    month_type month() const 
+    month_type month() const
     {
       return month_;
     }
@@ -303,20 +305,21 @@ namespace date_time {
   private:
     month_type month_;
     day_of_week_type dow_;
-  };
-
-
+   };
+  
+  
   //! Calculate something like "First Sunday after Jan 1,2002
   /*! Date generator that takes a date and finds kday after
    *@code
-      typedef boost::date_time::first_kday_after<date> firstkdayafter;
-      firstkdayafter fkaf(Monday);
-      fkaf.get_date(date(2002,Feb,1));
-    @endcode
+     typedef boost::date_time::first_kday_after<date> firstkdayafter;
+     firstkdayafter fkaf(Monday);
+     fkaf.get_date(date(2002,Feb,1));
+   @endcode
    *  \ingroup date_alg
    */
   template<class date_type>
-  class first_kday_after {
+  class first_kday_after
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::day_of_week_type day_of_week_type;
@@ -335,25 +338,26 @@ namespace date_time {
       return d;
     }
     // added for streaming
-    day_of_week_type day_of_week() const 
+    day_of_week_type day_of_week() const
     {
       return dow_;
     }
   private:
     day_of_week_type dow_;
   };
-
+  
   //! Calculate something like "First Sunday before Jan 1,2002
   /*! Date generator that takes a date and finds kday after
    *@code
-      typedef boost::date_time::first_kday_before<date> firstkdaybefore;
-      firstkdaybefore fkbf(Monday);
-      fkbf.get_date(date(2002,Feb,1));
-    @endcode
+     typedef boost::date_time::first_kday_before<date> firstkdaybefore;
+     firstkdaybefore fkbf(Monday);
+     fkbf.get_date(date(2002,Feb,1));
+   @endcode
    *  \ingroup date_alg
    */
   template<class date_type>
-  class first_kday_before {
+  class first_kday_before
+  {
   public:
     typedef typename date_type::calendar_type calendar_type;
     typedef typename calendar_type::day_of_week_type day_of_week_type;
@@ -379,8 +383,8 @@ namespace date_time {
   private:
     day_of_week_type dow_;
   };
-
-
+  
+  
 } } //namespace date_time
 
 
