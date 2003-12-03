228bc207fbc93ad784caa73b9d3724c8cb0371a2
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-12-03 03:30:48 +0000

    extract fix that didn't fix metrowerks
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21108 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/test/gregorian/testparse_date.cpp
+++ boost/libs/date_time/test/gregorian/testparse_date.cpp
@@ -42,17 +42,10 @@ main()
     date d(2000, 2, 29);
     date d2 = from_string("2000-2-29");
     check("2000-2-29", d2 == d);
-#if !defined(__MWERKS__)
     date d3 = from_string("2000-FEB-29");
     check("2000-FEB-29 (uppercase)", d3 == d);
     date d4 = from_string("2000-february-29");
     check("2000-february-29 (lowercase)", d4 == d);
-#else
-    date d3 = from_string("2000-Feb-29");
-    check("2000-Feb-29", d3 == d);
-    date d4 = from_string("2000-February-29");
-    check("2000-February-29", d4 == d);
-#endif
     date d5 = from_string(to_simple_string(d));
     check("date to string to date", d5 == d);
     date d6 = from_string(to_iso_extended_string(d));
@@ -68,13 +61,8 @@ main()
     std::cout << "With comma: " << to_simple_string(d10) << std::endl;
     check("american date with comma: Feb 29, 2000 ", d10 == d);
 
-#if !defined(__MWERKS__)
     date d11 = from_us_string("feb 29 2000");
     check("american date with comma: feb 29 2000 ", d11 == d);
-#else
-    date d11 = from_us_string("Feb 29 2000");
-    check("american date with comma: Feb 29 2000 ", d11 == d);
-#endif
 
 
 
