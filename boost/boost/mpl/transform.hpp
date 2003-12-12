620368056e1b589b2d03bbb75ad70656a23feed5
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-12-12 16:16:21 +0000

    added two-sequence forms
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21238 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/transform.hpp
+++ boost/boost/mpl/transform.hpp
@@ -3,8 +3,8 @@
 // See http://www.boost.org for updates, documentation, and revision history.
 //-----------------------------------------------------------------------------
 //
-// Copyright (c) 2000-02
-// Aleksey Gurtovoy
+// Copyright (c) 2000-03
+// Dave Abrahams, Aleksey Gurtovoy
 //
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee, 
@@ -22,9 +22,13 @@
 #include "boost/mpl/clear.hpp"
 #include "boost/mpl/lambda.hpp"
 #include "boost/mpl/apply.hpp"
+#include "boost/mpl/pair.hpp"
 #include "boost/mpl/protect.hpp"
+#include "boost/mpl/iterator_tag.hpp"
+#include "boost/mpl/apply_if.hpp"
 #include "boost/mpl/aux_/common_name_wknd.hpp"
 #include "boost/mpl/aux_/void_spec.hpp"
+#include "boost/type_traits/is_same.hpp"
 
 namespace boost {
 namespace mpl {
@@ -45,6 +49,48 @@ struct transform_op
     };
 };
 
+template< typename Op >
+struct transform2_op
+{
+    template< typename Sequence, typename T > struct apply
+    {
+        typedef typename push_front<
+              Sequence
+            , typename apply2<
+                    Op
+                  , typename T::first
+                  , typename T::second
+                  >::type
+            >::type type;
+    };
+};
+
+template< typename I1, typename I2 >
+struct pair_iterator
+{
+    typedef input_iter_tag_ category;
+    typedef pair<typename I1::type, typename I2::type> type;
+    typedef pair_iterator<typename I1::next, typename I2::next> next;
+};
+
+template<
+      typename Seq1, typename Seq2
+    >
+struct pair_view
+{
+ public:
+    typedef nested_begin_end_tag tag;
+    typedef pair_iterator<
+        typename mpl::begin<Seq1>::type
+      , typename mpl::begin<Seq2>::type
+    > begin;
+    
+    typedef pair_iterator<
+        typename mpl::end<Seq1>::type
+      , typename mpl::end<Seq2>::type
+    > end;
+};
+
 } // namespace aux
 
 BOOST_MPL_AUX_AGLORITHM_NAMESPACE_BEGIN
@@ -53,7 +99,7 @@ template<
       typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Sequence)
     , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Operation)
     >
-struct transform
+struct transform1
 {
  private:
     typedef typename lambda<Operation>::type op_;
@@ -67,9 +113,44 @@ struct transform
         >::type type;
 };
 
+template<
+      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Seq1)
+    , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Seq2)
+    , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Operation)
+    >
+struct transform2
+{
+ private:
+    typedef typename lambda<Operation>::type op_;
+    typedef typename clear<Seq1>::type result_;
+
+ public:
+    typedef typename fold_backward<
+          aux::pair_view<Seq1,Seq2>
+        , result_
+        , protect< aux::transform2_op<op_> >
+        >::type type;
+};
+
+template<
+      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Seq1)
+    , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Seq2OrOperation)
+    , typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Operation)
+    >
+struct transform
+  : apply_if<
+        is_same<Operation,void_>
+      , transform1<Seq1,Seq2OrOperation>
+      , transform2<Seq1,Seq2OrOperation,Operation>
+    >
+{
+};
+
 BOOST_MPL_AUX_AGLORITHM_NAMESPACE_END
 
-BOOST_MPL_AUX_ALGORITHM_VOID_SPEC(2, transform)
+BOOST_MPL_AUX_ALGORITHM_VOID_SPEC(2, transform1)
+BOOST_MPL_AUX_ALGORITHM_VOID_SPEC(3, transform2)
+BOOST_MPL_AUX_ALGORITHM_VOID_SPEC(3, transform)
 
 } // namespace mpl
 } // namespace boost
