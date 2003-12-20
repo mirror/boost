29970c3c73f5e2651cae69f2d1889a358efb693c
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-12-20 12:52:27 +0000

    Disabled dual wchar_t mode with vc7 + STLPort, as it doesn't work.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21352 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/regex/config.hpp
+++ boost/boost/regex/config.hpp
@@ -195,7 +195,7 @@ using std::distance;
  ****************************************************************************/
 
 #ifdef __cplusplus
-#if defined(BOOST_MSVC) && (BOOST_MSVC >= 1300) && !defined(BOOST_REGEX_V3)
+#if defined(BOOST_MSVC) && (BOOST_MSVC >= 1300) && !defined(BOOST_REGEX_V3) && !(defined(__SGI_STL_PORT) || defined(_STLPORT_VERSION))
 #  define BOOST_REGEX_HAS_SHORT_WCHAR_T
 namespace boost{ typedef __wchar_t regex_wchar_type; }
 #else
