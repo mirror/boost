5e35cad5d334f128f245be6051086d262064caf7
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-12-26 07:13:43 +0000

    Fix property_map_iterator.
    Patch from Jurgen Hunold.
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21398 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/property_map_iterator.hpp
+++ boost/boost/property_map_iterator.hpp
@@ -10,7 +10,9 @@
 #define BOOST_PROPERTY_MAP_ITERATOR_HPP
 
 #include <boost/property_map.hpp>
-#include <boost/iterator_adaptors.hpp>
+#include <boost/iterator/iterator_adaptor.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/type_traits/is_same.hpp>
 
 namespace boost {
 
@@ -19,70 +21,83 @@ namespace boost {
 
   namespace detail {
 
-    template <class LvaluePropertyMap>
-    struct lvalue_pmap_iter_policies : public default_iterator_policies
+    template <class Iterator, class LvaluePropertyMap>
+    class lvalue_pmap_iter
+      : public iterator_adaptor< lvalue_pmap_iter< Iterator, LvaluePropertyMap >,
+                                 Iterator,
+                                 typename property_traits<LvaluePropertyMap>::value_type,
+                                 use_default,
+                                 typename property_traits<LvaluePropertyMap>::reference>
     {
-      lvalue_pmap_iter_policies() { }
-      lvalue_pmap_iter_policies(LvaluePropertyMap m) : m_map(m) {}
+      friend class boost::iterator_core_access;
 
-      template <class Iter>
-      typename Iter::reference
-      dereference(const Iter& i) const 
-      {
-        return m_map[*i.base()];
-      }
+      typedef iterator_adaptor< lvalue_pmap_iter< Iterator, LvaluePropertyMap >,
+                                Iterator,
+                                typename property_traits<LvaluePropertyMap>::value_type,
+                                use_default,
+                                typename property_traits<LvaluePropertyMap>::reference> super_t;
+
+    public:
+      lvalue_pmap_iter() { }
+      lvalue_pmap_iter(Iterator const&     it,
+                       LvaluePropertyMap m)
+        : super_t(it),
+          m_map(m) {}
+
     private:
+      typename lvalue_pmap_iter::reference
+      dereference() const
+      {
+        return m_map[*(this->base_reference())];
+      }
+
       LvaluePropertyMap m_map;
     };
 
-    template <class ReadablePropertyMap>
-    struct readable_pmap_iter_policies : public default_iterator_policies
-    {
-      readable_pmap_iter_policies() { }
-      readable_pmap_iter_policies(ReadablePropertyMap m) : m_map(m) {}
+    template <class Iterator, class ReadablePropertyMap>
+    class readable_pmap_iter :
+      public iterator_adaptor< readable_pmap_iter< Iterator, ReadablePropertyMap >,
+                               Iterator,
+                               typename property_traits<ReadablePropertyMap>::value_type,
+                               use_default,
+                               typename property_traits<ReadablePropertyMap>::value_type>
 
-      template <class Iter>
-      typename Iter::reference
-      dereference(const Iter& i) const 
-      {
-        return get(m_map, *i.base());
-      }
+
+    {
+      friend class iterator_core_access;
+
+      typedef iterator_adaptor< readable_pmap_iter< Iterator, ReadablePropertyMap >,
+                                Iterator,
+                                typename property_traits<ReadablePropertyMap>::value_type,
+                                use_default,
+                                typename property_traits<ReadablePropertyMap>::value_type> super_t;
+
+    public:
+      readable_pmap_iter() { }
+      readable_pmap_iter(Iterator const&     it,
+                         ReadablePropertyMap m)
+        : super_t(it),
+          m_map(m) {}
+
     private:
+      typename readable_pmap_iter::reference
+      dereference() const
+      {
+        return get(m_map, *(this->base_reference()));
+      }
+
       ReadablePropertyMap m_map;
     };
 
-    template <class PMapCategory>
-    struct choose_pmap_iter {
-      template <class PMap, class Iter>
-      struct bind_ {     
-        typedef typename property_traits<PMap>::value_type value;
-        typedef iterator_adaptor<Iter,
-          readable_pmap_iter_policies<PMap>, value, value,
-          value*, std::input_iterator_tag> type;
-      };
-    };
 
-    template <>
-    struct choose_pmap_iter<lvalue_property_map_tag> {
-      template <class PMap, class Iter>
-      struct bind_ {
-        typedef typename property_traits<PMap>::value_type value;
-        typedef typename property_traits<PMap>::reference ref;
-        typedef iterator_adaptor<Iter,
-          lvalue_pmap_iter_policies<PMap>,
-          value, ref> type;
-      };
-    };
-    
   } // namespace detail
 
   template <class PropertyMap, class Iterator>
-  class property_map_iterator_generator {
-  public:
-    typedef typename property_traits<PropertyMap>::category Cat; 
-    typedef typename detail::choose_pmap_iter<Cat>::
-      template bind_<PropertyMap, Iterator>::type type;
-  };
+  struct property_map_iterator_generator :
+    mpl::if_< is_same< typename property_traits<PropertyMap>::category, lvalue_property_map_tag>,
+              detail::lvalue_pmap_iter<Iterator, PropertyMap>,
+              detail::readable_pmap_iter<Iterator, PropertyMap> >
+  {};
 
   template <class PropertyMap, class Iterator>
   typename property_map_iterator_generator<PropertyMap, Iterator>::type
