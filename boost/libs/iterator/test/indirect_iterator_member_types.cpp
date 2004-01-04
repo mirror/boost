4448d8d7fc4899a764f149eb4d829273c99be5a5
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-01-04 18:44:54 +0000

    change to iterator_traits for custom pointer
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21483 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/iterator/test/indirect_iterator_member_types.cpp
+++ boost/libs/iterator/test/indirect_iterator_member_types.cpp
@@ -18,16 +18,13 @@
 
 struct zow { };
 
-struct my_ptr1 {
-  typedef const zow element_type;
+struct my_ptr {
+  typedef zow value_type;
+  typedef const zow& reference;
+  typedef const zow* pointer;
+  typedef void difference_type;
+  typedef boost::no_traversal_tag iterator_category;
 };
-
-struct my_ptr2 { };
-namespace boost {
-  template <> struct referent<my_ptr2> {
-    typedef const zow type;
-  };
-}
 
 int main()
 {
@@ -62,7 +59,7 @@ int main()
     BOOST_STATIC_ASSERT((boost::is_same<Iter::pointer, const int*>::value));  
   }
   {
-    typedef boost::indirect_iterator<my_ptr1*> Iter;
+    typedef boost::indirect_iterator<my_ptr*> Iter;
     BOOST_STATIC_ASSERT((boost::is_same<Iter::value_type, zow>::value));
     BOOST_STATIC_ASSERT((boost::is_same<Iter::reference, const zow&>::value));  
     BOOST_STATIC_ASSERT((boost::is_same<Iter::pointer, const zow*>::value));  
@@ -72,12 +69,6 @@ int main()
 			 std::random_access_iterator_tag>::value));
     BOOST_STATIC_ASSERT((boost::is_convertible<boost::iterator_traversal<Iter>::type,
 			 boost::random_access_traversal_tag>::value));
-  }
-  {
-    typedef boost::indirect_iterator<my_ptr2*> Iter;
-    BOOST_STATIC_ASSERT((boost::is_same<Iter::value_type, zow>::value));
-    BOOST_STATIC_ASSERT((boost::is_same<Iter::reference, const zow&>::value));  
-    BOOST_STATIC_ASSERT((boost::is_same<Iter::pointer, const zow*>::value));  
   }
   {
     typedef boost::indirect_iterator<char**, int, std::random_access_iterator_tag, long&, short> Iter;
