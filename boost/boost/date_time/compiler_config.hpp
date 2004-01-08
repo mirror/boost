3b89eddb91fff5f01fe99401f1ef020450654c43
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-01-08 12:45:46 +0000

    Modified dll build logic to conform to more/separate_compilation.html. Added support for auto-linking.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21544 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/compiler_config.hpp
+++ boost/boost/date_time/compiler_config.hpp
@@ -61,24 +61,45 @@ namespace std {
  * 
  */
 
-#if defined(_MSC_VER) && defined(_DLL)
-# define BOOST_DATE_TIME_HAS_DLL_RUNTIME
-#endif
-
-// BOOST_DATE_TIME_STATIC_LINK is defined in libs/date_time/build/Jamfile
-#if (defined(BOOST_DATE_TIME_HAS_DLL_RUNTIME) && \
-     !defined(BOOST_DATE_TIME_STATIC_LINK))
-# if defined(BOOST_DATE_TIME_SOURCE)
-#  define BOOST_DATE_TIME_DECL __declspec(dllexport)
-#  define BOOST_DATE_TIME_BUILD_DLL
-# else
-#  define BOOST_DATE_TIME_DECL __declspec(dllimport)
-# endif
-#endif
-
+#ifdef BOOST_HAS_DECLSPEC // defined in config system
+   // we need to import/export our code only if the user has specifically
+   // asked for it by defining either BOOST_ALL_DYN_LINK if they want all boost
+   // libraries to be dynamically linked, or BOOST_DATE_TIME_DYN_LINK
+   // if they want just this one to be dynamically liked:
+#  if defined(BOOST_ALL_DYN_LINK) || defined(BOOST_DATE_TIME_DYN_LINK)
+      // export if this is our own source, otherwise import:
+#     ifdef BOOST_DATE_TIME_SOURCE
+#       define BOOST_DATE_TIME_DECL __declspec(dllexport)
+#     else
+#       define BOOST_DATE_TIME_DECL __declspec(dllimport)
+#     endif  // BOOST_DATE_TIME_SOURCE
+#  endif  // DYN_LINK
+#endif  // BOOST_HAS_DECLSPEC
+//
+// if BOOST_WHATEVER_DECL isn't defined yet define it now:
 #ifndef BOOST_DATE_TIME_DECL
-# define BOOST_DATE_TIME_DECL
+#  define BOOST_DATE_TIME_DECL
 #endif
 
+//
+// Automatically link to the correct build variant where possible. 
+// 
+#if !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_DATE_TIME_NO_LIB) && !defined(BOOST_DATE_TIME_SOURCE)
+//
+// Set the name of our library, this will get undef'ed by auto_link.hpp
+// once it's done with it:
+//
+#define BOOST_LIB_NAME boost_date_time
+//
+// If we're importing code from a dll, then tell auto_link.hpp about it:
+//
+#if defined(BOOST_ALL_DYN_LINK) || defined(BOOST_DATE_TIME_DYN_LINK)
+#  define BOOST_DYN_LINK
+#endif
+//
+// And include the header that does the work:
+//
+#include <boost/config/auto_link.hpp>
+#endif  // auto-linking disabled
 
 #endif
