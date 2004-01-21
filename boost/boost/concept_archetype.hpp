aa53f24c98c10d9b556e32c4b6c680ea74a5dd8e
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-01-21 19:03:32 +0000

    added const to the conversion operator of input_iterator_archetype
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21866 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/concept_archetype.hpp
+++ boost/boost/concept_archetype.hpp
@@ -418,7 +418,7 @@ namespace boost {
     typedef std::input_iterator_tag iterator_category;
     typedef T value_type;
     struct reference {
-      operator value_type() { return static_object<T>::get(); }
+      operator value_type() const { return static_object<T>::get(); }
     };
     typedef const T* pointer;
     typedef std::ptrdiff_t difference_type;
