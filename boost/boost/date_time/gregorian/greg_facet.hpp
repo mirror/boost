88a5d008ed4ae409fd712c4cc4da3fa484725ba1
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-01-25 18:42:25 +0000

    updates to support wide stream output
    git-svn-id: http://svn.boost.org/svn/boost/trunk@21923 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/gregorian/greg_facet.hpp
+++ boost/boost/date_time/gregorian/greg_facet.hpp
@@ -19,7 +19,7 @@
 
 namespace boost {
 namespace gregorian {
-
+  
   //! Configuration of the output facet template
   struct greg_facet_config
   {
@@ -31,7 +31,7 @@ namespace gregorian {
 
   //! Create the base facet type for gregorian::date
   typedef boost::date_time::date_names_put<greg_facet_config> greg_base_facet;
-  
+
   //! ostream operator for gregorian::date
   /*! Uses the date facet to determine various output parameters including:
    *  - string values for the month (eg: Jan, Feb, Mar) (default: English)
@@ -44,7 +44,8 @@ namespace gregorian {
   std::basic_ostream<charT, traits>&
   operator<<(std::basic_ostream<charT, traits>& os, const date& d)
   {
-    typedef boost::date_time::ostream_date_formatter<date, greg_base_facet, charT> greg_ostream_formatter;
+    typedef boost::date_time::date_names_put<greg_facet_config, charT> facet_def;
+    typedef boost::date_time::ostream_date_formatter<date, facet_def, charT> greg_ostream_formatter;
     greg_ostream_formatter::date_put(d, os);
     return os;
   }
@@ -59,15 +60,17 @@ namespace gregorian {
   std::basic_ostream<charT, traits>&
   operator<<(std::basic_ostream<charT, traits>& os, const greg_month& m)
   {
-    typedef boost::date_time::ostream_month_formatter<greg_base_facet, charT> greg_month_formatter;
+    typedef boost::date_time::date_names_put<greg_facet_config, charT> facet_def;
+    typedef boost::date_time::ostream_month_formatter<facet_def, charT> greg_month_formatter;
     std::locale locale = os.getloc();
-    if (std::has_facet<greg_base_facet>(locale)) {
-      const greg_base_facet& f = std::use_facet<greg_base_facet>(locale);
+    if (std::has_facet<facet_def>(locale)) {
+      const facet_def& f = std::use_facet<facet_def>(locale);
       greg_month_formatter::format_month(m, os, f);
 
     }
     else { //default to numeric
-      os  << std::setw(2) << std::setfill('0') << m.as_number();
+      charT fill_char = '0';
+      os  << std::setw(2) << std::setfill(fill_char) << m.as_number();
     }
 
     return os;
@@ -83,10 +86,11 @@ namespace gregorian {
   std::basic_ostream<charT, traits>&
   operator<<(std::basic_ostream<charT, traits>& os, const greg_weekday& wd)
   {
-    typedef boost::date_time::ostream_weekday_formatter<greg_weekday, greg_base_facet, charT> greg_weekday_formatter;
+    typedef boost::date_time::date_names_put<greg_facet_config, charT> facet_def;
+    typedef boost::date_time::ostream_weekday_formatter<greg_weekday, facet_def, charT> greg_weekday_formatter;
     std::locale locale = os.getloc();
-    if (std::has_facet<greg_base_facet>(locale)) {
-      const greg_base_facet& f = std::use_facet<greg_base_facet>(locale);
+    if (std::has_facet<facet_def>(locale)) {
+      const facet_def& f = std::use_facet<facet_def>(locale);
       greg_weekday_formatter::format_weekday(wd.as_enum(), os, f, true);
     }
     else { //default to short English string eg: Sun, Mon, Tue, Wed...
