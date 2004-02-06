a54ce5c8c47e558a59d03866879934fd79d46e9d
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-02-06 05:49:31 +0000

    change to conversion operator of input_iterator_archetype::proxy so as to not use the copy constructor of the value_type
    git-svn-id: http://svn.boost.org/svn/boost/trunk@22184 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/concept_archetype.hpp
+++ boost/boost/concept_archetype.hpp
@@ -409,7 +409,7 @@ namespace boost {
   //===========================================================================
   // Iterator Archetype Classes
 
-  template <class T>
+  template <class T, int I = 0>
   class input_iterator_archetype
   {
   private:
@@ -418,7 +418,7 @@ namespace boost {
     typedef std::input_iterator_tag iterator_category;
     typedef T value_type;
     struct reference {
-      operator value_type() const { return static_object<T>::get(); }
+      operator const value_type&() const { return static_object<T>::get(); }
     };
     typedef const T* pointer;
     typedef std::ptrdiff_t difference_type;
