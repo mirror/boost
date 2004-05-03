68c0ae45cdbbd2a62563172c11758e0296864ae3
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-05-03 11:26:53 +0000

    Updated for gcc 3.4
    git-svn-id: http://svn.boost.org/svn/boost/trunk@22722 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/stdlib/libstdcpp3.hpp
+++ boost/boost/config/stdlib/libstdcpp3.hpp
@@ -9,16 +9,22 @@
 //  config for libstdc++ v3
 //  not much to go in here:
 
+#ifdef __GLIBCXX__
+#define BOOST_STDLIB "GNU libstdc++ version " BOOST_STRINGIZE(__GLIBCXX__)
+#else
 #define BOOST_STDLIB "GNU libstdc++ version " BOOST_STRINGIZE(__GLIBCPP__)
+#endif
 
-#ifndef _GLIBCPP_USE_WCHAR_T
+#if !defined(_GLIBCPP_USE_WCHAR_T) && !defined(_GLIBCXX_USE_WCHAR_T)
 #  define BOOST_NO_CWCHAR
 #  define BOOST_NO_CWCTYPE
 #  define BOOST_NO_STD_WSTRING
 #  define BOOST_NO_STD_WSTREAMBUF
 #endif
  
-#ifndef _GLIBCPP_USE_LONG_LONG
+#if !defined(_GLIBCPP_USE_LONG_LONG) \
+    && !defined(_GLIBCXX_USE_LONG_LONG)\
+    && defined(BOOST_HAS_LONG_LONG)
 // May have been set by compiler/*.hpp, but "long long" without library
 // support is useless.
 #  undef BOOST_HAS_LONG_LONG
