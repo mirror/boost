1cec089e14df81ad21a59e4227594fd3273b8827
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-05-22 20:47:20 +0000

    Updated copyright notices
    git-svn-id: http://svn.boost.org/svn/boost/trunk@22895 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/statechart/fifo_worker.hpp
+++ boost/boost/statechart/fifo_worker.hpp
@@ -1,10 +1,9 @@
 #ifndef BOOST_FSM_FIFO_WORKER_HPP_INCLUDED
 #define BOOST_FSM_FIFO_WORKER_HPP_INCLUDED
 //////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Andreas Huber Doenni 2002-2004.
-// Use, modification and distribution are subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// (c) Copyright Andreas Huber Doenni 2002-2004
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////
 
 
--- boost/libs/statechart/example/Camera/Precompiled.hpp
+++ boost/libs/statechart/example/Camera/Precompiled.hpp
@@ -1,10 +1,9 @@
 #ifndef PRECOMPILED_HPP
 #define PRECOMPILED_HPP
 //////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Andreas Huber Doenni 2002-2004.
-// Use, modification and distribution are subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// (c) Copyright Andreas Huber Doenni 2002-2004
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////
 
 
