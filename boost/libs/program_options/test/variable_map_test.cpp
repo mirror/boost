800a154b4557a64f94ec7b6198c406f56839ca62
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-05-27 14:36:53 +0000

    Turns out intel just don't have templated vector constructor in some configurations. Introduce auxilliary function to conveniently create vectors from arrays.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@22952 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/program_options/test/variable_map_test.cpp
+++ boost/libs/program_options/test/variable_map_test.cpp
@@ -30,6 +30,13 @@ using namespace std;
     { \
     }
 
+vector<string> sv(char* array[], unsigned size)
+{
+    vector<string> r;
+    for (unsigned i = 0; i < size; ++i)
+        r.push_back(array[i]);
+    return r;
+}
 
 void test_variable_map()
 {
@@ -42,8 +49,8 @@ void test_variable_map()
         ("output,o", new untyped_value(), "")
         ;
     char* cmdline3_[] = { "--foo='12'", "--bar=11", "-z3", "-ofoo" };
-    vector<string> cmdline3((char**)cmdline3_,
-                            cmdline3_ + sizeof(cmdline3_)/sizeof(cmdline3_[0]));
+    vector<string> cmdline3 = sv(cmdline3_,
+                                 sizeof(cmdline3_)/sizeof(cmdline3_[0]));
     parsed_options a3 = command_line_parser(cmdline3).options(desc).run();
     variables_map vm;
     store(a3, vm);
@@ -63,8 +70,8 @@ void test_variable_map()
     ("opt", bool_switch(), "");
 
     char* cmdline4_[] = { "--zee=On", "--zoo", "--zak=13" };
-    vector<string> cmdline4((char**)cmdline4_,
-                            cmdline4_ + sizeof(cmdline4_)/sizeof(cmdline4_[0]));
+    vector<string> cmdline4 = sv(cmdline4_,
+                                 sizeof(cmdline4_)/sizeof(cmdline4_[0]));
     parsed_options a4 = command_line_parser(cmdline4).options(desc).run();
 
     variables_map vm2;
@@ -84,8 +91,8 @@ void test_variable_map()
     ("iii", value<int>()->default_value(123))
     ;
     char* cmdline5_[] = {  "--voo=1" };
-    vector<string> cmdline5((char**)cmdline5_,
-                            cmdline5_ + sizeof(cmdline5_)/sizeof(cmdline5_[0]));
+    vector<string> cmdline5 = sv(cmdline5_,
+                                 sizeof(cmdline5_)/sizeof(cmdline5_[0]));
     parsed_options a5 = command_line_parser(cmdline5).options(desc2).run();
 
     variables_map vm3;
@@ -178,14 +185,14 @@ void test_priority()
     ;
 
     char* cmdline1_[] = { "--first=1", "--aux=10", "--first=3", "--include=1" };
-    vector<string> cmdline1((char**)cmdline1_, 
-                            cmdline1_ + sizeof(cmdline1_)/sizeof(cmdline1_[0]));
+    vector<string> cmdline1 = sv(cmdline1_, 
+                                 sizeof(cmdline1_)/sizeof(cmdline1_[0]));
 
     parsed_options p1 = command_line_parser(cmdline1).options(desc).run();
 
     char* cmdline2_[] = { "--first=12", "--second=7", "--include=7" };
-    vector<string> cmdline2((char**)cmdline2_, 
-                            cmdline2_ + sizeof(cmdline2_)/sizeof(cmdline2_[0]));
+    vector<string> cmdline2 = sv(cmdline2_, 
+                                 sizeof(cmdline2_)/sizeof(cmdline2_[0]));
 
     parsed_options p2 = command_line_parser(cmdline2).options(desc).run();
 
