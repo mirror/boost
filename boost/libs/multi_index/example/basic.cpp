55fc558a1ef876760dd18b07b9e76929821703c4
joaquintides <joaquintides@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-05-28 08:06:41 +0000

    license update
    git-svn-id: http://svn.boost.org/svn/boost/trunk@22959 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/multi_index/example/basic.cpp
+++ boost/libs/multi_index/example/basic.cpp
@@ -1,7 +1,7 @@
 /* Boost.MultiIndex basic example.
  *
- * Copyright Joaquín M López Muñoz 2003-2004. Use, modification, and
- * distribution are subject to the Boost Software License, Version 1.0.
+ * Copyright 2003-2004 Joaquín M López Muñoz.
+ * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  *
--- boost/libs/multi_index/test/test_conv_iterators_main.cpp
+++ boost/libs/multi_index/test/test_conv_iterators_main.cpp
@@ -1,8 +1,8 @@
 /* Boost.MultiIndex test for interconvertibilty between const and
  * non-const iterators.
  *
- * Copyright Joaquín M López Muñoz 2003-2004. Use, modification, and
- * distribution are subject to the Boost Software License, Version 1.0.
+ * Copyright 2003-2004 Joaquín M López Muñoz.
+ * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  *
