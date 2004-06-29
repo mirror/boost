11999f0d2d9b2d09b93b9d2b98fa2035b415b1c1
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-06-29 01:22:26 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23236 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/assign/test/email_example.cpp
@@ -0,0 +1,134 @@
+// Boost.Assign library
+//
+//  Copyright Thorsten Ottosen 2003-2004. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/assign/
+//
+
+#include <boost/assign/list_inserter.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <vector>
+#include <map>
+
+namespace ba = boost::assign;
+
+class email
+{
+public:
+    enum address_option
+    {
+        check_addr_book,
+        dont_check_addr_book
+    };
+    
+    typedef std::pair<std::string,address_option> bcc_type;
+        
+private:
+
+    mutable std::map<std::string,address_option> cc_list;
+    mutable std::map<std::string,address_option> to_list;
+    std::vector< bcc_type >                      bcc_list;
+        
+    void add_cc_impl( const std::string& name, address_option ao )
+    {
+        cc_list[ name ] = ao;
+    }
+    
+    void add_to_impl( const std::string& name )
+    {
+        to_list[ name ] = check_addr_book;
+    }
+    
+    void add_bcc_impl( const bcc_type& bcc )
+    {
+        bcc_list.push_back( bcc ); 
+    }
+     
+public:
+    
+    ba::list_inserter< boost::function2< void, std::string, address_option > >
+    add_cc( std::string name, address_option ao )
+    {
+        add_cc_impl( name, ao );
+        return ba::make_list_inserter( boost::bind( &email::add_cc_impl, this, _1, _2 ) );
+    }
+
+    ba::list_inserter< boost::function1< void, std::string > >
+    add_to( const std::string& name )
+    {
+        add_to_impl( name );
+        return ba::make_list_inserter( boost::bind( &email::add_to_impl, this, _1 ) );
+    }
+    
+    ba::list_inserter< boost::function1< void, bcc_type >, bcc_type >
+    add_bcc( const bcc_type& bcc )
+    {
+        add_bcc_impl( bcc );
+        return ba::make_list_inserter( boost::bind( &email::add_bcc_impl, this, _1 ) );     
+    }
+    
+    address_option
+    cc_at( const std::string& name ) const
+    {
+        return cc_list[ name ];
+    }
+    
+    address_option 
+    to_at( const std::string& name ) const
+    {
+        return to_list[ name ];
+    }
+    
+    address_option
+    bcc_at( unsigned index ) const
+    {
+        return bcc_list.at( index ).second;
+    }
+};
+
+
+
+void check_list_inserter()
+{
+    using namespace boost::assign;
+
+    email e;
+    e.add_cc( "franz", email::dont_check_addr_book )
+            ( "hanz", email::check_addr_book )
+            ( "betty", email::dont_check_addr_book );
+    BOOST_CHECK_EQUAL( e.cc_at( "franz" ), email::dont_check_addr_book );
+    BOOST_CHECK_EQUAL( e.cc_at( "hanz" ), email::check_addr_book );
+    BOOST_CHECK_EQUAL( e.cc_at( "betty" ), email::dont_check_addr_book );
+
+    e.add_to( "betsy" )( "peter" );
+    BOOST_CHECK_EQUAL( e.cc_at( "betsy" ), email::check_addr_book );
+    BOOST_CHECK_EQUAL( e.cc_at( "peter" ), email::check_addr_book );
+    
+    e.add_bcc( email::bcc_type( "Mr. Foo", email::check_addr_book ) )
+             ( "Mr. Bar", email::dont_check_addr_book );
+    BOOST_CHECK_EQUAL( e.bcc_at( 0 ), email::check_addr_book );
+    BOOST_CHECK_EQUAL( e.bcc_at( 1 ), email::dont_check_addr_book );
+    
+}
+
+
+
+#include <boost/test/included/unit_test_framework.hpp> 
+
+using boost::unit_test_framework::test_suite;
+
+test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    test_suite* test = BOOST_TEST_SUITE( "List Test Suite" );
+
+    test->add( BOOST_TEST_CASE( &check_list_inserter ) );
+
+    return test;
+}
+
---
+++ boost/libs/assign/test/list_inserter.cpp
@@ -0,0 +1,125 @@
+// Boost.Assign library
+//
+//  Copyright Thorsten Ottosen 2003-2004. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/assign/
+//
+
+
+
+#include <boost/assign/list_inserter.hpp>
+#include <boost/assign/list_of.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+#include <boost/array.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/functional.hpp>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <utility>
+#include <stdexcept>
+#include <cstdlib>
+
+
+namespace ba = boost::assign;
+
+template< class T >
+void function_ptr( T )
+{
+    // do nothing
+}
+
+struct functor
+{
+    template< class T >
+    void operator()( T ) const
+    { 
+        // do nothing
+    }
+};
+
+
+
+void check_list_inserter()
+{
+    using namespace std;
+    using namespace boost;
+    using namespace boost::assign;
+    vector<int> v;
+    
+    make_list_inserter( &function_ptr<int> )( 5 ),3;
+    make_list_inserter( functor() )( 4 ),2;
+    make_list_inserter( bind( &vector<int>::push_back, &v, _1 ) )( 6 ),4;
+    BOOST_CHECK_EQUAL( v.size(), 2 );
+    BOOST_CHECK_EQUAL( v[0], 6 );
+    BOOST_CHECK_EQUAL( v[1], 4 );
+    
+    push_back( v ) = 1,2,3,4,5;
+    BOOST_CHECK_EQUAL( v.size(), 7 );
+    BOOST_CHECK_EQUAL( v[6], 5 );
+    
+    push_back( v )(6).repeat( 10, 7 )(8);
+    BOOST_CHECK_EQUAL( v.size(), 19 );
+    BOOST_CHECK_EQUAL( v[18], 8 );
+    BOOST_CHECK_EQUAL( v[8], 7 ); 
+    BOOST_CHECK_EQUAL( v[16], 7 ); 
+    push_back( v ) = repeat_fun( 10, &rand );
+    BOOST_CHECK_EQUAL( v.size(), 29 );
+    
+    push_back( v ) = 1,repeat( 10, 2 ),3;
+    BOOST_CHECK_EQUAL( v.size(), 41 );
+    push_back( v ) = 1,repeat_fun( 10, &rand ),2;
+    BOOST_CHECK_EQUAL( v.size(), 53 );
+    
+    typedef map<string,int> map_t;
+    typedef map_t::value_type V;
+    map_t m;
+
+    make_list_inserter( assign_detail::call_insert< map_t >( m ) )
+                      ( V("bar",3) )( V("foo", 2) );
+    BOOST_CHECK_EQUAL( m.size(), 2 );
+    BOOST_CHECK_EQUAL( m["foo"], 2 );
+
+    typedef vector<int>                   score_type;
+    typedef map<string,score_type>        team_score_map;
+    typedef std::pair<string,score_type>  score_pair;
+    team_score_map team_score;
+    insert( team_score )( "Team Foo",    list_of(1)(1)(0) )
+                        ( "Team Bar",    list_of(0)(0)(0) )
+                        ( "Team FooBar", list_of(0)(0)(1) ); 
+    BOOST_CHECK_EQUAL( team_score.size(), 3 );
+    BOOST_CHECK_EQUAL( team_score[ "Team Foo" ][1], 1 );
+    BOOST_CHECK_EQUAL( team_score[ "Team Bar" ][0], 0 );
+    
+    team_score = list_of< score_pair >
+                        ( "Team Foo",    list_of(1)(1)(0) )
+                        ( "Team Bar",    list_of(0)(0)(0) )
+                        ( "Team FooBar", list_of(0)(0)(1) );
+    BOOST_CHECK_EQUAL( team_score.size(), 3 );
+    BOOST_CHECK_EQUAL( team_score[ "Team Foo" ][1], 1 );
+    BOOST_CHECK_EQUAL( team_score[ "Team Bar" ][0], 0 );
+
+                        
+}
+
+
+
+#include <boost/test/included/unit_test_framework.hpp> 
+
+using boost::unit_test_framework::test_suite;
+
+test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    test_suite* test = BOOST_TEST_SUITE( "List Test Suite" );
+
+    test->add( BOOST_TEST_CASE( &check_list_inserter ) );
+
+    return test;
+}
+
