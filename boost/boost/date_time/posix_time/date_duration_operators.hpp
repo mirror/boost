5577a42050ce5145c89f703e889a65d47338942f
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-04 16:43:23 +0000

    fixes for missing inlines on operators
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23335 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/posix_time/date_duration_operators.hpp
+++ boost/boost/date_time/posix_time/date_duration_operators.hpp
@@ -17,39 +17,62 @@ namespace posix_time {
 
   /*** ptime operator functions ***/
   // ptime & months
-  ptime operator+(const ptime& t, const boost::gregorian::months& m)
+  inline
+  ptime 
+  operator+(const ptime& t, const boost::gregorian::months& m)
   {
     return t + m.get_offset(t.date());
   }
-  ptime operator+=(ptime& t, const boost::gregorian::months& m)
+  
+  inline
+  ptime 
+  operator+=(ptime& t, const boost::gregorian::months& m)
   {
     // get_neg_offset returns a negative duration, so we add
     return t += m.get_offset(t.date());
   }
-  ptime operator-(const ptime& t, const boost::gregorian::months& m)
+
+  inline
+  ptime 
+  operator-(const ptime& t, const boost::gregorian::months& m)
   {
     // get_neg_offset returns a negative duration, so we add
     return t + m.get_neg_offset(t.date());
   }
-  ptime operator-=(ptime& t, const boost::gregorian::months& m)
+  
+  inline
+  ptime 
+  operator-=(ptime& t, const boost::gregorian::months& m)
   {
     return t += m.get_neg_offset(t.date());
   }
+
   // ptime & years
-  ptime operator+(const ptime& t, const boost::gregorian::years& y)
+  inline
+  ptime 
+  operator+(const ptime& t, const boost::gregorian::years& y)
   {
     return t + y.get_offset(t.date());
   }
-  ptime operator+=(ptime& t, const boost::gregorian::years& y)
+
+  inline
+  ptime 
+  operator+=(ptime& t, const boost::gregorian::years& y)
   {
     return t += y.get_offset(t.date());
   }
-  ptime operator-(const ptime& t, const boost::gregorian::years& y)
+
+  inline
+  ptime 
+  operator-(const ptime& t, const boost::gregorian::years& y)
   {
     // get_neg_offset returns a negative duration, so we add
     return t + y.get_neg_offset(t.date());
   }
-  ptime operator-=(ptime& t, const boost::gregorian::years& y)
+
+  inline
+  ptime 
+  operator-=(ptime& t, const boost::gregorian::years& y)
   {
     // get_neg_offset returns a negative duration, so we add
     return t += y.get_neg_offset(t.date());
