b94d3bb4bf403ec8152de389ed1073dd6c6bcf1d
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-05 00:17:55 +0000

    july 2 doc patches
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23342 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/date_time/xmldoc/ref_tag_fix.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl -w
+# simply updates the id attribute of the library-reference tag in
+# the generated *.boostbook files. It will NOT update a file that already
+# has an id attribute set for library-reference.
+#
+# Bart Garst - 7/1/2004
+
+use strict;
+
+# key-value of file name and id attribute
+# if the attributes are changed here it would be a good idea to
+# also change the links in doxy.xml
+my %files = (
+  'date_time_autodoc.boostbook' 	   => 'date_time_reference',
+  'date_time_gregorian_autodoc.boostbook'  => 'gregorian_reference',
+  'date_time_posix_time_autodoc.boostbook' => 'posix_time_reference'
+);
+
+
+foreach my $key(keys %files) {
+  fix_id($key, $files{$key});
+}
+
+exit;
+
+### subroutines ###
+
+sub fix_id {
+  my $filename = shift || die "Error: argument 1 missing to sub $!";
+  my $id_tag = shift || die "Error: argument 2 missing to sub $!";
+  my ($line, @new_file);
+  
+  print "...processing $filename...\n";
+  
+  # open & read file and change appropriate line
+  open(INP, "<$filename") || die "File open (read) failed: $!";
+  while($line = <INP>){
+    if($line =~ /<library-reference>/) {
+      push(@new_file, "<library-reference id=\"$id_tag\">\n");
+    }
+    else{
+      push(@new_file, $line);
+    }
+  }
+  close(INP);
+  
+  # open & write new file w/ same name
+  open(OTP, ">$filename") || die "File open (write) failed: $!";
+  foreach(@new_file){
+    print OTP "$_";
+  }
+  close(OTP);
+}
+
+__END__
