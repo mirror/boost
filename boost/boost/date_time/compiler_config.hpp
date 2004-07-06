fab193214938ab91ded112d92bc2ca4d078f3c91
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-06 00:02:23 +0000

    config changes for new gregorian types
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23354 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/compiler_config.hpp
+++ boost/boost/date_time/compiler_config.hpp
@@ -1,9 +1,8 @@
 #ifndef DATE_TIME_COMPILER_CONFIG_HPP___
 #define DATE_TIME_COMPILER_CONFIG_HPP___
 
-/* Copyright (c) 2002,2003 CrystalClear Software, Inc.
- * Use, modification and distribution is subject to the 
- * Boost Software License, Version 1.0. (See accompanying
+/* Copyright (c) 2002-2004 CrystalClear Software, Inc.
+ * Subject to the Boost Software License, Version 1.0. (See accompanying
  * file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
  * Author: Jeff Garland, Bart Garst
  * $Date$
