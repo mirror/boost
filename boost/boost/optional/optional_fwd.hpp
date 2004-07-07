4f79234650d1f75a12ecfe6493fb6d215d20e621
fcacciola <fcacciola@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-07 15:13:25 +0000

    Pseudo-destructor fixed
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23393 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/optional/optional_fwd.hpp
+++ boost/boost/optional/optional_fwd.hpp
@@ -9,21 +9,12 @@
 // You are welcome to contact the author at:
 //  fernando_cacciola@hotmail.com
 //
-#ifndef BOOST_UTILITY_NONE_17SEP2003_HPP
-#define BOOST_UTILITY_NONE_17SEP2003_HPP
-
-#include "boost/detail/none_t.hpp"
-
-// NOTE: Borland users have to include this header outside any precompiled headers
-// (bcc<=5.64 cannot include instance data in a precompiled header)
+#ifndef BOOST_OPTIONAL_OPTIONAL_FWD_FLC_19NOV2002_HPP
+#define BOOST_OPTIONAL_OPTIONAL_FWD_FLC_19NOV2002_HPP
 
 namespace boost {
 
-namespace {
-
-detail::none_t const none = ((detail::none_t)0) ;
-
-}
+template<class T> class optional ;
 
 } // namespace boost
 
