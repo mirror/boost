29dbcf73805e57d21b01193a1c6dfc59d85c87f8
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-12 23:29:37 +0000

    removing test files
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23472 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/test/testfile_text_archive.hpp
+++
@@ -1,3 +0,0 @@
-22 serialization::archive 3 1 3
-0
-0
--- boost/libs/serialization/test/testfile_xml_archive.hpp
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<!DOCTYPE boost_serialization>
-<boost_serialization signature="serialization::archive" version="3">
-<a></a>
-<a></a>
-<b class_id="0" tracking_level="1" version="3" object_id="_0"></b>
-<b object_id_reference="_0"></b>
-</boost_serialization>
