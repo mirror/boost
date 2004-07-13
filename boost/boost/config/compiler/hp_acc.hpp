274bc0b16d276841a25e1298cce9e4fec6066d50
jerrydy <jerrydy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-13 23:55:44 +0000

    updated so BOOST_NO_MEMBER_TEMPLATE_KEYWORD is disabled for aC++ 3.13 & above
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23515 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/config/compiler/hp_acc.hpp
+++ boost/boost/config/compiler/hp_acc.hpp
@@ -42,7 +42,7 @@
 #    define BOOST_HAS_PARTIAL_STD_ALLOCATOR
 #endif
 
-#if (__HP_aCC <= 53800 )
+#if (__HP_aCC >= 50000 ) && (__HP_aCC <= 53800 ) || (__HP_aCC < 31300 )
 #    define BOOST_NO_MEMBER_TEMPLATE_KEYWORD
 #endif
 
