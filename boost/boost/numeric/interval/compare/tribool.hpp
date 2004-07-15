6112efad0fda0231e6386f434ac68c00c79ba4cd
gmelquio <gmelquio@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-15 12:06:47 +0000

    Adapt to tribool current namespace
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23578 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/numeric/interval/compare/tribool.hpp
+++ boost/boost/numeric/interval/compare/tribool.hpp
@@ -12,7 +12,7 @@
 
 #include <boost/numeric/interval/detail/interval_prototype.hpp>
 #include <boost/numeric/interval/detail/test_input.hpp>
-#include <boost/tribool.hpp>
+#include <boost/logic/tribool.hpp>
 
 namespace boost {
 namespace numeric {
@@ -21,111 +21,111 @@ namespace compare {
 namespace tribool {
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator<(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator<(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() < y.lower()) return true;
   if (x.lower() >= y.upper()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator<(const interval<T, Policies>& x, const T& y)
+logic::tribool operator<(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() < y) return true;
   if (x.lower() >= y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator<=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator<=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() <= y.lower()) return true;
   if (x.lower() > y.upper()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator<=(const interval<T, Policies>& x, const T& y)
+logic::tribool operator<=(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() <= y) return true;
   if (x.lower() > y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator>(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator>(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.lower() > y.upper()) return true;
   if (x.upper() <= y.lower()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator>(const interval<T, Policies>& x, const T& y)
+logic::tribool operator>(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.lower() > y) return true;
   if (x.upper() <= y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator>=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator>=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.lower() >= y.upper()) return true;
   if (x.upper() < y.lower()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator>=(const interval<T, Policies>& x, const T& y)
+logic::tribool operator>=(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.lower() >= y) return true;
   if (x.upper() < y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator==(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator==(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() == y.lower() && x.lower() == y.upper()) return true;
   if (x.upper() < y.lower() || x.lower() > y.upper()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator==(const interval<T, Policies>& x, const T& y)
+logic::tribool operator==(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() == y && x.lower() == y) return true;
   if (x.upper() < y || x.lower() > y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies1, class Policies2> inline
-tribool operator!=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
+logic::tribool operator!=(const interval<T, Policies1>& x, const interval<T, Policies2>& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() < y.lower() || x.lower() > y.upper()) return true;
   if (x.upper() == y.lower() && x.lower() == y.upper()) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 template<class T, class Policies> inline
-tribool operator!=(const interval<T, Policies>& x, const T& y)
+logic::tribool operator!=(const interval<T, Policies>& x, const T& y)
 {
   if (detail::test_input(x, y)) throw comparison_error();
   if (x.upper() < y || x.lower() > y) return true;
   if (x.upper() == y && x.lower() == y) return false;
-  return indeterminate;
+  return logic::indeterminate;
 }
 
 } // namespace tribool
