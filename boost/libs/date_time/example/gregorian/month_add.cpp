ff45b1d04550778992364317e8ffb1eba6b24e87
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-18 23:15:15 +0000

    change to use the new gregorian operators
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23736 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/date_time/example/gregorian/month_add.cpp
+++ boost/libs/date_time/example/gregorian/month_add.cpp
@@ -1,11 +1,11 @@
-
-
-// Simple program that uses the gregorian calendar to find the last
-// day of the month.
+/* Simple program that uses the gregorian calendar to progress by exactly
+ * one month, regardless of how many days are in that month.
+ *
+ * This method can be used as an alternative to iterators
+ */
 
 #include "boost/date_time/gregorian/gregorian.hpp"
 #include <iostream>
-
 
 int
 main()
@@ -15,10 +15,31 @@ main()
   typedef boost::date_time::month_functor<date> add_month;
 
   date d = day_clock::local_day();
-  add_month mf(1);
-  date d2 = d + mf.get_offset(d);
-  std::cout << to_simple_string(d2) << std::endl;
+  date d2 = d + months(1); 
+  date d3 = d - months(1);
+  std::cout << "Today is: " << to_simple_string(d) << ".\n"
+            << "One month from today will be: " << to_simple_string(d2) 
+            << "One month ago was: " << to_simple_string(d3)
+            << std::endl;
+  std::cout << "******** Warning read this ***********************\n";
+  std::cout << "Be aware that adding a month is not exactly like regular numeric math.\n"
+            << "Addition/Subtraction of months will 'snap to the end' of a month that\n"
+            << "is shorter than the current month.  For example consider "
+            << "Jan 31, 2004 + (1 month)\n";
+  date d4(2004, Jan, 31);
+  date d5 = d4 + months(1);
+  std::cout << "Result is: " << to_simple_string(d5) 
+            << std::endl;
+
+  std::cout << "\nSo what does this mean?  It means the result of adding months is order\n"
+            << "dependent, non-communitive, and may create problems for applications.\n"
+            << "Consider: \n"
+            << "Jan 30, 2004 + (1 month) + (1 month) != Jan 29, 2004 + (2 months)\n"
+            << "Why not? Because Jan 30, 2004 + 1 month is Feb 29 + 1 month is Mar 29th.\n"
+            << "while Jan 30, 2004 + 2 months is Mar 29th.\n"
+            << "All of this clears up as long as all the starting dates before the 28th of\n"
+            << "the month -- then all the behavior follows classical mathematical rules.\n";
+  
 
   return 0;
-
 }
