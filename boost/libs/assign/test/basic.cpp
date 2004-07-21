9e0f7f77a016f605f5049b7d677134cbf564ef79
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-21 22:10:23 +0000

    new simple test added
    git-svn-id: http://svn.boost.org/svn/boost/trunk@23920 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/assign/test/basic.cpp
@@ -0,0 +1,44 @@
+// Boost.Assign library
+//
+//  Copyright Thorsten Ottosen 2003-2004. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// For more information, see http://www.boost.org/libs/assign/
+//
+
+#include <boost/assign.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+using namespace boost::assign;
+
+void check_basic_usage()
+{
+    vector<int> v = list_of(1)(2);
+    v += 3,4,5,6,7,8,9;
+    push_back( v )(10)(11);
+    map<string,int> m;
+    insert( m )( "foo", 1 )( "bar", 2 );
+}
+
+
+
+#include <boost/test/included/unit_test_framework.hpp> 
+
+using boost::unit_test_framework::test_suite;
+
+test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    test_suite* test = BOOST_TEST_SUITE( "Assign Test Suite" );
+
+    test->add( BOOST_TEST_CASE( &check_basic_usage ) );
+
+    return test;
+}
+
