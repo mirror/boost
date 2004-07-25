84217629e234c575f28001692097be1f98d3b6d2
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-25 03:57:20 +0000

    Add license
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24021 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/utility/test/result_of_test.cpp
+++ boost/libs/utility/test/result_of_test.cpp
@@ -1,3 +1,11 @@
+// Boost result_of library
+
+//  Copyright Douglas Gregor 2003-2004. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org/libs/utility
 #include <boost/utility/result_of.hpp>
 #include <utility>
 #include <boost/static_assert.hpp>
