d34cb321c1db2130c685036b425feb1e29241958
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-07-26 00:32:12 +0000

    Converted to Boost Software License, Version 1.0
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24055 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/ref.hpp
+++ boost/boost/ref.hpp
@@ -18,10 +18,9 @@
 //  Copyright (C) 2001, 2002 Peter Dimov
 //  Copyright (C) 2002 David Abrahams
 //
-//  Permission to copy, use, modify, sell and distribute this software
-//  is granted provided this copyright notice appears in all copies.
-//  This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 //  See http://www.boost.org/libs/bind/ref.html for documentation.
 //
--- boost/libs/utility/iterators_test.cpp
+++ boost/libs/utility/iterators_test.cpp
@@ -1,10 +1,9 @@
 //  Demonstrate and test boost/operators.hpp on std::iterators  --------------//
 
-//  (C) Copyright Jeremy Siek 1999. Permission to copy, use, modify,
-//  sell and distribute this software is granted provided this
-//  copyright notice appears in all copies. This software is provided
-//  "as is" without express or implied warranty, and with no claim as
-//  to its suitability for any purpose.
+//  (C) Copyright Jeremy Siek 1999.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 //  See http://www.boost.org for most recent version including documentation.
 
