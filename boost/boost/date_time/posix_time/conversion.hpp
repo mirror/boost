dd26611e0973f0e341f1eda66587a159c2caead7
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-05 14:29:28 +0000

    add ftime function for win32
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24304 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/date_time/posix_time/conversion.hpp
+++ boost/boost/date_time/posix_time/conversion.hpp
@@ -5,12 +5,13 @@
  * Use, modification and distribution is subject to the 
  * Boost Software License, Version 1.0. (See accompanying
  * file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
- * Author: Jeff Garland 
+ * Author: Jeff Garland, Bart Garst
  * $Date$
  */
 
 #include "boost/date_time/posix_time/ptime.hpp"
 #include "boost/date_time/posix_time/posix_time_duration.hpp"
+#include "boost/date_time/filetime_functions.hpp"
 #include "boost/date_time/c_time.hpp"
 
 namespace boost {
@@ -26,6 +27,25 @@ namespace posix_time {
     return start + seconds(t);
   }
 
+#if defined(BOOST_HAS_FTIME)
+  
+  //! Function to create a time object from an initialized FILETIME struct.
+  /*! Function to create a time object from an initialized FILETIME struct.
+   * A FILETIME struct holds 100-nanosecond units (0.0000001). When 
+   * built with microsecond resolution the FILETIME's sub second value 
+   * will be truncated. Nanosecond resolution has no truncation. 
+   *
+   * Note ftime is part of the Win32 API, so it is not portable to non-windows
+   * platforms.
+   */
+  template<class time_type>
+  inline
+  time_type from_ftime(const FILETIME& ft)
+  {
+    return boost::date_time::time_from_ftime<time_type>(ft);
+  }
+
+#endif // BOOST_HAS_FTIME
 
 } } //namespace boost::posix_time
 
