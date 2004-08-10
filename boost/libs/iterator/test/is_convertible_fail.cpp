aba42cf03990ff998d93c507de1da02872eba7c6
witt <witt@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-10 14:41:52 +0000

    Fixed (C) and license.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24378 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/iterator/test/is_convertible_fail.cpp
+++ boost/libs/iterator/test/is_convertible_fail.cpp
@@ -1,3 +1,11 @@
+//
+// Copyright (c) Thomas Witt 2002.
+//
+// Use, modification and distribution is subject to the
+// Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <boost/iterator/reverse_iterator.hpp>
 #include <boost/cstdlib.hpp>
 
