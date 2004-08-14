c883c8bf4ac01472a28f4d794f5040098e5b73dd
memring <memring@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-14 23:02:20 +0000

    Licence from authors of previous versions changed to BSL (according to blanket-permission.txt)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24479 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/algorithm/string/collection_traits.hpp
+++ boost/boost/algorithm/string/collection_traits.hpp
@@ -5,17 +5,15 @@
 //  1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 
-// (C) Copyright Thorsten Ottosen 2002-2003. Permission to copy, use, modify,
-// sell and distribute this software is granted provided this
-// copyright notice appears in all copies. This software is provided
-// "as is" without express or implied warranty, and with no claim as
-// to its suitability for any purpose.
+// (C) Copyright Thorsten Ottosen 2002-2003. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 
-// (C) Copyright Jeremy Siek 2001. Permission to copy, use, modify,
-// sell and distribute this software is granted provided this
-// copyright notice appears in all copies. This software is provided
-// "as is" without express or implied warranty, and with no claim as
-// to its suitability for any purpose.
+// (C) Copyright Jeremy Siek 2001. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 
 //  Original idea of container traits was proposed by Jeremy Siek and
 //  Thorsten Ottosen. This implementation is lightweighted version
