23dbe604171d3423f11e55b9bb0b49da54f031e3
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-22 00:52:58 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24643 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/assign/test/multi_index_container.cpp
+++ boost/libs/assign/test/multi_index_container.cpp
@@ -26,20 +26,53 @@ using namespace boost;
 using namespace boost::multi_index;
 namespace ba =  boost::assign;
 
+
+
+//
+// Define a multi_index_container with a list-like index and an ordered index
+//
+typedef multi_index_container<
+  std::string,
+  indexed_by<
+    sequenced<>,                               // list-like index
+    ordered_non_unique<identity<std::string> > // words by alphabetical order
+  >
+> text_container;
+
+
+
 void test_multi_index_container()
 {
-    employee_set eset;
+    employee_set eset = ba::list_of< employee >(1,"Franz",30)(2,"Hanz",40)(3,"Ilse",50);
+    BOOST_CHECK( eset.size() == 3u );
     
-    eset = ba::list_of< employee >(1,"Franz",30)(2,"Hanz",40)(3,"Ilse",50);
-    BOOST_CHECK( eset.size() == 3 );
+    //
+    // This container is associative, hence we can use 'insert()' 
+    // 
     
     ba::insert( eset )(4,"Kurt",55)(5,"Bjarne",77)(7,"Thorsten",24);
-    BOOST_CHECK( eset.size() == 6 );
+    BOOST_CHECK( eset.size() == 6u );
     
     employee_set_by_name& name_index = eset.get<name>();
     employee_set_by_name::iterator i = name_index.find("Ilse");
     BOOST_CHECK( eset.project<1>(i)->id == 3 );
     BOOST_CHECK( eset.project<1>(i)->age == 50 );
+    
+    text_container text = ba::list_of< std::string >("Have")("you")("ever")("wondered")("how")("much")("Boost")("rocks?!");
+    BOOST_CHECK_EQUAL( text.size(), 8u );
+    BOOST_CHECK_EQUAL( *text.begin(), "Have" );
+    
+    //
+    // This container is a sequence, hence we can use 'push_back()' and 'push_font()'
+    //
+    
+    ba::push_back( text )("Well")(",")("A")("LOT")(",")("obviously!");
+    BOOST_CHECK_EQUAL( text.size(), 14u );
+    BOOST_CHECK_EQUAL( *--text.end(), "obviously!" );
+    
+    ba::push_front( text ) = "question:", "simple", "A";
+    BOOST_CHECK_EQUAL( text.size(), 17u );
+    BOOST_CHECK_EQUAL( text.front(), "A" );
 }
 
 
