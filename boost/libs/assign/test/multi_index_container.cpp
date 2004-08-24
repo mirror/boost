feec6d5af04cd78d77b4c311b3a08e8c650ace45
joaquintides <joaquintides@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-24 10:19:09 +0000

    some tweaks aimed to improve support for defective compilers, plus some polishing
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24705 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/assign/test/multi_index_container.cpp
+++ boost/libs/assign/test/multi_index_container.cpp
@@ -18,15 +18,92 @@
 
 #include <boost/assign/list_of.hpp>
 #include <boost/assign/list_inserter.hpp>
-#include <libs/multi_index/test/employee.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_tools.hpp>
+#include <cstddef>
+#include <ostream>
+#include <string>
 
 using namespace boost;
 using namespace boost::multi_index;
 namespace ba =  boost::assign;
 
+//
+// Define a classical multi_index_container for employees
+//
+struct employee
+{
+  int         id;
+  std::string name;
+  int         age;
 
+  employee(int id_,std::string name_,int age_):id(id_),name(name_),age(age_){}
+
+  bool operator==(const employee& x)const
+  {
+    return id==x.id&&name==x.name&&age==x.age;
+  }
+
+  bool operator<(const employee& x)const
+  {
+    return id<x.id;
+  }
+
+  bool operator!=(const employee& x)const{return !(*this==x);}
+  bool operator> (const employee& x)const{return x<*this;}
+  bool operator>=(const employee& x)const{return !(*this<x);}
+  bool operator<=(const employee& x)const{return !(x<*this);}
+
+  struct comp_id
+  {
+    bool operator()(int x,const employee& e2)const{return x<e2.id;}
+    bool operator()(const employee& e1,int x)const{return e1.id<x;}
+  };
+
+  friend std::ostream& operator<<(std::ostream& os,const employee& e)
+  {
+    os<<e.id<<" "<<e.name<<" "<<e.age<<std::endl;
+    return os;
+  }
+};
+
+struct name{};
+struct by_name{};
+struct age{};
+struct as_inserted{};
+
+typedef
+  multi_index_container<
+    employee,
+    indexed_by<
+      ordered_unique<
+        identity<employee> >,
+      ordered_non_unique<
+        tag<name,by_name>,
+        BOOST_MULTI_INDEX_MEMBER(employee,std::string,name)>,
+      ordered_non_unique<
+        tag<age>,
+        BOOST_MULTI_INDEX_MEMBER(employee,int,age)>,
+      sequenced<
+        tag<as_inserted> > > >
+  employee_set;
+
+#if defined(BOOST_NO_MEMBER_TEMPLATES)
+typedef nth_index<
+  employee_set,1>::type                       employee_set_by_name;
+#else
+typedef employee_set::nth_index<1>::type employee_set_by_name;
+#endif
+
+typedef boost::multi_index::index<
+         employee_set,age>::type         employee_set_by_age;
+typedef boost::multi_index::index<
+         employee_set,as_inserted>::type employee_set_as_inserted;
 
 //
 // Define a multi_index_container with a list-like index and an ordered index
@@ -53,10 +130,10 @@ void test_multi_index_container()
     ba::insert( eset )(4,"Kurt",55)(5,"Bjarne",77)(7,"Thorsten",24);
     BOOST_CHECK( eset.size() == 6u );
     
-    employee_set_by_name& name_index = eset.get<name>();
+    employee_set_by_name& name_index = boost::multi_index::get<name>(eset);
     employee_set_by_name::iterator i = name_index.find("Ilse");
-    BOOST_CHECK( eset.project<1>(i)->id == 3 );
-    BOOST_CHECK( eset.project<1>(i)->age == 50 );
+    BOOST_CHECK( i->id == 3 );
+    BOOST_CHECK( i->age == 50 );
     
     text_container text = ba::list_of< std::string >("Have")("you")("ever")("wondered")("how")("much")("Boost")("rocks?!");
     BOOST_CHECK_EQUAL( text.size(), 8u );
