67e06ad48ff3b68643a3263b31d0c981a08e57c3
dlwalker <dlwalker@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-08-28 20:51:19 +0000

    Updated copyright year list, since the interface changed
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24801 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/utility/base_from_member.hpp
+++ boost/boost/utility/base_from_member.hpp
@@ -1,8 +1,9 @@
 //  boost utility/base_from_member.hpp header file  --------------------------//
 
-//  Copyright 2001, 2003 Daryle Walker.  Use, modification, and distribution are
-//  subject to the Boost Software License, Version 1.0.  (See accompanying file
-//  LICENSE_1_0.txt or a copy at <http://www.boost.org/LICENSE_1_0.txt>.)
+//  Copyright 2001, 2003, 2004 Daryle Walker.  Use, modification, and
+//  distribution are subject to the Boost Software License, Version 1.0.  (See
+//  accompanying file LICENSE_1_0.txt or a copy at
+//  <http://www.boost.org/LICENSE_1_0.txt>.)
 
 //  See <http://www.boost.org/libs/utility/> for the library's home page.
 
