d5b936274c3c37360b59e4d83e6ddca8b86bfcb8
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-05 11:41:08 +0000

    DigitalMars workaround
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24917 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/if.hpp
+++ boost/boost/mpl/if.hpp
@@ -45,10 +45,12 @@ struct if_c<false,T1,T2>
     typedef T2 type;
 };
 
+// agurt, 05/sep/04: nondescriptive parameter names for the sake of DigitalMars
+// (and possibly MWCW < 8.0); see http://article.gmane.org/gmane.comp.lib.boost.devel/108959
 template<
-      typename BOOST_MPL_AUX_NA_PARAM(C)
-    , typename BOOST_MPL_AUX_NA_PARAM(T1)
+      typename BOOST_MPL_AUX_NA_PARAM(T1)
     , typename BOOST_MPL_AUX_NA_PARAM(T2)
+    , typename BOOST_MPL_AUX_NA_PARAM(T3)
     >
 struct if_
 {
@@ -56,18 +58,18 @@ struct if_
     // agurt, 02/jan/03: two-step 'type' definition for the sake of aCC 
     typedef if_c<
 #if defined(BOOST_MPL_CFG_BCC_INTEGRAL_CONSTANTS)
-          BOOST_MPL_AUX_VALUE_WKND(C)::value
+          BOOST_MPL_AUX_VALUE_WKND(T1)::value
 #else
-          BOOST_MPL_AUX_STATIC_CAST(bool, BOOST_MPL_AUX_VALUE_WKND(C)::value)
+          BOOST_MPL_AUX_STATIC_CAST(bool, BOOST_MPL_AUX_VALUE_WKND(T1)::value)
 #endif
-        , T1
         , T2
+        , T3
         > almost_type_;
  
  public:
     typedef typename almost_type_::type type;
     
-    BOOST_MPL_AUX_LAMBDA_SUPPORT(3,if_,(C,T1,T2))
+    BOOST_MPL_AUX_LAMBDA_SUPPORT(3,if_,(T1,T2,T3))
 };
 
 #else
