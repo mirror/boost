098a8d930b085f47557451ba76766714d291b542
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-09 13:59:01 +0000

    License update
    git-svn-id: http://svn.boost.org/svn/boost/trunk@24990 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/array.hpp
+++ boost/boost/array.hpp
@@ -9,10 +9,9 @@
  * for documentation.
  *
  * (C) Copyright Nicolai M. Josuttis 2001.
- * Permission to copy, use, modify, sell and distribute this software
- * is granted provided this copyright notice appears in all copies.
- * This software is provided "as is" without express or implied
- * warranty, and with no claim as to its suitability for any purpose.
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
  *
  * 29 Jan 2004 - c_array() added, BOOST_NO_PRIVATE_IN_AGGREGATE removed (Nico Josuttis)
  * 23 Aug 2002 - fix for Non-MSVC compilers combined with MSVC libraries.
