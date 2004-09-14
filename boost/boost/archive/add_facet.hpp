83ce88e9fe93f6036975e155eb543e701667303a
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-14 04:07:36 +0000

    correct for stlport
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25062 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/archive/add_facet.hpp
+++ boost/boost/archive/add_facet.hpp
@@ -23,7 +23,7 @@
 // does STLport uses native STL for locales?
 #if (defined _STLPORT_VERSION) && !(defined _STLP_NO_OWN_IOSTREAMS)
 // and this native STL lib is old Dinkumware (has not defined _CPPLIB_VER)
-#  if defined(_YVALS) && ! defined(__IBMCPP__) && ! defined(_CPPLIB_VER)
+#  if defined(_YVALS) && defined(__IBMCPP__) && ! defined(_CPPLIB_VER)
 #    define BOOST_ARCHIVE_OLD_DINKUMWARE_BENEATH_STLPORT
 #  endif
 #endif
