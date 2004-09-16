a9578305318b6a3436cf3c4a571a20717c06df41
mistevens <mistevens@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-16 06:03:19 +0000

    only include general_v_of_v if used
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25138 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/numeric/ublas/test3/test32.cpp
+++ boost/libs/numeric/ublas/test3/test32.cpp
@@ -21,7 +21,9 @@
 #include <boost/numeric/ublas/vector_sparse.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_sparse.hpp>
+#ifdef USE_GENERALIZED_VECTOR_OF_VECTOR
 #include <boost/numeric/ublas/vector_of_vector.hpp>
+#endif
 #include <boost/numeric/ublas/io.hpp>
 
 #include "test3.hpp"
