4ee1378e96fbdfe24ccd81b33b5dfef37c839e6b
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-25 15:06:48 +0000

    Updated to use the BSL: Permission from Kevlin Henney was granted in blanket-permission.txt, permission from Terje Slettebo was granted for this one file privately.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25402 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/conversion/lexical_cast_test.cpp
+++ boost/libs/conversion/lexical_cast_test.cpp
@@ -1,14 +1,12 @@
-// Unit test for boost::lexical_cast.
+//  Unit test for boost::lexical_cast.
 //
-// See http://www.boost.org for most recent version, including documentation.
+//  See http://www.boost.org for most recent version, including documentation.
 //
-// Copyright Terje Slettebø and Kevlin Henney, 2003.
+//  Copyright Terje Slettebø and Kevlin Henney, 2003.
 //
-// Permission to use, copy, modify, and distribute this software for any
-// purpose is hereby granted without fee, provided that this copyright and
-// permissions notice appear in all copies and derivatives.
-//
-// This software is provided "as is" without express or implied warranty.
+//  Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt).
 
 #include <boost/config.hpp>
 
