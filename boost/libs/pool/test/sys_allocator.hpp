009eff921eff58d6290e0ea8909b2a525d79344c
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-28 12:55:45 +0000

    Changed files to BSL based on permissions in blanket-permission.txt
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25451 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/pool/test/sys_allocator.hpp
+++ boost/libs/pool/test/sys_allocator.hpp
@@ -1,9 +1,8 @@
 // Copyright (C) 2000 Stephen Cleary
 //
-// This file can be redistributed and/or modified under the terms found
-//  in "copyright.html"
-// This software and its documentation is provided "as is" without express or
-//  implied warranty, and with no claim as to its suitability for any purpose.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_SYS_ALLOCATOR_H
 #define BOOST_SYS_ALLOCATOR_H
