14537be5b9fbbe2e5be2efc3a07ed8a6ce48b98b
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-09-28 12:08:47 +0000

    Converted to BSL based on permissions in blanket-permission.txt
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25448 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/format/benchmark/bench_format.cpp
+++ boost/libs/format/benchmark/bench_format.cpp
@@ -4,10 +4,9 @@
 
 //  (C) Samuel Krempp 2001
 //                  krempp@crans.ens-cachan.fr
-//  Permission to copy, use, modify, sell and
-//  distribute this software is granted provided this copyright notice appears
-//  in all copies. This software is provided "as is" without express or implied
-//  warranty, and with no claim as to its suitability for any purpose.
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 
 // several suggestions from Jens Maurer
 
