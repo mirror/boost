57669313db5a7dc6358d4cfdfb233957c53d101e
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-10-03 17:45:53 +0000

    update filename/path check to reflect current Boost requirements
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25542 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/tools/inspect/long_name_check.cpp
+++ boost/tools/inspect/long_name_check.cpp
@@ -7,6 +7,31 @@
 
 #include "long_name_check.hpp"
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/bind.hpp>
+#include <boost/next_prior.hpp>
+
+#include <locale>
+#include <algorithm>
+
+namespace { namespace aux {
+
+bool starts_with_nonalpha( std::string const& x )
+{
+  return !std::isalpha( *x.begin(), std::locale::classic() )
+      && *x.begin() != '_'
+      && x != ".cvsignore"
+      ;
+}
+
+bool contains_dot( std::string const& x )
+{
+    return x.find( '.' ) != std::string::npos;
+}
+
+}}
+
+
 namespace boost
 {
   namespace inspect
@@ -17,13 +42,54 @@ namespace boost
       const string & library_name,
       const path & full_path )
     {
-      string leaf( full_path.leaf() );
+      std::string const leaf( full_path.leaf() );
 
       if ( leaf.size() > 31 )
       {
         ++m_long_name_errors;
         error( library_name, full_path, "filename > 31 chars" );
       }
+/*
+      if ( std::count( leaf.begin(), leaf.end(), '.' ) > 1 )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "filename contains more than one dot character ('.')" );
+      }
+*/
+      if ( *leaf.rbegin() == '.' )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "filename ends with the dot character ('.')" );
+      }
+
+      path const relative_path( relative_to( full_path, filesystem::initial_path() ), &filesystem::no_check );
+
+      if ( std::find_if( relative_path.begin(), relative_path.end(), boost::bind( &aux::starts_with_nonalpha, _1 ) )
+            != relative_path.end() )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "leading character of one of the path compontents is not alphabetic" );
+      }
+
+      if ( std::find_if( relative_path.begin(), boost::prior( relative_path.end() )
+            , boost::bind( &aux::contains_dot, _1 ) ) != boost::prior( relative_path.end() ) )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "directory name contains the dot character ('.')" );
+      }
+
+      if ( std::distance( relative_path.begin(), boost::prior( relative_path.end() ) ) >= 8 )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "file's directory depth will exceed 8 levels if placed on a CD" );
+      }
+
+      if ( relative_path.string().size() > ( 100 - string( "boost_X_XX_X/" ).size() ) )
+      {
+        ++m_long_name_errors;
+        error( library_name, full_path, "file path will be > 100 chars if placed on a CD" );
+      }
+
     }
 
     long_name_check::~long_name_check()
