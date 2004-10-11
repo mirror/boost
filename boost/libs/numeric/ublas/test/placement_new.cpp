148594e175b650df401f4519ad85d95ed9d4e5f2
mistevens <mistevens@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-10-11 12:19:53 +0000

    Copyright
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25662 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/numeric/ublas/test/placement_new.cpp
+++ boost/libs/numeric/ublas/test/placement_new.cpp
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2004 Michael Stevens
+ * Use, modification and distribution are subject to the
+ * Boost Software License, Version 1.0. (See accompanying file
+ * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+/*
  * Test placement new and array placement new for uBLAS
  *  See if base pointer is effected by array count cookie
  */
