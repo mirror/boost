a5b73dd5f2df74f98bed814c3e03a5b02070b4f0
jsiek <jsiek@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-10-20 13:20:19 +0000

    added copyrights
    git-svn-id: http://svn.boost.org/svn/boost/trunk@25810 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/iterator/example/permutation_iter_example.cpp
+++ boost/libs/iterator/example/permutation_iter_example.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2004 Jeremy Siek <jsiek@cs.indiana.edu>
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <iostream>
 #include <vector>
 #include <deque>
--- boost/libs/iterator/test/lvalue_concept_fail.cpp
+++ boost/libs/iterator/test/lvalue_concept_fail.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2004 Jeremy Siek <jsiek@cs.indiana.edu>
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/iterator/iterator_concepts.hpp>
 #include <boost/iterator/iterator_archetypes.hpp>
 #include <boost/cstdlib.hpp>
