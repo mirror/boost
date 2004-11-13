b2919b4a65ec0ee853fc8982c912ea9b20a0eea2
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-11-13 21:36:56 +0000

    Added batch file building examples on Windows
    git-svn-id: http://svn.boost.org/svn/boost/trunk@26204 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/statechart/example/BuildExamples.bat
@@ -0,0 +1,1 @@
+bjam "-sTOOLS=mingw vc7.1 intel-win32" "-sMINGW_ROOT_DIRECTORY=C:\ProgramFiles\Dev-Cpp" "-sINTEL_PATH=C:\Program Files\Intel\CPP\Compiler80\Ia32" "-sINTEL_BASE_MSVC_TOOLSET=vc7.1" %1 %2 %3 %4
