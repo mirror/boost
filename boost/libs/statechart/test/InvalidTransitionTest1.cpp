4bd9af509f881fa2f0db07c19d1d4588111d1c55
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-12-07 22:40:47 +0000

    Added two invalid transition tests
    git-svn-id: http://svn.boost.org/svn/boost/trunk@26472 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/statechart/test/InvalidTransitionTest1.cpp
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////////////////
+// (c) Copyright Andreas Huber Doenni 2004
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+#include <boost/fsm/state_machine.hpp>
+#include <boost/fsm/simple_state.hpp>
+#include <boost/fsm/event.hpp>
+#include <boost/fsm/transition.hpp>
+
+#include <boost/mpl/list.hpp>
+
+
+
+namespace fsm = boost::fsm;
+namespace mpl = boost::mpl;
+
+
+
+struct EvX : fsm::event< EvX > {};
+
+struct Active;
+struct InvalidTransitionTest : fsm::state_machine<
+  InvalidTransitionTest, Active > {};
+
+struct Idle0;
+struct Idle1;
+struct Active : fsm::simple_state< Active, InvalidTransitionTest,
+  fsm::no_reactions, mpl::list< Idle0, Idle1 > >
+{
+};
+
+struct Idle0 : fsm::simple_state< Idle0, Active::orthogonal< 0 >, fsm::transition< EvX, Idle1 > > {};
+struct Idle1 : fsm::simple_state< Idle1, Active::orthogonal< 1 > > {};
+
+int main()
+{
+  InvalidTransitionTest machine;
+  machine.initiate();
+  return 0;
+}
