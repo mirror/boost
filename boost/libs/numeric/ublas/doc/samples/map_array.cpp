a09d246cdc4c91a9741b472dbb87c2190715215b
tknapen <tknapen@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-01-06 10:03:41 +0000

    merged doc in uBLAS CVS to boost CVS
    git-svn-id: http://svn.boost.org/svn/boost/trunk@26634 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/numeric/ublas/doc/samples/map_array.cpp
+++ boost/libs/numeric/ublas/doc/samples/map_array.cpp
@@ -18,7 +18,7 @@
 
 int main () {
     using namespace boost::numeric::ublas;
-    map_array<int, double> a;
+    map_array<int, double> a (3);
     for (unsigned i = 0; i < a.size (); ++ i) {
         a [i] = i;
         std::cout << a [i] << std::endl;
