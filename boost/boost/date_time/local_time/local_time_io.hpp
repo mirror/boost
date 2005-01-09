827a0f70133d384333caec486443659ea251841d
az_sw_dude <az_sw_dude@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-01-09 23:24:05 +0000

    add as part of the 11-03 io patchset
    git-svn-id: http://svn.boost.org/svn/boost/trunk@26655 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/date_time/local_time/local_time_io.hpp
@@ -0,0 +1,47 @@
+#ifndef BOOST_DATE_TIME_LOCAL_TIME_IO_HPP__
+#define BOOST_DATE_TIME_LOCAL_TIME_IO_HPP__
+
+/* Copyright (c) 2003-2004 CrystalClear Software, Inc.
+ * Subject to the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
+ * Author: Jeff Garland, Bart Garst
+ * $Date$
+ */
+
+#include <iostream>
+#include "boost/date_time/local_time/local_date_time.hpp"
+#include "boost/date_time/time_facet.hpp"
+
+namespace boost {
+namespace local_time {
+
+  template<class CharT, class TraitsT>
+  inline
+  std::basic_ostream<CharT, TraitsT>&
+  operator<<(std::basic_ostream<CharT, TraitsT>& os, const local_date_time& ldt)
+  {
+    typedef local_date_time time_type;//::utc_time_type typename 
+    typedef date_time::time_facet<time_type, CharT> custom_time_facet;
+    typedef std::time_put<CharT> std_time_facet;
+    std::ostreambuf_iterator<CharT> oitr(os);
+    
+    if(std::has_facet<custom_time_facet>(os.getloc())) {
+      std::use_facet<custom_time_facet>(os.getloc()).put(oitr, 
+                                                         os, 
+                                                         os.fill(), 
+                                                         ldt);
+    }
+    else {
+      custom_time_facet* f = new custom_time_facet();
+      std::locale l = std::locale(os.getloc(), f);
+      os.imbue(l);
+      f->put(oitr, os, os.fill(), ldt);
+    }
+
+    return os;
+  }
+
+  
+} } // namespaces
+
+#endif // BOOST_DATE_TIME_LOCAL_TIME_IO_HPP__
