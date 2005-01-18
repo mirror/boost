c200224e01b62d13ae210aa8b2d6d77a4af0dd97
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-01-18 21:40:36 +0000

    Added comment on event-copyability and updated copyright notice
    git-svn-id: http://svn.boost.org/svn/boost/trunk@26732 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/statechart/event.hpp
+++ boost/boost/statechart/event.hpp
@@ -1,7 +1,7 @@
 #ifndef BOOST_FSM_EVENT_HPP_INCLUDED
 #define BOOST_FSM_EVENT_HPP_INCLUDED
 //////////////////////////////////////////////////////////////////////////////
-// (c) Copyright Andreas Huber Doenni 2002-2004
+// (c) Copyright Andreas Huber Doenni 2002-2005
 // Distributed under the Boost Software License, Version 1.0. (See accompany-
 // ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////
@@ -25,6 +25,11 @@ template< class MostDerived >
 class event : public detail::rtti_policy::rtti_derived_type<
   MostDerived, event_base >
 {
+  // Events need to be copyable so that they can be reposted in a transition
+  // action (happens very rarely). This is because a transition action is only
+  // passed a reference to the (possibly stack-allocated) event but the
+  // post_event function requests an event allocated with new (and pointed to
+  // by an intrusive_ptr).
   protected:
     //////////////////////////////////////////////////////////////////////////
     event() {}
