b47d1fede0fbe5433c9b31c59abd61eeb667f194
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-02-02 05:40:43 +0000

    move function from template to code
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27009 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/src/xml_woarchive.cpp
+++ boost/libs/serialization/src/xml_woarchive.cpp
@@ -16,12 +16,45 @@
 #define BOOST_WARCHIVE
 #include <boost/archive/xml_woarchive.hpp>
 
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
+// implemenations of functions specific to wide char archives
+
 #include <boost/archive/impl/basic_xml_oarchive.ipp>
 #include <boost/archive/impl/xml_woarchive_impl.ipp>
 #include <boost/archive/impl/archive_pointer_oserializer.ipp>
 
 namespace boost {
 namespace archive {
+
+BOOST_DECL_WARCHIVE
+std::wostream &
+operator<<(std::wostream &os, const char *t){
+    for(;;){
+        wchar_t wc;
+        int result = std::mbtowc(&wc, t, 10 /* max number */);
+        if(0 < result)
+            os.put(wc);
+        else
+        if(0 == result)
+            break;
+        else
+            boost::throw_exception(
+                iterators::dataflow_exception(
+                    iterators::dataflow_exception::invalid_conversion
+                )
+            );
+    }
+    return os;
+}
+
+BOOST_DECL_WARCHIVE
+std::wostream &
+operator<<(std::wostream &os, const char t){
+    wchar_t wc;
+    std::mbtowc(&wc, &t, 1);
+    os.put(wc);
+    return os;
+}
 
 template class basic_xml_oarchive<xml_woarchive> ;
 template class xml_woarchive_impl<xml_woarchive> ;
