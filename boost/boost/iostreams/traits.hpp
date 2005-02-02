c3d95a26612cde49f337c86f303471ad97543601
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-02-02 06:34:24 +0000

    reorganized and removed special treatment of back_insert_iterator
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27015 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/traits.hpp
+++ boost/boost/iostreams/traits.hpp
@@ -23,115 +23,95 @@
 #include <string>                           // char_traits. 
 #include <boost/config.hpp>                 // partial spec.
 #include <boost/iostreams/categories.hpp>
-#include <boost/iostreams/detail/iterator_traits.hpp>
 #include <boost/iostreams/detail/select.hpp>        
 #include <boost/iostreams/detail/ios_traits.hpp>        
-#include <boost/iostreams/detail/wrap_unwrap.hpp>
+#include <boost/iostreams/detail/wrap_unwrap.hpp> 
+#include <boost/iterator/iterator_traits.hpp>
 #include <boost/mpl/eval_if.hpp>
 #include <boost/mpl/identity.hpp>      
 #include <boost/mpl/int.hpp>  
+#include <boost/range/iterator_range.hpp>
 #include <boost/type_traits/is_convertible.hpp>     
 #define BOOST_SELECT_BY_SIZE_MAX_CASE 9     
 #include <boost/iostreams/detail/select_by_size.hpp>                     
 
 namespace boost { namespace iostreams {          
 
-//------------------Definitions of io_char, io_category-----------------------//
+//------------------Definitions of io_char------------------------------------//
 
 namespace detail {
 
 template<typename T>
 struct member_char_type { typedef typename T::char_type type; };
 
+} // End namespace detail.
+
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //---------------------------//
+template<typename T>
+struct io_char 
+    : detail::member_char_type<
+          typename detail::unwrapped_type<T>::type
+      > 
+    { };
+template<typename Iter>
+struct io_char< iterator_range<Iter> > {
+    typedef typename iterator_value<Iter>::type type;
+};
+#else // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //------------------//
+
+#endif // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //-----------------//
+
+//------------------Definitions of io_char, io_category-----------------------//
+
+namespace detail {
+
 template<typename T>
 struct member_io_category { typedef typename T::io_category type; };
 
 } // End namespace detail.
 
-#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
-    template<typename T>
-    struct io_char : detail::member_char_type<T> { };
-    template<typename Container>
-    struct io_char< std::back_insert_iterator<Container> > {
-        typedef typename Container::value_type type;
-    };
-    template<typename T>
-    struct io_char< boost::reference_wrapper<T> > 
-        : io_char<T> 
-        { };
-    template<typename T>
-    struct io_category {
-        typedef typename  
-                select<
-                    is_iostream<T>,      iostream_tag, 
-                    is_istream<T>,       istream_tag, 
-                    is_ostream<T>,       ostream_tag,
-                    is_streambuf<T>,     streambuf_tag,
-                    is_stringstream<T>,  stringstream_tag,
-                    is_stringbuf<T>,     stringbuf_tag,
-                    mpl::true_,          lazy< detail::member_io_category<T> >
-                >::type type;     
-    };
-    template<typename Container>
-    struct io_category< std::back_insert_iterator<Container> > {
-        typedef insert_iterator_tag type;
-    };
-    template<typename Container>
-    struct io_category< const std::back_insert_iterator<Container> > {
-        typedef insert_iterator_tag type;
-    };
-    template<typename T>
-    struct io_category< boost::reference_wrapper<T> > 
-        : io_category<T> 
-        { };
-    template<typename T>
-    struct io_category< const boost::reference_wrapper<T> > 
-        : io_category<T> 
-        { };
-#else // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //------------------//
-    template<typename T>
-    struct io_char {
-        typedef typename detail::unwrapped_type<T>::type U;
-        template<typename V>
-        struct value_type {
-            typedef typename V::container_type      container;
-            typedef typename container::value_type  type;
-        };
-        typedef typename 
-                mpl::eval_if<
-                    is_back_inserter<U>,         
-                    value_type<U>,
-                    detail::member_char_type<U>
-                >::type type;
-    };
-    template<typename T>
-    struct io_category {
-        typedef typename detail::unwrapped_type<T>::type U;
-        typedef typename  
-                select<
-                    is_iostream<U>,       iostream_tag, 
-                    is_istream<U>,        istream_tag, 
-                    is_ostream<U>,        ostream_tag,
-                    is_streambuf<U>,      streambuf_tag,
-                    is_stringstream<U>,   stringstream_tag,
-                    is_stringbuf<U>,      stringbuf_tag,
-                    is_back_inserter<U>,  insert_iterator_tag,
-                    mpl::true_,           lazy< detail::member_io_category<U> >
-                >::type type;     
-    };
-#endif // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //-----------------//
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //---------------------------//
 
-// Macros for readability.
-#define BOOST_IOSTREAMS_CHAR_TYPE(T) typename boost::iostreams::io_char<T>::type
-#define BOOST_IOSTREAMS_CATEGORY(T) \
-    typename boost::iostreams::io_category<T>::type \
-    /**/
-#define BOOST_IOSTREAMS_STREAMBUF_TYPEDEFS(Tr) \
-    typedef Tr traits_type; \
-    typedef typename traits_type::int_type int_type; \
-    typedef typename traits_type::off_type off_type; \
-    typedef typename traits_type::pos_type pos_type; \
-    /**/
+template<typename T>
+struct io_category {
+    typedef typename  
+            select<
+                is_iostream<T>,      iostream_tag, 
+                is_istream<T>,       istream_tag, 
+                is_ostream<T>,       ostream_tag,
+                is_streambuf<T>,     streambuf_tag,
+                is_stringstream<T>,  stringstream_tag,
+                is_stringbuf<T>,     stringbuf_tag,
+                mpl::true_,          lazy< detail::member_io_category<T> >
+            >::type type;     
+};
+template<typename T>
+struct io_category< boost::reference_wrapper<T> > 
+    : io_category<T> 
+    { };
+template<typename T>
+struct io_category< const boost::reference_wrapper<T> > 
+    : io_category<T> 
+    { };
+
+#else // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //------------------//
+
+template<typename T>
+struct io_category {
+    typedef typename detail::unwrapped_type<T>::type U;
+    typedef typename  
+            select<
+                is_iostream<U>,       iostream_tag, 
+                is_istream<U>,        istream_tag, 
+                is_ostream<U>,        ostream_tag,
+                is_streambuf<U>,      streambuf_tag,
+                is_stringstream<U>,   stringstream_tag,
+                is_stringbuf<U>,      stringbuf_tag,
+                mpl::true_,           lazy< detail::member_io_category<U> >
+            >::type type;     
+};
+
+#endif // #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //-----------------//
 
 //------------------Definition of get_category--------------------------------//
 
@@ -139,8 +119,8 @@ struct member_io_category { typedef typename T::io_category type; };
 // Returns an object of type io_category<T>::type.
 // 
 template<typename T>
-inline BOOST_IOSTREAMS_CATEGORY(T) get_category(const T&) 
-{ typedef BOOST_IOSTREAMS_CATEGORY(T) category; return category(); }
+inline typename io_category<T>::type get_category(const T&) 
+{ typedef typename io_category<T>::type category; return category(); }
 
 //------------------Definition of mode----------------------------------------//
 
@@ -165,7 +145,7 @@ BOOST_IOSTREAMS_MODE_IMPL_SPEC(dual_use, 9)
 
 template<typename T>
 struct io_mode_id {
-    typedef BOOST_IOSTREAMS_CATEGORY(T) category;
+    typedef typename io_category<T>::type category;
     BOOST_SELECT_BY_SIZE(int, value, detail::io_mode_impl_helper(category()));
 };
 
@@ -185,18 +165,29 @@ struct is_smart
 
 } // End namespace detail.
 
-//----------Definition of macro BOOST_IOSTREAMS_INT_TYPE----------------------//
+//----------Definition of macros----------------------------------------------//
 
 namespace detail {
 
-template<typename T> // VC6 requires the intermediate traits template.
+template<typename T> // VC6 requires this intermediate traits template.
 struct get_int_type { 
-    typedef std::char_traits<BOOST_IOSTREAMS_CHAR_TYPE(T)>  traits_type; 
+    typedef std::char_traits<typename io_char<T>::type>  traits_type; 
     typedef typename traits_type::int_type                  type; 
 };
-#define BOOST_IOSTREAMS_INT_TYPE(T) typename detail::get_int_type<T>::type
                 
 } // End namespace detail.
+
+#define BOOST_IOSTREAMS_CHAR_TYPE(T) typename boost::iostreams::io_char<T>::type
+#define BOOST_IOSTREAMS_CATEGORY(T) \
+    typename boost::iostreams::io_category<T>::type \
+    /**/
+#define BOOST_IOSTREAMS_INT_TYPE(T) typename detail::get_int_type<T>::type
+#define BOOST_IOSTREAMS_STREAMBUF_TYPEDEFS(Tr) \
+    typedef Tr traits_type; \
+    typedef typename traits_type::int_type int_type; \
+    typedef typename traits_type::off_type off_type; \
+    typedef typename traits_type::pos_type pos_type; \
+    /**/
 
 } } // End namespaces iostreams, boost.
 
