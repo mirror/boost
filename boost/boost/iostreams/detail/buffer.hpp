c8f4a773c4690f8aee1712156cdf7159e17180f7
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-02-04 21:15:19 +0000

    removed allocator_utilities.hpp include
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27111 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/buffer.hpp
+++ boost/boost/iostreams/detail/buffer.hpp
@@ -14,7 +14,6 @@
 #include <algorithm>                           // swap.
 #include <memory>                              // allocator.
 #include <boost/config.hpp>                    // member templates.
-#include <boost/iostreams/detail/allocator_utilities.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/type_traits/is_same.hpp>     
 #include <boost/utility.hpp>                   // noncopyable.
