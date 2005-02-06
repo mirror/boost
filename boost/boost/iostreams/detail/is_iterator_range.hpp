589b4decfee5590b8fac92e3d3b2fdd0f7220507
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-02-06 22:43:25 +0000

    removed dead code
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27195 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/is_iterator_range.hpp
+++ boost/boost/iostreams/detail/is_iterator_range.hpp
@@ -7,40 +7,13 @@
 #ifndef BOOST_IOSTREAMS_DETAIL_IS_ITERATOR_RANGE_HPP_INCLUDED
 #define BOOST_IOSTREAMS_DETAIL_IS_ITERATOR_RANGE_HPP_INCLUDED       
  
-#include <boost/config.hpp>                   // partial spec.
 #include <boost/iostreams/detail/bool_trait_def.hpp>
-#include <boost/iterator/iterator_traits.hpp> // iterator_value.
 #include <boost/range/iterator_range.hpp>         
-
-//#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //---------------------------//
-//
-//namespace boost { namespace iostreams { 
-//
-//template<typename T>
-//struct is_iterator_range : mpl::false_ { };
-//
-//template<typename Iter>
-//struct is_iterator_range< iterator_range<Iter> >
-//    : mpl::true_ 
-//    { };
-//
-//} } // End namespaces iostreams, boost.
-//
-//#else // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //--------------------------//
-
-// We could make this exactly equivalent to the implementation using partial
-// specialization by adding a test for the condition:
-//
-//     is_same< T, iterator_range<typename iterator_value<T>::type> >
-//
-// Currently it does not seem to be worth the trouble.
 
 namespace boost { namespace iostreams {
 
 BOOST_IOSTREAMS_BOOL_TRAIT_DEF(is_iterator_range, boost::iterator_range, 1)
 
 } } // End namespaces iostreams, boost.
-
-//#endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION //-------------------------//
 
 #endif // #ifndef BOOST_IOSTREAMS_DETAIL_IS_ITERATOR_RANGE_HPP_INCLUDED
