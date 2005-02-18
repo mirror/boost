76d27e5c7bc72209549d29d4f7e08241a1371d30
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-02-18 08:29:38 +0000

    Allow 'any_cast' to a reference type. The original patch from Mikhail Glushenkov. Tests from Alexander Nasonov.
    See:
       http://lists.boost.org/MailArchives/boost/msg76737.php
       http://lists.boost.org/MailArchives/boost/msg79043.php
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27416 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/any/any_test.cpp
+++ boost/libs/any/any_test.cpp
@@ -35,6 +35,7 @@ namespace any_tests // test suite
     void test_bad_cast();
     void test_swap();
     void test_null_copying();
+    void test_cast_to_reference();
 
     const test_case test_cases[] =
     {
@@ -45,7 +46,8 @@ namespace any_tests // test suite
         { "converting assignment operator", test_converting_assign },
         { "failed custom keyword cast",     test_bad_cast          },
         { "swap member function",           test_swap              },
-        { "copying operations on a null",   test_null_copying      }
+        { "copying operations on a null",   test_null_copying      },
+        { "casting to reference",           test_cast_to_reference }
     };
 
     const test_case_iterator begin = test_cases;
@@ -184,6 +186,53 @@ namespace any_tests // test definitions
         check_true(null.empty(), "empty on null");
         check_true(copied.empty(), "empty on copied");
         check_true(assigned.empty(), "empty on copied");
+    }
+
+    void test_cast_to_reference()
+    {
+        int i = 7;
+        any a(i), b(a);
+        const any c(i);
+
+        int&                ra1 = any_cast<int&               >(a);
+        int const&          ra2 = any_cast<int const&         >(a);
+        int const volatile& ra3 = any_cast<int const volatile&>(a);
+
+        check_true(
+            &ra1 == &ra2 && &ra2 == &ra3,
+            "references refer to a same object");
+
+        int&                rb1 = any_cast<int&               >(b);
+        int const&          rb2 = any_cast<int const&         >(b);
+        int const volatile& rb3 = any_cast<int const volatile&>(b);
+
+        check_true(
+            &rb1 == &rb2 && &rb2 == &rb3,
+            "references refer to a same object");
+
+        int const&          rc1 = any_cast<int const&         >(c);
+        int const volatile& rc2 = any_cast<int const volatile&>(c);
+
+        check_true(&rc1 == &rc2, "references refer to a same object");
+
+        check_true(
+            &ra1 != &rb1 && &rb1 != &rc1 && &rc1 != &ra1,
+            "references refer to different objects");
+
+        check_true(
+            &ra1 != &i && &rb1 != &i && &rc1 != &i,
+            "references don't refer to original");
+
+        ++ra1;
+        check_true(any_cast<int>(a) == i + 1, "modified through reference");
+
+        --rb1;
+        check_true(any_cast<int>(b) == i - 1, "modified through reference");
+
+        TEST_CHECK_THROW(
+            any_cast<char&>(a),
+            bad_any_cast,
+            "any_cast to incorrect reference type");
     }
 }
 
