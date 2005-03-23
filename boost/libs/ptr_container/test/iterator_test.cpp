f3e85d3a85eff25a94011aa97de505067110bea1
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-03-23 13:48:25 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27782 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/ptr_container/test/iterator_test.cpp
@@ -0,0 +1,33 @@
+ 
+#include <boost/ptr_container/ptr_vector.hpp>
+
+void test_iterator()
+{
+    using namespace boost;
+    ptr_vector<int> vec;
+    ptr_vector<int>::iterator           mutable_i   = vec.begin();
+    ptr_vector<int>::const_iterator     const_i     = vec.begin();
+    ptr_vector<int>::ptr_iterator       mutable_ptr = vec.ptr_begin();
+    ptr_vector<int>::ptr_const_iterator const_ptr   = vec.ptr_begin();
+    
+    const ptr_vector<int>& rvec               = vec;
+//    const_i                                   = rvec.begin();
+//    const_ptr                                 = rvec.ptr_begin();
+    
+}
+
+#include <boost/test/unit_test.hpp>
+using boost::unit_test::test_suite;
+
+test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    test_suite* test = BOOST_TEST_SUITE( "Smart Container Test Suite" );
+
+    test->add( BOOST_TEST_CASE( &test_iterator ) );
+
+    return test;
+}
+
+
+
+
