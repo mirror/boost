baa26be92f01e8f7487693607bd990e3e214343f
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-03-24 21:18:35 +0000

    uncommented code
    git-svn-id: http://svn.boost.org/svn/boost/trunk@27809 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/iostreams/test/mapped_file_test.cpp
+++ boost/libs/iostreams/test/mapped_file_test.cpp
@@ -50,88 +50,88 @@ void mapped_file_test()
         first.close();
         second.close();
 
-        //// Test reading from a stream_facade based on a mapped_file_source,
-        //// in chunks. (Also tests reopening the stream_facade.)
-        //first.open(mapped_file_source(test.name()));
-        //second.open( test.name().c_str(), 
-        //             BOOST_IOS::in | BOOST_IOS::binary );
-        //BOOST_CHECK_MESSAGE(
-        //    compare_streams_in_chunks(first, second),
-        //    "failed reading from stream_facade<mapped_file_source> in chunks"
-        //);
+        // Test reading from a stream_facade based on a mapped_file_source,
+        // in chunks. (Also tests reopening the stream_facade.)
+        first.open(mapped_file_source(test.name()));
+        second.open( test.name().c_str(), 
+                     BOOST_IOS::in | BOOST_IOS::binary );
+        BOOST_CHECK_MESSAGE(
+            compare_streams_in_chunks(first, second),
+            "failed reading from stream_facade<mapped_file_source> in chunks"
+        );
 
-        //BOOST_MESSAGE(
-        //    "done reading from stream_facade<mapped_file_source> in chunks"
-        //);
+        BOOST_MESSAGE(
+            "done reading from stream_facade<mapped_file_source> in chunks"
+        );
     }
 
     //--------------Writing to a mapped_file_sink-----------------------------//
 
     {
-        //// Test writing to a stream_facade based on a mapped_file_sink, in 
-        //// chars.
-        //uppercase_file  first, second; // Will overwrite these.
-        //test_file       test;
+        // Test writing to a stream_facade based on a mapped_file_sink, in 
+        // chars.
+        uppercase_file  first, second; // Will overwrite these.
+        test_file       test;
 
-        //stream_facade<mapped_file_sink> 
-        //    out((mapped_file_sink(first.name()))); // CW8.0 workaround.
-        //write_data_in_chars(out);
-        //out.close();
-        //BOOST_CHECK_MESSAGE(
-        //    compare_files(first.name(), test.name()),
-        //    "failed writing to stream_facade<mapped_file_sink> in chars"
-        //);
+        stream_facade<mapped_file_sink> 
+            out((mapped_file_sink(first.name()))); // CW8.0 workaround.
+        write_data_in_chars(out);
+        out.close();
+        BOOST_CHECK_MESSAGE(
+            compare_files(first.name(), test.name()),
+            "failed writing to stream_facade<mapped_file_sink> in chars"
+        );
 
-        //BOOST_MESSAGE(
-        //    "done writing to stream_facade<mapped_file_source> in chars"
-        //);
+        BOOST_MESSAGE(
+            "done writing to stream_facade<mapped_file_source> in chars"
+        );
 
-        //// Test writing to a stream_facade based on a mapped_file_sink, in 
-        //// chunks. (Also tests reopening the stream.)
-        //out.open(mapped_file_sink(second.name()));
-        //write_data_in_chunks(out);
-        //out.close();
-        //BOOST_CHECK_MESSAGE(
-        //    compare_files(second.name(), test.name()),
-        //    "failed writing to stream_facade<mapped_file_sink> in chunks"
-        //);
+        // Test writing to a stream_facade based on a mapped_file_sink, in 
+        // chunks. (Also tests reopening the stream.)
+        out.open(mapped_file_sink(second.name()));
+        write_data_in_chunks(out);
+        out.close();
+        BOOST_CHECK_MESSAGE(
+            compare_files(second.name(), test.name()),
+            "failed writing to stream_facade<mapped_file_sink> in chunks"
+        );
 
-        //BOOST_MESSAGE(
-        //    "done writing to stream_facade<mapped_file_source> in chunks"
-        //);
+        BOOST_MESSAGE(
+            "done writing to stream_facade<mapped_file_source> in chunks"
+        );
     }
 
     //--------------Random access with a mapped_file--------------------------//
 
     {
-        //// Test reading, writing and seeking within a stream_facade based on a 
-        //// mapped_file, in chars.
-        //test_file test;
-        //stream_facade<mapped_file> 
-        //    io((mapped_file(test.name()))); // CW workaround.
-        //BOOST_CHECK_MESSAGE(
-        //    test_seekable_in_chars(io),
-        //    "failed seeking within stream_facade<mapped_file> in chars"
-        //);
+        // Test reading, writing and seeking within a stream_facade based on a 
+        // mapped_file, in chars.
+        test_file test;
+        stream_facade<mapped_file> 
+            io((mapped_file(test.name()))); // CW workaround.
+        BOOST_CHECK_MESSAGE(
+            test_seekable_in_chars(io),
+            "failed seeking within stream_facade<mapped_file> in chars"
+        );
 
-        //BOOST_MESSAGE(
-        //    "done seeking within stream_facade<mapped_file> in chars"
-        //);
+        BOOST_MESSAGE(
+            "done seeking within stream_facade<mapped_file> in chars"
+        );
 
-        //io.close();
+        io.close();
 
-        //// Test reading, writing and seeking within a stream_facade based on a 
-        //// mapped_file, in chunks. (Also tests reopening the 
-        //// stream_facade.)
-        //io.open(mapped_file(test.name()));
-        //BOOST_CHECK_MESSAGE(
-        //    test_seekable_in_chunks(io),
-        //    "failed seeking within stream_facade<mapped_file> in chunks"
-        //);
+        // Test reading, writing and seeking within a stream_facade based on a 
+        // mapped_file, in chunks. (Also tests reopening the 
+        // stream_facade.)
+        io.open(mapped_file(test.name()));
+        BOOST_CHECK_MESSAGE(
+            test_seekable_in_chunks(io),
+            "failed seeking within stream_facade<mapped_file> in chunks"
+        );
 
-        //BOOST_MESSAGE(
-        //    "done seeking within stream_facade<mapped_file> in chunks"
-        //);
+        BOOST_MESSAGE(
+            "done seeking within stream_facade<mapped_file> in chunks"
+        );
     }
 }
 
