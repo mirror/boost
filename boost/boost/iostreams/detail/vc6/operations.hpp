808621c9007f7ba80775dc726c4ef073d8456441
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-06 22:57:41 +0000

    added support for flush and optimal_buffer_size
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28030 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/vc6/operations.hpp
+++ boost/boost/iostreams/detail/vc6/operations.hpp
@@ -25,13 +25,14 @@ struct is_custom
 
 
 // Implementation templates for simulated tag dispatch.
-template<typename Category> struct read_impl;
-template<typename Category> struct write_impl;
-template<typename Category> struct filter_impl;
-template<typename Category> struct direct_impl;
-template<typename Category> struct seek_impl;
-template<typename Category> struct close_impl;
-template<typename Category> struct imbue_impl;
+template<typename T> struct read_impl;
+template<typename T> struct write_impl;
+template<typename T> struct filter_impl;
+template<typename T> struct direct_impl;
+template<typename T> struct seek_impl;
+template<typename T> struct close_impl;
+template<typename T> struct imbue_impl;
+template<typename T> struct optimal_buffer_size_impl;
 
 template<> struct read_impl<int> { typedef int type; };
 template<> struct write_impl<int> { typedef int type; };
@@ -40,6 +41,7 @@ template<> struct filter_impl<int> { typedef int type; };
 template<> struct direct_impl<int> { typedef int type; };
 template<> struct close_impl<int> { typedef int type; };
 template<> struct imbue_impl<int> { typedef int type; };
+template<> struct optimal_buffer_size_impl<int> { typedef int type; };
 
 } // End namespace detail.
 
@@ -116,9 +118,24 @@ void close(T& t, Sink& snk, BOOST_IOS::openmode which)
     detail::close_impl<T>::inner<unwrapped>::close(detail::unwrap(t), snk, which); 
 }
 
+template<typename T>
+bool flush(T& t)
+{ return detail::flush_device_impl<T>::flush(detail::unwrap(t)); }
+
+template<typename T, typename Sink>
+bool flush(T& t, Sink& snk)
+{ return detail::flush_filter_impl<T>::flush(detail::unwrap(t), snk); }
+
 template<typename T, typename Locale>
 void imbue(T& t, const Locale& loc)
 { detail::imbue_impl<T>::imbue(detail::unwrap(t), loc); }
+
+template<typename T>
+std::streamsize optimal_buffer_size(T& t)
+{ 
+    typedef detail::optimal_buffer_size_impl<T> impl;
+    return impl::optimal_buffer_size(detail::unwrap(t)); 
+}
 
 //----------------------------------------------------------------------------//
 
@@ -477,6 +494,76 @@ struct close_impl<two_sequence> {
     };
 };
 
+//------------------Definition of flush_device_impl---------------------------//
+
+template<typename T>
+struct flush_device_impl 
+    : mpl::if_<
+          detail::is_custom<T>,
+          operations<T>,
+          flush_device_impl<
+              BOOST_DEDUCED_TYPENAME 
+              detail::dispatch<
+                  T, ostream_tag, streambuf_tag, flushable_tag, any_tag
+              >::type
+          >
+      >::type
+    { };
+
+template<>
+struct flush_device_impl<ostream_tag> {
+    template<typename T>
+    static bool flush(T& t)
+    { return t.rdbuf()->BOOST_IOSTREAMS_PUBSYNC() == 0; }
+};
+
+template<>
+struct flush_device_impl<streambuf_tag> {
+    template<typename T>
+    static bool flush(T& t) 
+    { return t.BOOST_IOSTREAMS_PUBSYNC() == 0; }
+};
+
+template<>
+struct flush_device_impl<flushable_tag> {
+    template<typename T>
+    static bool flush(T& t) { return t.flush(); }
+};
+
+template<>
+struct flush_device_impl<any_tag> {
+    template<typename T>
+    static bool flush(T& t) { return true; }
+};
+
+//------------------Definition of flush_filter_impl---------------------------//
+
+template<typename T>
+struct flush_filter_impl 
+    : mpl::if_<
+          detail::is_custom<T>,
+          operations<T>,
+          flush_filter_impl<
+              BOOST_DEDUCED_TYPENAME 
+              detail::dispatch<
+                  T, flushable_tag, any_tag
+              >::type
+          >
+      >::type
+    { };
+
+template<>
+struct flush_filter_impl<flushable_tag> {
+    template<typename T, typename Sink>
+    static bool flush(T& t, Sink& snk) { return t.flush(snk); }
+};
+
+template<>
+struct flush_filter_impl<any_tag> {
+    template<typename T, typename Sink>
+    static bool flush(T& t, Sink& snk) { return false; }
+};
+
 //------------------Definition of imbue_impl----------------------------------//
 
 template<typename T>
@@ -509,6 +596,43 @@ template<>
 struct imbue_impl<localizable_tag> {
 template<typename T, typename Locale>
     static void imbue(T& t, const Locale& loc) { t.imbue(loc); }
+};
+
+//------------------Definition of optimal_buffer_size_impl--------------------//
+
+template<typename T>
+struct optimal_buffer_size_impl 
+    : mpl::if_<
+          detail::is_custom<T>,
+          operations<T>,
+          optimal_buffer_size_impl<
+              BOOST_DEDUCED_TYPENAME 
+              detail::dispatch<
+                  T, optimally_buffered_tag, device_tag, filter_tag
+              >::type
+          >
+      >::type
+    { };
+
+template<>
+struct optimal_buffer_size_impl<optimally_buffered_tag> {
+    template<typename T>
+    static std::streamsize optimal_buffer_size(T& t) 
+    { return t.optimal_buffer_size(); }
+};
+
+template<>
+struct optimal_buffer_size_impl<device_tag> {
+    template<typename T>
+    static std::streamsize optimal_buffer_size(T& t)
+    { return default_device_buffer_size; }
+};
+
+template<>
+struct optimal_buffer_size_impl<filter_tag> {
+    template<typename T>
+    static std::streamsize optimal_buffer_size(T& t)
+    { return default_filter_buffer_size; }
 };
 
 } // End namespace detail.
