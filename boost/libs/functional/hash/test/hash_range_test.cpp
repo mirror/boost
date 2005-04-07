a0f0ccb4aedba957d0644ec6204e0d1f2f8c4191
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-07 21:07:14 +0000

    Removes hash_range_test.cpp's dependency on Boost.Range, which isn't currently working on Borland.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28050 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/functional/hash/test/hash_range_test.cpp
+++ boost/libs/functional/hash/test/hash_range_test.cpp
@@ -11,36 +11,45 @@
 #include <boost/limits.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/type_traits/is_base_and_derived.hpp>
-#include <boost/assign/list_inserter.hpp>
 
 BOOST_AUTO_UNIT_TEST(hash_range_tests)
 {
-    using namespace boost::assign;
-
     std::vector<int> empty, values1, values2, values3, values4, values5;
-    push_back(values1)(0);
-    push_back(values2)(10);
-    push_back(values3)(10)(20);
-    push_back(values4)(15)(75);
-    push_back(values5)(10)(20)(15)(75)(10)(20);
+    values1.push_back(0);
+    values2.push_back(10);
+    values3.push_back(10);
+    values3.push_back(20);
+    values4.push_back(15);
+    values4.push_back(75);
+    values5.push_back(10);
+    values5.push_back(20);
+    values5.push_back(15);
+    values5.push_back(75);
+    values5.push_back(10);
+    values5.push_back(20);
 
     std::vector<int> x;
-    BOOST_CHECK(boost::hash_range(empty) == boost::hash_range(x));
-    BOOST_CHECK(boost::hash_range(empty) == boost::hash_range(x.begin(), x.end()));
-    BOOST_CHECK(boost::hash_range(empty) != boost::hash_range(values1));
+    BOOST_CHECK(boost::hash_range(empty.begin(), empty.end())
+        == boost::hash_range(x.begin(), x.end()));
+    BOOST_CHECK(boost::hash_range(empty.begin(), empty.end())
+        != boost::hash_range(values1.begin(), values1.end()));
 
     x.push_back(10);
-    BOOST_CHECK(boost::hash_range(empty) != boost::hash_range(x));
-    BOOST_CHECK(boost::hash_range(values2) == boost::hash_range(x));
-    BOOST_CHECk(boost::hash_range(values2) == boost::hash_range(x.begin(), x.end()));
+    BOOST_CHECK(boost::hash_range(empty.begin(), empty.end())
+        != boost::hash_range(x.begin(), x.end()));
+    BOOST_CHECK(boost::hash_range(values2.begin(), values2.end())
+        == boost::hash_range(x.begin(), x.end()));
 
     x.push_back(20);
-    BOOST_CHECK(boost::hash_range(empty) != boost::hash_range(x));
-    BOOST_CHECK(boost::hash_range(values2) != boost::hash_range(x));
-    BOOST_CHECK(boost::hash_range(values3) == boost::hash_range(x));
+    BOOST_CHECK(boost::hash_range(empty.begin(), empty.end())
+        != boost::hash_range(x.begin(), x.end()));
+    BOOST_CHECK(boost::hash_range(values2.begin(), values2.end())
+        != boost::hash_range(x.begin(), x.end()));
+    BOOST_CHECK(boost::hash_range(values3.begin(), values3.end())
+        == boost::hash_range(x.begin(), x.end()));
 
-    std::size_t seed = boost::hash_range(values3);
-    boost::hash_range(seed, boost::const_begin(values4), boost::const_end(values4));
-    boost::hash_range(seed, x);
-    BOOST_CHECK(seed == boost::hash_range(values5));
+    std::size_t seed = boost::hash_range(values3.begin(), values3.end());
+    boost::hash_range(seed, values4.begin(), values4.end());
+    boost::hash_range(seed, x.begin(), x.end());
+    BOOST_CHECK(seed == boost::hash_range(values5.begin(), values5.end()));
 }
