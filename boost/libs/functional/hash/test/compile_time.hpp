fa05c1c1bfb1d179c44080394b9db20ce21f6a4f
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-09 09:30:27 +0000

    Use static assert instead of mpl assert in the hash tests, as it's more portable.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28068 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/functional/hash/test/compile_time.hpp
+++ boost/libs/functional/hash/test/compile_time.hpp
@@ -5,12 +5,12 @@
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/config.hpp>
-#include <boost/mpl/assert.hpp>
+#include <boost/static_assert.hpp>
 
 template <class T>
 void compile_time_tests(T*)
 {
-    BOOST_MPL_ASSERT((boost::is_base_and_derived<
-        std::unary_function<T, std::size_t>, HASH_NAMESPACE::hash<T> >));
+    BOOST_STATIC_ASSERT((boost::is_base_and_derived<
+        std::unary_function<T, std::size_t>, HASH_NAMESPACE::hash<T> >::value));
 };
 
