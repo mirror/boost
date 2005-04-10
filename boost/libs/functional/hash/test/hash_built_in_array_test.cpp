315bd1dd30378ddc43ae27fde0930c81f151ff6d
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-10 12:38:58 +0000

    Add the unit tests for hashing built in arrays.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28079 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/functional/hash/test/hash_built_in_array_test.cpp
@@ -0,0 +1,65 @@
+
+//  (C) Copyright Daniel James 2005.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "./config.hpp"
+
+#ifdef TEST_EXTENSIONS
+#  ifdef TEST_STD_INCLUDES
+#    include <functional>
+#  else
+#    include <boost/functional/hash/hash.hpp>
+#  endif
+#endif
+
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+
+#ifdef TEST_EXTENSIONS
+
+BOOST_AUTO_UNIT_TEST(array_int_test)
+{
+    const int length1 = 25;
+    int array1[25] = {
+        26, -43, 32, 65, 45,
+        12, 67, 32, 12, 23,
+        0, 0, 0, 0, 0,
+        8, -12, 23, 65, 45,
+        -1, 93, -54, 987, 3
+    };
+    boost::hash<int[25]> hasher1;
+
+    const int length2 = 1;
+    int array2[1] = {3};
+    boost::hash<int[1]> hasher2;
+
+    const int length3 = 2;
+    int array3[2] = {2, 3};
+    boost::hash<int[2]> hasher3;
+
+    BOOST_CHECK(hasher1(array1) == boost::hash_range(array1, array1 + length1));
+    BOOST_CHECK(hasher2(array2) == boost::hash_range(array2, array2 + length2));
+    BOOST_CHECK(hasher3(array3) == boost::hash_range(array3, array3 + length3));
+}
+
+BOOST_AUTO_UNIT_TEST(two_dimensional_array_test)
+{
+    int array4[3][2] = {{-5, 6}, {7, -3}, {26, 1}};
+    boost::hash<int[3][2]> hasher4;
+
+    std::size_t seed1 = 0;
+    for(int i = 0; i < 3; ++i)
+    {
+        std::size_t seed2 = 0;
+        for(int j = 0; j < 2; ++j)
+            boost::hash_combine(seed2, array4[i][j]);
+        boost::hash_combine(seed1, seed2);
+    }
+
+    BOOST_CHECK(hasher4(array4) == seed1);
+}
+
+#endif // TEST_EXTENSIONS
+
