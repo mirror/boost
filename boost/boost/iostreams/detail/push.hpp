3ef4b0d53d127c6271fcc4bffed22ad69e7e9acb
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-11 15:23:25 +0000

    made ctors explicit; updated treatment of pipelines
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28118 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/push.hpp
+++ boost/boost/iostreams/detail/push.hpp
@@ -18,14 +18,29 @@
 #include <boost/iostreams/detail/adapter/range_adapter.hpp>
 #include <boost/iostreams/detail/config/wide_streams.hpp>
 #include <boost/iostreams/detail/enable_if_stream.hpp>   
-#include <boost/iostreams/pipable.hpp>   
+#include <boost/iostreams/pipeline.hpp>   
 #include <boost/iostreams/detail/push_params.hpp>   
 #include <boost/iostreams/detail/resolve.hpp>
 #include <boost/mpl/bool.hpp>   
 #include <boost/preprocessor/cat.hpp> 
-#include <boost/preprocessor/control/expr_if.hpp> 
+#include <boost/preprocessor/control/iif.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_convertible.hpp>
+
+namespace boost { namespace iostreams { namespace detail {
+
+template<typename Chain>
+struct pusher {
+    pusher(Chain& chn) : chn_(chn) { }
+    template<typename Component>
+    void operator() (const Component& cmp) const { chn_.push(cmp); }
+    Chain& chn_;
+};
+
+template<typename Chain, typename Pipeline>
+void push(Chain& chn, Pipeline& p) { p.for_each(pusher<Chain>(chn)); }
+
+} } } // End namspaces detail, iostreams, boost.
 
 //
 // Macro: BOOST_IOSTREAMS_DEFINE_PUSH_CONSTRUCTOR(mode, name, helper).
@@ -58,69 +73,69 @@
 # ifndef BOOST_IOSTREAMS_NO_STREAM_TEMPLATES
 #  define BOOST_IOSTREAMS_DEFINE_PUSH_IMPL(name, mode, ch, helper, has_return, result) \
     template<typename CharType, typename TraitsType> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(::std::basic_streambuf<CharType, TraitsType>& sb BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, sb, helper, has_return); } \
     template<typename CharType, typename TraitsType> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(::std::basic_istream<CharType, TraitsType>& is BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_STATIC_ASSERT((!is_convertible<mode, output>::value)); \
       BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, is, helper, has_return); } \
     template<typename CharType, typename TraitsType> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(::std::basic_ostream<CharType, TraitsType>& os BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_STATIC_ASSERT((!is_convertible<mode, input>::value)); \
       BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, os, helper, has_return); } \
     template<typename CharType, typename TraitsType> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(::std::basic_iostream<CharType, TraitsType>& io BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, io, helper, has_return); } \
     template<typename Iter> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(const iterator_range<Iter>& rng BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_PP_EXPR_IF(has_return, return) \
     this->helper( ::boost::iostreams::detail::range_adapter< \
                       mode, iterator_range<Iter> \
                   >(rng) \
                   BOOST_IOSTREAMS_PUSH_ARGS() ); } \
-    template<typename Piper, typename Concept> \
-    BOOST_PP_EXPR_IF(has_return, result) \
-    name(const ::boost::iostreams::detail::piper<Piper, Concept>& p) \
-    { p.push(*this); } \
+    template<typename Pipeline, typename Concept> \
+    BOOST_PP_IIF(has_return, result, explicit) \
+    name(const ::boost::iostreams::pipeline<Pipeline, Concept>& p) \
+    { ::boost::iostreams::detail::push(*this, p); } \
     template<typename T> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IIF(has_return, result, explicit) \
     name(const T& t BOOST_IOSTREAMS_PUSH_PARAMS() BOOST_IOSTREAMS_DISABLE_IF_STREAM(T)) \
     { this->helper( ::boost::iostreams::detail::resolve<mode, ch>(t) \
                     BOOST_IOSTREAMS_PUSH_ARGS() ); } \
     /**/
 # else // # ifndef BOOST_IOSTREAMS_NO_STREAM_TEMPLATES
 #  define BOOST_IOSTREAMS_DEFINE_PUSH_IMPL(name, mode, ch, helper, has_return, result) \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(::std::streambuf& sb BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, sb, helper, has_return); } \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(::std::istream& is BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_STATIC_ASSERT((!is_convertible<mode, output>::value)); \
       BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, is, helper, has_return); } \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(::std::ostream& os BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_STATIC_ASSERT((!is_convertible<mode, input>::value)); \
       BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, os, helper, has_return); } \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(::std::iostream& io BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_IOSTREAMS_ADAPT_STREAM(mode, ch, io, helper, has_return); } \
     template<typename Iter> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(const iterator_range<Iter>& rng BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { BOOST_PP_EXPR_IF(has_return, return) \
     this->helper( ::boost::iostreams::detail::range_adapter< \
                       mode, iterator_range<Iter> \
                   >(rng) \
                   BOOST_IOSTREAMS_PUSH_ARGS() ); } \
-    template<typename Piper, typename Concept> \
-    BOOST_PP_EXPR_IF(has_return, result) \
-    name(const ::boost::iostreams::detail::piper<Piper, Concept>& p) \
-    { p.push(*this); } \
+    template<typename Pipeline, typename Concept> \
+    BOOST_PP_IF(has_return, result, explicit) \
+    name(const ::boost::iostreams::pipeline<Pipeline, Concept>& p) \
+    { ::boost::iostreams::detail::push(*this, p); } \
     template<typename T> \
     BOOST_PP_EXPR_IF(has_return, result) \
     name(const T& t BOOST_IOSTREAMS_PUSH_PARAMS() BOOST_IOSTREAMS_DISABLE_IF_STREAM(T)) \
@@ -131,22 +146,20 @@
 #else // #if VC6, VC7.0, Borland 5.x
 # define BOOST_IOSTREAMS_DEFINE_PUSH_IMPL(name, mode, ch, helper, has_return, result) \
     template<typename T> \
-    BOOST_PP_EXPR_IF(has_return, result) \
-    BOOST_PP_CAT(name, _msvc_impl) \
+    void BOOST_PP_CAT(name, _msvc_impl) \
     ( ::boost::mpl::true_, const T& t BOOST_IOSTREAMS_PUSH_PARAMS() ) \
-    { t.push(*this); } \
+    { ::boost::iostreams::detail::push(*this, t); } \
     template<typename T> \
-    BOOST_PP_EXPR_IF(has_return, result) \
-    BOOST_PP_CAT(name, _msvc_impl) \
+    void BOOST_PP_CAT(name, _msvc_impl) \
     ( ::boost::mpl::false_, const T& t BOOST_IOSTREAMS_PUSH_PARAMS() ) \
     { this->helper( ::boost::iostreams::detail::resolve<mode, ch>(t) \
                     BOOST_IOSTREAMS_PUSH_ARGS() ); } \
     template<typename T> \
-    BOOST_PP_EXPR_IF(has_return, result) \
+    BOOST_PP_IF(has_return, result, explicit) \
     name(const T& t BOOST_IOSTREAMS_PUSH_PARAMS()) \
     { \
         this->BOOST_PP_CAT(name, _msvc_impl) \
-              ( ::boost::iostreams::detail::is_piper<T>(), \
+              ( ::boost::iostreams::detail::is_pipeline<T>(), \
                 t BOOST_IOSTREAMS_PUSH_ARGS() ); \
     } \
     /**/
