90fd0e615cf9a5ece3ed54bc5fe59a5ae8543b4f
daniel_wallin <daniel_wallin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-11 12:26:02 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28104 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/parameter.hpp
@@ -0,0 +1,12 @@
+// Copyright David Abrahams, Daniel Wallin 2005. Use, modification and 
+// distribution is subject to the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_PARAMETER_050401_HPP
+#define BOOST_PARAMETER_050401_HPP
+
+#include <boost/parameter/parameter.hpp>
+
+#endif // BOOST_PARAMETER_050401_HPP
+
