c245cc6d7cb23a3cd4da8cdf83a28c9fee0764bb
schoepflin <schoepflin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-19 15:05:23 +0000

    Added workaround for Tru64/CXX-6.5.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28323 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/regex/v4/sub_match.hpp
+++ boost/boost/regex/v4/sub_match.hpp
@@ -44,7 +44,8 @@ struct sub_match : public std::pair<BidiIterator, BidiIterator>
    sub_match(BidiIterator i) : std::pair<BidiIterator, BidiIterator>(i, i), matched(false) {}
 #if !defined(BOOST_NO_TEMPLATED_ITERATOR_CONSTRUCTORS)\
                && !BOOST_WORKAROUND(BOOST_MSVC, < 1310)\
-               && !BOOST_WORKAROUND(__BORLANDC__, <= 0x0551)
+               && !BOOST_WORKAROUND(__BORLANDC__, <= 0x0551)\
+               && !BOOST_WORKAROUND(__DECCXX_VER, BOOST_TESTED_AT(60590042))
    template <class T, class A>
    operator std::basic_string<value_type, T, A> ()const
    {
