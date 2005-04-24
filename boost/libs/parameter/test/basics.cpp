c0d87bd01dd2ce5b9112b5dba213d1f393b4a913
daniel_wallin <daniel_wallin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-04-24 20:59:30 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28463 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/parameter/test/basics.cpp
+++ boost/libs/parameter/test/basics.cpp
@@ -10,29 +10,11 @@
 #include <boost/ref.hpp>
 #include <boost/static_assert.hpp>
 
+#include "basics.hpp"
+
 namespace test
 {
-  using namespace boost::parameter;
-  
-  keyword<struct name_> name;
-  keyword<struct value_> value;
-  keyword<struct index_> index;
-  keyword<struct tester_> tester;
-
-  struct f_parameters // vc6 is happier with inheritance than with a typedef
-    : parameters<
-          tester_
-        , name_
-        , value_
-        , index_
-      >
-  {};
-
-  double value_default()
-  {
-      return 666.222;
-  }
-  
+ 
   template<class Params>
   int f_impl(const Params& p)
   { 
@@ -69,65 +51,7 @@ namespace test
       return f_impl(params);
   }
 
-  template <class T>
-  bool equal(T const& x, T const& y)
-  {
-      return x == y;
-  }
-  
-  bool equal(char const* s1, char const* s2)
-  {
-      return !strcmp(s1,s2);
-  }
-  
-  template <class Name, class Value, class Index>
-  struct values_t
-  {
-      values_t(Name const& n, Value const& v, Index const& i)
-        : n(n), v(v), i(i)
-      {}
-
-      template <class Name_, class Value_, class Index_>
-      void operator()(Name_ const& n_, Value_ const& v_, Index_ const& i_) const
-      {
-
-          // Only VC and its emulators fail this; they seem to have
-          // problems with deducing the constness of string literal
-          // arrays.
- #if defined(_MSC_VER)                                      \
-     && (BOOST_WORKAROUND(BOOST_INTEL_CXX_VERSION, <= 700)  \
-         || BOOST_WORKAROUND(BOOST_MSVC, < 1310))
-# else 
-          BOOST_STATIC_ASSERT((boost::is_same<Index,Index_>::value));
-          BOOST_STATIC_ASSERT((boost::is_same<Value,Value_>::value));
-          BOOST_STATIC_ASSERT((boost::is_same<Name,Name_>::value));
-#endif
-          assert(equal(n, n_));
-          assert(equal(v, v_));
-          assert(equal(i, i_));
-      }
-      
-      Name const& n;
-      Value const& v;
-      Index const& i;
-  };
-
-  template <class Name, class Value, class Index>
-  values_t<Name,Value,Index>
-  values(Name const& n, Value const& v, Index const& i)
-  {
-      return values_t<Name,Value,Index>(n,v,i);
-  }
 }
-
-// GCC2 has a problem with char (&)[] deduction, so we'll cast string
-// literals there.
-#undef S
-#if BOOST_WORKAROUND(__GNUC__, == 2)
-# define S(s) (char const*)s
-#else
-# define S(s) s
-#endif
 
 int main()
 {
@@ -158,3 +82,4 @@ int main()
    //f(index = 56, name = 55); // won't compile
    return 0;
 }
+
--- boost/libs/parameter/test/macros.cpp
+++ boost/libs/parameter/test/macros.cpp
@@ -11,29 +11,11 @@
 #include <cassert>
 #include <string.h>
 
+#include "basics.hpp"
+
 namespace test
 {
-  using namespace boost::parameter;
-  
-  keyword<struct name_> name;
-  keyword<struct value_> value;
-  keyword<struct index_> index;
-  keyword<struct tester_> tester;
 
-  struct f_parameters // vc6 is happier with inheritance than with a typedef
-    : parameters<
-          tester_
-        , name_
-        , value_
-        , index_
-      >
-  {};
-
-  double value_default()
-  {
-      return 666.222;
-  }
-  
   BOOST_PARAMETER_FUN(int, f, 2, 4, f_parameters)
   {
       p[tester](
@@ -44,67 +26,8 @@ namespace test
 
       return 1;
   }
-
-  template <class T>
-  bool equal(T const& x, T const& y)
-  {
-      return x == y;
-  }
-  
-  bool equal(char const* s1, char const* s2)
-  {
-      return !strcmp(s1,s2);
-  }
-  
-  template <class Name, class Value, class Index>
-  struct values_t
-  {
-      values_t(Name const& n, Value const& v, Index const& i)
-        : n(n), v(v), i(i)
-      {}
-
-      template <class Name_, class Value_, class Index_>
-      void operator()(Name_ const& n_, Value_ const& v_, Index_ const& i_) const
-      {
-
-          // Only VC and its emulators fail this; they seem to have
-          // problems with deducing the constness of string literal
-          // arrays.
- #if defined(_MSC_VER)                                      \
-     && (BOOST_WORKAROUND(BOOST_INTEL_CXX_VERSION, <= 700)  \
-         || BOOST_WORKAROUND(BOOST_MSVC, < 1310))
-# else 
-          BOOST_STATIC_ASSERT((boost::is_same<Index,Index_>::value));
-          BOOST_STATIC_ASSERT((boost::is_same<Value,Value_>::value));
-          BOOST_STATIC_ASSERT((boost::is_same<Name,Name_>::value));
-#endif
-          assert(equal(n, n_));
-          assert(equal(v, v_));
-          assert(equal(i, i_));
-      }
-      
-      Name const& n;
-      Value const& v;
-      Index const& i;
-  };
-
-  template <class Name, class Value, class Index>
-  values_t<Name,Value,Index>
-  values(Name const& n, Value const& v, Index const& i)
-  {
-      return values_t<Name,Value,Index>(n,v,i);
-  }
   
 } // namespace test
-
-// GCC2 has a problem with char (&)[] deduction, so we'll cast string
-// literals there.
-#undef S
-#if BOOST_WORKAROUND(__GNUC__, == 2)
-# define S(s) (char const*)s
-#else
-# define S(s) s
-#endif
 
 int main()
 {
