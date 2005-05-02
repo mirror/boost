dff3343673d9326dd58045ade1d38d0ffe1fb571
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-02 19:38:41 +0000

    moved implementation to operations.hpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28595 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/adapter/non_blocking_adapter.hpp
+++ boost/boost/iostreams/detail/adapter/non_blocking_adapter.hpp
@@ -7,55 +7,10 @@
 #ifndef BOOST_IOSTREAMS_DETAIL_NON_BLOCKING_ADAPTER_HPP_INCLUDED
 #define BOOST_IOSTREAMS_DETAIL_NON_BLOCKING_ADAPTER_HPP_INCLUDED
 
-#include <boost/iostreams/categories.hpp>
-#include <boost/iostreams/detail/call_traits.hpp>
-#include <boost/iostreams/detail/ios.hpp>
 #include <boost/iostreams/operations.hpp>
-#include <boost/iostreams/traits.hpp>
 
-namespace boost { namespace iostreams { namespace detail {
-
-template<typename Device>
-class non_blocking_adapter {
-private:
-    typedef typename detail::value_type<Device>::type  value_type;
-public:
-    typedef typename io_char<Device>::type             char_type;
-    struct io_category
-        : io_mode<Device>::type, device_tag
-        { };
-    explicit non_blocking_adapter(Device& dev) : device_(dev) { }
-    std::streamsize read(char_type* s, std::streamsize n)
-    { 
-        std::streamsize result = 0;
-        while (result < n) {
-            std::streamsize amt = iostreams::read(device_, s, n);
-            if (amt == -1)
-                break;
-            result += amt;
-        }
-        return result != 0 ? result : -1;
-    }
-    std::streamsize write(const char_type* s, std::streamsize n)
-    { 
-        std::streamsize result = 0;
-        while (result < n) {
-            std::streamsize amt = 
-                iostreams::write(device_, s + result, n - result);
-            result += amt;
-        }
-        return result;    
-    }
-    stream_offset seek( stream_offset off, BOOST_IOS::seekdir way,
-                        BOOST_IOS::openmode which = 
-                            BOOST_IOS::in | BOOST_IOS::out )
-    { return iostreams::seek(device_, off, way, which); }
-public:
-    value_type device_;
-};
-
-//----------------------------------------------------------------------------//
-
-} } } // End namespaces detail, iostreams, boost.
+// Content has been moved to operations.hpp to break a cyclic dependency.
+// A better long-term solution might be to break up operations.hpp into
+// separate headers for each operation.
 
 #endif // #ifndef BOOST_IOSTREAMS_DETAIL_NON_BLOCKING_ADAPTER_HPP_INCLUDED
