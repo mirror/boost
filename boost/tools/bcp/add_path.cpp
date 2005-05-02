96c20c5c3a7b1526cd55f2ff15436a215d7de335
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-02 10:58:02 +0000

    Added scanning for preprocessor iteration includes (correctly traces through pp library dependencies). Updated list of special cases to match Boost-1.33 source. Added checking of macro-based includes, and warn if we don't recognise them. Added check for existance of dependent file when adding to the list - warn if it doesn't exist.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28575 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/tools/bcp/add_path.cpp
+++ boost/tools/bcp/add_path.cpp
@@ -19,14 +19,22 @@
 #include <boost/filesystem/exception.hpp>
 #include <iostream>
 
+
 void bcp_implementation::add_path(const fs::path& p)
 {
    fs::path normalized_path = p;
    normalized_path.normalize();
-   if(fs::exists(m_boost_path / normalized_path) && fs::is_directory(m_boost_path / normalized_path))
-      add_directory(normalized_path);
+   if(fs::exists(m_boost_path / normalized_path))
+   {
+      if(fs::is_directory(m_boost_path / normalized_path))
+         add_directory(normalized_path);
+      else
+         add_file(normalized_path);
+   }
    else
-      add_file(normalized_path);
+   {
+      std::cerr << "CAUTION: dependent file " << p.string() << " does not exist." << std::endl;
+   }
 }
 
 void bcp_implementation::add_directory(const fs::path& p)
@@ -131,26 +139,33 @@ void bcp_implementation::add_file(const fs::path& p)
    // now scan for "special" dependencies:
    // anything that we can't automatically detect...
    //
-   static const std::pair<fs::path, fs::path>
-      specials[] = {
-         std::pair<fs::path, fs::path>("boost/config.hpp", "boost/config"),
-         std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "Jamrules"),
-         std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "project-root.jam"),
-         std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "boost-build.jam"),
-         std::pair<fs::path, fs::path>("boost/preprocessor/iterate.hpp", "boost/preprocessor/iteration"),
-         std::pair<fs::path, fs::path>("boost/function.hpp", "boost/function/detail"),
-         std::pair<fs::path, fs::path>("boost/regex/config.hpp", "boost/regex/user.hpp"),
-         std::pair<fs::path, fs::path>("boost/signals/signal_template.hpp", "boost/function"),
-         std::pair<fs::path, fs::path>("boost/mpl/list.hpp", "boost/mpl/list"),
-         std::pair<fs::path, fs::path>("boost/mpl/list_c.hpp", "boost/mpl/list"),
-         std::pair<fs::path, fs::path>("boost/mpl/vector.hpp", "boost/mpl/vector"),
-         std::pair<fs::path, fs::path>("boost/mpl/vector_c.hpp", "boost/mpl/vector"),
-         std::pair<fs::path, fs::path>("boost/mpl/aux_/include_preprocessed.hpp", "boost/mpl/aux_/preprocessed"),
-         std::pair<fs::path, fs::path>("boost/mpl/vector/aux_/include_preprocessed.hpp", "boost/mpl/vector/aux_/preprocessed"),
-         std::pair<fs::path, fs::path>("boost/mpl/set/aux_/include_preprocessed.hpp", "boost/mpl/set/aux_/preprocessed"),
-         std::pair<fs::path, fs::path>("boost/mpl/map/aux_/include_preprocessed.hpp", "boost/mpl/map/aux_/preprocessed"),
-         std::pair<fs::path, fs::path>("boost/mpl/list/aux_/include_preprocessed.hpp", "boost/mpl/list/aux_/preprocessed"),
-      };
+static const std::pair<fs::path, fs::path>
+   specials[] = {
+      std::pair<fs::path, fs::path>("boost/config.hpp", "boost/config"),
+      std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "Jamrules"),
+      std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "project-root.jam"),
+      std::pair<fs::path, fs::path>("tools/build/allyourbase.jam", "boost-build.jam"),
+      std::pair<fs::path, fs::path>("boost/preprocessor/iterate.hpp", "boost/preprocessor/iteration"),
+      std::pair<fs::path, fs::path>("boost/preprocessor/slot/slot.hpp", "boost/preprocessor/slot/detail"),
+      std::pair<fs::path, fs::path>("boost/function.hpp", "boost/function/detail"),
+      std::pair<fs::path, fs::path>("boost/regex/config.hpp", "boost/regex/user.hpp"),
+      std::pair<fs::path, fs::path>("boost/signals/signal_template.hpp", "boost/function"),
+      std::pair<fs::path, fs::path>("boost/mpl/list.hpp", "boost/mpl/list"),
+      std::pair<fs::path, fs::path>("boost/mpl/list_c.hpp", "boost/mpl/list"),
+      std::pair<fs::path, fs::path>("boost/mpl/vector.hpp", "boost/mpl/vector"),
+      std::pair<fs::path, fs::path>("boost/mpl/deque.hpp", "boost/mpl/vector"),
+      std::pair<fs::path, fs::path>("boost/mpl/vector_c.hpp", "boost/mpl/vector"),
+      std::pair<fs::path, fs::path>("boost/mpl/map.hpp", "boost/mpl/map"),
+      std::pair<fs::path, fs::path>("boost/mpl/set.hpp", "boost/mpl/set"),
+      std::pair<fs::path, fs::path>("boost/mpl/set_c.hpp", "boost/mpl/set"),
+      std::pair<fs::path, fs::path>("boost/mpl/aux_/include_preprocessed.hpp", "boost/mpl/aux_/preprocessed"),
+      std::pair<fs::path, fs::path>("boost/mpl/vector/aux_/include_preprocessed.hpp", "boost/mpl/vector/aux_/preprocessed"),
+      std::pair<fs::path, fs::path>("boost/mpl/set/aux_/include_preprocessed.hpp", "boost/mpl/set/aux_/preprocessed"),
+      std::pair<fs::path, fs::path>("boost/mpl/map/aux_/include_preprocessed.hpp", "boost/mpl/map/aux_/preprocessed"),
+      std::pair<fs::path, fs::path>("boost/mpl/list/aux_/include_preprocessed.hpp", "boost/mpl/list/aux_/preprocessed"),
+      std::pair<fs::path, fs::path>("libs/graph/src/python/visitor.hpp", "libs/graph/src/python"),
+   };
+
    for(unsigned int n = 0; n < (sizeof(specials)/sizeof(specials[0])); ++n)
    {
       if(0 == compare_paths(specials[n].first, p))
@@ -231,6 +246,112 @@ void bcp_implementation::add_file_dependencies(const fs::path& p, bool scanfile)
          if(!m_dependencies.count(include_file)) 
             m_dependencies[include_file] = p;
          add_path(include_file);
+      }
+      ++i;
+   }
+   //
+   // Now we need to scan for Boost.Preprocessor includes that
+   // are included via preprocessor iteration:
+   //
+   boost::regex ppfiles("^[[:blank:]]*#[[:blank:]]*define[[:blank:]]+(?:BOOST_PP_FILENAME|BOOST_PP_ITERATION_PARAMS|BOOST_PP_INDIRECT_SELF)[^\\n]+?[\"<]([^\">]+)[\">]");
+   i = boost::regex_token_iterator<const char*>(view.begin(), view.end(), ppfiles, 1);
+   while(i != j)
+   {
+      //
+      // *i contains the name of the include file,
+      // check first for a file of that name in the
+      // same directory as the file we are scanning,
+      // and if that fails, then check the boost-root:
+      //
+      fs::path include_file;
+      try{
+         include_file = i->str();
+      }
+      catch(const fs::filesystem_error& e)
+      {
+         std::cerr << "Can't parse filename " << *i << " included by file " << p.string() << std::endl;
+         ++i;
+         continue;
+      }
+      fs::path test_file(m_boost_path / p.branch_path() / include_file);
+      if(fs::exists(test_file) && !fs::is_directory(test_file) && (p.branch_path().string() != "boost"))
+      {
+         if(!m_dependencies.count(p.branch_path() / include_file)) 
+            m_dependencies[p.branch_path() / include_file] = p;
+         add_path(p.branch_path() / include_file);
+      }
+      else if(fs::exists(m_boost_path / include_file))
+      {
+         if(!m_dependencies.count(include_file)) 
+            m_dependencies[include_file] = p;
+         add_path(include_file);
+      }
+      else
+      {
+         std::cerr << "CAUTION: Boost.Preprocessor iterated file " << include_file.string() << " does not exist." << std::endl;
+      }
+      ++i;
+   }
+
+   //
+   // Scan for any #include MACRO includes that we don't recognise.
+   //
+   // Begin by declaring all of the macros that get #included that 
+   // we know about and are correctly handled as special cases:
+   //
+   static const std::string known_macros[] = {
+      "BOOST_USER_CONFIG",
+      "BOOST_COMPILER_CONFIG",
+      "BOOST_STDLIB_CONFIG",
+      "BOOST_PLATFORM_CONFIG",
+      "BOOST_PP_FILENAME_1",
+      "BOOST_PP_ITERATION_PARAMS_1",
+      "BOOST_PP_FILENAME_2",
+      "BOOST_PP_ITERATION_PARAMS_2",
+      "BOOST_PP_FILENAME_3",
+      "BOOST_PP_ITERATION_PARAMS_3",
+      "BOOST_PP_FILENAME_4",
+      "BOOST_PP_ITERATION_PARAMS_4",
+      "BOOST_PP_FILENAME_5",
+      "BOOST_PP_ITERATION_PARAMS_5",
+      "BOOST_PP_INDIRECT_SELF",
+      "BOOST_PP_INCLUDE_SELF()",
+      "BOOST_PP_ITERATE",
+      "BOOST_PP_LOCAL_ITERATE",
+      "BOOST_PP_ITERATE()",
+      "BOOST_PP_LOCAL_ITERATE()",
+      "BOOST_PP_ASSIGN_SLOT(1)",
+      "BOOST_PP_ASSIGN_SLOT(2)",
+      "BOOST_PP_ASSIGN_SLOT(3)",
+      "BOOST_PP_ASSIGN_SLOT(4)",
+      "BOOST_PP_ASSIGN_SLOT(5)",
+      "BOOST_ABI_PREFIX",
+      "BOOST_ABI_SUFFIX",
+      "BOOST_PP_STRINGIZE(boost/mpl/aux_/preprocessed/AUX_PREPROCESSED_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/list/AUX778076_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/list/AUX778076_LIST_C_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/list/AUX778076_LIST_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/map/aux_/preprocessed/AUX778076_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/map/AUX778076_MAP_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/set/aux_/preprocessed/AUX778076_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/set/AUX778076_SET_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/set/AUX778076_SET_C_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/vector/AUX778076_VECTOR_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/vector/aux_/preprocessed/AUX778076_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/vector/AUX778076_DEQUE_HEADER)",
+      "BOOST_PP_STRINGIZE(boost/mpl/vector/AUX778076_VECTOR_C_HEADER)",
+      "BOOST_REGEX_USER_CONFIG",
+      "BGL_PYTHON_EVENTS_HEADER",
+   };
+
+   boost::regex indirect_includes("^[[:blank:]]*#[[:blank:]]*include[[:blank:]]+([^\"<][^\n]*?)[[:blank:]]*$");
+   i = boost::regex_token_iterator<const char*>(view.begin(), view.end(), indirect_includes, 1);
+   while(i != j)
+   {
+      const std::string* known_macros_end = known_macros + sizeof(known_macros)/sizeof(known_macros[0]);
+      if(known_macros_end == std::find(known_macros, known_macros_end, i->str()))
+      {
+         std::cerr << "CAUTION: don't know how to trace depenencies through macro: " << *i << " in file: " << p.string() << std::endl;
       }
       ++i;
    }
