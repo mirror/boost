3c32e5f4c7088ffc415c11322b4ddfb590f952aa
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-05 16:18:00 +0000

    Added intrinsic support for has_nothrow* traits. Enabled intrinsic support for VC8.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28660 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/type_traits/has_virtual_destructor.hpp
+++ boost/boost/type_traits/has_virtual_destructor.hpp
@@ -10,12 +10,13 @@
 #ifndef BOOST_TT_HAS_VIRTUAL_DESTRUCTOR_HPP_INCLUDED
 #define BOOST_TT_HAS_VIRTUAL_DESTRUCTOR_HPP_INCLUDED
 
+#include <boost/type_traits/intrinsics.hpp>
 // should be the last #include
 #include "boost/type_traits/detail/bool_trait_def.hpp"
 
 namespace boost {
 
-BOOST_TT_AUX_BOOL_TRAIT_DEF1(has_virtual_destructor,T,false)
+BOOST_TT_AUX_BOOL_TRAIT_DEF1(has_virtual_destructor,T,BOOST_HAS_VIRTUAL_DESTRUCTOR(T))
 
 } // namespace boost
 
