4a734e807034d4b993e57bc2dff8294f974e68c3
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-10 22:24:30 +0000

    added license info
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28799 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/ptr_container/detail/default_deleter.hpp
+++ boost/boost/ptr_container/detail/default_deleter.hpp
@@ -1,4 +1,4 @@
-// (C) Copyright Jonathan Turkanis.
+// (C) Copyright Jonathan Turkanis 2004-2005.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
 
