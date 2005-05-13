c336c832e9569f961918bb4420a64aa528ed76c1
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-13 09:08:02 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@28857 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/ptr_container/test/ptr_list.cpp
+++ boost/libs/ptr_container/test/ptr_list.cpp
@@ -14,8 +14,6 @@
 #include "sequence_test_data.hpp"
 #include <boost/ptr_container/ptr_list.hpp>
 
-void list_algorithms_test();
-
 void test_list()
 {
     reversible_container_test< ptr_list<Base>, Base, Derived_class >();
@@ -24,64 +22,16 @@ void test_list()
     reversible_container_test< ptr_list< nullable<Value> >, Value, Value >();
 
     random_access_algorithms_test< ptr_list<int> >();
-    //list_algorithms_test();
-}
-
-
-/*
-void list_algorithms_test()
-{
-    ptr_set<int> c;
-    IntContainer c;
-    assign::push_back( c )
-                    ( new int(1) )
-                    ( new int(3) )
-                    ( new int(6) )
-                    ( new int(7) )
-                    ( new int(2) )
-                    ( new int(2) )
-                    ( new int(0) )
-                    ( new int(6) )
-                    ( new int(3) );
-    c.sort();
-    BOOST_CHECK( is_sorted< std::less_equal<int> >( c ) );
-    BOOST_CHECK_EQUAL( c.size(), 9u );
-
-    c.unique();
-    BOOST_CHECK_EQUAL( c.size(), 6u );
-    BOOST_CHECK( c[0] == 0 );
-    BOOST_CHECK( c[1] == 1 );
-    BOOST_CHECK( c[2] == 2 );
-    BOOST_CHECK( c[3] == 3 );
-    BOOST_CHECK( c[4] == 6 );
-    BOOST_CHECK( c[5] == 7 );
-
-    c.erase_if( equal_to_int(1) );
-    BOOST_CHECK_EQUAL( c.size(), 5u );
-    BOOST_CHECK_EQUAL( c[0], 0 );
-    BOOST_CHECK_EQUAL( c[1], 2 );
-
-    c.erase_if( equal_to_int(7) );
-    BOOST_CHECK_EQUAL( c.size(), 4u );
-    BOOST_CHECK_EQUAL( c[3], 6 );
-
-    // C = [0,2,3,6]
-
-    IntContainer c2;
-    assign::push_back( c2 )
-                   ( new int(-1) )
-                   ( new int(1) )
-                   ( new int(4) )
-                   ( new int(5) )
-                   ( new int(7) );
-    BOOST_CHECK( c2.size() == 5u );
-    c.merge( c2 );
-    BOOST_CHECK( c2.empty() );
-    BOOST_CHECK( c.size() == 9u );
-    BOOST_CHECK( is_sorted< std::less_equal<int> >( c ) );
+    ptr_list<int> list;
+    list.push_back( new int(0) );
+    list.push_back( new int(2) );
+    list.push_back( new int(1) );
+    list.reverse();
+    
 
 }
-*/
+
+
 
 #include <boost/test/unit_test.hpp>
 using boost::unit_test::test_suite;
