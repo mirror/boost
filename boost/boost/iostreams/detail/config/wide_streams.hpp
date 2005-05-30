7b6b70219fdc715d6372a91af636e51d2077afb5
turkanis <turkanis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-05-30 21:54:40 +0000

    fix for GCC (Cygwin) + MSL
    git-svn-id: http://svn.boost.org/svn/boost/trunk@29306 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/iostreams/detail/config/wide_streams.hpp
+++ boost/boost/iostreams/detail/config/wide_streams.hpp
@@ -40,13 +40,10 @@
 //------------------Locale support--------------------------------------------//
 
 #ifndef BOOST_IOSTREAMS_NO_LOCALE
-# if defined(BOOST_NO_STD_LOCALE) || defined(__CYGWIN__)
-#  define BOOST_IOSTREAMS_NO_LOCALE
-# endif
-#endif // #ifndef BOOST_IOSTREAMS_NO_LOCALE
-
-#ifndef BOOST_IOSTREAMS_NO_LOCALE
-# if defined(BOOST_NO_STD_LOCALE) || defined(__CYGWIN__)
+# if defined(BOOST_NO_STD_LOCALE) || \
+     defined(__CYGWIN__) && \
+     ( !defined(__MSL_CPP__) || defined(_MSL_NO_WCHART_CPP_SUPPORT) ) \
+     /**/
 #  define BOOST_IOSTREAMS_NO_LOCALE
 # endif
 #endif // #ifndef BOOST_IOSTREAMS_NO_LOCALE
