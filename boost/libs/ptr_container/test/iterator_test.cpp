8a4848942a5d0df6e0c6bab2620a0f780f1afb4e
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-06-14 21:18:44 +0000

    added test for comparison operators between mutable and const iterators
    git-svn-id: http://svn.boost.org/svn/boost/trunk@29570 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/ptr_container/test/iterator_test.cpp
+++ boost/libs/ptr_container/test/iterator_test.cpp
@@ -10,6 +10,8 @@
 //
  
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/ptr_container/ptr_map.hpp>
+#include <boost/test/test_tools.hpp>
 
 void test_iterator()
 {
@@ -17,10 +19,35 @@ void test_iterator()
     ptr_vector<int> vec;
     ptr_vector<int>::iterator           mutable_i   = vec.begin();
     ptr_vector<int>::const_iterator     const_i     = vec.begin();
+
+    BOOST_CHECK( mutable_i == const_i );
+    BOOST_CHECK( ! (mutable_i != const_i ) );
+    BOOST_CHECK( const_i == mutable_i );
+    BOOST_CHECK( ! ( const_i != mutable_i ) );
+
+    BOOST_CHECK( !( mutable_i < const_i ) );
+    BOOST_CHECK( mutable_i <= const_i );
+    BOOST_CHECK( ! ( mutable_i > const_i ) );
+    BOOST_CHECK( mutable_i >= const_i  );
+    BOOST_CHECK( !( const_i < mutable_i ) );
+    BOOST_CHECK( const_i <= mutable_i );
+    BOOST_CHECK( ! ( const_i > mutable_i ) );
+    BOOST_CHECK( const_i >= mutable_i );
     
     const ptr_vector<int>& rvec               = vec;
     const_i                                   = rvec.begin();
 
+    ptr_map<int,int> map;
+    ptr_map<int,int>::iterator map_mutable_i     = map.begin();
+    ptr_map<int,int>::const_iterator map_const_i = map.begin();
+
+    BOOST_CHECK( map_mutable_i == map_const_i );
+    BOOST_CHECK( ! ( map_mutable_i != map_const_i ) );
+    BOOST_CHECK( map_const_i == map_mutable_i );
+    BOOST_CHECK( ! ( map_const_i != map_mutable_i ) );
+
+    const ptr_map<int,int>& rmap = map;
+    map_const_i = rmap.begin();
 }
 
 #include <boost/test/unit_test.hpp>
