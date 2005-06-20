c51adb58515421b311f4a5e8bbf841b944f436d1
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-06-20 21:45:54 +0000

    Add tests for linking two object files that include the hash libraries.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@29710 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/functional/hash/test/link_test.cpp
@@ -0,0 +1,18 @@
+
+//  (C) Copyright Daniel James 2005.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/functional/hash/pair.hpp>
+#include <boost/functional/hash/vector.hpp>
+#include <boost/functional/hash/list.hpp>
+#include <boost/functional/hash/deque.hpp>
+#include <boost/functional/hash/set.hpp>
+#include <boost/functional/hash/map.hpp>
+#include <boost/functional/hash/hash.hpp>
+#include <boost/functional/hash.hpp>
+#include <boost/functional/hash.hpp>
+
+extern int f();
+int main() { return f(); }
