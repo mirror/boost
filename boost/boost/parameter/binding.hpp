3cda6fcdf7ffceb6a7b7b348c9555bfec995d216
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-07-13 14:50:57 +0000

    Change the default binding to void
    git-svn-id: http://svn.boost.org/svn/boost/trunk@30048 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/parameter/binding.hpp
+++ boost/boost/parameter/binding.hpp
@@ -5,21 +5,37 @@
 # define BOOST_PARAMETER_BINDING_DWA200558_HPP
 
 # include <boost/mpl/apply.hpp>
-# include <boost/parameter/aux_/void.hpp>
 # include <boost/parameter/aux_/result_of0.hpp>
+
+#  include <boost/parameter/aux_/void.hpp>
+
+# if BOOST_WORKAROUND(BOOST_MSVC, < 1300)
+#  include <boost/parameter/aux_/void.hpp>
+#  include <boost/type_traits/is_same.hpp>
+# endif 
 
 namespace boost { namespace parameter { 
 
 // A metafunction that, given an argument pack, returns the type of
 // the parameter identified by the given keyword.  If no such
 // parameter has been specified, returns Default
+# if !BOOST_WORKAROUND(BOOST_MSVC, < 1300)
+template <class Parameters, class Keyword, class Default = void>
+struct binding
+  : mpl::apply_wrap2<
+        typename Parameters::binding,Keyword,Default
+    >
+{};
+# else
 template <class Parameters, class Keyword, class Default = aux::void_>
 struct binding
 {
     typedef typename mpl::apply_wrap2<
-        typename Parameters::binding,Keyword,Default
+        typename Parameters::binding,Keyword,
+        typename mpl::if_<is_same<Default,aux::void_>,void,Default>::type
     >::type type;
 };
+# endif
 
 // A metafunction that, given an argument pack, returns the type of
 // the parameter identified by the given keyword.  If no such
