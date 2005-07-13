35845850d6991cc080b0a719462c1c35b809da3d
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-07-13 13:13:38 +0000

    Fix workaround for vc-8.0 beta
    git-svn-id: http://svn.boost.org/svn/boost/trunk@30043 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/mpl/aux_/na_assert.hpp
+++ boost/boost/mpl/aux_/na_assert.hpp
@@ -18,7 +18,7 @@
 #include <boost/mpl/aux_/config/msvc.hpp>
 #include <boost/mpl/aux_/config/workaround.hpp>
 
-#if !BOOST_WORKAROUND(_MSC_FULL_VER, < 140050601)    \
+#if !BOOST_WORKAROUND(_MSC_FULL_VER, <= 140050601)    \
     && !BOOST_WORKAROUND(__EDG_VERSION__, <= 243)
 #   include <boost/mpl/assert.hpp>
 #   define BOOST_MPL_AUX_ASSERT_NOT_NA(x) \
