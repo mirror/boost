39c8621abd0dd19928b737eca4e039623a4fd332
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-07-15 05:16:52 +0000

    better header inclusion
    git-svn-id: http://svn.boost.org/svn/boost/trunk@30101 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/src/basic_serializer_map.cpp
+++ boost/libs/serialization/src/basic_serializer_map.cpp
@@ -25,6 +25,12 @@ namespace boost {
 namespace archive {
 namespace detail {
 
+bool type_info_pointer_compare::operator()(
+    const basic_serializer * lhs, const basic_serializer * rhs
+) const    {
+    return *lhs < *rhs;
+}
+
 BOOST_ARCHIVE_DECL(BOOST_PP_EMPTY())
 basic_serializer_map::basic_serializer_map(){
 }
