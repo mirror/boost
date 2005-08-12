025b99024cf777917986492500b7f2d05cd8ab21
hkaiser <hkaiser@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-08-12 19:06:10 +0000

    Disabiguated the detail namespace.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@30558 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/call_traits.hpp
+++ boost/boost/detail/call_traits.hpp
@@ -76,7 +76,7 @@ public:
    // however compiler bugs prevent this - instead pass three bool's to
    // ct_imp<T,bool,bool,bool> and add an extra partial specialisation
    // of ct_imp to handle the logic. (JM)
-   typedef typename detail::ct_imp<
+   typedef typename boost::detail::ct_imp<
       T,
       ::boost::is_pointer<T>::value,
       ::boost::is_arithmetic<T>::value
--- boost/boost/type_traits/remove_cv.hpp
+++ boost/boost/type_traits/remove_cv.hpp
@@ -25,7 +25,7 @@ namespace boost {
 #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
 //  convert a type T to a non-cv-qualified type - remove_cv<T>
-BOOST_TT_AUX_TYPE_TRAIT_DEF1(remove_cv,T,typename detail::cv_traits_imp<T*>::unqualified_type)
+BOOST_TT_AUX_TYPE_TRAIT_DEF1(remove_cv,T,typename boost::detail::cv_traits_imp<T*>::unqualified_type)
 BOOST_TT_AUX_TYPE_TRAIT_PARTIAL_SPEC1_1(typename T,remove_cv,T&,T&)
 #if !defined(BOOST_NO_ARRAY_TYPE_SPECIALIZATIONS)
 BOOST_TT_AUX_TYPE_TRAIT_PARTIAL_SPEC1_2(typename T,std::size_t N,remove_cv,T const[N],T type[N])
@@ -45,7 +45,7 @@ struct remove_cv_impl
 };
 }
 
-BOOST_TT_AUX_TYPE_TRAIT_DEF1(remove_cv,T,typename detail::remove_cv_impl<T>::type)
+BOOST_TT_AUX_TYPE_TRAIT_DEF1(remove_cv,T,typename boost::detail::remove_cv_impl<T>::type)
 
 #endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
