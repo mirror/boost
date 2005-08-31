b95e947e46f1ca6fbf964caca0b62beb7d8c8ff7
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-08-31 03:01:35 +0000

    attempt to fix metrowerks by defining BOOST_FOREACH_NO_CONST_RVALUE_DETECTION
    git-svn-id: http://svn.boost.org/svn/boost/trunk@30741 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/foreach.hpp
+++ boost/boost/foreach.hpp
@@ -46,7 +46,8 @@
  || (BOOST_WORKAROUND(__GNUC__, == 3) && (__GNUC_MINOR__ <= 2))                                 \
  || (BOOST_WORKAROUND(__GNUC__, == 3) && (__GNUC_MINOR__ <= 3) && defined(__APPLE_CC__))        \
  || BOOST_WORKAROUND(__IBMCPP__, BOOST_TESTED_AT(600))                                          \
- || BOOST_WORKAROUND(__SUNPRO_CC, BOOST_TESTED_AT(0x570))
+ || BOOST_WORKAROUND(__SUNPRO_CC, BOOST_TESTED_AT(0x570))                                       \
+ || BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3206))
 # define BOOST_FOREACH_NO_CONST_RVALUE_DETECTION
 #endif
 
