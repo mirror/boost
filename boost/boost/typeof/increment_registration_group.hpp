8cf9b4c5a903ff28331c4c02e1e2b1c40e0ab4c6
vertleyb <vertleyb@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-10-01 21:50:23 +0000

    started using PP_COUNTER instead of PP_SLOT
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31159 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/typeof/increment_registration_group.hpp
+++ boost/boost/typeof/increment_registration_group.hpp
@@ -1,21 +1,18 @@
-// Copyright (C) 2004 Arkadiy Vertleyb
+// Copyright (C) 2004, 2005 Arkadiy Vertleyb
 // Use, modification and distribution is subject to the Boost Software
 // License, Version 1.0. (http://www.boost.org/LICENSE_1_0.txt)
 
 // Inclusion of this file increments BOOST_TYPEOF_REGISTRATION_GROUP 
 // This method was suggested by Paul Mensonides
 
-#ifndef BOOST_TYPEOF_INCREMENT_REGISTRATION_GROUP_HPP_INCLUDED
-
-#   define BOOST_TYPEOF_INCREMENT_REGISTRATION_GROUP_HPP_INCLUDED
-#   include <boost/preprocessor/slot/slot.hpp>
-#   define BOOST_TYPEOF_REGISTRATION_GROUP 0
-
-#else
-
-#   define BOOST_PP_VALUE BOOST_TYPEOF_REGISTRATION_GROUP + 1
-#   include BOOST_PP_ASSIGN_SLOT(1)
+#ifdef BOOST_TYPEOF_EMULATION
 #   undef BOOST_TYPEOF_REGISTRATION_GROUP
-#   define BOOST_TYPEOF_REGISTRATION_GROUP BOOST_PP_SLOT(1)
 
-#endif//BOOST_TYPEOF_INCREMENT_REGISTRATION_GROUP_HPP_INCLUDED
+#   ifndef _MSC_VER
+#       include <boost/preprocessor/slot/counter.hpp>
+#       include BOOST_PP_UPDATE_COUNTER()
+#       define BOOST_TYPEOF_REGISTRATION_GROUP BOOST_PP_COUNTER
+#   else
+#       define BOOST_TYPEOF_REGISTRATION_GROUP __COUNTER__
+#   endif
+#endif
