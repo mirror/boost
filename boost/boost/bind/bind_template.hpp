6296b01f3660be5aea55f6001ca717e7a5eef609
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-10-25 21:01:41 +0000

    Rvalue-allowing overloads for one/two arguments added.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31470 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/bind/bind_template.hpp
+++ boost/boost/bind/bind_template.hpp
@@ -38,6 +38,22 @@
         BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
     }
 
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING)
+
+    template<class A1> result_type operator()(A1 const & a1)
+    {
+        list1<A1 const &> a(a1);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+    template<class A1> result_type operator()(A1 const & a1) const
+    {
+        list1<A1 const &> a(a1);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+#endif
+
     template<class A1, class A2> result_type operator()(A1 & a1, A2 & a2)
     {
         list2<A1 &, A2 &> a(a1, a2);
@@ -49,6 +65,48 @@
         list2<A1 &, A2 &> a(a1, a2);
         BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
     }
+
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING)
+
+    template<class A1, class A2> result_type operator()(A1 const & a1, A2 & a2)
+    {
+        list2<A1 const &, A2 &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+    template<class A1, class A2> result_type operator()(A1 const & a1, A2 & a2) const
+    {
+        list2<A1 const &, A2 &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+
+    template<class A1, class A2> result_type operator()(A1 & a1, A2 const & a2)
+    {
+        list2<A1 &, A2 const &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+    template<class A1, class A2> result_type operator()(A1 & a1, A2 const & a2) const
+    {
+        list2<A1 &, A2 const &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+
+    template<class A1, class A2> result_type operator()(A1 const & a1, A2 const & a2)
+    {
+        list2<A1 const &, A2 const &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+    template<class A1, class A2> result_type operator()(A1 const & a1, A2 const & a2) const
+    {
+        list2<A1 const &, A2 const &> a(a1, a2);
+        BOOST_BIND_RETURN l_(type<result_type>(), f_, a, 0);
+    }
+
+#endif
 
     template<class A1, class A2, class A3> result_type operator()(A1 & a1, A2 & a2, A3 & a3)
     {
