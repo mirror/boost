6e2722b3b3b8d2b7821f3098be8cdafb8b3e1ad2
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-11-09 17:33:13 +0000

    Added define to regex tests to suppress some definitions in the headers when testing the std version. Removed surplus include paths from Jamfiles: they mess up Intel C++
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31610 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/tr1/test/test_regex.cpp
+++ boost/libs/tr1/test/test_regex.cpp
@@ -3,6 +3,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_REGEX_TEST_STD
+
 #ifdef TEST_STD_HEADERS
 #include <regex>
 #else
