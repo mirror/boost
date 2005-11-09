cae031a4c5489f79c1929b14dfd7af6e848d9b8f
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-11-09 17:31:46 +0000

    Update so that gcc's native tr1 lib actually works.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31609 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/tr1/detail/config.hpp
+++ boost/boost/tr1/detail/config.hpp
@@ -44,7 +44,7 @@
 #if defined(__GLIBCXX__) && !defined(BOOST_TR1_PATH)
 #  define BOOST_TR1_PATH(name) tr1/name
 #endif
-#if !defined(BOOST_TR1_HEADER)
+#if !defined(BOOST_TR1_PATH)
 #  define BOOST_TR1_PATH(name) name
 #endif
 
@@ -64,6 +64,28 @@
 #  define BOOST_HAS_TR1_SHARED_PTR
 #  define BOOST_HAS_TR1_RANDOM
 #  define BOOST_HAS_TR1_REGEX
+#  define BOOST_HAS_TR1_TUPLE
+#  define BOOST_HAS_TR1_TYPE_TRAITS
+#  define BOOST_HAS_TR1_UTILITY
+#  define BOOST_HAS_TR1_UNORDERED_MAP
+#  define BOOST_HAS_TR1_UNORDERED_SET
+
+#endif
+
+#ifdef BOOST_HAS_GCC_TR1
+   // turn on support for everything in gcc 4.0.x:
+#  define BOOST_HAS_TR1_ARRAY
+//#  define BOOST_HAS_TR1_COMPLEX_OVERLOADS
+//#  define BOOST_HAS_TR1_COMPLEX_INVERSE_TRIG
+#  define BOOST_HAS_TR1_REFERENCE_WRAPPER
+#  define BOOST_HAS_TR1_RESULT_OF
+#  define BOOST_HAS_TR1_MEM_FN
+#  define BOOST_HAS_TR1_BIND
+#  define BOOST_HAS_TR1_FUNCTION
+#  define BOOST_HAS_TR1_HASH
+#  define BOOST_HAS_TR1_SHARED_PTR
+//#  define BOOST_HAS_TR1_RANDOM
+//#  define BOOST_HAS_TR1_REGEX
 #  define BOOST_HAS_TR1_TUPLE
 #  define BOOST_HAS_TR1_TYPE_TRAITS
 #  define BOOST_HAS_TR1_UTILITY
