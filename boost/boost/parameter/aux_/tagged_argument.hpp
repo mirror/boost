6af413cc405a04846cf87ea734c7609aedfbe0ce
daniel_wallin <daniel_wallin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-06 11:37:17 +0000

    Made singular ArgPack's work.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31930 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/parameter/aux_/tagged_argument.hpp
+++ boost/boost/parameter/aux_/tagged_argument.hpp
@@ -8,7 +8,11 @@
 
 #include <boost/parameter/aux_/void.hpp>
 #include <boost/parameter/aux_/arg_list.hpp>
+#include <boost/parameter/aux_/result_of0.hpp>
 #include <boost/detail/is_xxx.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/apply_wrap.hpp>
+#include <boost/type_traits/is_same.hpp>
 
 namespace boost { namespace parameter { namespace aux {
 
@@ -23,6 +27,22 @@ struct tagged_argument
     typedef Arg& reference;
 
     tagged_argument(reference x) : value(x) {}
+
+    // A metafunction class that, given a keyword and a default
+    // type, returns the appropriate result type for a keyword
+    // lookup given that default
+    struct binding
+    {
+        template <class KW, class Default>
+        struct apply
+        {
+          typedef typename mpl::if_<
+                boost::is_same<KW, key_type>
+              , reference
+              , Default
+          >::type type;
+        };
+    };
 
     // Comma operator to compose argument list without using parameters<>.
     // Useful for argument lists with undetermined length.
@@ -41,7 +61,90 @@ struct tagged_argument
           , arg_list<tagged_argument<Keyword2, Arg2> >(x, empty_arg_list())
         );
     }
-    
+
+    reference operator[](keyword<Keyword> const&) const
+    {
+        return value;
+    }
+
+#ifdef BOOST_NO_FUNCTION_TEMPLATE_ORDERING
+    template <class KW, class Default>
+    Default& get_with_default(default_<KW,Default> const& x, int) const
+    {
+        return x.value;
+    }
+
+    template <class Default>
+    reference get_with_default(default_<key_type,Default> const&, long) const
+    {
+        return value;
+    }
+
+    template <class KW, class Default>
+    typename mpl::apply_wrap2<binding, KW, Default&>::type
+    operator[](default_<KW,Default> const& x) const
+    {
+        return get_with_default(x, 0L);
+    }
+
+    template <class KW, class F>
+    typename result_of0<F>::type 
+    get_with_lazy_default(lazy_default<KW,F> const& x, int) const
+    {
+        return x.compute_default();
+    }
+
+    template <class F>
+    reference get_with_lazy_default(lazy_default<key_type,F> const&, long) const
+    {
+        return value;
+    }
+
+    template <class KW, class F>
+    typename mpl::apply_wrap2<
+        binding,KW
+      , typename result_of0<F>::type
+    >::type
+    operator[](lazy_default<KW,F> const& x) const
+    {
+        return get_with_lazy_default(x, 0L);
+    }
+#else
+    template <class Default>
+    reference operator[](default_<key_type,Default> const& x) const
+    {
+        return value;
+    }
+
+    template <class F>
+    reference operator[](lazy_default<key_type,F> const& x) const
+    {
+        return value;
+    }
+
+    template <class KW, class Default>
+    Default& operator[](default_<KW,Default> const& x) const
+    {
+        return x.value;
+    }
+
+    template <class KW, class F>
+    typename result_of0<F>::type operator[](lazy_default<KW,F> const& x) const
+    {
+        return x.compute_default();
+    }
+
+    template <class ParameterRequirements>
+    static typename ParameterRequirements::has_default
+    satisfies(ParameterRequirements*);
+
+    template <class HasDefault, class Predicate>
+    static typename mpl::apply1<Predicate, value_type>::type
+    satisfies(
+        parameter_requirements<key_type,Predicate,HasDefault>*
+    );
+#endif
+
     reference value;
 #if BOOST_WORKAROUND(BOOST_MSVC, BOOST_TESTED_AT(1310))
     // warning suppression
