e4ec513dc6db6d3dcfb56ca5004c090d66ce077f
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-11 17:54:46 +0000

    2nd attempt to fix como problem in basic_regex.hpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31990 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/xpressive/xpressive_dynamic.hpp
+++ boost/boost/xpressive/xpressive_dynamic.hpp
@@ -14,12 +14,12 @@
 # pragma once
 #endif
 
+#include <boost/xpressive/regex_compiler.hpp>
 #include <boost/xpressive/basic_regex.hpp>
 #include <boost/xpressive/sub_match.hpp>
 #include <boost/xpressive/match_results.hpp>
 #include <boost/xpressive/regex_algorithms.hpp>
 #include <boost/xpressive/regex_iterator.hpp>
 #include <boost/xpressive/regex_token_iterator.hpp>
-#include <boost/xpressive/regex_compiler.hpp>
 
 #endif
--- boost/boost/xpressive/xpressive_static.hpp
+++ boost/boost/xpressive/xpressive_static.hpp
@@ -21,12 +21,12 @@
 # pragma inline_depth(255)    // max inline depth
 #endif
 
+#include <boost/xpressive/regex_primitives.hpp>
 #include <boost/xpressive/basic_regex.hpp>
 #include <boost/xpressive/sub_match.hpp>
 #include <boost/xpressive/match_results.hpp>
 #include <boost/xpressive/regex_algorithms.hpp>
 #include <boost/xpressive/regex_iterator.hpp>
 #include <boost/xpressive/regex_token_iterator.hpp>
-#include <boost/xpressive/regex_primitives.hpp>
 
 #endif
