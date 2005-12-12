089d59feb28ebc885637e45a3f23d991ee9b2e53
vawjr <vawjr@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-12 15:22:02 +0000

    Changed at the request of  Jim Douglas <jim@dramatec.co.uk>
    git-svn-id: http://svn.boost.org/svn/boost/trunk@31997 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/archive/codecvt_null.hpp
+++ boost/boost/archive/codecvt_null.hpp
@@ -9,7 +9,7 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // codecvt_null.hpp:
 
-// (C) Copyright 2004 Robert Ramey - http://www.rrsd.com . 
+// (C) Copyright 2004 Robert Ramey - http://www.rrsd.com .
 // Use, modification and distribution is subject to the Boost Software
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -21,10 +21,10 @@
 
 #include <boost/config.hpp>
 
-namespace std{ 
+namespace std{
     #if defined(__LIBCOMO__)
         using ::mbstate_t;
-    #elif defined(_QNXNTO_)
+    #elif defined(__QNXNTO__)
         using std::mbstate_t;
     #elif defined(BOOST_DINKUMWARE_STDLIB)
         using ::mbstate_t;
@@ -35,17 +35,17 @@ namespace std{
     #endif
 } // namespace std
 
-namespace boost { 
+namespace boost {
 namespace archive {
 
 template<class Ch>
 class codecvt_null;
 
 template<>
-class codecvt_null<char> : public std::codecvt<char, char, std::mbstate_t> 
+class codecvt_null<char> : public std::codecvt<char, char, std::mbstate_t>
 {
     virtual bool do_always_noconv() const throw() {
-        return true;    
+        return true;
     }
 public:
     explicit codecvt_null(std::size_t no_locale_manage = 0) :
@@ -54,23 +54,23 @@ public:
 };
 
 template<>
-class codecvt_null<wchar_t> : public std::codecvt<wchar_t, char, std::mbstate_t>    
+class codecvt_null<wchar_t> : public std::codecvt<wchar_t, char, std::mbstate_t>
 {
-    virtual std::codecvt_base::result 
+    virtual std::codecvt_base::result
     do_out(
         std::mbstate_t & state,
-        const wchar_t * first1, 
+        const wchar_t * first1,
         const wchar_t * last1,
         const wchar_t * & next1,
-        char * first2, 
-        char * last2, 
+        char * first2,
+        char * last2,
         char * & next2
     ) const;
-    virtual std::codecvt_base::result  
+    virtual std::codecvt_base::result
     do_in(
         std::mbstate_t & state,
-        const char * first1, 
-        const char * last1, 
+        const char * first1,
+        const char * last1,
         const char * & next1,
         wchar_t * first2,
         wchar_t * last2,
@@ -91,14 +91,14 @@ class codecvt_null<wchar_t> : public std::codecvt<wchar_t, char, std::mbstate_t>
 #if ! ((defined _MSC_VER) && (_MSC_VER <= 1300)) \
 &&  ! defined(__BORLANDC__)
 
-#if defined(__SGI_STL_PORT) 
+#if defined(__SGI_STL_PORT)
 #if defined(_STLPORT_VERSION) && (_STLPORT_VERSION < 0x500)
 namespace std {
 
 #if 0
-template <> 
+template <>
 locale::locale(
-    const locale& __loc, 
+    const locale& __loc,
     boost::archive::codecvt_null<char> * __f
 ){
      _M_impl = 0;
@@ -108,9 +108,9 @@ locale::locale(
         this->_M_insert(__f, boost::archive::codecvt_null<char> ::id);
 }
 
-template <> 
+template <>
 locale::locale(
-    const locale& __loc, 
+    const locale& __loc,
     boost::archive::codecvt_null<wchar_t> * __f
 ){
      _M_impl = 0;
