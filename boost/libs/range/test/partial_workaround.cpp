3e21aeb18d7b7e3a89be0179270d8a360acd7e8e
nesotto <nesotto@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-17 15:15:32 +0000

    added pass test for good compilers
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32088 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/range/test/partial_workaround.cpp
+++ boost/libs/range/test/partial_workaround.cpp
@@ -105,14 +105,10 @@ test_suite* init_unit_test_suite( int argc, char* argv[] )
 
 #else // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
-#include <boost/test/unit_test.hpp>
-using boost::unit_test::test_suite;
-
-test_suite* init_unit_test_suite( int, char** )
+int main()
 {
-    test_suite* test = BOOST_TEST_SUITE( "Range Test Suite" );
-    
-    return test;
+    // always succeed
+    return 0;
 }
 
 #endif
