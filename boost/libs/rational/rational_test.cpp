9343b65b810aaf9f267f617403b56b268f92b493
dlwalker <dlwalker@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-24 09:48:38 +0000

    Moved test sections that do not need common variables to separate unit tests, grouped in suites.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32148 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/rational/rational_test.cpp
+++ boost/libs/rational/rational_test.cpp
@@ -113,10 +113,13 @@ public:
     typedef ::boost::rational<IntType>  rat;
 };
 
-// The basic test suite
-BOOST_FIXTURE_TEST_CASE( rational_test, my_configuration )
+
+// The factoring function template suite
+BOOST_FIXTURE_TEST_SUITE( factoring_suite, my_configuration );
+
+// GCD tests
+BOOST_AUTO_TEST_CASE( gcd_test )
 {
-    /* gcd tests */
     BOOST_CHECK_EQUAL( boost::gcd<IntType>(  1,  -1),  1 );
     BOOST_CHECK_EQUAL( boost::gcd<IntType>( -1,   1),  1 );
     BOOST_CHECK_EQUAL( boost::gcd<IntType>(  1,   1),  1 );
@@ -130,8 +133,11 @@ BOOST_FIXTURE_TEST_CASE( rational_test, my_configuration )
     BOOST_CHECK_EQUAL( boost::gcd<IntType>(  6,  -9),  3 );
     BOOST_CHECK_EQUAL( boost::gcd<IntType>(-10, -10), 10 );
     BOOST_CHECK_EQUAL( boost::gcd<IntType>(-25, -10),  5 );
+}
 
-    /* lcm tests */
+// LCM tests
+BOOST_AUTO_TEST_CASE( lcm_test )
+{
     BOOST_CHECK_EQUAL( boost::lcm<IntType>(  1,  -1),  1 );
     BOOST_CHECK_EQUAL( boost::lcm<IntType>( -1,   1),  1 );
     BOOST_CHECK_EQUAL( boost::lcm<IntType>(  1,   1),  1 );
@@ -145,7 +151,14 @@ BOOST_FIXTURE_TEST_CASE( rational_test, my_configuration )
     BOOST_CHECK_EQUAL( boost::lcm<IntType>( -6,   9), 18 );
     BOOST_CHECK_EQUAL( boost::lcm<IntType>(-10, -10), 10 );
     BOOST_CHECK_EQUAL( boost::lcm<IntType>( 25, -10), 50 );
+}
 
+BOOST_AUTO_TEST_SUITE_END();
+
+
+// The basic test suite
+BOOST_FIXTURE_TEST_CASE( rational_test, my_configuration )
+{
     /* initialization tests */
     rat r1, r2(0), r3(1), r4(-3), r5(7,2), r6(5,15), r7(14,-21),
         r8(-4,6), r9(-14,-70);
@@ -234,7 +247,22 @@ using boost::abs;
     BOOST_CHECK_EQUAL( abs(r5), r5       );
     BOOST_CHECK_EQUAL( abs(r8), rat(2, 3) );
 
-    /* addition/subtraction tests */
+    /* unary operator tests */
+    BOOST_CHECK_EQUAL( +r5, r5 );
+    BOOST_CHECK( -r3 != r3 );
+    BOOST_CHECK_EQUAL( -(-r3), r3 );
+    BOOST_CHECK_EQUAL( -r4, 3 );
+    BOOST_CHECK( !r2 );
+    BOOST_CHECK( !!r3 );
+}
+
+
+// The rational arithmetic operations suite
+BOOST_FIXTURE_TEST_SUITE( rational_arithmetic_suite, my_configuration );
+
+// Addition & subtraction tests
+BOOST_AUTO_TEST_CASE( rational_additive_test )
+{
     BOOST_CHECK_EQUAL( rat( 1, 2) + rat(1, 2), 1                        );
     BOOST_CHECK_EQUAL( rat(11, 3) + rat(1, 2), rat( 25,  6)             );
     BOOST_CHECK_EQUAL( rat(-8, 3) + rat(1, 5), rat(-37, 15)             );
@@ -244,145 +272,196 @@ using boost::abs;
     BOOST_CHECK_EQUAL( rat(12, 7) - 2,         rat( -2,  7)             );
     BOOST_CHECK_EQUAL(          3 + rat(4, 5), rat( 19,  5)             );
     BOOST_CHECK_EQUAL(          4 - rat(9, 2), rat( -1,  2)             );
-    rat r0(11);
-    r0 -= rat(20,3);
-    BOOST_CHECK_EQUAL( r0, rat(13, 3) );
-    r0 += rat(1,2);
-    BOOST_CHECK_EQUAL( r0, rat(29, 6) );
-    r0 -= 5;
-    BOOST_CHECK_EQUAL( r0, rat(1, -6) );
-    r0 += rat(1,5);
-    BOOST_CHECK_EQUAL( r0, rat(1, 30) );
-    r0 += 2;
-    BOOST_CHECK_EQUAL( r0, rat(61, 30) );
 
-    /* assignment tests */
-    r0 = rat(1,10);
-    BOOST_CHECK_EQUAL( r0, rat(1, 10) );
-    r0 = -9;
-    BOOST_CHECK_EQUAL( r0, rat(-9, 1) );
+    rat  r( 11 );
 
-    /* unary operator tests */
-    BOOST_CHECK_EQUAL( +r5, r5 );
-    BOOST_CHECK( -r3 != r3 );
-    BOOST_CHECK_EQUAL( -(-r3), r3 );
-    BOOST_CHECK_EQUAL( -r4, 3 );
-    BOOST_CHECK( !r2 );
-    BOOST_CHECK( !!r3 );
+    r -= rat( 20, 3 );
+    BOOST_CHECK_EQUAL( r, rat(13,  3) );
 
-    /* multiplication tests */
+    r += rat( 1, 2 );
+    BOOST_CHECK_EQUAL( r, rat(29,  6) );
+
+    r -= 5;
+    BOOST_CHECK_EQUAL( r, rat( 1, -6) );
+
+    r += rat( 1, 5 );
+    BOOST_CHECK_EQUAL( r, rat( 1, 30) );
+
+    r += 2;
+    BOOST_CHECK_EQUAL( r, rat(61, 30) );
+}
+
+// Assignment tests
+BOOST_AUTO_TEST_CASE( rational_assignment_test )
+{
+    rat  r;
+
+    r = rat( 1, 10 );
+    BOOST_CHECK_EQUAL( r, rat( 1, 10) );
+
+    r = -9;
+    BOOST_CHECK_EQUAL( r, rat(-9,  1) );
+}
+
+// Multiplication tests
+BOOST_AUTO_TEST_CASE( rational_multiplication_test )
+{
     BOOST_CHECK_EQUAL( rat(1, 3) * rat(-3, 4), rat(-1, 4) );
     BOOST_CHECK_EQUAL( rat(2, 5) * 7,          rat(14, 5) );
     BOOST_CHECK_EQUAL(        -2 * rat(1, 6),  rat(-1, 3) );
-    r0 = rat(3,7);
-    r0 *= 14;
-    BOOST_CHECK_EQUAL( r0, 6 );
-    r0 *= rat(3,8);
-    BOOST_CHECK_EQUAL( r0, rat(9, 4) );
 
-    /* division tests */
+    rat  r = rat( 3, 7 );
+
+    r *= 14;
+    BOOST_CHECK_EQUAL( r, 6 );
+
+    r *= rat( 3, 8 );
+    BOOST_CHECK_EQUAL( r, rat(9, 4) );
+}
+
+// Division tests
+BOOST_AUTO_TEST_CASE( rational_division_test )
+{
     BOOST_CHECK_EQUAL( rat(-1, 20) / rat(4, 5), rat(-1, 16) );
     BOOST_CHECK_EQUAL( rat( 5,  6) / 7,         rat( 5, 42) );
     BOOST_CHECK_EQUAL(           8 / rat(2, 7), 28          );
-    r0 = rat(4,3);
-    r0 /= rat(5,4);
-    BOOST_CHECK_EQUAL( r0, rat(16, 15) );
-    r0 /= 4;
-    BOOST_CHECK_EQUAL( r0, rat(4, 15) );
+
+    rat  r = rat( 4, 3 );
+
+    r /= rat(5,4);
+    BOOST_CHECK_EQUAL( r, rat(16, 15) );
+
+    r /= 4;
+    BOOST_CHECK_EQUAL( r, rat( 4, 15) );
+
     BOOST_CHECK_EQUAL( rat(-1) / rat(-3), rat(1, 3) );
-
-    /* tests for operations on self */
-    r0 = rat(4,3);
-    r0 += r0;
-    BOOST_CHECK_EQUAL( r0, rat(8, 3) );
-    r0 *= r0;
-    BOOST_CHECK_EQUAL( r0, rat(64, 9) );
-    r0 /= r0;
-    BOOST_CHECK_EQUAL( r0, rat(1, 1) );
-    r0 -= r0;
-    BOOST_CHECK_EQUAL( r0, rat(0, 1) );
-
-    /* operator<< and operator>> tests */
-    std::ostringstream oss;
-    
-    oss << rat(44,14);
-    BOOST_CHECK_EQUAL( oss.str(), "22/7" );
-    typedef std::istringstream input_string_stream;
-    {
-        input_string_stream iss("");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("42");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("57A");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("20-20");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("1/");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("1/ 2");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        input_string_stream iss("1 /2");
-        iss >> r0;
-        BOOST_CHECK( !iss );
-    }
-    {
-        int n;
-        input_string_stream iss("1/2 12");
-        BOOST_CHECK( iss >> r0 >> n );
-        BOOST_CHECK_EQUAL( r0, rat(1, 2) );
-        BOOST_CHECK_EQUAL( n, 12 );
-    }
-    {
-        input_string_stream iss("34/67");
-        BOOST_CHECK( iss >> r0 );
-        BOOST_CHECK_EQUAL( r0, rat(34, 67) );
-    }
-    {
-        input_string_stream iss("-3/-6");
-        BOOST_CHECK( iss >> r0 );
-        BOOST_CHECK_EQUAL( r0, rat(1, 2) );
-    }
-
-    /* rational cast tests */
-    /* Note that these are not generic. The problem is that rational_cast<T>
-     * requires a conversion from IntType to T. However, for a user-defined
-     * IntType, it is not possible to define such a conversion except as
-     * an operator T(). This causes problems with overloading resolution.
-     */
-    {
-        boost::rational<int> half(1,2);
-        BOOST_CHECK_EQUAL( boost::rational_cast<double>(half), 0.5 );
-        BOOST_CHECK_EQUAL( boost::rational_cast<int>(half), 0 );
-    }
-
-    /* End of main tests.  */
-
-    /* mean number of times a fair 6-sided die must be thrown
-       until each side has appeared at least once */
-    r0 = IntType(0);
-    for (int i=1; i<=6; ++i)
-        r0 += rat(1,i);
-    r0 *= 6;
-    BOOST_CHECK_EQUAL( r0, rat(147, 10) );
 }
+
+// Tests for operations on self
+BOOST_AUTO_TEST_CASE( rational_self_operations_test )
+{
+    rat  r = rat( 4, 3 );
+
+    r += r;
+    BOOST_CHECK_EQUAL( r, rat( 8, 3) );
+
+    r *= r;
+    BOOST_CHECK_EQUAL( r, rat(64, 9) );
+
+    r /= r;
+    BOOST_CHECK_EQUAL( r, rat( 1, 1) );
+
+    r -= r;
+    BOOST_CHECK_EQUAL( r, rat( 0, 1) );
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+
+// The non-basic rational operations suite
+BOOST_FIXTURE_TEST_SUITE( rational_extras_suite, my_configuration );
+
+// Output test
+BOOST_AUTO_TEST_CASE( rational_output_test )
+{
+    ::std::ostringstream  oss;
+    
+    oss << rat( 44, 14 );
+    BOOST_CHECK_EQUAL( oss.str(), "22/7" );
+}
+
+// Input test, failing
+BOOST_AUTO_TEST_CASE( rational_input_failing_test )
+{
+    ::std::istringstream  iss( "" );
+    rat                   r;
+
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "42" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "57A" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "20-20" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "1/" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "1/ 2" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+
+    iss.clear();
+    iss.str( "1 /2" );
+    iss >> r;
+    BOOST_CHECK( !iss );
+}
+
+// Input test, passing
+BOOST_AUTO_TEST_CASE( rational_input_passing_test )
+{
+    ::std::istringstream  iss( "1/2 12" );
+    rat                   r;
+    int                   n = 0;
+
+    BOOST_CHECK( iss >> r >> n );
+    BOOST_CHECK_EQUAL( r, rat(1, 2) );
+    BOOST_CHECK_EQUAL( n, 12 );
+
+    iss.clear();
+    iss.str( "34/67" );
+    BOOST_CHECK( iss >> r );
+    BOOST_CHECK_EQUAL( r, rat(34, 67) );
+
+    iss.clear();
+    iss.str( "-3/-6" );
+    BOOST_CHECK( iss >> r );
+    BOOST_CHECK_EQUAL( r, rat(1, 2) );
+}
+
+// Conversion test
+BOOST_AUTO_TEST_CASE( rational_cast_test )
+{
+    // Note that these are not generic.  The problem is that rational_cast<T>
+    // requires a conversion from IntType to T.  However, for a user-defined
+    // IntType, it is not possible to define such a conversion except as an
+    // "operator T()".  This causes problems with overloading resolution.
+    ::boost::rational<int> const  half( 1, 2 );
+
+    BOOST_CHECK_EQUAL( ::boost::rational_cast<double>(half), 0.5 );
+    BOOST_CHECK_EQUAL( ::boost::rational_cast<int>(half), 0 );
+}
+
+// Dice tests (a non-main test)
+BOOST_AUTO_TEST_CASE( dice_roll_test )
+{
+    // Determine the mean number of times a fair six-sided die
+    // must be thrown until each side has appeared at least once.
+    rat  r = IntType( 0 );
+
+    for ( int  i = 1 ; i <= 6 ; ++i )
+    {
+        r += rat( 1, i );
+    }
+    r *= 6;
+
+    BOOST_CHECK_EQUAL( r, rat(147, 10) );
+}
+
+BOOST_AUTO_TEST_SUITE_END();
 
 } // namespace
 
