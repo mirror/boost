62bbc3d27472cf1494ead90c6d897ce128de3a57
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2005-12-30 02:27:13 +0000

    BOOST_CRITICAL_ERROR is no longer usable
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32185 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/function/test/function_test_fail1.cpp
+++ boost/libs/function/test/function_test_fail1.cpp
@@ -1,6 +1,6 @@
 // Boost.Function library
 
-//  Copyright Douglas Gregor 2001-2003. Use, modification and
+//  Copyright (C) Douglas Gregor 2001-2005. Use, modification and
 //  distribution is subject to the Boost Software License, Version
 //  1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
@@ -22,7 +22,7 @@ test_main(int, char*[])
   if (f1 == f2) {
   }
 
-  BOOST_CRITICAL_ERROR("This should not have compiled.");
+  BOOST_ERROR("This should not have compiled.");
 
   return 0;
 }
--- boost/libs/function/test/function_test_fail2.cpp
+++ boost/libs/function/test/function_test_fail2.cpp
@@ -1,6 +1,6 @@
 // Boost.Function library
 
-//  Copyright Douglas Gregor 2001-2003. Use, modification and
+//  Copyright (C) Douglas Gregor 2001-2005. Use, modification and
 //  distribution is subject to the Boost Software License, Version
 //  1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
@@ -21,7 +21,7 @@ test_main(int, char*[])
   function0<int> f1;
   f1 = bad_fn;
 
-  BOOST_CRITICAL_ERROR("This should not have compiled.");
+  BOOST_ERROR("This should not have compiled.");
 
   return 0;
 }
