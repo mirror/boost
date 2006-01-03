11e11ac910204dc941e852be499465572d310c08
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-01-03 19:16:19 +0000

    add a test for a simple grammar
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32215 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/xpressive/test/misc1.cpp
+++ boost/libs/xpressive/test/misc1.cpp
@@ -43,5 +43,98 @@ int test_main( int, char*[] )
     BOOST_CHECK(what(word, 5)[0] == "maD");
     BOOST_CHECK(what(word, 6)[0] == "world");
 
+    // test for a simple regex grammar
+    std::string buffer =
+        "FROGGIE\r\n"
+        "Volume = 1\r\n"
+        "Other1= 2\r\n"
+        "Channel=3\r\n"
+        "Other =4\r\n"
+        "\r\n"
+        "FROGGIE\r\n"
+        "Volume = 5\r\n"
+        "Other1= 6\r\n"
+        "Channel=7\r\n"
+        "Other =8\r\n"
+        "\r\n"
+        "FROGGIE\r\n"
+        "Volume = 9\r\n"
+        "Other1= 0\r\n"
+        "Channel=10\r\n"
+        "\r\n";
+
+    mark_tag name(1), value(2);
+
+    sregex name_value_pair_ =
+        (name= +alnum) >> *_s >> "=" >> *_s >>
+        (value= +_d) >> *_s >> _ln;
+
+    sregex message_ =
+        *_s >> "FROGGIE" >> _ln >> +name_value_pair_ >> _ln;
+
+    sregex re_ = +message_;
+
+    typedef smatch::nested_results_type::const_iterator nested_iterator;
+    nested_iterator msg, nvp;
+
+    BOOST_REQUIRE(regex_search(buffer, what, re_));
+    BOOST_REQUIRE(3 == what.nested_results().size());
+
+    msg = what.nested_results().begin();
+    BOOST_REQUIRE(4 == msg->nested_results().size());
+
+    nvp = msg->nested_results().begin();
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Volume" == (*nvp)[name]);
+    BOOST_CHECK("1" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Other1" == (*nvp)[name]);
+    BOOST_CHECK("2" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Channel" == (*nvp)[name]);
+    BOOST_CHECK("3" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Other" == (*nvp)[name]);
+    BOOST_CHECK("4" == (*nvp)[value]);
+
+    ++msg;
+    BOOST_REQUIRE(4 == msg->nested_results().size());
+
+    nvp = msg->nested_results().begin();
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Volume" == (*nvp)[name]);
+    BOOST_CHECK("5" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Other1" == (*nvp)[name]);
+    BOOST_CHECK("6" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Channel" == (*nvp)[name]);
+    BOOST_CHECK("7" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Other" == (*nvp)[name]);
+    BOOST_CHECK("8" == (*nvp)[value]);
+
+    ++msg;
+    BOOST_REQUIRE(3 == msg->nested_results().size());
+
+    nvp = msg->nested_results().begin();
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Volume" == (*nvp)[name]);
+    BOOST_CHECK("9" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Other1" == (*nvp)[name]);
+    BOOST_CHECK("0" == (*nvp)[value]);
+    ++nvp;
+    BOOST_REQUIRE(3 == nvp->size());
+    BOOST_CHECK("Channel" == (*nvp)[name]);
+    BOOST_CHECK("10" == (*nvp)[value]);
+
     return 0;
 }
