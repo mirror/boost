0788c7b1a3245526d55269803629d8172dfacee3
vertleyb <vertleyb@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-03 01:42:21 +0000

    undo last commit
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32521 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/typeof/test/function_ptr_from_tpl.cpp
+++ boost/libs/typeof/test/function_ptr_from_tpl.cpp
@@ -1,27 +1,4 @@
-//#undef BOOST_TYPEOF_COMPLIANT
 #include <boost/typeof/typeof.hpp>
-
-#include <boost/utility/enable_if.hpp>
-#include <boost/type_traits/function_traits.hpp>
-
-namespace boost { namespace type_of {
-
-#ifdef BOOST_TYPEOF_EMULATION
-
-    template<class T> 
-    typename enable_if<
-        is_function<T>, 
-        sizer<typename encode_type<BOOST_TYPEOF_VECTOR(0)<>, T*>::type>
-    >::type encode(T*);
-
-#else
-
-    template<typename T>
-    char (*encode_start(T*))[encode_type<T*>::value];
-
-#endif
-
-}}
 
 void f()
 {}
@@ -29,5 +6,5 @@ void f()
 template<class T> 
 class x
 {
-   typedef BOOST_TYPEOF(&::f) type;
+   typedef BOOST_TYPEOF(&f) type;
 };
