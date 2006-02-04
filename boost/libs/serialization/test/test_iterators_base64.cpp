ec6f21742332ed043338eab04f97e5ac03d25f9e
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-04 22:16:01 +0000

    fix msvc version
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32568 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/test/test_iterators_base64.cpp
+++ boost/libs/serialization/test/test_iterators_base64.cpp
@@ -9,7 +9,7 @@
 #include <algorithm>
 #include <list>
 
-#if (defined _MSC_VER) && (_MSC_VER < 1300)
+#if (defined _MSC_VER) && (_MSC_VER == 1200)
 #  pragma warning (disable : 4786) // too long name, harmless warning
 #endif
 
