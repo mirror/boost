7080b5aba9446050a16f53e67cb515a4082cccdf
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-05 14:02:28 +0000

    Add copyright to container_fwd_test.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32580 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/functional/hash/test/container_fwd_test.cpp
+++ boost/libs/functional/hash/test/container_fwd_test.cpp
@@ -1,3 +1,8 @@
+
+//  Copyright Daniel James 2005. Use, modification, and distribution are
+//  subject to the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/functional/detail/container_fwd.hpp>
 
 #if BOOST_WORKAROUND(__GNUC__, < 3) && !defined(__SGI_STL_PORT) && !defined(_STLPORT_VERSION)
