1cf87ab949539c685447d934e17c746b21a4d30d
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-08 01:35:57 +0000

    verify that we're getting the right answers for is_lightweight_proxy
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32717 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/foreach/test/cstr_byval.cpp
+++ boost/libs/foreach/test/cstr_byval.cpp
@@ -33,6 +33,8 @@ char const *my_const_ntcs  = my_ntcs;
 //   
 int test_main( int, char*[] )
 {
+    boost::mpl::true_ *p = BOOST_FOREACH_IS_LIGHTWEIGHT_PROXY(my_ntcs);
+
     // non-const containers by value
     BOOST_CHECK(sequence_equal_byval_n(my_ntcs, "\1\2\3\4\5"));
 
--- boost/libs/foreach/test/stl_byval.cpp
+++ boost/libs/foreach/test/stl_byval.cpp
@@ -48,6 +48,8 @@ std::list<int> const &my_const_list = my_list;
 //   
 int test_main( int, char*[] )
 {
+    boost::mpl::false_ *p = BOOST_FOREACH_IS_LIGHTWEIGHT_PROXY(my_list);
+
     // non-const containers by value
     BOOST_CHECK(sequence_equal_byval_n(my_list, "\1\2\3\4\5"));
 
