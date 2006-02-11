7e7e5fe8599dc8e6bc26bb1dea4af5ddf2d59e41
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-11 19:08:25 +0000

    Use ~Functor instead of ~function_type always, since it makes Borland and vc6 and who knows what else happy.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@32832 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/function/function_base.hpp
+++ boost/boost/function/function_base.hpp
@@ -286,11 +286,8 @@ namespace boost {
           } else if (op == destroy_functor_tag) {
             functor_type* out_functor = 
               reinterpret_cast<functor_type*>(&out_buffer.data);
-#if BOOST_WORKAROUND( __BORLANDC__, BOOST_TESTED_AT(0x581) )
+            // Some compilers (Borland, vc6, ...) are unhappy with ~functor_type.
             out_functor->~Functor();
-#else
-            out_functor->~functor_type();
-#endif
           } else /* op == check_functor_type_tag */ {
             const std::type_info& check_type = 
               *static_cast<const std::type_info*>(out_buffer.const_obj_ptr);
