823e0a6f8a462fe15cbcb0eaee3be51e8f5e7c27
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-02-26 16:09:27 +0000

    *** empty log message ***
    git-svn-id: http://svn.boost.org/svn/boost/trunk@33116 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/statechart/test/RunTests.bat
+++ boost/libs/statechart/test/RunTests.bat
@@ -1,1 +1,1 @@
-bjam "-sTOOLS=mingw vc-7_1" "-sMINGW_ROOT_DIRECTORY=C:\ProgramFiles\MinGW3.2.0" "-sINTEL_PATH=C:\Program Files\Intel\Compiler\C++\9.0\IA32" "-sINTEL_BASE_MSVC_TOOLSET=vc-7_1" %1 %2 %3 %4
+bjam "-sTOOLS=mingw vc-7_1" "-sMINGW_ROOT_DIRECTORY=C:\ProgramFiles\MinGW3.2.0" "-sINTEL_PATH=C:\Program Files\Intel\Compiler\C++\9.0\IA32" "-sINTEL_BASE_MSVC_TOOLSET=vc-7_1" %1 %2 %3 %4 >log.txt 2>&1
