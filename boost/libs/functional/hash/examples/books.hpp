7da6d4d4d1b03bcd643688dcdc40c9fa39771738
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-03-18 15:49:21 +0000

    Update the copyright comments in the hash tests and documentation.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@33369 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/functional/hash/examples/books.hpp
+++ boost/libs/functional/hash/examples/books.hpp
@@ -1,5 +1,5 @@
 
-//  Copyright Daniel James 2005. Use, modification, and distribution are
+//  Copyright Daniel James 2005-2006. Use, modification, and distribution are
 //  subject to the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
