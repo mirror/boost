512284ab4a269ca4874c5a67e454c1e2411f87de
troyer <troyer@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-05-01 00:06:32 +0000

    Added array-optimized binary archives
    git-svn-id: http://svn.boost.org/svn/boost/trunk@33881 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/src/array_binary_oarchive.cpp
+++ boost/libs/serialization/src/array_binary_oarchive.cpp
@@ -1,5 +1,5 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// polymorphic_oarchive.cpp:
+// binary_oarchive.cpp:
 
 // (C) Copyright 2002 Robert Ramey - http://www.rrsd.com . 
 // Use, modification and distribution is subject to the Boost Software
@@ -8,22 +8,31 @@
 
 //  See http://www.boost.org for updates, documentation, and revision history.
 
-#if (defined _MSC_VER) && (_MSC_VER == 1200)
-#  pragma warning (disable : 4786) // too long name, harmless warning
-#endif
-
 #include <ostream>
 
 #define BOOST_ARCHIVE_SOURCE
-#include <boost/archive/polymorphic_oarchive.hpp>
+#include <boost/archive/array/binary_oarchive.hpp>
 
-// explicitly instantiate for this type of text stream
+#include <boost/archive/impl/basic_binary_oprimitive.ipp>
+#include <boost/archive/impl/basic_binary_oarchive.ipp>
 #include <boost/archive/impl/archive_pointer_oserializer.ipp>
 
 namespace boost {
 namespace archive {
 
-template class detail::archive_pointer_oserializer<polymorphic_oarchive> ;
+// explicitly instantiate for this type of binary stream
+template class basic_binary_oprimitive<
+    array::binary_oarchive, 
+    char, 
+    std::char_traits<char> 
+>;
+template class basic_binary_oarchive<array::binary_oarchive> ;
+template class binary_oarchive_impl<
+    array::binary_oarchive, 
+    char, 
+    std::char_traits<char> 
+>;
+template class detail::archive_pointer_oserializer<array::binary_oarchive> ;
 
 } // namespace archive
 } // namespace boost
