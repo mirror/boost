f5be423b80ae69caaace4f763583565faa18ad8d
david_abrahams <david_abrahams@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-05-01 19:45:04 +0000

    Added legibility.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@33895 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/concept/detail/concept_def.hpp
+++ boost/boost/concept/detail/concept_def.hpp
@@ -3,9 +3,10 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_CONCEPT_DETAIL_CONCEPT_DEF_DWA200651_HPP
 # define BOOST_CONCEPT_DETAIL_CONCEPT_DEF_DWA200651_HPP
-# include <boost/preprocessor/comma_if.hpp>
 # include <boost/preprocessor/seq/for_each_i.hpp>
 # include <boost/preprocessor/seq/enum.hpp>
+# include <boost/preprocessor/comma_if.hpp>
+# include <boost/preprocessor/cat.hpp>
 #endif // BOOST_CONCEPT_DETAIL_CONCEPT_DEF_DWA200651_HPP
 
 // BOOST_concept(SomeName, (p1)(p2)...(pN))
@@ -14,16 +15,21 @@
 //
 // Also defines an equivalent SomeNameConcept for backward compatibility.
 // Maybe in the next release we can kill off the "Concept" suffix for good.
-# define BOOST_CONCEPT_typename(r, ignored, index, t) \
-    BOOST_PP_COMMA_IF(index) typename t
-
 # define BOOST_concept(name, params)                                            \
-    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename, ~, params) >     \
-    struct name;                                                                \
-    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename, ~, params) >     \
-    struct BOOST_PP_CAT(name,Concept) : name< BOOST_PP_SEQ_ENUM(params) >       \
-    { BOOST_PP_CAT(name,Concept)(); }; /* ctor needed to satisfy gcc-3.4.4 */   \
-    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename, ~, params) >     \
+    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename,~,params) >       \
+    struct name; /* forward decl */                                             \
+                                                                                \
+    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename,~,params) >       \
+    struct BOOST_PP_CAT(name,Concept)                                           \
+      : name< BOOST_PP_SEQ_ENUM(params) >                                       \
+    {                                                                           \
+        BOOST_PP_CAT(name,Concept)(); /* ctor needed to satisfy gcc-3.4.4 */    \
+    };                                                                          \
+                                                                                \
+    template < BOOST_PP_SEQ_FOR_EACH_I(BOOST_CONCEPT_typename,~,params) >       \
     struct name                                                                
     
+// Helper for BOOST_concept, above.
+# define BOOST_CONCEPT_typename(r, ignored, index, t) \
+    BOOST_PP_COMMA_IF(index) typename t
 
