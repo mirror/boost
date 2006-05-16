36a4c5aff9458444b900d2c2116d13f54619a3d5
t_schwinger <t_schwinger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-05-16 13:37:14 +0000

    introduces test case for is_sequence< int<0> >
    git-svn-id: http://svn.boost.org/svn/boost/trunk@33979 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/mpl/test/is_sequence.cpp
+++ boost/libs/mpl/test/is_sequence.cpp
@@ -12,6 +12,7 @@
 // $Revision$
 
 #include <boost/mpl/is_sequence.hpp>
+#include <boost/mpl/int.hpp>
 #include <boost/mpl/list.hpp>
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/range_c.hpp>
@@ -25,6 +26,7 @@ template< typename T > struct std_vector
 MPL_TEST_CASE()
 {
     MPL_ASSERT_NOT(( is_sequence< std_vector<int> > ));
+    MPL_ASSERT_NOT(( is_sequence< int_<0> > ));
     MPL_ASSERT_NOT(( is_sequence< int > ));
     MPL_ASSERT_NOT(( is_sequence< int& > ));
     MPL_ASSERT_NOT(( is_sequence< UDT > ));
