c070c890e5c18d837ed3040f6a96079b2898261c
joaquintides <joaquintides@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-06-19 06:10:23 +0000

    added testcases for SourceForge bugs #1450260 and #1506914
    git-svn-id: http://svn.boost.org/svn/boost/trunk@34351 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/format/test/format_test3.cpp
+++ boost/libs/format/test/format_test3.cpp
@@ -100,5 +100,14 @@ int test_main(int, char* [])
     f.bind_arg(1, x);
     f.clear();
 
+    // testcase for bug reported at
+    // http://lists.boost.org/boost-users/2005/11/15454.php
+    std::string l_param;
+    std::string l_str = (boost::format("here is an empty string: %1%") % l_param).str(); 
+
+    // testcase for SourceForge bug #1506914
+    std::string arg; // empty string  
+    s = str(format("%=8s") % arg);
+
     return 0;
 }
