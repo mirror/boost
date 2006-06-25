3058bb9f8b0e8eb430f2119f320d6a6eced54cce
gennaro_prota <gennaro_prota@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-06-25 19:05:46 +0000

    updated copyright notice (years); added slash to doc url; introduced shifter<> template, for the implementation of the constructor from ulong
    git-svn-id: http://svn.boost.org/svn/boost/trunk@34396 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/dynamic_bitset.hpp
+++ boost/boost/detail/dynamic_bitset.hpp
@@ -1,7 +1,7 @@
 // --------------------------------------------------
 //
 // (C) Copyright Chuck Allison and Jeremy Siek 2001 - 2002.
-// (C) Copyright Gennaro Prota                 2003 - 2004.
+// (C) Copyright Gennaro Prota                 2003 - 2006.
 //
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
@@ -9,7 +9,7 @@
 //
 // -----------------------------------------------------------
 
-//  See http://www.boost.org/libs/dynamic_bitset for documentation.
+//  See http://www.boost.org/libs/dynamic_bitset/ for documentation.
 
 
 #ifndef BOOST_DETAIL_DYNAMIC_BITSET_HPP
@@ -35,6 +35,14 @@ namespace boost {
         return static_cast<const unsigned char *>(static_cast<const void *>(p));
     }
 
+    template<typename T, int amount, int width /* = default */>
+    struct shifter
+    {
+        static void left_shift(T & v) {
+            amount >= width ? (v = 0)
+                : (v >>= BOOST_DYNAMIC_BITSET_WRAP_CONSTANT(amount));
+        }
+    };
 
     // ------- count function implementation --------------
 
