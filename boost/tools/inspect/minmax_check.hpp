1fff51b0cfe77434fbf28b23551249170516de2f
gennaro_prota <gennaro_prota@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-07-29 15:20:27 +0000

    Inspect Tool Update:
    * slightly changed desc() message, in consequence of the tightened min/max checks
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@34772 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/tools/inspect/minmax_check.hpp
+++ boost/tools/inspect/minmax_check.hpp
@@ -1,7 +1,9 @@
 //  minmax_check header  -------------------------------------------------------//
 
-//  Copyright Beman Dawes 2002
-//  Copyright Rene Rivera 2004.
+//  Copyright Beman Dawes   2002
+//  Copyright Rene Rivera   2004.
+//  Copyright Gennaro Prota 2006.
+//
 //  Distributed under the Boost Software License, Version 1.0.
 //  (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
@@ -23,7 +25,9 @@ namespace boost
 
       minmax_check();
       virtual const char * name() const { return "*M*"; }
-      virtual const char * desc() const { return "uses of min and max that have not been protected from the min/max macros"; }
+      virtual const char * desc() const { return "uses of min and max that"
+          " have not been protected from the min/max macros"
+          " or unallowed #undef-s"; }
 
       virtual void inspect(
         const std::string & library_name,
