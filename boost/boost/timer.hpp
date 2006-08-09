cecb5a421f7733d6e8ea87e5a174f1c4f354c15a
gennaro_prota <gennaro_prota@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-08-09 20:22:01 +0000

    revert
    git-svn-id: http://svn.boost.org/svn/boost/trunk@34858 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/timer.hpp
+++ boost/boost/timer.hpp
@@ -4,10 +4,9 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-//  See http://www.boost.org/libs/timer/ for documentation.
+//  See http://www.boost.org/libs/timer for documentation.
 
 //  Revision History
-//  07 Jun 06  Introduced exception class for std::clock() failures (G. Prota)
 //  01 Apr 01  Modified to use new <boost/limits.hpp> header. (JMaddock)
 //  12 Jan 01  Change to inline implementation to allow use without library
 //             builds. See docs for more rationale. (Beman Dawes) 
@@ -18,9 +17,8 @@
 #ifndef BOOST_TIMER_HPP
 #define BOOST_TIMER_HPP
 
-#include <ctime>
-#include <exception>
 #include <boost/config.hpp>
+#include <ctime>
 #include <boost/limits.hpp>
 
 # ifdef BOOST_NO_STDC_NAMESPACE
@@ -46,32 +44,11 @@ namespace boost {
 class timer
 {
  public:
-
-    // exception class to report construction or restart failure
-    //
-    class availability_error : public std::exception
-    {
-     public:
-      availability_error() throw() {}
-      availability_error(const availability_error&) throw() {}
-      availability_error& operator=(const availability_error&) throw() { return *this; }
-      virtual const char* what() const throw()
-        { return static_cast<const char*>("boost:timer cannot start"); }
-      virtual ~availability_error() throw() {}
-    };
-
-
-  timer() { restart(); } // postcondition: elapsed()==0
-
+         timer() { _start_time = std::clock(); } // postcondition: elapsed()==0
 //         timer( const timer& src );      // post: elapsed()==src.elapsed()
 //        ~timer(){}
 //  timer& operator=( const timer& src );  // post: elapsed()==src.elapsed()
-
-  void restart()                           // post: elapsed()==0
-    {
-      if ((_start_time = std::clock()) == ((std::clock_t) -1))
-        throw availability_error();
-    }
+  void   restart() { _start_time = std::clock(); } // post: elapsed()==0
   double elapsed() const                  // return elapsed time in seconds
     { return  double(std::clock() - _start_time) / CLOCKS_PER_SEC; }
 
@@ -80,7 +57,7 @@ class timer
   // where std::clock_t overflows or resets at surprising values.
   {
     return (double((std::numeric_limits<std::clock_t>::max)())
-       - double(_start_time)) / double(CLOCKS_PER_SEC);
+       - double(_start_time)) / double(CLOCKS_PER_SEC); 
   }
 
   double elapsed_min() const            // return minimum value for elapsed()
@@ -88,7 +65,6 @@ class timer
 
  private:
   std::clock_t _start_time;
-
 }; // timer
 
 } // namespace boost
