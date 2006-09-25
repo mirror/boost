f9e83076ec4cc1e9cfa99a0e854563103c2d2094
schoepflin <schoepflin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-09-25 10:40:35 +0000

    Python header must be included first.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@35319 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/parameter/test/python_test.cpp
+++ boost/libs/parameter/test/python_test.cpp
@@ -2,10 +2,10 @@
 // subject to the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/python.hpp>
 #include <boost/parameter/preprocessor.hpp>
 #include <boost/parameter/keyword.hpp>
 #include <boost/parameter/python.hpp>
-#include <boost/python.hpp>
 #include <boost/utility/enable_if.hpp>
 
 namespace test {
