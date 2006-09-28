cda4c4ca91617968ef5548d199316574d08fa72a
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-09-28 17:08:38 +0000

    add is_abstract include
    git-svn-id: http://svn.boost.org/svn/boost/trunk@35387 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/serialization/example/demo.cpp
+++ boost/libs/serialization/example/demo.cpp
@@ -20,6 +20,7 @@
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/list.hpp>
+#include <boost/serialization/is_abstract.hpp>
 
 /////////////////////////////////////////////////////////////
 // The intent of this program is to serve as a tutorial for
