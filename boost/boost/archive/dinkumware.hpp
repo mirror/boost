070d392ea47477cb834866fdadbdcc78e66d5f77
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-09-28 16:32:17 +0000

    better support for dinkumware
    git-svn-id: http://svn.boost.org/svn/boost/trunk@35369 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/archive/dinkumware.hpp
+++ boost/boost/archive/dinkumware.hpp
@@ -27,12 +27,6 @@
 
 namespace std {
 
-// and this native STL lib is old Dinkumware (has not defined _CPPLIB_VER)
-//#if (defined _STLPORT_VERSION) \
-//&& !(defined _STLP_USE_NO_IOSTREAMS) \
-//&& (defined _YVALS)  \
-//&& (defined __IBMCPP__)
-
 // define i/o operators for 64 bit integers
 template<class CharType>
 basic_ostream<CharType> & 
