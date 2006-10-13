5d247493279a99a9d38ad3850eadd75176bd77dd
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-10-13 12:40:40 +0000

    Added 64-bit Intel processor support.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@35587 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/endian.hpp
+++ boost/boost/detail/endian.hpp
@@ -47,7 +47,10 @@
 #elif defined(__i386__) || defined(__alpha__) \
    || defined(__ia64) || defined(__ia64__) \
    || defined(_M_IX86) || defined(_M_IA64) \
-   || defined(_M_ALPHA)
+   || defined(_M_ALPHA) || defined(__amd64) \
+   || defined(__amd64__) || defined(_M_AMD64) \
+   || defined(__x86_64) || defined(__x86_64__) \
+   || defined(_M_X64)
 # define BOOST_LITTLE_ENDIAN
 # define BOOST_BYTE_ORDER 1234
 #else
