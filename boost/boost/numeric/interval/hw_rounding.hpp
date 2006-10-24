fe33fe0d327255c8d9304decc8bf30dd4bdee8be
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-10-24 17:51:38 +0000

    Added HP-UX and itanium support.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@35725 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/numeric/interval/hw_rounding.hpp
+++ boost/boost/numeric/interval/hw_rounding.hpp
@@ -25,6 +25,8 @@
 #  include <boost/numeric/interval/detail/sparc_rounding_control.hpp>
 #elif defined(alpha) || defined(__alpha__)
 #  include <boost/numeric/interval/detail/alpha_rounding_control.hpp>
+#elif defined(ia64) || defined(__ia64) || defined(__ia64__)
+#  include <boost/numeric/interval/detail/ia64_rounding_control.hpp>
 #endif
 
 #if defined(BOOST_NUMERIC_INTERVAL_NO_HARDWARE) && (defined(__USE_ISOC99) || defined(__MSL__))
