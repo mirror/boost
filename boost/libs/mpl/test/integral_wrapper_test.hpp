432a681f4ed9f1736b47a0c89712653d72c8b1e2
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-11-20 18:02:17 +0000

    Merge Borland 5.8.2 fixes from RC_1_34_0
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36117 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/mpl/test/integral_wrapper_test.hpp
+++ boost/libs/mpl/test/integral_wrapper_test.hpp
@@ -1,5 +1,5 @@
 
-// Copyright Aleksey Gurtovoy 2001-2004
+// Copyright Aleksey Gurtovoy 2001-2006
 //
 // Distributed under the Boost Software License, Version 1.0. 
 // (See accompanying file LICENSE_1_0.txt or copy at 
@@ -28,6 +28,21 @@
     /**/
 #endif
 
+#if BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x582))
+// agurt 20/nov/06: see http://article.gmane.org/gmane.comp.lib.boost.devel/151065
+#define INTEGRAL_WRAPPER_TEST(unused1, i, T) \
+    { \
+      typedef WRAPPER(T,i) borland_tested_type; \
+    { typedef is_same< borland_tested_type::value_type, T > borland_test_type; \
+      MPL_ASSERT(( borland_test_type )); } \
+    { MPL_ASSERT(( is_same< borland_tested_type::type, WRAPPER(T,i) > )); } \
+    { MPL_ASSERT(( is_same< next< borland_tested_type >::type, WRAPPER(T,i+1) > )); } \
+    { MPL_ASSERT(( is_same< prior< borland_tested_type >::type, WRAPPER(T,i-1) > )); } \
+    { MPL_ASSERT_RELATION( (borland_tested_type::value), ==, i ); } \
+    INTEGRAL_WRAPPER_RUNTIME_TEST(i, T) \
+    } \
+/**/
+#else
 #define INTEGRAL_WRAPPER_TEST(unused1, i, T) \
     { MPL_ASSERT(( is_same< WRAPPER(T,i)::value_type, T > )); } \
     { MPL_ASSERT(( is_same< WRAPPER(T,i)::type, WRAPPER(T,i) > )); } \
@@ -36,3 +51,5 @@
     { MPL_ASSERT_RELATION( (WRAPPER(T,i)::value), ==, i ); } \
     INTEGRAL_WRAPPER_RUNTIME_TEST(i, T) \
 /**/
+#endif
+
