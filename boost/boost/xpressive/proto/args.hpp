25c07ee4eb0f80f7886ef8b9e2b8e65162e65e85
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-11-22 17:32:22 +0000

    add missing file
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36150 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/xpressive/proto/args.hpp
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file args.hpp
+/// Contains definition of args\<\> class template.
+//
+//  Copyright 2004 Eric Niebler. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_PP_IS_ITERATING
+    #ifndef BOOST_PROTO_ARGS_HPP_EAN_04_01_2005
+    #define BOOST_PROTO_ARGS_HPP_EAN_04_01_2005
+
+    #include <boost/mpl/void.hpp>
+    #include <boost/preprocessor/cat.hpp>
+    #include <boost/preprocessor/iterate.hpp>
+    #include <boost/preprocessor/repetition/enum_params.hpp>
+    #include <boost/preprocessor/repetition/repeat.hpp>
+    #include <boost/preprocessor/repetition/repeat_from_to.hpp>
+    #include <boost/xpressive/proto/proto_fwd.hpp>
+
+    namespace boost { namespace proto
+    {
+    #define BOOST_PROTO_DEFINE_ARG_N(z, n, data)\
+        typedef BOOST_PP_CAT(Arg, n) BOOST_PP_CAT(arg, n);
+
+    #define BOOST_PROTO_DEFINE_VOID_N(z, n, data)\
+        typedef mpl::void_ BOOST_PP_CAT(arg, n);
+
+    #define BOOST_PP_ITERATION_PARAMS_1 (3, (1, BOOST_PROTO_MAX_ARITY, <boost/xpressive/proto/args.hpp>))
+    #include BOOST_PP_ITERATE()
+    #undef BOOST_PP_ITERATION_PARAMS_1
+
+    #undef BOOST_PROTO_DEFINE_ARG_N
+    #undef BOOST_PROTO_DEFINE_VOID_N
+
+    }}
+    #endif
+
+#else
+
+    #define N BOOST_PP_ITERATION()
+
+        template< BOOST_PP_ENUM_PARAMS(N, typename Arg) >
+        struct BOOST_PP_CAT(args, N)
+        {
+            BOOST_STATIC_CONSTANT(long, size = N);
+            BOOST_PP_REPEAT(N, BOOST_PROTO_DEFINE_ARG_N, ~)
+            BOOST_PP_REPEAT_FROM_TO(N, BOOST_PROTO_MAX_ARITY, BOOST_PROTO_DEFINE_VOID_N, ~)
+        };
+
+    #undef N
+
+#endif
