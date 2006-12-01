fa4ead8c69a888b3a42d95054135bc6217fbd435
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-12-01 14:40:09 +0000

    Fixed license & copyright issues and converted to HTML 4.01
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36232 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/format/benchmark/bench_format.cpp
+++ boost/libs/format/benchmark/bench_format.cpp
@@ -2,7 +2,7 @@
 //  Boost general library 'format'  ---------------------------
 //  See http://www.boost.org for updates, documentation, and revision history.
 
-//  (C) Samuel Krempp 2001
+//  (C) Copyright Samuel Krempp 2001
 //                  krempp@crans.ens-cachan.fr
 //  Distributed under the Boost Software License, Version 1.0. (See
 //  accompanying file LICENSE_1_0.txt or copy at
