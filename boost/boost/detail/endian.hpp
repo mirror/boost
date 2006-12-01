8f6d6f68663253403947d3889a92bffe7bc99e56
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-12-01 14:27:03 +0000

    Added Caleb as copyright owner.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36230 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/detail/endian.hpp
+++ boost/boost/detail/endian.hpp
@@ -1,4 +1,5 @@
-// (c) Copyright John Maddock 2001
+// (c) Copyright Caleb Epstein 2005
+// (c) Copyright John Maddock 2006
 // Distributed under the Boost Software License, Version 1.0. (See accompany-
 // ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
