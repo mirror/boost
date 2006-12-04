4bd4e3157c15756d63ce73d3ddc76d36c218a600
joaquintides <joaquintides@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-12-04 15:06:29 +0000

    removed another VC7.1 warning 4800
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36264 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/pool/pool.hpp
+++ boost/boost/pool/pool.hpp
@@ -502,7 +502,7 @@ void * pool<UserAllocator>::ordered_malloc(const size_type n)
   const size_type partition_size = alloc_size();
   const size_type total_req_size = n * requested_size;
   const size_type num_chunks = total_req_size / partition_size +
-      static_cast<bool>(total_req_size % partition_size);
+      ((total_req_size % partition_size) ? true : false);
 
   void * ret = store().malloc_n(num_chunks, partition_size);
 
