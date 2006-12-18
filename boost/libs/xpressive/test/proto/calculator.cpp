815a28e987733f222fbd4c97c3f85d687ac604cd
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-12-18 07:00:14 +0000

    test reorg, add calculator test
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36445 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/libs/xpressive/test/proto/calculator.cpp
@@ -0,0 +1,108 @@
+///////////////////////////////////////////////////////////////////////////////
+// calculator.hpp
+//
+//  Copyright 2006 Eric Niebler. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/xpressive/proto/proto.hpp>
+#include <boost/test/unit_test.hpp>
+
+using namespace boost;
+
+struct placeholder {};
+proto::meta::terminal<placeholder>::type const _1 = {{}};
+
+struct calculator
+{
+    typedef int result_type;
+
+    calculator(int i)
+      : i_(i)
+    {}
+
+    int operator()(proto::tag::terminal, placeholder) const
+    {
+        return this->i_;
+    }
+
+    int operator()(proto::tag::terminal, int j) const
+    {
+        return j;
+    }
+
+    template<typename Left, typename Right>
+    int operator()(proto::tag::add, Left const &left, Right const &right) const
+    {
+        return left.eval(*this) + right.eval(*this);
+    }
+
+    template<typename Left, typename Right>
+    int operator()(proto::tag::subtract, Left const &left, Right const &right) const
+    {
+        return left.eval(*this) - right.eval(*this);
+    }
+
+    template<typename Left, typename Right>
+    int operator()(proto::tag::multiply, Left const &left, Right const &right) const
+    {
+        return left.eval(*this) * right.eval(*this);
+    }
+
+    template<typename Left, typename Right>
+    int operator()(proto::tag::divide, Left const &left, Right const &right) const
+    {
+        return left.eval(*this) / right.eval(*this);
+    }
+
+private:
+    int i_;
+};
+
+template<typename Fun, typename Expr>
+struct functor
+{
+    typedef typename proto::meta::eval<Expr, Fun>::type result_type;
+
+    functor(Expr const &expr)
+      : expr_(expr)
+    {}
+    
+    template<typename T>
+    result_type operator()(T const &t) const
+    {
+        Fun fun(t);
+        return this->expr_.eval(fun);
+    }
+
+private:
+    Expr const &expr_;
+};
+
+template<typename Fun, typename Expr>
+functor<Fun, Expr> as(Expr const &expr)
+{
+    return functor<Fun, Expr>(expr);
+}
+
+void test_calculator()
+{
+    BOOST_CHECK_EQUAL(10, (((_1 + 42)-3)/4).eval(calculator(1)));
+    BOOST_CHECK_EQUAL(11, (((_1 + 42)-3)/4).eval(calculator(5)));
+
+    BOOST_CHECK_EQUAL(10, as<calculator>(((_1 + 42)-3)/4)(1));
+    BOOST_CHECK_EQUAL(11, as<calculator>(((_1 + 42)-3)/4)(5));
+}
+
+using namespace unit_test;
+///////////////////////////////////////////////////////////////////////////////
+// init_unit_test_suite
+//
+test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    test_suite *test = BOOST_TEST_SUITE("test immediate evaluation of proto parse trees");
+
+    test->add(BOOST_TEST_CASE(&test_calculator));
+
+    return test;
+}
