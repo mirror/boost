703e66977e3fad305c0a9bc645eb02bc76c8287f
andreas_huber69 <andreas_huber69@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-12-29 13:30:52 +0000

    Removed BBV1 related files (obsolete since the switch to V2).
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36535 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/statechart/test/RunTests.bat
+++
@@ -1,5 +0,0 @@
-REM Copyright 2004-2006 Andreas Huber Doenni
-REM Distributed under the Boost Software License, Version 1.0. (See accompany-
-REM ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-bjam "-sTOOLS=mingw vc-7_1" "-sMINGW_ROOT_DIRECTORY=C:\ProgramFiles\MinGW3.2.0" "-sINTEL_PATH=C:\Program Files\Intel\Compiler\C++\9.0\IA32" "-sINTEL_BASE_MSVC_TOOLSET=vc-7_1" %1 %2 %3 %4 >log.txt 2>&1
