90b73facbd0d5934e90dcee4095991bbe9444ef3
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-02-18 16:07:55 +0000

    added flat include directory
    git-svn-id: http://svn.boost.org/svn/boost/trunk@36993 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/boost/fusion/include/joint_view.hpp
@@ -0,0 +1,13 @@
+/*=============================================================================
+    Copyright (c) 2001-2007 Joel de Guzman
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+#if !defined(FUSION_INCLUDE_JOINT_VIEW)
+#define FUSION_INCLUDE_JOINT_VIEW
+
+#include <boost/fusion/sequence/view/joint_view.hpp>
+
+#endif
