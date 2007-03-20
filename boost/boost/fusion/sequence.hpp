1a005bad752cda478c664c96abf0f2b18a27ef83
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-03-20 16:07:52 +0000

    removed obsolete include
    git-svn-id: http://svn.boost.org/svn/boost/trunk@37241 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/fusion/sequence.hpp
+++ boost/boost/fusion/sequence.hpp
@@ -1,7 +1,7 @@
 /*=============================================================================
     Copyright (c) 2001-2006 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_SEQUENCE_10022005_0559)
@@ -16,6 +16,5 @@
 #include <boost/fusion/sequence/io.hpp>
 #include <boost/fusion/sequence/view.hpp>
 #include <boost/fusion/sequence/adapted.hpp>
-#include <boost/fusion/sequence/utility.hpp>
 
 #endif
