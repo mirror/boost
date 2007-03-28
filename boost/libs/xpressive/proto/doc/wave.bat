cd7302cdc893f575d3789cbe36740c18bc2bd25d
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-03-28 19:48:54 +0000

    update documentation for new-and-improved evaluation contexts
    git-svn-id: http://svn.boost.org/svn/boost/trunk@37313 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/xpressive/proto/doc/wave.bat
+++
@@ -1,1 +0,0 @@
-..\..\..\..\dist\bin\wave.exe -S ..\..\..\.. -D BOOST_PROTO_DOXYGEN_INVOKED -D "BOOST_TYPEOF_NESTED_TYPEDEF_TPL(X,Y)=struct X{ typedef detail::type type; };" -D "BOOST_WORKAROUND(X,Y)=0" -D BOOST_PROTO_MAX_ARITY=5 -p 1 %*
