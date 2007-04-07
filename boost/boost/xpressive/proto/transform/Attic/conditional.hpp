4ab89f8fedca5030acc6338978695dd67b9b6303
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-04-07 05:57:10 +0000

    simplify/generalize conditional transform
    git-svn-id: http://svn.boost.org/svn/boost/trunk@37386 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/xpressive/proto/transform/Attic/conditional.hpp
+++ boost/boost/xpressive/proto/transform/Attic/conditional.hpp
@@ -11,9 +11,6 @@
 #define BOOST_PROTO_TRANSFORM_CONDITIONAL_HPP_EAN_04_02_2007
 
 #include <boost/xpressive/proto/detail/prefix.hpp>
-#include <boost/mpl/assert.hpp>
-#include <boost/mpl/apply.hpp>
-#include <boost/type_traits/is_same.hpp>
 #include <boost/xpressive/proto/proto_fwd.hpp>
 #include <boost/xpressive/proto/detail/suffix.hpp>
 
@@ -22,36 +19,17 @@ namespace boost { namespace proto { namespace transform
 
     template<typename Predicate, typename Grammar0, typename Grammar1>
     struct conditional
-      : Grammar0
-    {
-        conditional();
-        BOOST_MPL_ASSERT((is_same<typename Grammar0::type, typename Grammar1::type>));
-
-        template<typename Expr, typename State, typename Visitor>
-        struct apply
-        {
-            typedef typename mpl::if_<
-                typename mpl::apply1<Predicate, Expr>::type
+      : proto::or_<
+            proto::and_<
+                proto::if_<Predicate>
               , Grammar0
+            >
+          , proto::and_<
+                proto::not_<proto::if_<Predicate> >
               , Grammar1
-            >::type grammar_type;
-
-            typedef typename grammar_type::template apply<Expr, State, Visitor>::type type;
-        };
-
-        template<typename Expr, typename State, typename Visitor>
-        static typename apply<Expr, State, Visitor>::type
-        call(Expr const &expr, State const &state, Visitor &visitor)
-        {
-            typedef typename mpl::if_<
-                typename mpl::apply1<Predicate, Expr>::type
-              , Grammar0
-              , Grammar1
-            >::type grammar_type;
-
-            return grammar_type::call(expr, state, visitor);
-        }
-    };
+            >
+        >
+    {};
 
 }}}
 
