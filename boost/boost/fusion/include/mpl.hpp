f6e808dd2f1cd29783aa3c2465d1e52afbdb0222
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-04-22 08:06:36 +0000

    mpl.hpp header now includes 2 way mapping to and from mpl and fusion #include <boost/fusion/sequence/adapted/mpl.hpp> #include <boost/fusion/sequence/intrinsic/mpl.hpp>
    git-svn-id: http://svn.boost.org/svn/boost/trunk@37484 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/boost/fusion/include/mpl.hpp
+++ boost/boost/fusion/include/mpl.hpp
@@ -1,12 +1,13 @@
 /*=============================================================================
     Copyright (c) 2001-2007 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_INCLUDE_MPL)
 #define FUSION_INCLUDE_MPL
 
 #include <boost/fusion/sequence/adapted/mpl.hpp>
+#include <boost/fusion/sequence/intrinsic/mpl.hpp>
 
 #endif
