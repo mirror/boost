c87ec4cbae7d70976026417726916f6e9ba16a7f
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-09-14 19:19:09 +0000

    CINT support (Nils Krumnack)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@39282 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/shared_ptr.hpp
+++ boost/shared_ptr.hpp
@@ -383,7 +383,7 @@ public:
 
     // implicit conversion to "bool"
 
-#if defined(__SUNPRO_CC) && BOOST_WORKAROUND(__SUNPRO_CC, <= 0x530)
+#if ( defined(__SUNPRO_CC) && BOOST_WORKAROUND(__SUNPRO_CC, <= 0x530) ) || defined(__CINT__)
 
     operator bool () const
     {
