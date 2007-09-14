5614f482b03f6153de4b0ef1a45b2ab3daf2b2dd
bgubenko <bgubenko@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-09-14 02:25:06 +0000

    on HP-UX, macro BOOST_DATE_TIME_HAS_REENTRANT_STD_FUNCTIONS should be defined only if macro _REENTRANT is defined
    git-svn-id: http://svn.boost.org/svn/boost/trunk@39256 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/date_time/compiler_config.hpp
+++ boost/date_time/compiler_config.hpp
@@ -140,7 +140,7 @@ namespace std {
 #if defined(BOOST_HAS_THREADS) 
 #  if defined(_MSC_VER) || defined(__MWERKS__) || defined(__MINGW32__) ||  defined(__BORLANDC__)
      //no reentrant posix functions (eg: localtime_r)
-#  else
+#  elif (!defined(__hpux) || (defined(__hpux) && defined(_REENTRANT)))
 #   define BOOST_DATE_TIME_HAS_REENTRANT_STD_FUNCTIONS
 #  endif
 #endif
