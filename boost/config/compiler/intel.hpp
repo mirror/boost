047d6ac9a5c48c464d1dc78580453d30a729ed6c
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-09-22 11:27:25 +0000

    Updated license declaration.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@39475 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/config/compiler/intel.hpp
+++ boost/config/compiler/intel.hpp
@@ -1,4 +1,4 @@
-//  (C) Copyright John Maddock 2001.
+//  (C) Copyright John Maddock 2001-7.
 //  (C) Copyright Peter Dimov 2001.
 //  (C) Copyright Jens Maurer 2001.
 //  (C) Copyright David Abrahams 2002 - 2003.
