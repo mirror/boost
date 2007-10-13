ae943335a892fc417638ee3ed6c1437bb410b69c
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-10-13 21:50:05 +0000

    Use a pragma for warnings in Visual C++.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@39991 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/functional/hash/set.hpp
+++ boost/functional/hash/set.hpp
@@ -1,5 +1,5 @@
 
-// Copyright 2005-2006 Daniel James.
+// Copyright 2005-2007 Daniel James.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
@@ -14,8 +14,12 @@
 # pragma once
 #endif
 
+#if defined(_MSC_VER)
+#pragma message("Warning: boost/functional/hash/set.hpp is deprecated, use boost/functional/hash.hpp instead.")
+#else
 #warning "boost/functional/hash/set.hpp is deprecated, use boost/functional/hash.hpp instead."
+#endif
+
 #include <boost/functional/hash.hpp>
 
 #endif
-
