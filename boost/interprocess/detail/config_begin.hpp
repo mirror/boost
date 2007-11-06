2858e712adda129e19a0158fbea86e8480a42db2
igaztanaga <igaztanaga@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-06 16:56:28 +0000

    Corrected _CRT_SECURE_NO_DEPRECATE detection
    git-svn-id: http://svn.boost.org/svn/boost/trunk@40841 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/interprocess/detail/config_begin.hpp
+++ boost/interprocess/detail/config_begin.hpp
@@ -4,7 +4,7 @@
 #endif
 
 #ifdef BOOST_MSVC
-   #ifdef _CRT_SECURE_NO_DEPRECATE
+   #ifndef _CRT_SECURE_NO_DEPRECATE
    #define  BOOST_INTERPROCESS_CRT_SECURE_NO_DEPRECATE
    #define _CRT_SECURE_NO_DEPRECATE
    #endif
