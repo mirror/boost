c038fe2e37f8177be204cef3db61f0a18fdee078
bemandawes <bemandawes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-07 16:08:09 +0000

    Copyright and/or License cleanup
    git-svn-id: http://svn.boost.org/svn/boost/trunk@40890 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/utility/value_init_test_fail3.cpp
+++ libs/utility/value_init_test_fail3.cpp
@@ -1,4 +1,4 @@
-// (C) 2002, Fernando Luis Cacciola Carballal.
+// Copyright 2002, Fernando Luis Cacciola Carballal.
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
