866d30f8f3dd6563c4281203c6a2adc0f0c34a7e
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-12 17:21:47 +0000

    Disable long double tests if there are no long double math functions.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@41038 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/tr1/test/test_complex.cpp
+++ libs/tr1/test/test_complex.cpp
@@ -15,31 +15,45 @@ int main()
 {
    verify_return_type(std::arg(0), double(0));
    verify_return_type(std::arg(0.0), double(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::arg(0.0L), (long double)(0));
+#endif
    verify_return_type(std::arg(0.0F), float(0));
    verify_return_type(std::norm(0), double(0));
    verify_return_type(std::norm(0.0), double(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::norm(0.0L), (long double)(0));
+#endif
    verify_return_type(std::norm(0.0F), float(0));
    verify_return_type(std::conj(0), std::complex<double>(0));
    verify_return_type(std::conj(0.0), std::complex<double>(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::conj(0.0L), std::complex<long double>(0));
+#endif
    verify_return_type(std::conj(0.0F), std::complex<float>(0));
    verify_return_type(std::imag(0), double(0));
    verify_return_type(std::imag(0.0), double(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::imag(0.0L), (long double)(0));
+#endif
    verify_return_type(std::imag(0.0F), float(0));
    verify_return_type(std::real(0), double(0));
    verify_return_type(std::real(0.0), double(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::real(0.0L), (long double)(0));
+#endif
    verify_return_type(std::real(0.0F), float(0));
    verify_return_type(std::polar(0), std::complex<double>(0));
    verify_return_type(std::polar(0.0), std::complex<double>(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::polar(0.0L), std::complex<long double>(0));
+#endif
    verify_return_type(std::polar(0.0F), std::complex<float>(0));
    verify_return_type(std::polar(0, 0L), std::complex<double>(0));
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::polar(0.0, 0.0L), std::complex<long double>(0));
    verify_return_type(std::polar(0.0L, 0.0F), std::complex<long double>(0));
+#endif
    verify_return_type(std::polar(0.0F, 0), std::complex<double>(0));
 
    std::complex<float> f;
@@ -87,25 +101,39 @@ int main()
 
    verify_return_type(std::tr1::acos(f), f);
    verify_return_type(std::tr1::acos(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::acos(l), l);
+#endif
    verify_return_type(std::tr1::asin(f), f);
    verify_return_type(std::tr1::asin(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::asin(l), l);
+#endif
    verify_return_type(std::tr1::atan(f), f);
    verify_return_type(std::tr1::atan(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::atan(l), l);
+#endif
    verify_return_type(std::tr1::asinh(f), f);
    verify_return_type(std::tr1::asinh(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::asinh(l), l);
+#endif
    verify_return_type(std::tr1::acosh(f), f);
    verify_return_type(std::tr1::acosh(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::acosh(l), l);
+#endif
    verify_return_type(std::tr1::atanh(f), f);
    verify_return_type(std::tr1::atanh(d), d);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::atanh(l), l);
+#endif
    verify_return_type(std::tr1::fabs(f), sf);
    verify_return_type(std::tr1::fabs(d), sd);
+#ifndef BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
    verify_return_type(std::tr1::fabs(l), sl);
+#endif
    return 0;
 }
 
