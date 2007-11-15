c6b19772a4716f8e743077971b88c3fdd908eeb0
danmarsden <danmarsden@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-15 21:15:47 +0000

    removing failing test on EDG compilers
    git-svn-id: http://svn.boost.org/svn/boost/trunk@41117 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/fusion/test/sequence/filter_view.cpp
+++ libs/fusion/test/sequence/filter_view.cpp
@@ -95,6 +95,7 @@ main()
         // (e.g. comeau 4.3.3, intel) have problems with this.
         // vc7.1 and g++ are ok. The errors from comeau are useless.
         
+#ifndef __EDG_VERSION__
         typedef vector_c<int, 5, 1, 2, 3, 6, 0, -1> vector_type;
         typedef filter_view<vector_type const, less<_, int_<3> > > filter_view_type;
         vector_type v;
@@ -102,6 +103,7 @@ main()
         std::cout << view << std::endl;
         BOOST_TEST((view == make_vector(1, 2, 0, -1)));
         BOOST_STATIC_ASSERT(result_of::size<filter_view_type>::value == 4);
+#endif
     }
 
     {
