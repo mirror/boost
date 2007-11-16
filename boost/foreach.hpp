dc46adfcb7ef42523e142772ce487fe5f9d48b36
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-16 19:24:52 +0000

    add comment pointing to BOOST_FOREACH's online docs
    git-svn-id: http://svn.boost.org/svn/boost/trunk@41155 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/foreach.hpp
+++ boost/foreach.hpp
@@ -5,6 +5,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
+// See http://www.boost.org/libs/foreach for documentation
 //
 // Credits:
 //  Anson Tsao        - for the initial inspiration and several good suggestions.
