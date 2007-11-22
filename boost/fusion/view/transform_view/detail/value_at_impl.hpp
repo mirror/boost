d8b66b5c6e043cc66c9bdad3cd313f80705f2950
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-11-22 08:39:46 +0000

    bug fix
    git-svn-id: http://svn.boost.org/svn/boost/trunk@41299 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/view/transform_view/detail/value_at_impl.hpp
+++ boost/fusion/view/transform_view/detail/value_at_impl.hpp
@@ -2,7 +2,7 @@
     Copyright (c) 2001-2006 Joel de Guzman
     Copyright (c) 2005-2006 Dan Marsden
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(BOOST_FUSION_VALUE_AT_IMPL_20061101_0745)
@@ -10,7 +10,7 @@
 
 #include <boost/mpl/apply.hpp>
 #include <boost/fusion/view/transform_view/detail/apply_transform_result.hpp>
-#include <boost/fusion/sequence/intrinsic/at.hpp>
+#include <boost/fusion/sequence/intrinsic/value_at.hpp>
 
 namespace boost { namespace fusion {
     struct transform_view_tag;
@@ -29,7 +29,7 @@ namespace boost { namespace fusion {
             {
                 typedef typename Seq::transform_type F;
                 typedef detail::apply_transform_result<F> transform_type;
-                typedef typename boost::fusion::result_of::at<typename Seq::sequence_type, N>::type value_type;
+                typedef typename boost::fusion::result_of::value_at<typename Seq::sequence_type, N>::type value_type;
                 typedef typename mpl::apply<transform_type, value_type>::type type;
             };
         };
@@ -42,8 +42,8 @@ namespace boost { namespace fusion {
             {
                 typedef typename Seq::transform_type F;
                 typedef detail::apply_transform_result<F> transform_type;
-                typedef typename boost::fusion::result_of::at<typename Seq::sequence1_type, N>::type value1_type;
-                typedef typename boost::fusion::result_of::at<typename Seq::sequence2_type, N>::type value2_type;
+                typedef typename boost::fusion::result_of::value_at<typename Seq::sequence1_type, N>::type value1_type;
+                typedef typename boost::fusion::result_of::value_at<typename Seq::sequence2_type, N>::type value2_type;
                 typedef typename mpl::apply<transform_type, value1_type, value2_type>::type type;
             };
         };
