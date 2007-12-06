91eafc18ddae5855110824c213411c56843be504
dgregor <dgregor@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2007-12-06 17:17:38 +0000

    Bump VC++ version check to continue applying VC-specific workaround for Visual C++ 9.0. Fixes #1499
    git-svn-id: http://svn.boost.org/svn/boost/trunk@41795 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/signals/detail/named_slot_map.hpp
+++ boost/signals/detail/named_slot_map.hpp
@@ -127,7 +127,7 @@ public:
         || slot_ == other.slot_));
   }
 
-#if BOOST_WORKAROUND(_MSC_VER, <= 1400)
+#if BOOST_WORKAROUND(_MSC_VER, <= 1500)
   void decrement();
   void advance(difference_type);
 #endif
