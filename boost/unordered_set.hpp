2a0fd7bdab7c59019e92b50b1c53723c29bfdae4
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-01-06 17:45:18 +0000

    Add library identification.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@42538 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/unordered_set.hpp
+++ boost/unordered_set.hpp
@@ -4,6 +4,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+//  See http://www.boost.org/libs/unordered for documentation
+
 #ifndef BOOST_UNORDERED_SET_HPP_INCLUDED
 #define BOOST_UNORDERED_SET_HPP_INCLUDED
 
