30d0aa9283578923e8e7ace3f1cf110d13f3d149
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-01-14 09:53:42 +0000

    Merge bug fixes from main Trunk: these fix security issues reported by Will Drewry.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@42746 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/regex/test/regress/test_perl_ex.cpp
+++ libs/regex/test/regress/test_perl_ex.cpp
@@ -121,6 +121,17 @@ void test_conditionals()
    TEST_INVALID_REGEX("(?:(a)|b)(?(?:", perl);
    TEST_INVALID_REGEX("(?:(a)|b)(?(?<", perl);
    TEST_INVALID_REGEX("(?:(a)|b)(?(?<a", perl);
+
+   TEST_INVALID_REGEX("(?(?!#?)+)", perl);
+   TEST_INVALID_REGEX("(?(?=:-){0})", perl);
+   TEST_INVALID_REGEX("(?(123){1})", perl);
+   TEST_INVALID_REGEX("(?(?<=A)*)", perl);
+   TEST_INVALID_REGEX("(?(?<=A)+)", perl);
+
+   TEST_INVALID_REGEX("(?<!*|^)", perl);
+   TEST_INVALID_REGEX("(?<!*|A)", perl);
+   TEST_INVALID_REGEX("(?<=?|A)", perl);
+   TEST_INVALID_REGEX("(?<=*|\\B)", perl);
 }
 
 void test_options()
