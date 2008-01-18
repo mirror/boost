aef2550698efac37bf806cb8bb53cd408a4e97f4
t_schwinger <t_schwinger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-01-18 20:52:06 +0000

    attempts to fix BCB 5.9 regression in synthesis/mem_func_ptr_cv_ptr_to_this test
    git-svn-id: http://svn.boost.org/svn/boost/trunk@42858 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/function_types/detail/cv_traits.hpp
+++ boost/function_types/detail/cv_traits.hpp
@@ -12,7 +12,7 @@
 #include <cstddef>
 #include <boost/detail/workaround.hpp>
 
-#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) || BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x564))
+#if defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) || BOOST_WORKAROUND(__BORLANDC__, <= 0x582)
 #   include <boost/type_traits/remove_cv.hpp>
 #   include <boost/type_traits/remove_pointer.hpp>
 #   include <boost/type_traits/remove_reference.hpp>
@@ -25,7 +25,7 @@ namespace boost { namespace function_types { namespace detail {
 namespace ft = boost::function_types;
 
 
-#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x564))
+#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !BOOST_WORKAROUND(__BORLANDC__, <= 0x582)
 
 template<typename T> struct cv_traits 
 { typedef non_cv tag; typedef T type; };
