33bcc8c7eb7330bdf5ce3ab2892b4cd96174e2c4
hkaiser <hkaiser@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-01-26 01:44:32 +0000

    Wave: Tweaked new testcase.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@42980 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/wave/test/testwave/testfiles/t_9_019.hpp
+++ libs/wave/test/testwave/testfiles/t_9_019.hpp
@@ -7,4 +7,5 @@
     LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
 
-// this file intentionally has been left empty
+// this shouldn't give an recursion
+#include_next "t_9_019.hpp"
