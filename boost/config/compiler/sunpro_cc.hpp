0d61fd9997b5d084661bb093432925e643b3845f
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-02-07 09:55:41 +0000

    Fix last checked version.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@43141 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/config/compiler/sunpro_cc.hpp
+++ boost/config/compiler/sunpro_cc.hpp
@@ -84,8 +84,8 @@
 #error "Compiler not supported or configured - please reconfigure"
 #endif
 //
-// last known and checked version is 0x570:
-#if (__SUNPRO_CC > 0x580)
+// last known and checked version is 0x590:
+#if (__SUNPRO_CC > 0x590)
 #  if defined(BOOST_ASSERT_CONFIG)
 #     error "Unknown compiler version - please run the configure tests and report the results"
 #  endif
