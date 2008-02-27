7f488732b38cb180f224dd88f173e575b1c3c0b5
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-02-27 20:00:24 +0000

    Merged revisions 43211,43214-43219,43222-43225,43227-43238,43242,43244-43245,43249-43250,43257-43259,43261,43263,43265,43267-43268,43270-43271,43273,43275-43279,43284-43289,43291,43295,43297-43298,43304-43305,43307,43313,43315,43324,43326-43327,43331,43333,43339-43343,43345,43348,43350,43352-43353,43355-43356,43358,43360,43366-43367,43369-43370,43372-43376,43378-43389,43394,43396-43398,43400-43401,43403-43404,43406-43408,43413-43415,43417-43418,43420,43422-43423 via svnmerge from https://svn.boost.org/svn/boost/trunk
    ........
      r43417 | danieljames | 2008-02-26 22:04:55 +0000 (Tue, 26 Feb 2008) | 2 lines
      
      Fix a link to Boost.Bimap.
    ........
      r43418 | danieljames | 2008-02-26 22:07:25 +0000 (Tue, 26 Feb 2008) | 2 lines
      
      Change another link that's no longer in the repository to link to the website.
    ........
      r43422 | danieljames | 2008-02-27 18:51:14 +0000 (Wed, 27 Feb 2008) | 1 line
      
      Fix broken copyright urls. Fixes #1573.
    ........
      r43423 | danieljames | 2008-02-27 19:22:01 +0000 (Wed, 27 Feb 2008) | 1 line
      
      Fix incorrect links to copyright of the form 'http:#www.boost.org
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@43425 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/date_time/example/posix_time/time_periods.cpp
+++ libs/date_time/example/posix_time/time_periods.cpp
@@ -53,6 +53,6 @@ main()
  *  http://www.crystalclearsoftware.com
  *
  *  Subject to the Boost Software License, Version 1.0.
- * (See accompanying file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
+ * (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
  */
 
