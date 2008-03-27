71e617ae738c768de9a5b3bdceb5657ad9443053
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-03-27 17:12:22 +0000

    added test to check warnings on type trait
    git-svn-id: http://svn.boost.org/svn/boost/trunk@43883 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/serialization/test/test_static_warning.cpp
+++ libs/serialization/test/test_static_warning.cpp
@@ -8,20 +8,34 @@
 // note: this is a compile only test.
 
 #include <boost/static_warning.hpp>
+#include <boost/type_traits/is_polymorphic.hpp>
 
 #pragma warning(disable:4094)
 
 typedef char a1[2];
 typedef char a2[3];
 
+class polymorphic {
+    virtual ~polymorphic();
+};
+
+class non_polymorphic {
+};
+
+// note: compilation of this program should show 8 warning messages
+
 BOOST_STATIC_WARNING(true);
 BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
-BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
+BOOST_STATIC_WARNING(sizeof(a1) != sizeof(a1)); // Warn.
+BOOST_STATIC_WARNING(! boost::is_polymorphic<polymorphic>::value); // Warn.
+BOOST_STATIC_WARNING(boost::is_polymorphic<non_polymorphic>::value); // Warn.
 
 void warnings() {
     BOOST_STATIC_WARNING(true);
     BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
-    BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
+    BOOST_STATIC_WARNING(sizeof(a1) != sizeof(a1)); // Warn.
+    BOOST_STATIC_WARNING(! boost::is_polymorphic<polymorphic>::value); // Warn.
+    BOOST_STATIC_WARNING(boost::is_polymorphic<non_polymorphic>::value); // Warn.
 }                               
 
 int main(int /* argc */, char * /* argv */[]){
