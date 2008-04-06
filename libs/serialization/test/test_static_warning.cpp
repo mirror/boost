b1e02a6ae96257d6ff2cf2c8bce7fb353c1423d2
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-04-06 20:04:20 +0000

    enhanced test to detect more errors
    git-svn-id: http://svn.boost.org/svn/boost/trunk@44082 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/serialization/test/test_static_warning.cpp
+++ libs/serialization/test/test_static_warning.cpp
@@ -7,6 +7,7 @@
 
 // note: this is a compile only test.
 
+#include <boost/config.hpp> // BOOST_STATIC_CONST
 #include <boost/static_warning.hpp>
 #include <boost/type_traits/is_polymorphic.hpp>
 
@@ -22,23 +23,42 @@ class polymorphic {
 class non_polymorphic {
 };
 
-// note: compilation of this program should show 8 warning messages
+class test_class {
+};
 
+template <class T>
+int f()
+{
+    BOOST_STATIC_WARNING(T::value);
+    return 0;
+}       
+
+struct A {
+    BOOST_STATIC_CONSTANT(bool, value = 0);
+};
+
+/////////////////////////////////////////////////////////////////////////
+// compilation of this program should show a total of 10 warning messages
+
+// should show NO warning message
 BOOST_STATIC_WARNING(true);
+
+// the following should show 5 warning message
+int x = f<A>(); 
 BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
 BOOST_STATIC_WARNING(sizeof(a1) != sizeof(a1)); // Warn.
 BOOST_STATIC_WARNING(! boost::is_polymorphic<polymorphic>::value); // Warn.
 BOOST_STATIC_WARNING(boost::is_polymorphic<non_polymorphic>::value); // Warn.
 
-void warnings() {
+int main(int /* argc */, char * /* argv */[]){
+    // should show NO warning message
     BOOST_STATIC_WARNING(true);
+
+    // the following should show 5 warning message
+    f<A>();
     BOOST_STATIC_WARNING(sizeof(a1) == sizeof(a2)); // Warn.
     BOOST_STATIC_WARNING(sizeof(a1) != sizeof(a1)); // Warn.
     BOOST_STATIC_WARNING(! boost::is_polymorphic<polymorphic>::value); // Warn.
     BOOST_STATIC_WARNING(boost::is_polymorphic<non_polymorphic>::value); // Warn.
-}                               
-
-int main(int /* argc */, char * /* argv */[]){
-    warnings();
     return 0;
 }
