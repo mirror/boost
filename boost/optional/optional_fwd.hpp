3444094b30de33a3f43b3064a90be82a3a77f527
niels_dekker <niels_dekker@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-04-27 21:07:10 +0000

    Added forward declaration of optional<T>'s boost::swap overload, as mentioned at http://article.gmane.org/gmane.comp.lib.boost.devel/174350 "Re: [optional] problems with swap()"
    git-svn-id: http://svn.boost.org/svn/boost/trunk@44825 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/optional/optional_fwd.hpp
+++ boost/optional/optional_fwd.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2003, Fernando Luis Cacciola Carballal.
+// Copyright (C) 2003, 2008 Fernando Luis Cacciola Carballal.
 //
 // Use, modification, and distribution is subject to the Boost Software
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
@@ -9,12 +9,17 @@
 // You are welcome to contact the author at:
 //  fernando_cacciola@hotmail.com
 //
+// Revisions:
+// 27 Apr 2008 (added forward declaration of boost::swap overload) Niels Dekker
+// 
 #ifndef BOOST_OPTIONAL_OPTIONAL_FWD_FLC_19NOV2002_HPP
 #define BOOST_OPTIONAL_OPTIONAL_FWD_FLC_19NOV2002_HPP
 
 namespace boost {
 
 template<class T> class optional ;
+
+template<class T> void swap ( optional<T>& , optional<T>& ) ;
 
 } // namespace boost
 
