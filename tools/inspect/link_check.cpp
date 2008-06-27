a8dcdb34814b4551f92f5ffc5e965342eb4f15aa
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-06-27 16:52:27 +0000

    Better message when there's not hostname. And add a comment explaining the reasoning behind this rule.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@46779 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/inspect/link_check.cpp
+++ tools/inspect/link_check.cpp
@@ -150,10 +150,13 @@ namespace boost
       // Protocol checks
       if(scheme_matched) {
         if(scheme == "http" || scheme == "https") {
+          // All http links should have a hostname. Generally if they don't
+          // it's by mistake. If they shouldn't, then a protocol isn't
+          // required.
           if(!authority_matched) {
             if(!no_link_errors) {
               ++m_invalid_errors;
-              error( library_name, source_path, string(name()) + " http protocol without hostname: " + url );
+              error( library_name, source_path, string(name()) + " no hostname: " + url );
             }
           }
 
