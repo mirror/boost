4d435773edcb9fee8080942f15c6ec73af4f0c75
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-07-02 07:31:40 +0000

    Merged revisions 46985 via svnmerge from https://svn.boost.org/svn/boost/trunk
    ........
      r46985 | eric_niebler | 2008-07-02 00:25:09 -0700 (Wed, 02 Jul 2008) | 1 line
      
      fix inaccurate doxygen comments
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@46986 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/xpressive/traits/cpp_regex_traits.hpp
+++ boost/xpressive/traits/cpp_regex_traits.hpp
@@ -451,7 +451,7 @@ struct cpp_regex_traits
     /// such that if the character sequence [G1, G2) sorts before the character sequence [H1, H2)
     /// then v.transform(G1, G2) \< v.transform(H1, H2).
     ///
-    /// \attention Not used in xpressive 1.0
+    /// \attention Not currently used
     template<typename FwdIter>
     string_type transform(FwdIter begin, FwdIter end) const
     {
@@ -467,7 +467,7 @@ struct cpp_regex_traits
     /// when character case is not considered then
     /// v.transform_primary(G1, G2) \< v.transform_primary(H1, H2).
     ///
-    /// \attention Not used in xpressive 1.0
+    /// \attention Not currently used
     template<typename FwdIter>
     string_type transform_primary(FwdIter begin, FwdIter end) const
     {
@@ -479,7 +479,7 @@ struct cpp_regex_traits
     /// consisting of the character sequence designated by the iterator range [F1, F2).
     /// Returns an empty string if the character sequence is not a valid collating element.
     ///
-    /// \attention Not used in xpressive 1.0
+    /// \attention Not currently used
     template<typename FwdIter>
     string_type lookup_collatename(FwdIter begin, FwdIter end) const
     {
