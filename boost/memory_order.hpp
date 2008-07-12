4f7fdc56577cdfbe069e706f4587b8e777f7cb9e
pdimov <pdimov@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-07-12 12:21:19 +0000

    Merged 44686 from trunk to release.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@47350 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/memory_order.hpp
+++ boost/memory_order.hpp
@@ -27,7 +27,7 @@ enum memory_order
     memory_order_acquire = 1,
     memory_order_release = 2,
     memory_order_acq_rel = 3, // acquire | release
-    memory_order_seq_cst = 7, // acq_rel | 4
+    memory_order_seq_cst = 7  // acq_rel | 4
 };
 
 } // namespace boost
