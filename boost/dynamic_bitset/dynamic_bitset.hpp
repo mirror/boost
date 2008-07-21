5bf37dff76889f44c003cf15ed392138bb48fcb0
gennaro_prota <gennaro_prota@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-07-21 23:46:39 +0000

    dynamic_bitset.hpp: (minor) removed tab characters introduced with revision 35066 on trunk
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@47666 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/dynamic_bitset/dynamic_bitset.hpp
+++ boost/dynamic_bitset/dynamic_bitset.hpp
@@ -769,11 +769,11 @@ dynamic_bitset<Block, Allocator>::operator<<=(size_type n)
             b[div] = b[0];
         }
 
-	// disable std::fill_n deprecated warning in MSVC++ 8.0 (warning C4996)
-	// This will only work in MSVC++ 8.0 SP1, which brings up the warning
-	// in the line of user code; otherwise, the warning will come up
-	// in the line in the header itself, so if the user includes stdlib
-	// headers before dynamic_bitset, he will still get the warning.
+    // disable std::fill_n deprecated warning in MSVC++ 8.0 (warning C4996)
+    // This will only work in MSVC++ 8.0 SP1, which brings up the warning
+    // in the line of user code; otherwise, the warning will come up
+    // in the line in the header itself, so if the user includes stdlib
+    // headers before dynamic_bitset, he will still get the warning.
 #if defined(_MSC_VER) && _MSC_VER >= 1400
 #pragma warning(push)
 #pragma warning(disable:4996)
