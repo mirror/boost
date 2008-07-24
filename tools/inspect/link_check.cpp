43629c7b5b74efd067feb40e54ecd8b7ef0efc95
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-07-24 10:15:12 +0000

    Better inspect errors for empty URLs.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@47751 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/inspect/link_check.cpp
+++ tools/inspect/link_check.cpp
@@ -127,6 +127,12 @@ namespace boost
       const path & source_path, bool no_link_errors )
         // precondition: source_path.is_complete()
     {
+      if(url.empty()) {
+        ++m_invalid_errors;
+        error( library_name, source_path, string(name()) + " empty URL." );
+        return;
+      }
+    
       boost::smatch m;
       if(!boost::regex_match(url, m, url_decompose_regex)) {
         if(!no_link_errors) {
