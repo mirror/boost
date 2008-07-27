601d5682065ffe40d0bdfa5262b139fd4512c4bd
niels_dekker <niels_dekker@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-07-27 12:46:45 +0000

    Fixed comment in swap/test/specialized_in_boost_and_other.cpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@47840 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/utility/swap/test/specialized_in_boost_and_other.cpp
+++ libs/utility/swap/test/specialized_in_boost_and_other.cpp
@@ -4,15 +4,15 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-// Tests whether two object in a namespace other than boost are properly
-// swapped, when both boost and the other namespace have a custom swap function
-// for their class. Note that it shouldn't be necessary for a class in an other
-// namespace to have a custom swap function in boost, because the boost::swap
-// utility should find the swap function in the other namespace, by argument
-// dependent lookup (ADL). Unfortunately ADL isn't fully implemented by some
-// specific compilers, including Intel C++ 8.1, MSVC 7.1, and Borland 5.9.3.
-// Users of those compilers might consider adding a swap overload to the boost
-// namespace.
+// Tests whether instances of a class from a namespace other than boost are
+// properly swapped, when both boost and the other namespace have a custom
+// swap function for that class. Note that it shouldn't be necessary for a class
+// in an other namespace to have a custom swap function in boost, because the
+// boost::swap utility should find the swap function in the other namespace, by
+// argument dependent lookup (ADL). Unfortunately ADL isn't fully implemented
+// by some specific compiler versions, including Intel C++ 8.1, MSVC 7.1, and 
+// Borland 5.9.3. Users of those compilers might consider adding a swap overload
+// to the boost namespace.
 
 #include <boost/utility/swap.hpp>
 #define BOOST_INCLUDE_MAIN
