9180396570987d2fe76e990efcb9e868c6a9775c
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-10-05 04:04:57 +0000

    documentation for proto::switch_
    git-svn-id: http://svn.boost.org/svn/boost/trunk@49134 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/proto/doc/propdocs.sh
+++ libs/proto/doc/propdocs.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-rm html.tar.gz
-tar -czf html.tar.gz html
-
-scp html.tar.gz eric_niebler@shell.sf.net:/home/groups/b/bo/boost-sandbox/htdocs/libs/proto/doc
-ssh eric_niebler@shell.sf.net 'cd /home/groups/b/bo/boost-sandbox/htdocs/libs/proto/doc; rm -rf html; tar -xzf html.tar.gz'
-
+rsync --delete --rsh=ssh --recursive html/* eric_niebler,boost-sandbox@web.sourceforge.net:/home/groups/b/bo/boost-sandbox/htdocs/libs/proto/doc/html
