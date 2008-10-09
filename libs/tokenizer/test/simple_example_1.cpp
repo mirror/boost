1d4e75921678be24dd178cc15d97d9391f33ac69
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-10-09 18:05:35 +0000

    Merged revisions 43283,43992,44388 via svnmerge from https://svn.boost.org/svn/boost/trunk
    ........
      r43283 | bemandawes | 2008-02-16 18:01:32 -0800 (Sat, 16 Feb 2008) | 1 line
      
      Show output of example program
    ........
      r43992 | marshall | 2008-04-01 18:42:32 -0700 (Tue, 01 Apr 2008) | 1 line
      
      Replaced all occurrences of non-ASCII copyright symbol with '(c)' for people using non-ASCII code pages
    ........
      r44388 | grafik | 2008-04-13 16:55:58 -0700 (Sun, 13 Apr 2008) | 1 line
      
      Move tokenizer tests into canonical test subdir structure.
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@49216 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/tokenizer/test/simple_example_1.cpp
+++ libs/tokenizer/test/simple_example_1.cpp
@@ -1,4 +1,4 @@
-// © Copyright John R. Bandela 2001. 
+// (c) Copyright John R. Bandela 2001. 
 
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
@@ -6,7 +6,8 @@
 
 // See http://www.boost.org/libs/tokenizer for documenation
 
-// simple_example_3.cpp
+
+// simple_example_1.cpp
 #include<iostream>
 #include<boost/tokenizer.hpp>
 #include<string>
@@ -14,12 +15,11 @@
 int main(){
    using namespace std;
    using namespace boost;
-   string s = "12252001";
-   int offsets[] = {2,2,4};
-   offset_separator f(offsets, offsets+3);
-   tokenizer<offset_separator> tok(s,f);
-   for(tokenizer<offset_separator>::iterator beg=tok.begin(); beg!=tok.end();++beg){
+   string s = "This is,  a test";
+   tokenizer<> tok(s);
+   for(tokenizer<>::iterator beg=tok.begin(); beg!=tok.end();++beg){
        cout << *beg << "\n";
    }
    return 0;
 }
+
--- libs/tokenizer/test/simple_example_2.cpp
+++ libs/tokenizer/test/simple_example_2.cpp
@@ -1,4 +1,4 @@
-// © Copyright John R. Bandela 2001. 
+// (c) Copyright John R. Bandela 2001. 
 
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
@@ -6,7 +6,7 @@
 
 // See http://www.boost.org/libs/tokenizer for documenation
 
-// simple_example_4.cpp
+// simple_example_2.cpp
 #include<iostream>
 #include<boost/tokenizer.hpp>
 #include<string>
@@ -14,9 +14,9 @@
 int main(){
    using namespace std;
    using namespace boost;
-   string s = "This is,  a test";
-   tokenizer<char_delimiters_separator<char> > tok(s);
-   for(tokenizer<char_delimiters_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg){
+   string s = "Field 1,\"putting quotes around fields, allows commas\",Field 3";
+   tokenizer<escaped_list_separator<char> > tok(s);
+   for(tokenizer<escaped_list_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg){
        cout << *beg << "\n";
    }
    return 0;
