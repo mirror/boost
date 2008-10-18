e5b0f9d69fba0f156376d928fe4c3825ef829539
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-10-18 22:52:04 +0000

    fix typos and misspellings
    git-svn-id: http://svn.boost.org/svn/boost/trunk@49382 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/proto/example/mixed.cpp
+++ libs/proto/example/mixed.cpp
@@ -4,7 +4,7 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// This is an example of using BOOST_PROTO_DEFINE_OPERATORS to proto-ify
+// This is an example of using BOOST_PROTO_DEFINE_OPERATORS to Protofy
 // expressions using std::vector<> and std::list, non-proto types. It is a port
 // of the Mixed example from PETE.
 // (http://www.codesourcery.com/pooma/download.html).
--- libs/proto/example/vector.cpp
+++ libs/proto/example/vector.cpp
@@ -4,7 +4,7 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// This is an example of using BOOST_PROTO_DEFINE_OPERATORS to proto-ify
+// This is an example of using BOOST_PROTO_DEFINE_OPERATORS to Protofy
 // expressions using std::vector<>, a non-proto type. It is a port of the
 // Vector example from PETE (http://www.codesourcery.com/pooma/download.html).
 
