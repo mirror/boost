c08f7a6b47d4030a9a2d8327a3f1fc5c48cf1811
gennaro_prota <gennaro_prota@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-10-21 17:57:39 +0000

    dynamic_bitset: full (recursive) sync with trunk for libs/dynamic_bitset/
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@49423 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/dynamic_bitset/example/example2.cpp
+++ libs/dynamic_bitset/example/example2.cpp
@@ -3,27 +3,30 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-// Sample output:
+//  Sample output:
+//
 //  bits(0) = 00
 //  bits(1) = 01
 //  bits(2) = 10
 //  bits(3) = 11
 
+
 #include <iostream>
 #include <boost/dynamic_bitset.hpp>
+
 int main()
 {
-  const boost::dynamic_bitset<> b0(2, 0ul); 
+  const boost::dynamic_bitset<> b0(2, 0ul);
   std::cout << "bits(0) = " << b0 << std::endl;
 
-  const boost::dynamic_bitset<> b1(2, 1ul); 
+  const boost::dynamic_bitset<> b1(2, 1ul);
   std::cout << "bits(1) = " << b1 << std::endl;
 
-  const boost::dynamic_bitset<> b2(2, 2ul); 
+  const boost::dynamic_bitset<> b2(2, 2ul);
   std::cout << "bits(2) = " << b2 << std::endl;
 
-  const boost::dynamic_bitset<> b3(2, 3ul); 
+  const boost::dynamic_bitset<> b3(2, 3ul);
   std::cout << "bits(3) = " << b3 << std::endl;
 
-  return EXIT_SUCCESS;
+  return 0;
 }
