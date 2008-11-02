e3ad70bca135c91a0b9583c06d3fc5922dbce536
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-11-02 12:20:59 +0000

    improved reference section
    git-svn-id: http://svn.boost.org/svn/boost/trunk@49530 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/proto/doc/wave.sh
+++
@@ -1,7 +0,0 @@
-../../../../dist/bin/wave\
-    -S ../../../..\
-    -S "C:\\Program Files\\Microsoft Visual Studio .NET 2003\\vc7\\include"\
-    -D _WIN32\
-    -D BOOST_PROTO_DOXYGEN_INVOKED\
-    -p 1\
-    `cygpath -d $1`
