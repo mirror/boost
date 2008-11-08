291cd38d82cdb87ac32c8bdec709244b32daad86
andysem <andysem@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-11-08 17:54:38 +0000

    Fixed order of nth_as_str forward declaration and its first usage.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@49650 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/date_time/date_generators.hpp
+++ boost/date_time/date_generators.hpp
@@ -154,6 +154,9 @@ namespace date_time {
  };
 
 
+  //! Returns nth arg as string. 1 -> "first", 2 -> "second", max is 5.
+  BOOST_DATE_TIME_DECL const char* nth_as_str(int n);
+
   //! Useful generator functor for finding holidays
   /*! Based on the idea in Cal. Calc. for finding holidays that are
    *  the 'first Monday of September'. When instantiated with
@@ -242,9 +245,6 @@ namespace date_time {
     day_of_week_type dow_;
   };
   
-  //! Returns nth arg as string. 1 -> "first", 2 -> "second", max is 5.
-  BOOST_DATE_TIME_DECL const char* nth_as_str(int n);
-
   //! Useful generator functor for finding holidays and daylight savings
   /*! Similar to nth_kday_of_month, but requires less paramters
    *  \ingroup date_alg
