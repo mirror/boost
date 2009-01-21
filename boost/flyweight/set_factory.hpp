6891888de67da1b72a43c0014c61aea7eb99d70b
joaquin <joaquin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-01-21 19:52:01 +0000

    reverted rev. 50283 prevented assoc_container_factory_class<Container> from instantiating Container when this type is an MPL placeholder expression
    git-svn-id: http://svn.boost.org/svn/boost/trunk@50709 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/flyweight/set_factory.hpp
+++ boost/flyweight/set_factory.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2006-2008 Joaquin M Lopez Munoz.
+/* Copyright 2006-2009 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -55,20 +55,6 @@ public:
   BOOST_MPL_AUX_LAMBDA_SUPPORT(
     4,set_factory_class,(Entry,Key,Compare,Allocator))
 };
-
-#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION)
-/* This is preferred to deriving from factory_marker since checking for
- * derivation forces the instantiation of the specifier, which is not
- * needed when the specifier is a placeholder expression.
- */
-
-template<
-  typename Entry,typename Key,
-  typename Compare,typename Allocator
->
-struct is_factory<set_factory_class<Entry,Key,Compare,Allocator> >:
-  boost::mpl::true_{};
-#endif
 
 /* set_factory_class specifier */
 
