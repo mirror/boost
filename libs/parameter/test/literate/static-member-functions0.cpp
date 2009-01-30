bc326db0ffca15d43224f545c6d9b0ab6b952886
dave <dave@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-01-30 00:52:33 +0000

    Check in missing file (thanks for pointing that out, David Deakins!)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@50889 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/parameter/test/literate/static-member-functions0.cpp
@@ -0,0 +1,16 @@
+
+#line 1173 "../../../../libs/parameter/doc/index.rst"
+#include <boost/parameter.hpp>
+#include <iostream>
+using namespace boost::parameter;
+#line 1162 "../../../../libs/parameter/doc/index.rst"
+BOOST_PARAMETER_NAME(arg1)
+
+struct somebody
+{
+    BOOST_PARAMETER_MEMBER_FUNCTION(
+        (void), static f, tag, (optional (arg1,(int))))
+    {
+        std::cout << arg1 << std::endl;
+    }
+};
