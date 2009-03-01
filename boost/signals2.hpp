a6a01f13714c6c1bc1a5f7413b3d86e5f980b6b0
fmhess <fmhess@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-01 20:54:34 +0000

    Merged Signals2 from sandbox to trunk.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@51520 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/signals2.hpp
@@ -0,0 +1,15 @@
+//  A convenience header for Boost.Signals2, should pull in everying in the library.
+
+// Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org
+
+#include <boost/signals2/deconstruct.hpp>
+#include <boost/signals2/deconstruct_ptr.hpp>
+#include <boost/signals2/dummy_mutex.hpp>
+#include <boost/signals2/last_value.hpp>
+#include <boost/signals2/signal.hpp>
+#include <boost/signals2/signal_type.hpp>
