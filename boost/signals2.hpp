30dfa9f250e8fbc48eb9d8c73e029c033d507647
fmhess <fmhess@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-09 21:23:14 +0000

    Fixed a few more copyright/license issues detected by inspection report.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@51671 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/signals2.hpp
+++ boost/signals2.hpp
@@ -1,5 +1,7 @@
 //  A convenience header for Boost.Signals2, should pull in everying in the library.
 
+//  Copyright (c) 2008-2009 Frank Mori Hess
+
 // Use, modification and
 // distribution is subject to the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
