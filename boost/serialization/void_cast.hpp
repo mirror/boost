d894a4147278594d2645d48267a0ce3688ee8368
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-22 19:06:55 +0000

    fix for virtual base classes
    git-svn-id: http://svn.boost.org/svn/boost/trunk@51905 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/serialization/void_cast.hpp
+++ boost/serialization/void_cast.hpp
@@ -167,7 +167,9 @@ void_caster_primitive<Derived, Base>::void_caster_primitive() :
     void_caster( 
         & type_info_implementation<Derived>::type::get_const_instance(), 
         & type_info_implementation<Base>::type::get_const_instance(),
-        reinterpret_cast<unsigned long int>(
+        // note:I wanted to display from 0 here, but at least one compiler
+        // treated 0 by not shifting it at all.
+        reinterpret_cast<std::ptrdiff_t>(
             static_cast<Derived *>(
                 reinterpret_cast<Base *>(1)
             )
