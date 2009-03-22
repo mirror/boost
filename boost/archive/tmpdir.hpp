e11e49a0054999fb574fcfb55d208296b63e9551
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-22 19:05:10 +0000

    const correctness
    git-svn-id: http://svn.boost.org/svn/boost/trunk@51904 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/archive/tmpdir.hpp
+++ boost/archive/tmpdir.hpp
@@ -30,8 +30,8 @@ namespace std {
 namespace boost {
 namespace archive {
 
-inline char * tmpdir(){
-    char *dirname;
+inline const char * tmpdir(){
+    const char *dirname;
     dirname = std::getenv("TMP");
     if(NULL == dirname)
         dirname = std::getenv("TMPDIR");
