769eae5ff39a66ea78ebbd1adc8f1313cad2bde3
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-03-24 17:33:52 +0000

    Header has moved.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@51963 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/tr1/test/cyclic_depend/shared_count.cpp
+++ libs/tr1/test/cyclic_depend/shared_count.cpp
@@ -4,7 +4,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file 
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/detail/shared_count.hpp>
+#include <boost/smart_ptr/detail/shared_count.hpp>
 
 int main()
 {
