8e8336e2ac0a7a1062cd74d6c05d484ae9b93d5e
emildotchevski <emildotchevski@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-04-07 03:02:15 +0000

    comments
    git-svn-id: http://svn.boost.org/svn/boost/trunk@52227 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/throw_exception.hpp
+++ boost/throw_exception.hpp
@@ -11,7 +11,7 @@
 //  boost/throw_exception.hpp
 //
 //  Copyright (c) 2002 Peter Dimov and Multi Media Ltd.
-//  Copyright (c) 2008 Emil Dotchevski and Reverge Studios, Inc.
+//  Copyright (c) 2008-2009 Emil Dotchevski and Reverge Studios, Inc.
 //
 //  Distributed under the Boost Software License, Version 1.0. (See
 //  accompanying file LICENSE_1_0.txt or copy at
