d03e5a8aa247b4a252488d785746a6664ea837fa
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-04-12 10:54:44 +0000

    Some small documentation fixes for the release.
    Merged revisions 52348-52350 via svnmerge from 
    https://svn.boost.org/svn/boost/trunk
    
    ........
      r52348 | danieljames | 2009-04-12 11:50:31 +0100 (Sun, 12 Apr 2009) | 1 line
      
      Use a test file that's actually valid C++.
    ........
      r52349 | danieljames | 2009-04-12 11:50:43 +0100 (Sun, 12 Apr 2009) | 3 lines
      
      Revert support for static mutable variables.
      
      Of course there's no such thing.
    ........
      r52350 | danieljames | 2009-04-12 11:50:57 +0100 (Sun, 12 Apr 2009) | 1 line
      
      Note change to operator[] in the unordered release notes.
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@52351 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/boostbook/test/doxygen/boost/example.hpp
+++ tools/boostbook/test/doxygen/boost/example.hpp
@@ -1,15 +1,15 @@
 int global_integer;
 static int global_static_integer;
-const int global_const_integer;
-static const int global_static_const_integer;
+const int global_const_integer = 1;
+static const int global_static_const_integer = 2;
 enum global_enum { enumerator };
 
 namespace example
 {
     int namespace_integer;
     static int namespace_static_integer;
-    const int namespace_const_integer;
-    static const int namespace_static_const_integer;
+    const int namespace_const_integer = 1;
+    static const int namespace_static_const_integer = 2;
     enum namespace_enum { enumerator };
 
     class example
@@ -19,7 +19,6 @@ namespace example
         static int static_integer;
         mutable int mutable_integer;
         const int const_integer;
-        static mutable int static_mutable_integer;
         static const int static_const_integer;
 
         enum class_enum { enumerator };
@@ -28,19 +27,17 @@ namespace example
         static int protected_static_integer;
         mutable int protected_mutable_integer;
         const int protected_const_integer;
-        static mutable int protected_static_mutable_integer;
         static const int protected_static_const_integer;
 
-        enum protected_class_enum { enumerator };
+        enum protected_class_enum { enumerator2 };
     private:
         int private_integer;
         static int private_static_integer;
         mutable int private_mutable_integer;
         const int private_const_integer;
-        static mutable int private_static_mutable_integer;
         static const int private_static_const_integer;
 
-        enum private_class_enum { enumerator };
+        enum private_class_enum { enumerator3 };
     };
     
     template <typename TypeParameter, int NonTypeParameter,
