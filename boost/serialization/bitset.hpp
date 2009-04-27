46589f1999549c5d8d0ae7f1240c0ec4646a89f9
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-04-27 23:08:54 +0000

    Add support for std::bitset.hpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@52637 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/serialization/bitset.hpp
@@ -0,0 +1,71 @@
+/*!
+ * \file      bitset.hpp
+ * \brief     Provides Boost.Serialization support for std::bitset
+ * \author    Brian Ravnsgaard Riis
+ * \author    Kenneth Riddile
+ * \date      16.09.2004, updated 04.03.2009
+ * \copyright 2004 Brian Ravnsgaard Riis
+ * \license   Boost Software License 1.0
+ */
+#ifndef BOOST_SERIALIZATION_BITSET_HPP
+#define BOOST_SERIALIZATION_BITSET_HPP
+
+#include <bitset>
+#include <string>
+
+#include <boost/config.hpp>
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/nvp.hpp>
+
+namespace boost{
+namespace serialization{
+
+template <class Archive, unsigned size>
+inline void save(
+    Archive & ar,
+    std::bitset<size> const & t,
+    const unsigned int version
+){
+    std::string bits = t.template to_string<
+        std::string::value_type,
+        std::string::traits_type,
+        std::string::allocator_type
+    >();
+    ar << BOOST_SERIALIZATION_NVP( bits );
+}
+
+template <class Archive, unsigned size>
+inline void load(
+    Archive & ar,
+    std::bitset<size> & t,
+    const unsigned int version
+){
+    std::string bits;
+    ar >> BOOST_SERIALIZATION_NVP( bits );
+    t = std::bitset<size>(bits);
+}
+
+template <class Archive, unsigned size>
+inline void serialize(
+    Archive & ar,
+    std::bitset<size> & t,
+    const unsigned int version
+){
+    boost::serialization::split_free( ar, t, version );
+}
+
+// don't track bitsets since that would trigger tracking
+// all over the program - which probably would be a surprise.
+// also, tracking would be hard to implement since, we're
+// serialization a representation of the data rather than
+// the data itself.
+template <unsigned size>
+struct tracking_level<std::bitset<size> >
+    : mpl::int_<track_never> {} ;
+
+} //serialization
+} //boost
+
+#endif // BOOST_SERIALIZATION_BITSET_HPP
+
