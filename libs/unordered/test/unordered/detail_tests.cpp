79fe87d5bc59cfaf00e318083f403a6074c4cc4d
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-04-29 10:05:17 +0000

    Fix the length of the prime number list. Refs #2975
    git-svn-id: http://svn.boost.org/svn/boost/trunk@52658 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/unordered/test/unordered/detail_tests.cpp
@@ -0,0 +1,27 @@
+
+// Copyright 2009 Daniel James.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// Test some of the library's implementation details.
+
+#include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
+
+#include <boost/detail/lightweight_test.hpp>
+#include <boost/range.hpp>
+
+int main() {
+    
+    // Test that the prime list has the right length.
+    // 
+    // It would have been better to automatically set the length in the header
+    // but that doesn't work on some older compilers.
+
+    BOOST_TEST_EQ(
+        boost::end(boost::unordered_detail::prime_list::value) -
+        boost::begin(boost::unordered_detail::prime_list::value),
+        boost::unordered_detail::prime_list::length);
+
+    return boost::report_errors();
+}
