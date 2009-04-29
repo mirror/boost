bce56c65d5d70e65fe010fcc45560cc6ed3d64e2
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-04-29 21:43:41 +0000

    Don't test prime_list::length on Visual C++.
    Checking the array size doesn't seem to work on it.
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@52669 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/unordered/test/unordered/detail_tests.cpp
+++ libs/unordered/test/unordered/detail_tests.cpp
@@ -16,12 +16,17 @@ int main() {
     // Test that the prime list has the right length.
     // 
     // It would have been better to automatically set the length in the header
-    // but that doesn't work on some older compilers.
+    // but that doesn't work on some compilers. It's okay that this isn't tested
+    // on all compilers, as it really only needs to be checked on one.
+
+#if !defined(BOOST_MSVC)
 
     BOOST_TEST_EQ(
         boost::end(boost::unordered_detail::prime_list::value) -
         boost::begin(boost::unordered_detail::prime_list::value),
         boost::unordered_detail::prime_list::length);
+        
+#endif
 
     return boost::report_errors();
 }
