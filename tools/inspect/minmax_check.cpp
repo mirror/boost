9537c68324ee58fdf9e4407d431dd11e2356d69e
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-05-20 20:01:53 +0000

    Filter out comments and string literals in the min/max check.  Fixes #3055
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53144 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/inspect/minmax_check.cpp
+++ tools/inspect/minmax_check.cpp
@@ -21,6 +21,14 @@ namespace
     "^\\s*#\\s*undef\\s*" // # undef
     "\\b(min|max)\\b"     // followed by min or max, whole word
     ")"
+    "|"                   // or (ignored)
+    "("
+    "//[^\\n]*"           // single line comments (//)
+    "|"
+    "/\\*.*?\\*/"         // multi line comments (/**/)
+    "|"
+    "\"(?:\\\\\\\\|\\\\\"|[^\"])*\"" // string literals
+    ")"
     "|"                   // or
     "("
     "\\b(min|max)\\b" // min or max, whole word
@@ -64,52 +72,26 @@ namespace boost
 
       for( ; cur != end; ++cur /*, ++m_errors*/ )
       {
-        // ~ experimental: try to filter out apparent
-        // ~ min/max guideline violations in one-line comments
-        //
-        // This is just a quick hack to avoid impacting the
-        // overall application design. To be on the safe side,
-        // we only aim at one-line comments; the comment must be
-        // the only non-blank content of the line, and no quote
-        // character or "*/" shall appear within it. Otherwise we
-        // give up filtering, at the cost of possible false positives.
-        //
-        const string one_line_comment_line ( "^\\s*//" );
 
-        string::const_iterator it = contents.begin();
-        string::const_iterator match_it = (*cur)[0].first;
+        if(!(*cur)[3].matched)
+        {
+          string::const_iterator it = contents.begin();
+          string::const_iterator match_it = (*cur)[0].first;
 
-        string::const_iterator line_start = it;
+          string::const_iterator line_start = it;
 
-        string::size_type line_number = 1;
-        for ( ; it != match_it; ++it) {
-            if (string::traits_type::eq(*it, '\n')) {
-                ++line_number;
-                line_start = it + 1; // could be end()
-            }
-        }
+          string::size_type line_number = 1;
+          for ( ; it != match_it; ++it) {
+              if (string::traits_type::eq(*it, '\n')) {
+                  ++line_number;
+                  line_start = it + 1; // could be end()
+              }
+          }
 
-        string::const_iterator past_line_content =
-            std::find(it, contents.end(), '\n');
-
-        // one-line comment spanning the whole line
-        // with no quotes and no "*/" pair
-        //
-        match_results<string::const_iterator> m;
-        const string whole_line( line_start, past_line_content );
-        const bool filter_out =
-               regex_search( line_start, past_line_content
-               , m, boost::regex(one_line_comment_line) )
-                 && string::npos == whole_line.find('\"')
-                 && string::npos == whole_line.find("*/")
-               ;
-
-        if (!filter_out) {
-
-                ++m_errors;
-                error( library_name, full_path, string(name())
-                    + " violation of Boost min/max guidelines on line "
-                    + boost::lexical_cast<string>( line_number ) );
+          ++m_errors;
+          error( library_name, full_path, string(name())
+              + " violation of Boost min/max guidelines on line "
+              + boost::lexical_cast<string>( line_number ) );
         }
 
       }
