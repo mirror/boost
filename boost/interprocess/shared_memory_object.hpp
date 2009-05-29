fbec98e2959b7511b53babd9188ac3f0fa87ed75
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-05-29 08:39:44 +0000

    Fix != vs. == mismatch.
    The remove method is documented to return false on error and
    true on success. The shm_unlink functions returns 0 on success.
    Therefore, we should return "shm_unlink_result == 0".
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53382 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/interprocess/shared_memory_object.hpp
+++ boost/interprocess/shared_memory_object.hpp
@@ -307,7 +307,7 @@ inline bool shared_memory_object::remove(const char *filename)
       #else
       detail::tmp_filename(filename, file_str);
       #endif
-      return 0 != shm_unlink(file_str.c_str());
+      return 0 == shm_unlink(file_str.c_str());
    }
    catch(...){
       return false;
