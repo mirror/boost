97a425fc8dddb69744ed8c5bdac843b2fc962d0b
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-05-30 10:34:37 +0000

    Properly convert original_tokens and unregistered to woption.
    Fixes #2425.
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53441 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/program_options/src/parsers.cpp
+++ libs/program_options/src/parsers.cpp
@@ -67,9 +67,15 @@ namespace boost { namespace program_options {
             woption result;
             result.string_key = opt.string_key;
             result.position_key = opt.position_key;
+            result.unregistered = opt.unregistered;
             
             std::transform(opt.value.begin(), opt.value.end(),
                            back_inserter(result.value),
+                           bind(from_utf8, _1));
+
+            std::transform(opt.original_tokens.begin(), 
+                           opt.original_tokens.end(),
+                           back_inserter(result.original_tokens),
                            bind(from_utf8, _1));
             return result;
         }
