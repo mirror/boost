075adea215227983bb031f17f373c41b37306e25
vladimir_prus <vladimir_prus@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-05-30 06:16:27 +0000

    Avoid non-inline non-template functions in headers.
    This fixes #2406. gzip.hpp had a few functions
    that are not templates or inline, which caused link errors.
    Since the functions are fairly big, this patch moves
    them into a cpp file.
    
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53427 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/iostreams/filter/gzip.hpp
+++ boost/iostreams/filter/gzip.hpp
@@ -313,7 +313,7 @@ typedef basic_gzip_compressor<> gzip_compressor;
 namespace detail {
 
 // Processes gzip headers
-class gzip_header {
+class BOOST_IOSTREAMS_DECL gzip_header {
 public:
     gzip_header() { reset(); }
 
@@ -355,7 +355,7 @@ private:
 };
 
 // Processes gzip footers
-class gzip_footer {
+class BOOST_IOSTREAMS_DECL gzip_footer {
 public:
     gzip_footer() { reset(); }
     
@@ -658,157 +658,6 @@ std::streamsize basic_gzip_compressor<Alloc>::read_string
     }
     return amt;
 }
-
-//------------------Implementation of gzip_header-----------------------------//
-
-namespace detail {
-
-void gzip_header::process(char c)
-{
-    uint8_t value = static_cast<uint8_t>(c);
-    switch (state_) {
-    case s_id1:
-        if (value != gzip::magic::id1)
-            throw gzip_error(gzip::bad_header);
-        state_ = s_id2;
-        break;
-    case s_id2:
-        if (value != gzip::magic::id2)
-            throw gzip_error(gzip::bad_header);
-        state_ = s_cm;
-        break;
-    case s_cm:
-        if (value != gzip::method::deflate)
-            throw gzip_error(gzip::bad_method);
-        state_ = s_flg;
-        break;
-    case s_flg:
-        flags_ = value;
-        state_ = s_mtime;
-        break;
-    case s_mtime:
-        mtime_ += value << (offset_ * 8);
-        if (offset_ == 3) {
-            state_ = s_xfl;
-            offset_ = 0;
-        } else {
-            ++offset_;
-        }
-        break;
-    case s_xfl:
-        state_ = s_os;
-        break;
-    case s_os:
-        os_ = value;
-        if (flags_ & gzip::flags::extra) {
-            state_ = s_extra;
-        } else if (flags_ & gzip::flags::name) {
-            state_ = s_name;
-        } else if (flags_ & gzip::flags::comment) {
-            state_ = s_comment;
-        } else if (flags_ & gzip::flags::header_crc) {
-            state_ = s_hcrc;
-        } else {
-            state_ = s_done;
-        }
-        break;
-    case s_xlen:
-        xlen_ += value << (offset_ * 8);
-        if (offset_ == 1) {
-            state_ = s_extra;
-            offset_ = 0;
-        } else {
-            ++offset_;
-        }
-        break;
-    case s_extra:
-        if (--xlen_ == 0) {
-            if (flags_ & gzip::flags::name) {
-                state_ = s_name;
-            } else if (flags_ & gzip::flags::comment) {
-                state_ = s_comment;
-            } else if (flags_ & gzip::flags::header_crc) {
-                state_ = s_hcrc;
-            } else {
-                state_ = s_done;
-            }
-        }
-        break;
-    case s_name:
-        if (c != 0) {
-            file_name_ += c;
-        } else if (flags_ & gzip::flags::comment) {
-            state_ = s_comment;
-        } else if (flags_ & gzip::flags::header_crc) {
-            state_ = s_hcrc;
-        } else {
-            state_ = s_done;
-        }
-        break;
-    case s_comment:
-        if (c != 0) {
-            comment_ += c;
-        } else if (flags_ & gzip::flags::header_crc) {
-            state_ = s_hcrc;
-        } else {
-            state_ = s_done;
-        }
-        break;
-    case s_hcrc:
-        if (offset_ == 1) {
-            state_ = s_done;
-            offset_ = 0;
-        } else {
-            ++offset_;
-        }
-        break;
-    default:
-        assert(0);
-    }
-}
-
-void gzip_header::reset()
-{
-    file_name_.clear();
-    comment_.clear();
-    os_ = flags_ = offset_ = xlen_ = 0;
-    mtime_ = 0;
-    state_ = s_id1;
-}
-
-//------------------Implementation of gzip_footer-----------------------------//
-
-void gzip_footer::process(char c)
-{
-    uint8_t value = static_cast<uint8_t>(c);
-    if (state_ == s_crc) {
-        crc_ += value << (offset_ * 8);
-        if (offset_ == 3) {
-            state_ = s_isize;
-            offset_ = 0;
-        } else {
-            ++offset_;
-        }
-    } else if (state_ == s_isize) {
-        isize_ += value << (offset_ * 8);
-        if (offset_ == 3) {
-            state_ = s_done;
-            offset_ = 0;
-        } else {
-            ++offset_;
-        }
-    } else {
-        assert(0);
-    }
-}
-
-void gzip_footer::reset()
-{
-    crc_ = isize_ = offset_ = 0;
-    state_ = s_crc;
-}
-
-} // End namespace boost::iostreams::detail.
 
 //------------------Implementation of gzip_decompressor-----------------------//
 
