2dfe30dd2b9209d855c50c06cca6143ef99e2f2a
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-06-05 01:43:39 +0000

    Use boost/detail/container_fwd.hpp. Fixes #2601
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53649 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/lambda/detail/operator_return_type_traits.hpp
+++ boost/lambda/detail/operator_return_type_traits.hpp
@@ -15,6 +15,7 @@
 #include "boost/type_traits/same_traits.hpp"
 
 #include "boost/indirect_reference.hpp"
+#include "boost/detail/container_fwd.hpp"
 
 #include <cstddef> // needed for the ptrdiff_t
 #include <iosfwd>  // for istream and ostream
@@ -57,10 +58,6 @@ template <> struct promote_code<long double> { static const int value = 700; };
 } // namespace detail
 } // namespace lambda 
 } // namespace boost
-
-namespace std {
-  template<class T> class complex;
-}
 
 namespace boost { 
 namespace lambda {
@@ -855,44 +852,6 @@ struct return_type_2<other_action<subscript_action>, A, B> {
     >::type type;
 
 };
-
-
-} // namespace lambda
-} // namespace boost
-
-
-// Forward declarations are incompatible with the libstdc++ debug mode.
-#if BOOST_WORKAROUND(__GNUC__, >= 3) && defined(_GLIBCXX_DEBUG)
-#include <string>
-#include <vector>
-#include <map>
-#include <deque>
-#else
-
-// The GCC 2.95.x uses a non-conformant deque
-#if BOOST_WORKAROUND(__GNUC__, == 2) && __GNUC_MINOR__ <= 96
-#include <deque>
-#else
-
-namespace std {
-  template <class T, class Allocator> class deque;
-}
-
-#endif
-
-namespace std {
- template <class Char, class Traits, class Allocator> class basic_string;
- template <class T, class Allocator> class vector;
- template <class Key, class T, class Cmp, class Allocator> class map;
- template <class Key, class T, class Cmp, class Allocator> class multimap;
-}
-
-#endif
-
-
-
-namespace boost { 
-namespace lambda {
 
 template<class Key, class T, class Cmp, class Allocator, class B> 
 struct plain_return_type_2<other_action<subscript_action>, std::map<Key, T, Cmp, Allocator>, B> { 
