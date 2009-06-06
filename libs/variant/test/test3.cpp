d6484870360d1dbab49c47fc0a8603d516f7798c
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-06-06 11:46:24 +0000

    Support BOOST_NO_TYPEID and BOOST_NO_IOSTREAM in Boost.Variant. Fixes #3051.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@53682 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/variant/test/test3.cpp
+++ libs/variant/test/test3.cpp
@@ -122,7 +122,9 @@ int test_main(int, char* [])
    std::ostringstream e1_str;
    e1_str << e1;
 
+#if !defined(BOOST_NO_TYPEID)
    BOOST_CHECK(e1.type() == typeid(Add));
+#endif
    BOOST_CHECK(e1_str.str() == "(13+((40+2)-(10+4)))");
 
    //Evaluate expression
