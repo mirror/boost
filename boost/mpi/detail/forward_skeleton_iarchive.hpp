9266edfea2fcaa949fa5c2df281305dc98b2a2a5
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-06-08 19:03:11 +0000

    Switch to boost/serialization/pfto.hpp. Fixes #3061.
    Merged revisions 48610 via svnmerge from 
    https://svn.boost.org/svn/boost/trunk
    
    ........
      r48610 | dgregor | 2008-09-05 14:39:26 +0100 (Fri, 05 Sep 2008) | 1 line
      
      Silence -Wunused warnings and switch to boost/serialization/pfto.hpp
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@53755 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/mpi/detail/forward_skeleton_iarchive.hpp
+++ boost/mpi/detail/forward_skeleton_iarchive.hpp
@@ -9,7 +9,7 @@
 #ifndef BOOST_MPI_DETAIL_FORWARD_SKELETON_IARCHIVE_HPP
 #define BOOST_MPI_DETAIL_FORWARD_SKELETON_IARCHIVE_HPP
 
-#include <boost/pfto.hpp>
+#include <boost/serialization/pfto.hpp>
 
 #include <boost/archive/detail/auto_link_archive.hpp>
 #include <boost/archive/detail/iserializer.hpp>
