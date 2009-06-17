442b40838dc7048ae218ed219aa8fb3d0c716f8c
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-06-17 23:24:28 +0000

    Add copyright to namespace_fail_test.cpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@54033 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/functional/hash/test/namespace_fail_test.cpp
+++ libs/functional/hash/test/namespace_fail_test.cpp
@@ -1,5 +1,13 @@
+
+// Copyright 2009 Daniel James.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// Check that I haven't inadvertantly pulled namespace std into the global
+// namespace.
+
 #include <list>
-#include <boost/functional/hash/detail/float_functions.hpp>
+#include <boost/functional/hash.hpp>
 
 typedef list<int> foo;
 
