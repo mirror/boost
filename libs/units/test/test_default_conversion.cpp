0d8ced816ad05b3502dd36077f7cd377e35e2ae5
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-07-24 02:48:02 +0000

    Fix msvc-7.1 regression
    git-svn-id: http://svn.boost.org/svn/boost/trunk@55140 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/units/test/test_default_conversion.cpp
+++ libs/units/test/test_default_conversion.cpp
@@ -43,9 +43,9 @@ BOOST_UNITS_DEFINE_CONVERSION_FACTOR(unit2_tag, unit3_tag, double, 3.0);
 typedef boost::units::multiply_typeof_helper<unit3_tag::unit_type, unit4_tag::unit_type>::type unit34_type;
 BOOST_UNITS_DEFINE_CONVERSION_FACTOR(unit5_tag, unit34_type , double, 5.0);
 
-BOOST_UNITS_DEFAULT_CONVERSION(unit1_tag, unit2_tag::unit_type);
+BOOST_UNITS_DEFAULT_CONVERSION(unit1_tag, unit2_tag);
 
-BOOST_UNITS_DEFAULT_CONVERSION(unit3_tag, unit2_tag::unit_type);
+BOOST_UNITS_DEFAULT_CONVERSION(unit3_tag, unit2_tag);
 
 BOOST_UNITS_DEFAULT_CONVERSION(unit5_tag, unit34_type);
 
