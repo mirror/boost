2286f831e1bd04f062989271f6f0754ba7cd7f12
guwi17 <guwi17@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-10-05 20:44:06 +0000

    see #3449: introduce new free functions to access matrix/vector properties (Incomplete)
     * move experimental traits of T[N] and T[M][N] to separate file
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@56600 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/ublas/traits.hpp
+++ boost/numeric/ublas/traits.hpp
@@ -647,64 +647,7 @@ namespace boost { namespace numeric { namespace ublas {
     };
 
 
-    
-    template < class T, int M, int N > 
-    struct matrix_traits < T[M][N] > {
-        typedef T              matrix_type[M][N];
-
-        typedef std::size_t          size_type;
-        typedef std::ptrdiff_t       difference_type;
-
-        typedef row_major_tag  orientation_category;
-        typedef dense_tag      storage_category;
-  
-        typedef T            value_type;
-        typedef T            *reference;
-        typedef const T      *const_reference;
-  
-        // \todo { define correct wrapper }
-        typedef matrix_reference<matrix_type>                closure_type;
-        typedef const matrix_reference<const matrix_type>    const_closure_type;
-
-	// \todo { define appropriate iterators }
-    };
-
-    template < class T, int N  > 
-    struct vector_view_traits < T[N] > {
-        typedef T              vector_type[N];
-
-        typedef std::size_t          size_type;
-        typedef std::ptrdiff_t       difference_type;
-
-        typedef dense_tag      storage_category;
-  
-        typedef T            value_type;
-        typedef const T      *const_reference;
-  
-        // \todo { define correct wrapper }
-        typedef const vector_reference<const vector_type>    const_closure_type;
-
-	typedef const_reference const_iterator;
-
-	/// iterator pointing to the first element
-	static
-	const_iterator begin(const vector_type & v) {
-	    return & (v[0]);
-	}
-	/// iterator pointing behind the last element
-	static
-	const_iterator end(const vector_type & v) {
-	    return & (v[N]);
-	}
-    };
-
-    template < class T, int N  > 
-    struct mutable_vector_traits < T[N] >  {
-  
-        typedef T *reference;
-        typedef vector_reference< T[N] > closure_type;
-
-    };
+    // Note: specializations for T[N] and T[M][N] have been moved to traits/c_array.hpp
 
 }}}
 
