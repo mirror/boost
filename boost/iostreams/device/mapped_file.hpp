9f155177b30cb35081556556df228a5d3ae624a2
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-11-12 19:18:58 +0000

    Add missing include guard. Refs #3010.
    Thanks to Bjørn Roald and Richard Smith.
    
    git-svn-id: http://svn.boost.org/svn/boost/trunk@57606 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/iostreams/device/mapped_file.hpp
+++ boost/iostreams/device/mapped_file.hpp
@@ -4,6 +4,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
 
+#ifndef BOOST_IOSTREAMS_MAPPED_FILE_HPP_INCLUDED
+#define BOOST_IOSTREAMS_MAPPED_FILE_HPP_INCLUDED
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
 #endif
@@ -592,3 +595,5 @@ operator^=(mapped_file::mapmode& a, mapped_file::mapmode b)
 } } // End namespaces iostreams, boost.
 
 #include <boost/config/abi_suffix.hpp> // pops abi_suffix.hpp pragmas
+
+#endif // #ifndef BOOST_IOSTREAMS_MAPPED_FILE_HPP_INCLUDED
