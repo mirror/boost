7797f2505d5945cac9ba028438a1c68ec0f38ca8
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-12-13 11:40:28 +0000

    Symbian patches. Fixes #3764. Fixes #3763.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@58342 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/regex/v4/perl_matcher.hpp
+++ boost/regex/v4/perl_matcher.hpp
@@ -61,7 +61,7 @@ inline bool can_start(unsigned short c, const unsigned char* map, unsigned char 
 {
    return ((c >= (1 << CHAR_BIT)) ? true : map[c] & mask);
 }
-#if !defined(__hpux) // WCHAR_MIN not usable in pp-directives.
+#if !defined(__hpux) && !defined(__WINSCW__)// WCHAR_MIN not usable in pp-directives.
 #if defined(WCHAR_MIN) && (WCHAR_MIN == 0) && !defined(BOOST_NO_INTRINSIC_WCHAR_T)
 inline bool can_start(wchar_t c, const unsigned char* map, unsigned char mask)
 {
