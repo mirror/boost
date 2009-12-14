c256c078c2c6a98061ff27b2a751490b12367ed7
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-12-14 23:53:16 +0000

    Merge from trunk to release
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@58391 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/serialization/extended_type_info_no_rtti.hpp
+++ boost/serialization/extended_type_info_no_rtti.hpp
@@ -31,6 +31,9 @@
 #include <boost/serialization/factory.hpp>
 #include <boost/serialization/throw_exception.hpp>
 
+// hijack serialization access
+#include <boost/serialization/access.hpp>
+
 #include <boost/config/abi_prefix.hpp> // must be the last header
 #ifdef BOOST_MSVC
 #  pragma warning(push)
@@ -141,7 +144,10 @@ public:
         }
     }
     virtual void destroy(void const * const p) const{
-        delete static_cast<T const *>(p) ;
+        boost::serialization::access::destroy(
+            static_cast<T const * const>(p)
+        );
+        //delete static_cast<T const * const>(p) ;
     }
 };
 
