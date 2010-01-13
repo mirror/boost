d5426f029ad1af9bf94e3c9ab79fe43d157b5450
atompkins <atompkins@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-13 11:50:43 +0000

    add uuid library
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@58968 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/uuid/test/test_tagging.cpp
@@ -0,0 +1,81 @@
+//  (C) Copyright Andy Tompkins 2009. Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  libs/uuid/test/test_tagging.cpp  -------------------------------//
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
+#include <boost/test/included/test_exec_monitor.hpp>
+#include <boost/test/test_tools.hpp>
+
+class object
+{
+public:
+    object()
+        : tag(boost::uuids::random_generator()())
+        , state(0)
+    {}
+    
+    explicit object(int state)
+        : tag(boost::uuids::random_generator()())
+        , state(state)
+    {}
+    
+    object(object const& rhs)
+        : tag(rhs.tag)
+        , state(rhs.state)
+    {}
+    
+    bool operator==(object const& rhs) const {
+        return tag == rhs.tag;
+    }
+    
+    object& operator=(object const& rhs) {
+        tag = rhs.tag;
+        state = rhs.state;
+        return *this;
+    }
+    
+    int get_state() const { return state; }
+    void set_state(int new_state) { state = new_state; }
+    
+private:
+    boost::uuids::uuid tag;
+    
+    int state;
+};
+
+template <typename elem, typename traits>
+std::basic_ostream<elem, traits>& operator<<(std::basic_ostream<elem, traits>& os, object const& o)
+{
+    os << o.get_state();
+    return os;
+}
+
+int test_main(int, char*[])
+{
+    //using boost::test_tools::output_test_stream;
+    
+    object o1(1);
+
+    object o2 = o1;
+    BOOST_CHECK_EQUAL(o1, o2);
+
+    o2.set_state(2);
+    BOOST_CHECK_EQUAL(o1, o2);
+    
+    object o3;
+    o3.set_state(3);
+
+    BOOST_CHECK_NE(o1, o3);
+    BOOST_CHECK_NE(o2, o3);
+    
+    return 0;
+}
---
+++ libs/uuid/test/test_uuid_class.cpp
@@ -0,0 +1,43 @@
+//  (C) Copyright Andy Tompkins 2009. Permission to copy, use, modify, sell and
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies. This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
+
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  libs/uuid/test/test_uuid_class.cpp  -------------------------------//
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+#include <boost/test/included/test_exec_monitor.hpp>
+#include <boost/test/test_tools.hpp>
+
+class uuid_class : public boost::uuids::uuid
+{
+public:
+    uuid_class()
+        : boost::uuids::uuid(boost::uuids::random_generator()())
+    {}
+    
+    explicit uuid_class(boost::uuids::uuid const& u)
+        : boost::uuids::uuid(u)
+    {}
+};
+
+int test_main(int, char*[])
+{
+    uuid_class u1;
+    uuid_class u2;
+    BOOST_CHECK_NE(u1, u2);
+    BOOST_CHECK_EQUAL(u1.is_nil(), false);
+    BOOST_CHECK_EQUAL(u2.is_nil(), false);
+
+    u2 = u1;
+    BOOST_CHECK_EQUAL(u1, u2);
+
+    return 0;
+}
