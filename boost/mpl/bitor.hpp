c12a09e3918868a291991a06c0c25b4114f01cdc
agurtovoy <agurtovoy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-24 05:45:36 +0000

    Fix bitand/bitor conflicts with iso64.h header
    git-svn-id: http://svn.boost.org/svn/boost/trunk@59248 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/mpl/bitor.hpp
+++ boost/mpl/bitor.hpp
@@ -2,7 +2,7 @@
 #ifndef BOOST_MPL_BITOR_HPP_INCLUDED
 #define BOOST_MPL_BITOR_HPP_INCLUDED
 
-// Copyright Aleksey Gurtovoy 2000-2004
+// Copyright Aleksey Gurtovoy 2000-2009
 // Copyright Jaap Suter 2003
 //
 // Distributed under the Boost Software License, Version 1.0. 
@@ -15,9 +15,31 @@
 // $Date$
 // $Revision$
 
+// agurt, 23/jan/10: workaround a conflict with <iso646.h> header's 
+// macros, see http://tinyurl.com/ycwdxco; 'defined(bitor)'
+// has to be checked in a separate condition, otherwise GCC complains 
+// about 'bitor' being an alternative token
+#if defined(_MSC_VER) 
+#ifndef __GCCXML__
+#if defined(bitor)
+#   pragma push_macro("bitor")
+#   undef bitor
+#   define bitor(x)
+#endif
+#endif
+#endif
+
 #define AUX778076_OP_NAME   bitor_
 #define AUX778076_OP_PREFIX bitor
 #define AUX778076_OP_TOKEN  |
 #include <boost/mpl/aux_/arithmetic_op.hpp>
+
+#if defined(_MSC_VER)
+#ifndef __GCCXML__
+#if defined(bitor)
+#   pragma pop_macro("bitor")
+#endif
+#endif
+#endif
 
 #endif // BOOST_MPL_BITOR_HPP_INCLUDED
