b3bd31fd91fcad2577f68f491e8fccf3ea218761
hkaiser <hkaiser@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-01-29 01:58:50 +0000

    Fusion: more work on adapt_struct_named
    git-svn-id: http://svn.boost.org/svn/boost/trunk@59337 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/adapted/struct/adapt_struct_named.hpp
+++ boost/fusion/adapted/struct/adapt_struct_named.hpp
@@ -5,8 +5,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#if !defined(BOOST_FUSION_NAMED_ADAPT_STRUCT_SEP_29_2009_0748PM)
-#define BOOST_FUSION_NAMED_ADAPT_STRUCT_SEP_29_2009_0748PM
+#if !defined(BOOST_FUSION_ADAPT_STRUCT_NAMED_SEP_29_2009_0748PM)
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_SEP_29_2009_0748PM
 
 #include <boost/fusion/support/tag_of_fwd.hpp>
 #include <boost/fusion/adapted/struct/extension.hpp>
@@ -31,41 +31,41 @@
 #include <boost/preprocessor/cat.hpp>
 #include <boost/mpl/int.hpp>
 #include <boost/config/no_tr1/utility.hpp>
+#include <boost/type_traits/is_const.hpp>
+#include <boost/type_traits/add_const.hpp>
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT(name, newname, bseq)                    \
-    BOOST_FUSION_ADAPT_NAMED_STRUCT_I((())(name)(newname),                      \
-        BOOST_PP_CAT(BOOST_FUSION_ADAPT_NAMED_STRUCT_X bseq, 0))                \
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED(name, newname, bseq)                    \
+    BOOST_FUSION_ADAPT_STRUCT_NAMED_I(name, (boost)(fusion)(adapted), newname,  \
+        BOOST_PP_CAT(BOOST_FUSION_ADAPT_STRUCT_NAMED_X bseq, 0))                \
     /***/
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_NS(name, ns, newname, bseq)             \
-    BOOST_FUSION_ADAPT_NAMED_STRUCT_I((ns)(name)(newname),                      \
-        BOOST_PP_CAT(BOOST_FUSION_ADAPT_NAMED_STRUCT_X bseq, 0))                \
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_NS(name, ns, newname, bseq)             \
+    BOOST_FUSION_ADAPT_STRUCT_NAMED_I(name, ns, newname,                        \
+        BOOST_PP_CAT(BOOST_FUSION_ADAPT_STRUCT_NAMED_X bseq, 0))                \
     /***/
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_X(x, y) ((x, y)) BOOST_FUSION_ADAPT_NAMED_STRUCT_Y
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_Y(x, y) ((x, y)) BOOST_FUSION_ADAPT_NAMED_STRUCT_X
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_X0
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_Y0
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_X(x, y) ((x, y)) BOOST_FUSION_ADAPT_STRUCT_NAMED_Y
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_Y(x, y) ((x, y)) BOOST_FUSION_ADAPT_STRUCT_NAMED_X
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_X0
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_Y0
 
-// BOOST_FUSION_ADAPT_NAMED_STRUCT_I generates the overarching structure and uses
+// BOOST_FUSION_ADAPT_STRUCT_NAMED_I generates the overarching structure and uses
 // uses SEQ_FOR_EACH_I to generate the "linear" substructures.
 // Thanks to Paul Mensonides for the PP macro help
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_I(names, seq)                           \
-    BOOST_PP_SEQ_FOR_EACH(                                                      \
-        BOOST_FUSION_ADAPT_NAMED_STRUCT_NS_B, _, BOOST_PP_SEQ_ELEM(0, names))   \
-        struct BOOST_PP_SEQ_ELEM(2, names)                                      \
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_I(name, ns, newname, seq)               \
+    BOOST_PP_SEQ_FOR_EACH_R(1, BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_B, _, ns)     \
+        struct newname                                                          \
         {                                                                       \
-            BOOST_PP_SEQ_ELEM(2, names)(BOOST_PP_SEQ_ELEM(1, names)& s)         \
-              : s_(s) {}                                                        \
-            BOOST_PP_SEQ_ELEM(1, names)& s_;                                    \
+            newname(name& s) : s_(s) {}                                         \
+            name& s_;                                                           \
         };                                                                      \
-    BOOST_PP_SEQ_FOR_EACH(                                                      \
-        BOOST_FUSION_ADAPT_NAMED_STRUCT_NS_E, _, BOOST_PP_SEQ_ELEM(0, names))   \
+    BOOST_PP_SEQ_FOR_EACH_R(1, BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_E, _, ns)     \
+                                                                                \
     namespace boost { namespace fusion { namespace traits                       \
     {                                                                           \
         template <>                                                             \
-        struct tag_of<BOOST_PP_SEQ_ELEM(2, names)>                              \
+        struct tag_of<BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname)>        \
         {                                                                       \
             typedef struct_tag type;                                            \
         };                                                                      \
@@ -77,13 +77,13 @@
         struct sequence_tag;                                                    \
                                                                                 \
         template<>                                                              \
-        struct sequence_tag<BOOST_PP_SEQ_ELEM(2, names)>                        \
+        struct sequence_tag<BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname)>  \
         {                                                                       \
             typedef fusion::fusion_sequence_tag type;                           \
         };                                                                      \
                                                                                 \
         template<>                                                              \
-        struct sequence_tag<BOOST_PP_SEQ_ELEM(2, names) const>                  \
+        struct sequence_tag<BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname) const> \
         {                                                                       \
             typedef fusion::fusion_sequence_tag type;                           \
         };                                                                      \
@@ -92,23 +92,32 @@
     namespace boost { namespace fusion { namespace extension                    \
     {                                                                           \
         template <>                                                             \
-        struct struct_size<BOOST_PP_SEQ_ELEM(2, names)>                         \
+        struct struct_size<BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname)>   \
           : mpl::int_<BOOST_PP_SEQ_SIZE(seq)> {};                               \
-        BOOST_PP_SEQ_FOR_EACH_I(BOOST_FUSION_ADAPT_NAMED_STRUCT_C, names, seq)  \
+        template <typename T, int N> struct class_member_proxy;                 \
+        BOOST_PP_SEQ_FOR_EACH_I(BOOST_FUSION_ADAPT_STRUCT_NAMED_C,              \
+            (BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname), name), seq)     \
     }}}                                                                         \
     /***/
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_NS_B(r, _, ns) namespace ns {           \
-    /***/
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_NS_E(r, _, ns) }                        \
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_NAME(ns, newname)                       \
+    BOOST_PP_SEQ_FOR_EACH_R(1, BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_D, _, ns)     \
+    newname                                                                     \
     /***/
 
-#define BOOST_FUSION_ADAPT_NAMED_STRUCT_C(r, names, i, xy)                      \
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_B(r, _, ns) namespace ns { 
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_D(r, _, ns) ns::
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_NS_E(r, _, ns) }
+
+#define BOOST_FUSION_ADAPT_STRUCT_NAMED_C(r, names, i, xy)                      \
     template <>                                                                 \
-    struct struct_member<BOOST_PP_SEQ_ELEM(1, names), i>                        \
+    struct struct_member<BOOST_PP_TUPLE_ELEM(2, 0, names), i>                   \
     {                                                                           \
-        typedef BOOST_PP_TUPLE_ELEM(2, 0, xy) type;                             \
-        static type& call(BOOST_PP_SEQ_ELEM(1, names)& struct_)                 \
+        typedef boost::mpl::if_<                                                \
+            boost::is_const<BOOST_PP_TUPLE_ELEM(2, 1, names)>                   \
+          , boost::add_const<BOOST_PP_TUPLE_ELEM(2, 0, xy)>::type               \
+          , BOOST_PP_TUPLE_ELEM(2, 0, xy)>::type type;                          \
+        static type& call(BOOST_PP_TUPLE_ELEM(2, 0, names)& struct_)            \
         {                                                                       \
             return struct_.s_.BOOST_PP_TUPLE_ELEM(2, 1, xy);                    \
         }                                                                       \
