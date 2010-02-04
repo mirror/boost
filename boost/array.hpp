9aa583b52d8866387611dece3f82060e1056bd49
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-02-04 18:22:13 +0000

    Applied patches to fix #2886
    git-svn-id: http://svn.boost.org/svn/boost/trunk@59476 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/array.hpp
+++ boost/array.hpp
@@ -29,6 +29,8 @@
 #if BOOST_WORKAROUND(BOOST_MSVC, >= 1400)  
 # pragma warning(push)  
 # pragma warning(disable:4996) // 'std::equal': Function call with parameters that may be unsafe
+# pragma warning(disable:4510) // boost::array<T,N>' : default constructor could not be generated 
+# pragma warning(disable:4610) // warning C4610: class 'boost::array<T,N>' can never be instantiated - user defined constructor required 
 #endif
 
 #include <cstddef>
