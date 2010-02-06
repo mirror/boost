4f5946181a3b82d1a0c0c72c58bf4625d62d22ab
t_schwinger <t_schwinger@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-02-06 14:20:05 +0000

    checks for write permission on working copy :-)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@59524 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/functional/forward_adapter.hpp
+++ boost/functional/forward_adapter.hpp
@@ -3,7 +3,7 @@
   
     Use modification and distribution are subject to the Boost Software 
     License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-    http://www.boost.org/LICENSE_1_0.txt).
+    http://www.boost.org/LICENSE_1_0.txt).  
 ==============================================================================*/
 
 #ifndef BOOST_FUNCTIONAL_FORWARD_ADAPTER_HPP_INCLUDED
