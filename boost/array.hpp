257291f5520c908decea2a69693430bd6a5dc84f
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-03-10 18:03:30 +0000

    Added support for SunCC and 'fill' operation; tickets #3893 and #3168 respectively
    git-svn-id: http://svn.boost.org/svn/boost/trunk@60436 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/array.hpp
+++ boost/array.hpp
@@ -13,6 +13,10 @@
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  *
+ * 10 Mar 2010 - (mtc) fill method added, matching resolution of the standard library working group.
+ *		See <http://www.open-std.org/jtc1/sc22/wg21/docs/lwg-defects.html#776> or Trac issue #3168
+ *		Eventually, we should remove "assign" which is now a synonym for "fill" (Marshall Clow)
+ * 10 Mar 2010 - added workaround for SUNCC and !STLPort [trac #3893] (Marshall Clow)
  * 29 Jan 2004 - c_array() added, BOOST_NO_PRIVATE_IN_AGGREGATE removed (Nico Josuttis)
  * 23 Aug 2002 - fix for Non-MSVC compilers combined with MSVC libraries.
  * 05 Aug 2001 - minor update (Nico Josuttis)
@@ -80,6 +84,11 @@ namespace boost {
                                       reference, iterator, reference> > reverse_iterator;
         typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
                                       const_reference, iterator, reference> > const_reverse_iterator;
+#elif defined(_RWSTD_NO_CLASS_PARTIAL_SPEC) 
+        typedef std::reverse_iterator<iterator, std::random_access_iterator_tag, 
+              value_type, reference, iterator, difference_type> reverse_iterator; 
+        typedef std::reverse_iterator<const_iterator, std::random_access_iterator_tag,
+              value_type, const_reference, const_iterator, difference_type> const_reverse_iterator;
 #else
         // workaround for broken reverse_iterator implementations
         typedef std::reverse_iterator<iterator,T> reverse_iterator;
@@ -160,7 +169,8 @@ namespace boost {
         }
 
         // assign one value to all elements
-        void assign (const T& value)
+        void assign (const T& value) { fill ( value ); }	// A synonym for fill
+        void fill   (const T& value)
         {
             std::fill_n(begin(),size(),value);
         }
@@ -205,6 +215,11 @@ namespace boost {
                                       reference, iterator, reference> > reverse_iterator;
         typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
                                       const_reference, iterator, reference> > const_reverse_iterator;
+#elif defined(_RWSTD_NO_CLASS_PARTIAL_SPEC) 
+        typedef std::reverse_iterator<iterator, std::random_access_iterator_tag, 
+              value_type, reference, iterator, difference_type> reverse_iterator; 
+        typedef std::reverse_iterator<const_iterator, std::random_access_iterator_tag,
+              value_type, const_reference, const_iterator, difference_type> const_reverse_iterator;
 #else
         // workaround for broken reverse_iterator implementations
         typedef std::reverse_iterator<iterator,T> reverse_iterator;
@@ -279,8 +294,9 @@ namespace boost {
         }
 
         // assign one value to all elements
-        void assign (const T& ) {   }
-
+        void assign (const T& value) { fill ( value ); }
+		void fill   (const T& ) {}
+		
         // check range (may be private because it is static)
         static reference failed_rangecheck () {
                 std::out_of_range e("attempt to access element of an empty array");
