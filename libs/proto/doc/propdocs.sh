fcc07f9b63f314d94c8ff8614f4650dbffb792ed
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-03-17 01:17:06 +0000

    Merged revisions 60633,60639,60659,60671 via svnmerge from https://svn.boost.org/svn/boost/trunk
    ........
      r60633 | eric_niebler | 2010-03-16 12:20:57 +1100 (Tue, 16 Mar 2010) | 1 line
      
      add proto::assert_matches function and friends to debug.hpp, flesh out docs for Expr concept
    ........
      r60639 | eric_niebler | 2010-03-16 15:55:13 +1100 (Tue, 16 Mar 2010) | 1 line
      
      simplify and generalize display_expr
    ........
      r60659 | eric_niebler | 2010-03-17 09:35:40 +1100 (Wed, 17 Mar 2010) | 1 line
      
      BOOST_PROTO_ASSERT_MATCHES should incur no runtime overhead
    ........
      r60671 | eric_niebler | 2010-03-17 11:43:14 +1100 (Wed, 17 Mar 2010) | 1 line
      
      add assert_matches/assert_matches_not to function list
    ........
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@60672 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/proto/doc/propdocs.sh
+++ libs/proto/doc/propdocs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync --delete --rsh=ssh --recursive html/* eric_niebler,boost-sandbox@web.sourceforge.net:/home/groups/b/bo/boost-sandbox/htdocs/libs/proto/doc/html
+rsync --delete --rsh=ssh --recursive -p html/* eric_niebler,boost-sandbox@web.sourceforge.net:/home/groups/b/bo/boost-sandbox/htdocs/libs/proto/doc/html
