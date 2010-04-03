af19f8280edfd3f0d53bbb0b6ae33f79af7b885a
neilgroves <neilgroves@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-04-03 19:31:43 +0000

    Boost.Range fixed C++0x problem due to accidental argument dependent lookup in the const_ranges unit test.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@61025 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/range/test/const_ranges.cpp
+++ libs/range/test/const_ranges.cpp
@@ -21,9 +21,6 @@
 #include <boost/test/unit_test.hpp>
 #include <string>
 
-using namespace boost;
-using namespace std;
-
 template< class T >
 const T& as_const( const T& r )
 {
@@ -33,27 +30,23 @@ const T& as_const( const T& r )
 void check_const_ranges()
 {
     std::string       foo( "foo" );
-    const std::string bar( "bar" ); 
-    
-    BOOST_CHECK( const_begin( foo )  == begin( as_const( foo ) ) );
-    BOOST_CHECK( const_end( foo )    == end( as_const( foo ) ) );
-    BOOST_CHECK( const_rbegin( foo ) == rbegin( as_const( foo ) ) );
-    BOOST_CHECK( const_rend( foo )   == rend( as_const( foo ) ) );
+    const std::string bar( "bar" );
 
-    BOOST_CHECK( const_begin( bar )  == begin( as_const( bar ) ) );
-    BOOST_CHECK( const_end( bar )    == end( as_const( bar ) ) );
-    BOOST_CHECK( const_rbegin( bar ) == rbegin( as_const( bar ) ) );
-    BOOST_CHECK( const_rend( bar )   == rend( as_const( bar ) ) );
+    BOOST_CHECK( boost::const_begin( foo )  == boost::begin( as_const( foo ) ) );
+    BOOST_CHECK( boost::const_end( foo )    == boost::end( as_const( foo ) ) );
+    BOOST_CHECK( boost::const_rbegin( foo ) == boost::rbegin( as_const( foo ) ) );
+    BOOST_CHECK( boost::const_rend( foo )   == boost::rend( as_const( foo ) ) );
+
+    BOOST_CHECK( boost::const_begin( bar )  == boost::begin( as_const( bar ) ) );
+    BOOST_CHECK( boost::const_end( bar )    == boost::end( as_const( bar ) ) );
+    BOOST_CHECK( boost::const_rbegin( bar ) == boost::rbegin( as_const( bar ) ) );
+    BOOST_CHECK( boost::const_rend( bar )   == boost::rend( as_const( bar ) ) );
 
 }
 
-
-
-using boost::unit_test::test_suite;
-
-test_suite* init_unit_test_suite( int argc, char* argv[] )
+boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
 {
-    test_suite* test = BOOST_TEST_SUITE( "Range Test Suite" );
+    boost::unit_test::test_suite* test = BOOST_TEST_SUITE( "Range Test Suite" );
 
     test->add( BOOST_TEST_CASE( &check_const_ranges ) );
 
