31455c497251310ca090047ac9bb197287c56de5
neilgroves <neilgroves@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-04-07 23:40:53 +0000

    Boost.Range modification in an attempt to improve the compatibility of the adjacent_filtered and uniqued adaptors.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@61142 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/range/adaptor/uniqued.hpp
+++ boost/range/adaptor/uniqued.hpp
@@ -15,68 +15,68 @@
 
 namespace boost
 {
-	
+
 	namespace range_detail
 	{
 		struct unique_forwarder { };
-		
+
 		struct unique_not_equal_to
 		{
 			typedef bool result_type;
-			
+
 			template< class T >
 			bool operator()( const T& l, const T& r ) const
 			{
 				return !(l == r);
 			}
 		};
-		
+
 		template<class ForwardRng>
-		class unique_range : public adjacent_filter_range<unique_not_equal_to, ForwardRng>
+		class unique_range : public adjacent_filter_range<unique_not_equal_to, ForwardRng, true>
 		{
-			typedef adjacent_filter_range<unique_not_equal_to, ForwardRng> base;
+			typedef adjacent_filter_range<unique_not_equal_to, ForwardRng, true> base;
 		public:
 		    explicit unique_range(ForwardRng& rng)
-		        : base(unique_not_equal_to(), rng, true)
+		        : base(unique_not_equal_to(), rng)
 		    {
 		    }
 		};
-		
+
 		template< class ForwardRng >
-		inline unique_range<ForwardRng> 
-		operator|( ForwardRng& r, 
+		inline unique_range<ForwardRng>
+		operator|( ForwardRng& r,
 				   unique_forwarder )
 		{
 			return unique_range<ForwardRng>(r);
 		}
-	
+
 		template< class ForwardRng >
-		inline unique_range<const ForwardRng> 
-		operator|( const ForwardRng& r, 
+		inline unique_range<const ForwardRng>
+		operator|( const ForwardRng& r,
 				   unique_forwarder )
 		{
 			return unique_range<const ForwardRng>(r);
 		}
-		
+
 	} // 'range_detail'
-	
+
 	using range_detail::unique_range;
-	
+
 	namespace adaptors
-	{ 
+	{
 		namespace
 		{
-			const range_detail::unique_forwarder uniqued = 
+			const range_detail::unique_forwarder uniqued =
 				       range_detail::unique_forwarder();
 		}
-		
+
 		template<class ForwardRange>
 		inline unique_range<ForwardRange>
 		unique(ForwardRange& rng)
 		{
 		    return unique_range<ForwardRange>(rng);
 	    }
-	    
+
 	    template<class ForwardRange>
 	    inline unique_range<const ForwardRange>
 	    unique(const ForwardRange& rng)
@@ -84,7 +84,7 @@ namespace boost
 	        return unique_range<const ForwardRange>(rng);
         }
 	} // 'adaptors'
-	
+
 }
 
 #endif
