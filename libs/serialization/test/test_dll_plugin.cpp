2cd2bd2faa11219f27a1fd8e35ea59b8e1131099
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-04-18 17:13:17 +0000

    changed name from test_plugin to test_dll_plugin
    git-svn-id: http://svn.boost.org/svn/boost/trunk@61364 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/serialization/test/test_dll_plugin.cpp
+++ libs/serialization/test/test_dll_plugin.cpp
@@ -1,5 +1,5 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// test_plugin.cpp
+// test_dll_plugin.cpp
 
 // (C) Copyright 2002 Robert Ramey - http://www.rrsd.com . 
 // Use, modification and distribution is subject to the Boost Software
