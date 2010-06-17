335941a48b742530a5df116366c3bad5c950da14
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-06-17 19:14:14 +0000

    Allow bzip2_decompressor to process multiple concatenated streams.  Fixes #3853.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@63057 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/iostreams/test/bzip2_test.cpp
+++ libs/iostreams/test/bzip2_test.cpp
@@ -18,6 +18,7 @@ using namespace boost;
 using namespace boost::iostreams;
 using namespace boost::iostreams::test;
 using boost::unit_test::test_suite;  
+namespace io = boost::iostreams;
 
 struct bzip2_alloc : std::allocator<char> { };
 
@@ -51,9 +52,45 @@ void bzip2_test()
     }
 }    
 
+void multiple_member_test()
+{
+    text_sequence      data;
+    std::vector<char>  temp, dest;
+
+    // Write compressed data to temp, twice in succession
+    filtering_ostream out;
+    out.push(bzip2_compressor());
+    out.push(io::back_inserter(temp));
+    io::copy(make_iterator_range(data), out);
+    out.push(io::back_inserter(temp));
+    io::copy(make_iterator_range(data), out);
+
+    // Read compressed data from temp into dest
+    filtering_istream in;
+    in.push(bzip2_decompressor());
+    in.push(array_source(&temp[0], temp.size()));
+    io::copy(in, io::back_inserter(dest));
+
+    // Check that dest consists of two copies of data
+    BOOST_REQUIRE_EQUAL(data.size() * 2, dest.size());
+    BOOST_CHECK(std::equal(data.begin(), data.end(), dest.begin()));
+    BOOST_CHECK(std::equal(data.begin(), data.end(), dest.begin() + dest.size() / 2));
+
+    dest.clear();
+    io::copy(
+        array_source(&temp[0], temp.size()),
+        io::compose(bzip2_decompressor(), io::back_inserter(dest)));
+
+    // Check that dest consists of two copies of data
+    BOOST_REQUIRE_EQUAL(data.size() * 2, dest.size());
+    BOOST_CHECK(std::equal(data.begin(), data.end(), dest.begin()));
+    BOOST_CHECK(std::equal(data.begin(), data.end(), dest.begin() + dest.size() / 2));
+}
+
 test_suite* init_unit_test_suite(int, char* []) 
 {
     test_suite* test = BOOST_TEST_SUITE("bzip2 test");
     test->add(BOOST_TEST_CASE(&bzip2_test));
+    test->add(BOOST_TEST_CASE(&multiple_member_test));
     return test;
 }
