5e4433471920e293cb9eff7f16de208033404922
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-06-24 20:30:57 +0000

    Add back missing Boost license.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@63297 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/iostreams/src/mapped_file.cpp
+++ libs/iostreams/src/mapped_file.cpp
@@ -2,6 +2,8 @@
 // (C) Copyright Jonathan Turkanis 2004.
 // (C) Copyright Jonathan Graehl 2004.
 // (C) Copyright Jorge Lodos 2008.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
 
 // Define BOOST_IOSTREAMS_SOURCE so that <boost/iostreams/detail/config.hpp>
 // knows that we are building the library (possibly exporting code), rather
