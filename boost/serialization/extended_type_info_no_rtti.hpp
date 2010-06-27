3f0ef7029f3a1d42bac0454bb27a2a18a598d622
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-06-27 18:46:39 +0000

    attempt to address failures with MingW
    git-svn-id: http://svn.boost.org/svn/boost/trunk@63394 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/serialization/extended_type_info_no_rtti.hpp
+++ boost/serialization/extended_type_info_no_rtti.hpp
@@ -26,6 +26,7 @@
 #include <boost/type_traits/is_polymorphic.hpp>
 #include <boost/type_traits/remove_const.hpp>
 
+#include <boost/serialization/config.hpp>
 #include <boost/serialization/static_warning.hpp>
 #include <boost/serialization/singleton.hpp>
 #include <boost/serialization/extended_type_info.hpp>
