049de6dd52c84d9b66eeb9eec4db7d13e2c6d959
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-07-01 11:59:21 +0000

    Merge a few minor config tweaks from Trunk
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@63470 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/config/compiler/visualc.hpp
+++ boost/config/compiler/visualc.hpp
@@ -68,7 +68,6 @@
 #  define BOOST_NO_POINTER_TO_MEMBER_TEMPLATE_PARAMETERS
 #  define BOOST_NO_IS_ABSTRACT
 #  define BOOST_NO_FUNCTION_TYPE_SPECIALIZATIONS
-#  define BOOST_NO_EXTERN_TEMPLATE
 // TODO: what version is meant here? Have there really been any fixes in cl 12.01 (as e.g. shipped with eVC4)?
 #  if (_MSC_VER > 1200)
 #     define BOOST_NO_MEMBER_FUNCTION_SPECIALIZATIONS
