0a23d5e6764ca06d7125196da61c78d2a1daf2d4
david.bellot <david.bellot@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-07-05 06:06:24 +0000

    last update for 1.44
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@63644 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/ublas/traits.hpp
+++ boost/numeric/ublas/traits.hpp
@@ -23,6 +23,11 @@
 
 #include <boost/type_traits.hpp>
 #include <complex>
+#include <boost/typeof/typeof.hpp>
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits/is_float.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/mpl/and.hpp>
 
 // anonymous namespace to avoid ADL issues
 namespace {
