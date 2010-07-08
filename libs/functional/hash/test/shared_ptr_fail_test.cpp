97bb4c13e7ff3238200eca174cffdd49c4e38c37
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-07-08 20:48:30 +0000

    Fix inspect issues.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@63762 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/functional/hash/test/shared_ptr_fail_test.cpp
+++ libs/functional/hash/test/shared_ptr_fail_test.cpp
@@ -1,3 +1,8 @@
+
+// Copyright 2010 Daniel James.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/functional/hash.hpp>
 #include <boost/shared_ptr.hpp>
 
--- libs/iostreams/test/deprecated_file_descriptor_test.cpp
+++ libs/iostreams/test/deprecated_file_descriptor_test.cpp
@@ -1,3 +1,7 @@
+// (C) Copyright 2010 Daniel James
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
+
 #include <boost/iostreams/device/file_descriptor.hpp>
 #include <boost/iostreams/stream.hpp>
 #include <boost/test/unit_test.hpp>
