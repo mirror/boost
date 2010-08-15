a27eeacf9c952149487f1e221434feb3db36c436
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-08-15 16:45:27 +0000

    Don't put nested lists inside a list item's paragraph.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@64828 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/quickbook/detail/actions.cpp
+++ tools/quickbook/detail/actions.cpp
@@ -204,7 +204,7 @@ namespace quickbook
             list_marks.pop();
             out << std::string((mark == '#') ? "\n</orderedlist>" : "\n</itemizedlist>");
             if (list_marks.size() >= 1)
-                out << list_item_post;
+                out << "</listitem>";
         }
 
         list_indent = -1; // reset
@@ -244,12 +244,12 @@ namespace quickbook
             {
                 // Make this new list a child of the previous list.
                 // The previous listelem has already ended so we erase
-                // list_item_post to accomodate this sub-list. We'll close
+                // "</listitem>" to accomodate this sub-list. We'll close
                 // the listelem later.
 
                 std::string str;
                 out.swap(str);
-                std::string::size_type pos = str.rfind(list_item_post);
+                std::string::size_type pos = str.rfind("</listitem>");
                 BOOST_ASSERT(pos <= str.size());
                 str.erase(str.begin()+pos, str.end());
                 out << str;
@@ -268,7 +268,7 @@ namespace quickbook
                 list_marks.pop();
                 out << std::string((mark == '#') ? "\n</orderedlist>" : "\n</itemizedlist>");
                 if (list_marks.size() >= 1)
-                    out << list_item_post;
+                    out << "</listitem>";
             }
         }
 
