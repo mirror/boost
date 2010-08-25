580e6cdce0211c9392c904e9c61b50314d3d23d3
fmhess <fmhess@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-08-25 18:30:08 +0000

    Don't need mpl/not.hpp workaround for Boost.Variant any more.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@65001 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/signals2/slot_base.hpp
+++ boost/signals2/slot_base.hpp
@@ -19,7 +19,6 @@
 #include <boost/signals2/expired_slot.hpp>
 #include <boost/signals2/signal_base.hpp>
 #include <boost/throw_exception.hpp>
-#include <boost/mpl/not.hpp> // work around missing include in Boost.Variant headers
 #include <boost/variant/apply_visitor.hpp>
 #include <boost/variant/variant.hpp>
 #include <vector>
