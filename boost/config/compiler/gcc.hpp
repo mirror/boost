3a7814071a456e5aa67589e164ccc3d6908e5f5e
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-09-22 08:24:07 +0000

    Fix for cygwin symbol visibility - sometimes _WIN32 may be defined even on cygwin. Fixes #4670.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@65530 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/config/compiler/gcc.hpp
+++ boost/config/compiler/gcc.hpp
@@ -115,7 +115,7 @@
 // Dynamic shared object (DSO) and dynamic-link library (DLL) support
 //
 #if __GNUC__ >= 4
-#  if defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+#  if (defined(_WIN32) || defined(__WIN32__) || defined(WIN32)) && !defined(__CYGWIN__)
      // All Win32 development environments, including 64-bit Windows and MinGW, define 
      // _WIN32 or one of its variant spellings. Note that Cygwin is a POSIX environment,
      // so does not define _WIN32 or its variants.
