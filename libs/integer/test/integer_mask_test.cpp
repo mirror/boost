8d49c8b69b9f60fc961bfc63b0e61fa066fc91dd
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-10-02 12:14:04 +0000

    Fix logic in cstdint to prefer smaller integers when possible. Improve PP-logic in integer-traits. Suppress some compiler warnings.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@65721 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/integer/test/integer_mask_test.cpp
+++ libs/integer/test/integer_mask_test.cpp
@@ -21,25 +21,35 @@
 #pragma warning(disable:4127) // conditional expression is constant
 #endif
 
+#if defined(BOOST_HAS_LONG_LONG)
+#define MASK_TYPE ::boost::ulong_long_type
+#elif defined(BOOST_HAS_MS_INT64)
+#define MASK_TYPE unsigned __int64
+#else
+#define MASK_TYPE unsigned long
+#endif
+
+#define ONE (static_cast<MASK_TYPE>(1))
+
 #define PRIVATE_HIGH_BIT_SLOW_TEST(v)  BOOST_TEST( ::boost::high_bit_mask_t< \
- (v) >::high_bit == (1ul << (v)) );
+ (v) >::high_bit == (ONE << (v)) );
 #define PRIVATE_HIGH_BIT_FAST_TEST(v)  BOOST_TEST( ::boost::high_bit_mask_t< \
- (v) >::high_bit_fast == (1ul << (v)) );
+ (v) >::high_bit_fast == (ONE << (v)) );
 #define PRIVATE_HIGH_BIT_TEST(v)  do { PRIVATE_HIGH_BIT_SLOW_TEST(v); \
  PRIVATE_HIGH_BIT_FAST_TEST(v); } while (false)
 
 #define PRIVATE_LOW_BITS_SLOW_TEST(v)  \
    do{ \
-      unsigned long mask = 0;\
+      MASK_TYPE mask = 0;\
       if(v > 0)\
-         { mask = ((1ul << (v-1)) - 1); mask <<= 1; mask |= 1; }\
+         { mask = ((ONE << (v-1)) - 1); mask <<= 1; mask |= 1; }\
       BOOST_TEST( ::boost::low_bits_mask_t< (v) >::sig_bits ==  mask); \
    }while(false);
 #define PRIVATE_LOW_BITS_FAST_TEST(v)  \
    do{ \
-      unsigned long mask = 0;\
+      MASK_TYPE mask = 0;\
       if(v > 0)\
-         { mask = ((1ul << (v-1)) - 1); mask <<= 1; mask |= 1; }\
+         { mask = ((ONE << (v-1)) - 1); mask <<= 1; mask |= 1; }\
       BOOST_TEST( ::boost::low_bits_mask_t< (v) >::sig_bits_fast == mask);\
    }while(false);
 #define PRIVATE_LOW_BITS_TEST(v)  do { PRIVATE_LOW_BITS_SLOW_TEST(v); \
@@ -52,6 +62,41 @@ int main( int, char*[] )
     using std::endl;
 
     cout << "Doing high_bit_mask_t tests." << endl;
+
+#if defined(BOOST_HAS_LONG_LONG) || defined(BOOST_HAS_MS_INT64)
+    PRIVATE_HIGH_BIT_TEST( 63 );
+    PRIVATE_HIGH_BIT_TEST( 62 );
+    PRIVATE_HIGH_BIT_TEST( 61 );
+    PRIVATE_HIGH_BIT_TEST( 60 );
+    PRIVATE_HIGH_BIT_TEST( 59 );
+    PRIVATE_HIGH_BIT_TEST( 58 );
+    PRIVATE_HIGH_BIT_TEST( 57 );
+    PRIVATE_HIGH_BIT_TEST( 56 );
+    PRIVATE_HIGH_BIT_TEST( 55 );
+    PRIVATE_HIGH_BIT_TEST( 54 );
+    PRIVATE_HIGH_BIT_TEST( 53 );
+    PRIVATE_HIGH_BIT_TEST( 52 );
+    PRIVATE_HIGH_BIT_TEST( 51 );
+    PRIVATE_HIGH_BIT_TEST( 50 );
+    PRIVATE_HIGH_BIT_TEST( 49 );
+    PRIVATE_HIGH_BIT_TEST( 48 );
+    PRIVATE_HIGH_BIT_TEST( 47 );
+    PRIVATE_HIGH_BIT_TEST( 46 );
+    PRIVATE_HIGH_BIT_TEST( 45 );
+    PRIVATE_HIGH_BIT_TEST( 44 );
+    PRIVATE_HIGH_BIT_TEST( 43 );
+    PRIVATE_HIGH_BIT_TEST( 42 );
+    PRIVATE_HIGH_BIT_TEST( 41 );
+    PRIVATE_HIGH_BIT_TEST( 40 );
+    PRIVATE_HIGH_BIT_TEST( 39 );
+    PRIVATE_HIGH_BIT_TEST( 38 );
+    PRIVATE_HIGH_BIT_TEST( 37 );
+    PRIVATE_HIGH_BIT_TEST( 36 );
+    PRIVATE_HIGH_BIT_TEST( 35 );
+    PRIVATE_HIGH_BIT_TEST( 34 );
+    PRIVATE_HIGH_BIT_TEST( 33 );
+    PRIVATE_HIGH_BIT_TEST( 32 );
+#endif
     PRIVATE_HIGH_BIT_TEST( 31 );
     PRIVATE_HIGH_BIT_TEST( 30 );
     PRIVATE_HIGH_BIT_TEST( 29 );
@@ -86,6 +131,41 @@ int main( int, char*[] )
     PRIVATE_HIGH_BIT_TEST(  0 );
 
     cout << "Doing low_bits_mask_t tests." << endl;
+
+#if defined(BOOST_HAS_LONG_LONG) || defined(BOOST_HAS_MS_INT64)
+    PRIVATE_LOW_BITS_TEST( 64 );
+    PRIVATE_LOW_BITS_TEST( 63 );
+    PRIVATE_LOW_BITS_TEST( 62 );
+    PRIVATE_LOW_BITS_TEST( 61 );
+    PRIVATE_LOW_BITS_TEST( 60 );
+    PRIVATE_LOW_BITS_TEST( 59 );
+    PRIVATE_LOW_BITS_TEST( 58 );
+    PRIVATE_LOW_BITS_TEST( 57 );
+    PRIVATE_LOW_BITS_TEST( 56 );
+    PRIVATE_LOW_BITS_TEST( 55 );
+    PRIVATE_LOW_BITS_TEST( 54 );
+    PRIVATE_LOW_BITS_TEST( 53 );
+    PRIVATE_LOW_BITS_TEST( 52 );
+    PRIVATE_LOW_BITS_TEST( 51 );
+    PRIVATE_LOW_BITS_TEST( 50 );
+    PRIVATE_LOW_BITS_TEST( 49 );
+    PRIVATE_LOW_BITS_TEST( 48 );
+    PRIVATE_LOW_BITS_TEST( 47 );
+    PRIVATE_LOW_BITS_TEST( 46 );
+    PRIVATE_LOW_BITS_TEST( 45 );
+    PRIVATE_LOW_BITS_TEST( 44 );
+    PRIVATE_LOW_BITS_TEST( 43 );
+    PRIVATE_LOW_BITS_TEST( 42 );
+    PRIVATE_LOW_BITS_TEST( 41 );
+    PRIVATE_LOW_BITS_TEST( 40 );
+    PRIVATE_LOW_BITS_TEST( 39 );
+    PRIVATE_LOW_BITS_TEST( 38 );
+    PRIVATE_LOW_BITS_TEST( 37 );
+    PRIVATE_LOW_BITS_TEST( 36 );
+    PRIVATE_LOW_BITS_TEST( 35 );
+    PRIVATE_LOW_BITS_TEST( 34 );
+    PRIVATE_LOW_BITS_TEST( 33 );
+#endif
     PRIVATE_LOW_BITS_TEST( 32 );
     PRIVATE_LOW_BITS_TEST( 31 );
     PRIVATE_LOW_BITS_TEST( 30 );
