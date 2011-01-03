217e397b5c8779875832065888b9d8ead205434e
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-03 23:30:32 +0000

    Boost.Ratio: Added missing boost/ratio.hpp
    git-svn-id: http://svn.boost.org/svn/boost/trunk@67641 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/ratio.hpp
@@ -0,0 +1,14 @@
+//  ratio.hpp  ---------------------------------------------------------------//
+
+//  Copyright 2010 Vicente J. Botet Escriba
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+
+#ifndef BOOST_RATIO_HPP
+#define BOOST_RATIO_HPP
+
+#include <boost/ratio/ratio.hpp>
+
+#endif  // BOOST_RATIO_HPP
