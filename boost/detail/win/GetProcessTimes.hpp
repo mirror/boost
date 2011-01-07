8805f3774d30ebae256d4c0be78c6b5e37c642af
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-07 07:51:15 +0000

    Boost.Chrono: Added detail/win files used by Boost.Chrono to manage with HEAADER_ONLY libs
    git-svn-id: http://svn.boost.org/svn/boost/trunk@67749 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/detail/win/GetProcessTimes.hpp
@@ -0,0 +1,33 @@
+//  GetProcessTimes.hpp  --------------------------------------------------------------//
+
+//  Copyright 2010 Vicente J. Botet Escriba
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+
+#ifndef BOOST_DETAIL_WIN_GETPROCESSTIMES_HPP
+#define BOOST_DETAIL_WIN_GETPROCESSTIMES_HPP
+
+#include <boost/detail/win/time.hpp>
+
+namespace boost {
+namespace detail {
+namespace win32 {
+#if defined( BOOST_USE_WINDOWS_H )
+    using ::GetProcessTimes;
+#else
+    extern "C" __declspec(dllimport) BOOL_ WINAPI
+        GetProcessTimes(
+            HANDLE_ hProcess,
+            LPFILETIME_ lpCreationTime,
+            LPFILETIME_ lpExitTime,
+            LPFILETIME_ lpKernelTime,
+            LPFILETIME_ lpUserTime
+        );
+#endif
+}
+}
+}
+
+#endif // BOOST_DETAIL_WIN_GETPROCESSTIMES_HPP
