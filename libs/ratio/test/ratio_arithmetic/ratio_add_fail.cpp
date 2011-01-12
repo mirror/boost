7539390a242a44a67fe23cb3065b7730bd55a726
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-12 05:05:08 +0000

    Boost.Ratio: Force failure to debug condition
    git-svn-id: http://svn.boost.org/svn/boost/trunk@68029 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/ratio/test/ratio_arithmetic/ratio_add_fail.cpp
+++ libs/ratio/test/ratio_arithmetic/ratio_add_fail.cpp
@@ -14,10 +14,14 @@
 #include <boost/ratio.hpp>
 #include <boost/integer_traits.hpp>
 
+#if !defined(BOOST_NO_STATIC_ASSERT)
+#define NOTHING ""
+#endif
+
 #define BOOST_RATIO_INTMAX_T_MAX boost::integer_traits<boost::intmax_t>::const_max
 
 typedef boost::ratio<BOOST_RATIO_INTMAX_T_MAX, 1> R1;
 typedef boost::ratio<1, 1> R2;
 typedef boost::ratio_add<R1, R2>::type RT;
 
-BOOST_RATIO_STATIC_ASSERT(RT::num==0, (RT));
+BOOST_RATIO_STATIC_ASSERT(RT::num==0, NOTHING, (RT));
