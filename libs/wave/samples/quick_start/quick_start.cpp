fc6c8118736f1b2ca1579cdf4cfce0de14375f4c
hkaiser <hkaiser@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-14 03:35:24 +0000

    Wave: After preprocessing the body of any #pragma wave option() the wave tool now concatenates all adjacent string literals into a single string literal.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@68146 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/wave/samples/quick_start/quick_start.cpp
+++ libs/wave/samples/quick_start/quick_start.cpp
@@ -3,7 +3,7 @@
 
     http://www.boost.org/
 
-    Copyright (c) 2001-2010 Hartmut Kaiser. Distributed under the Boost 
+    Copyright (c) 2001-2011 Hartmut Kaiser. Distributed under the Boost 
     Software License, Version 1.0. (See accompanying file 
     LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
@@ -36,10 +36,10 @@ boost::wave::util::file_position_type current_position;
 
     try {
 //[quick_start_main
-    //  The following preprocesses the given input file (given by argv[1]).
+    //  The following preprocesses the input file given by argv[1].
     //  Open and read in the specified input file.
-    std::ifstream instream(argv[1]);
-    std::string instring;
+        std::ifstream instream(argv[1]);
+        std::string instring;
 
         if (!instream.is_open()) {
             std::cerr << "Could not open input file: " << argv[1] << std::endl;
@@ -48,40 +48,42 @@ boost::wave::util::file_position_type current_position;
         instream.unsetf(std::ios::skipws);
         instring = std::string(std::istreambuf_iterator<char>(instream.rdbuf()),
                                 std::istreambuf_iterator<char>());
-            
-    //  This token type is one of the central types used throughout the library, 
-    //  because it is a template parameter to some of the public classes and  
-    //  instances of this type are returned from the iterators.
+
+    //  This token type is one of the central types used throughout the library. 
+    //  It is a template parameter to some of the public classes and instances 
+    //  of this type are returned from the iterators.
         typedef boost::wave::cpplexer::lex_token<> token_type;
-    
+
     //  The template boost::wave::cpplexer::lex_iterator<> is the lexer type to
     //  to use as the token source for the preprocessing engine. It is 
     //  parametrized with the token type.
         typedef boost::wave::cpplexer::lex_iterator<token_type> lex_iterator_type;
-        
-    //  This is the resulting context type to use. The first template parameter
-    //  should match the iterator type to be used during construction of the
-    //  corresponding context object (see below).
+
+    //  This is the resulting context type. The first template parameter should
+    //  match the iterator type used during construction of the context 
+    //  instance (see below). It is the type of the underlying input stream.
         typedef boost::wave::context<std::string::iterator, lex_iterator_type>
             context_type;
 
     //  The preprocessor iterator shouldn't be constructed directly. It is 
-    //  to be generated through a wave::context<> object. This wave:context<> 
-    //  object is to be used additionally to initialize and define different 
-    //  parameters of the actual preprocessing (not done here).
+    //  generated through a wave::context<> object. This wave:context<> object 
+    //  is additionally used to initialize and define different parameters of 
+    //  the actual preprocessing (not done here).
     //
     //  The preprocessing of the input stream is done on the fly behind the 
-    //  scenes during iteration over the context_type::iterator_type stream.
-    context_type ctx (instring.begin(), instring.end(), argv[1]);
+    //  scenes during iteration over the range of context_type::iterator_type 
+    //  instances.
+        context_type ctx (instring.begin(), instring.end(), argv[1]);
 
-    //  Get the preprocessor iterators and use them to generate 
-    //  the token sequence.
-    context_type::iterator_type first = ctx.begin();
-    context_type::iterator_type last = ctx.end();
-        
+    //  Get the preprocessor iterators and use them to generate the token 
+    //  sequence.
+        context_type::iterator_type first = ctx.begin();
+        context_type::iterator_type last = ctx.end();
 
     //  The input stream is preprocessed for you while iterating over the range
-    //  [first, last)
+    //  [first, last). The dereferenced iterator returns tokens holding 
+    //  information about the preprocessed input stream, such as token type,
+    //  token value, and position.
         while (first != last) {
             current_position = (*first).get_position();
             std::cout << (*first).get_value();
