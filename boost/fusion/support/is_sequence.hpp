6d40708f171f14916710c7a0ed3156f81979d827
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-19 04:28:17 +0000

    fix 1.46 showstopper, fix #5084
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@68264 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/support/is_sequence.hpp
+++ boost/fusion/support/is_sequence.hpp
@@ -1,7 +1,7 @@
 /*=============================================================================
     Copyright (c) 2001-2006 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_IS_SEQUENCE_05052005_1002)
@@ -62,6 +62,11 @@ namespace boost { namespace fusion
           : extension::is_sequence_impl<
                 typename fusion::detail::tag_of<T>::type
             >::template apply<T>
+        {};
+
+        template <typename Sequence, typename Enable = void>
+        struct is_native_fusion_sequence
+          : is_convertible<Sequence, detail::from_sequence_convertible_type>
         {};
     }
 }}
