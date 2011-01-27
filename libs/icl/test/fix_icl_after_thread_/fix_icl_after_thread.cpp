430dab7224469e72dd2d3552081646033f8298a4
jofaber <jofaber@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-27 22:29:16 +0000

    Tests to check for the bug fix described in Ticket #5135. The affected code in boost/icl/type_traits/is_interval.hpp has been fixed before this commit already.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@68492 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/icl/test/fix_icl_after_thread_/fix_icl_after_thread.cpp
+++ libs/icl/test/fix_icl_after_thread_/fix_icl_after_thread.cpp
@@ -1,37 +1,35 @@
 /*-----------------------------------------------------------------------------+    
-Copyright (c) 2008-2009: Joachim Faulhaber
+Copyright (c) 2011-2011: Joachim Faulhaber
 +------------------------------------------------------------------------------+
    Distributed under the Boost Software License, Version 1.0.
       (See accompanying file LICENCE.txt or copy at
            http://www.boost.org/LICENSE_1_0.txt)
 +-----------------------------------------------------------------------------*/
-#define BOOST_TEST_MODULE icl::casual unit test
+#define BOOST_TEST_MODULE icl::fix_icl_after_thread unit test
 #include <libs/icl/test/disable_test_warnings.hpp>
 #include <string>
-#include <vector>
 #include <boost/mpl/list.hpp>
 #include "../unit_test_unwarned.hpp"
-#include <boost/test/test_case_template.hpp>
 
 // interval instance types
 #include "../test_type_lists.hpp"
 #include "../test_value_maker.hpp"
+#include "../test_functions.hpp"
 
+#include <boost/thread.hpp>
 #include <boost/icl/interval_map.hpp>
+#include <boost/icl/split_interval_map.hpp>
+#include <boost/icl/separate_interval_set.hpp>
+#include <boost/icl/split_interval_set.hpp>
 
 using namespace std;
 using namespace boost;
 using namespace unit_test;
 using namespace boost::icl;
 
-BOOST_AUTO_TEST_CASE(casual)
-{
-    //typedef int T;
-    //typedef int U;
-    //typedef interval_map<T,U, total_absorber> IntervalMapT;
-    //typedef interval_set<T>                   IntervalSetT;
-    //typedef IntervalMapT::interval_type       IntervalT;
 
-    BOOST_CHECK_EQUAL(true, true);
+BOOST_AUTO_TEST_CASE(dummy)
+{
+    BOOST_CHECK(true);
 }
 
