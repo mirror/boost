30442098607ab9603dc236b0fdeac7f72832b4ed
jofaber <jofaber@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-01-31 21:13:17 +0000

    Added compiler specific tests to look for compiler flaws on all available platforms.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@68588 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/icl/test/cmp_clang_ttp_passing_/cmp_clang_ttp_passing.cpp
@@ -0,0 +1,45 @@
+/*-----------------------------------------------------------------------------+    
+Copyright (c) 2011-2011: Joachim Faulhaber
++------------------------------------------------------------------------------+
+   Distributed under the Boost Software License, Version 1.0.
+      (See accompanying file LICENCE.txt or copy at
+           http://www.boost.org/LICENSE_1_0.txt)
++-----------------------------------------------------------------------------*/
+#define BOOST_TEST_MODULE icl::cmp_clang_ttp_passing unit test
+#include <libs/icl/test/disable_test_warnings.hpp>
+#include <boost/config.hpp>
+#include "../unit_test_unwarned.hpp"
+
+
+namespace sep
+{
+
+template<class T>class less{};
+
+template
+<
+	class T, 
+    template<class>class less_T = sep::less
+>
+class cont{};
+
+template
+<
+	class T, 
+    template<class T2, 
+	template<class>class less_T2 = sep::less>class Cont
+>
+void func()
+{
+	typedef Cont<T> cont_type; 
+}
+
+}//namespace sep
+
+
+BOOST_AUTO_TEST_CASE(dummy)
+{
+	sep::func<int, sep::cont>();
+	BOOST_CHECK( true );
+}
+
