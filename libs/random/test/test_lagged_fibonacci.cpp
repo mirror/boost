9d2f997b8d6fdf7e3d908c23a1e721d10b251ba9
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-02-09 22:34:46 +0000

    Update lagged_fibonacci to match C++0x.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@68754 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/random/test/test_lagged_fibonacci.cpp
@@ -0,0 +1,17 @@
+/* test_lagged_fibonacci.cpp
+ *
+ * Copyright Steven Watanabe 2011
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * $Id$
+ *
+ */
+
+#include <boost/random/lagged_fibonacci.hpp>
+
+typedef boost::random::lagged_fibonacci_engine<boost::uint32_t, 24, 607, 273> lagged_fibonacci;
+#define BOOST_RANDOM_URNG lagged_fibonacci
+
+#include "test_generator.ipp"
---
+++ libs/random/test/test_lagged_fibonacci607.cpp
@@ -0,0 +1,16 @@
+/* test_lagged_fibonacci607.cpp
+ *
+ * Copyright Steven Watanabe 2011
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * $Id$
+ *
+ */
+
+#include <boost/random/lagged_fibonacci.hpp>
+
+#define BOOST_RANDOM_URNG boost::random::lagged_fibonacci607
+
+#include "test_generator.ipp"
