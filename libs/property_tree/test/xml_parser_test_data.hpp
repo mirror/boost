3d2caac66afeee00e739a6de740c986d32aa8d0e
cornedbee <cornedbee@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-07 09:45:58 +0000

    Better support for UTF-8. See bug 5203. Thanks to Qiaozhiqiang for tracking down the actual problem.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@69622 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/property_tree/test/xml_parser_test_data.hpp
+++ libs/property_tree/test/xml_parser_test_data.hpp
@@ -768,4 +768,9 @@ const char *bug_data_pr2855 =
 const char *bug_data_pr1678 =
     "\xEF\xBB\xBF<?xml version=\"1.0\" encoding=\"utf-8\"?><root/>";
 
+// russian
+const char *bug_data_pr5203 =
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+    "<name>\xD0\xAE\xD0\xBD\xD0\xB8\xD1\x82</name>";
+
 #endif
