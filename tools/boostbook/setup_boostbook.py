388d91699d87da82ba08d83dcdf7cf8c4ea1f69e
garcia <garcia@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-03-12 16:58:52 +0000

    FOP 1.0 is broken, so I found a working link to FOP 0.94. curl was mis-naming docbook-xsl-*.tar.gz.  Changing the form of the URL seems to do the trick (remove the "/download" part).
    git-svn-id: http://svn.boost.org/svn/boost/trunk@69895 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/boostbook/setup_boostbook.py
+++ tools/boostbook/setup_boostbook.py
@@ -13,9 +13,10 @@
 # (MAINTANERS: please, keep in synch with setup_boostbook.sh)
 DOCBOOK_XSL_VERSION = "1.75.2"
 DOCBOOK_DTD_VERSION = "4.2"
-FOP_VERSION = "1.0"
+FOP_VERSION = "0.94"
 FOP_JDK_VERSION="1.4"
-FOP_MIRROR = "http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries"
+# FOP_MIRROR = "http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries"
+FOP_MIRROR = "http://archive.apache.org/dist/xmlgraphics/fop/binaries/"
 SOURCEFORGE_DOWNLOAD = "http://sourceforge.net/projects/docbook/files"
 
 # No user configuration below this point-------------------------------------
