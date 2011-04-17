4f4c4e953797488374b01e00a4dadbd9e698ac57
steven_watanabe <steven_watanabe@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-04-17 00:23:44 +0000

    Merge [71083] from the trunk.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@71345 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/variant/test/test1.cpp
+++ libs/variant/test/test1.cpp
@@ -10,6 +10,12 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
+#include "boost/config.hpp"
+
+#ifdef BOOST_MSVC
+#pragma warning(disable:4244) // conversion from const int to const short
+#endif
+
 #include "boost/test/minimal.hpp"
 #include "boost/variant.hpp"
 
--- libs/variant/test/test2.cpp
+++ libs/variant/test/test2.cpp
@@ -11,6 +11,11 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include "boost/config.hpp"
+
+#ifdef BOOST_MSVC
+#pragma warning(disable:4244) // conversion from 'const int' to 'const short'
+#endif
+
 #include "boost/test/minimal.hpp"
 #include "boost/variant.hpp"
 
@@ -38,10 +43,10 @@ struct short_string
       using std::strlen;
 #endif // BOOST_NO_STDC_NAMESPACE
 
-      size_t e_limit = this->e_limit; // avoid warnings on some compilers
+      size_t limit = this->e_limit; // avoid warnings on some compilers
       size_t src_len = strlen(src);
       
-      len_ = (std::min)(src_len, e_limit-1);
+      len_ = (std::min)(src_len, limit-1);
       std::copy(src, src + len_, buffer_);
       buffer_[len_] = '\0';
    }
