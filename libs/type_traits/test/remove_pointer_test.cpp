b3d1657b629db31a7acd845e880267810720fa93
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-04-19 11:03:17 +0000

    Fix remove pointer so it works for cv-qualified function pointers in VC-10 (compiler bug workaround). Fixes #5484.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@71378 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/type_traits/test/remove_pointer_test.cpp
+++ libs/type_traits/test/remove_pointer_test.cpp
@@ -15,6 +15,7 @@
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_1, ::tt::remove_pointer, const, const)
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_2, ::tt::remove_pointer, volatile, volatile)
 BOOST_DECL_TRANSFORM_TEST3(remove_pointer_test_3, ::tt::remove_pointer, *)
+BOOST_DECL_TRANSFORM_TEST3(remove_pointer_test_3b, ::tt::remove_pointer, *const)
 BOOST_DECL_TRANSFORM_TEST0(remove_pointer_test_4, ::tt::remove_pointer)
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_5, ::tt::remove_pointer, const &, const&)
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_6, ::tt::remove_pointer, &, &)
@@ -30,12 +31,15 @@ BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_5a, ::tt::remove_pointer, const &&
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_6a, ::tt::remove_pointer, &&, &&)
 BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_13a, ::tt::remove_pointer, (&&)[2], (&&)[2])
 #endif
+BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_14, ::tt::remove_pointer, (*)(long), (long))
+BOOST_DECL_TRANSFORM_TEST(remove_pointer_test_14b, ::tt::remove_pointer, (*const)(long), (long))
 
 TT_TEST_BEGIN(remove_pointer)
 
    remove_pointer_test_1();
    remove_pointer_test_2();
    remove_pointer_test_3();
+   remove_pointer_test_3b();
    remove_pointer_test_4();
    remove_pointer_test_5();
    remove_pointer_test_6();
@@ -51,7 +55,8 @@ TT_TEST_BEGIN(remove_pointer)
    remove_pointer_test_6a();
    remove_pointer_test_13a();
 #endif
-
+   remove_pointer_test_14();
+   remove_pointer_test_14b();
 TT_TEST_END
 
 
