45ca29d940f82fb15f9cb04d50a2d48e790ad5f1
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-15 08:18:23 +0000

    Use of CLOCK_THREAD_CPUTIME_ID when defined
    git-svn-id: http://svn.boost.org/svn/boost/trunk@71951 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/chrono/detail/inlined/posix/thread_clock.hpp
+++ boost/chrono/detail/inlined/posix/thread_clock.hpp
@@ -21,6 +21,10 @@ namespace boost { namespace chrono {
 
     thread_clock::time_point thread_clock::now( ) 
     {
+#if defined CLOCK_THREAD_CPUTIME_ID
+        // get the timespec associated to the thread clock
+        if ( ::clock_gettime( CLOCK_THREAD_CPUTIME_ID, &ts ) )
+#else
         // get the current thread
         pthread_t pth=pthread_self();
         // get the clock_id associated to the current thread
@@ -29,6 +33,7 @@ namespace boost { namespace chrono {
         // get the timespec associated to the thread clock
         struct timespec ts;
         if ( ::clock_gettime( clock_id, &ts ) )
+#endif
         {
             boost::throw_exception(
                     system::system_error( 
@@ -44,9 +49,9 @@ namespace boost { namespace chrono {
     }
     thread_clock::time_point thread_clock::now( system::error_code & ec ) 
     {
-      struct timespec ts;
 #if defined CLOCK_THREAD_CPUTIME_ID
-      if ( ::clock_gettime( CLOCK_THREAD_CPUTIME_ID, &ts ) )
+        // get the timespec associated to the thread clock
+        if ( ::clock_gettime( CLOCK_THREAD_CPUTIME_ID, &ts ) )
 #else
         // get the current thread
         pthread_t pth=pthread_self();
@@ -54,6 +59,7 @@ namespace boost { namespace chrono {
         clockid_t clock_id;
         pthread_getcpuclockid(pth, &clock_id);
         // get the timespec associated to the thread clock
+        struct timespec ts;
         if ( ::clock_gettime( clock_id, &ts ) )
 #endif
         {
