5eb178c1923f8dd88542b71a09497078bb4fa237
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-24 15:11:37 +0000

    Applied patch; refs #1674
    git-svn-id: http://svn.boost.org/svn/boost/trunk@72132 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/date_time/date_formatting.hpp
+++ boost/date_time/date_formatting.hpp
@@ -79,7 +79,13 @@ namespace date_time {
     {
       typedef typename ymd_type::month_type month_type;
       std::basic_ostringstream<charT> ss;
+
+      // Temporarily switch to classic locale to prevent possible formatting
+      // of year with comma or other character (for example 2,008).
+      ss.imbue(std::locale::classic());
       ss << ymd.year;
+      ss.imbue(std::locale());
+
       if (format_type::has_date_sep_chars()) {
         ss << format_type::month_sep_char();
       }
