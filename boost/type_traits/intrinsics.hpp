ae5957847e3248ef540224eb108092a2dc126355
wash <wash@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-06-05 19:13:25 +0000

    Merge from trunk.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@72423 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/type_traits/intrinsics.hpp
+++ boost/type_traits/intrinsics.hpp
@@ -124,7 +124,7 @@
 #   define BOOST_HAS_TYPE_TRAITS_INTRINSICS
 #endif
 
-#if defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3) && !defined(__GCCXML__)))
+#if (defined(__GNUC__) && ((__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3) && !defined(__GCCXML__)))) || (__clang_major__ >= 3)
 #   include <boost/type_traits/is_same.hpp>
 #   include <boost/type_traits/is_reference.hpp>
 #   include <boost/type_traits/is_volatile.hpp>
