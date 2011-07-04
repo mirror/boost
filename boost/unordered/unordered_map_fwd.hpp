6f218b237f142f70f9dce527e8e0d00ba6272548
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-07-04 21:55:40 +0000

    Unordered: detab.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@72906 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/unordered/unordered_map_fwd.hpp
+++ boost/unordered/unordered_map_fwd.hpp
@@ -37,8 +37,8 @@ namespace boost
                 unordered_multimap<K, T, H, P, A>&);
     }
 
-	using ::boost::unordered::unordered_map;
-	using ::boost::unordered::unordered_multimap;
+    using ::boost::unordered::unordered_map;
+    using ::boost::unordered::unordered_multimap;
     using ::boost::unordered::swap;
     using ::boost::unordered::operator==;
     using ::boost::unordered::operator!=;
