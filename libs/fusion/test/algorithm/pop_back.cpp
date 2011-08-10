69ad2ea9c06e7859afd34b4594758c803866525b
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-10 22:01:37 +0000

    Added pop_back test for lists
    git-svn-id: http://svn.boost.org/svn/boost/trunk@73642 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/fusion/test/algorithm/pop_back.cpp
+++ libs/fusion/test/algorithm/pop_back.cpp
@@ -1,15 +1,17 @@
 /*=============================================================================
     Copyright (c) 2001-2006 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/fusion/container/vector/vector.hpp>
+#include <boost/fusion/container/list/list.hpp>
 #include <boost/fusion/adapted/mpl.hpp>
 #include <boost/fusion/sequence/io/out.hpp>
 #include <boost/fusion/sequence/comparison/equal_to.hpp>
 #include <boost/fusion/container/generation/make_vector.hpp>
+#include <boost/fusion/container/generation/make_list.hpp>
 #include <boost/fusion/algorithm/transformation/pop_back.hpp>
 #include <boost/mpl/vector_c.hpp>
 
@@ -39,6 +41,12 @@ main()
         typedef boost::mpl::vector_c<int, 1, 2, 3, 4, 5> mpl_vec;
         std::cout << boost::fusion::pop_back(mpl_vec()) << std::endl;
         BOOST_TEST((boost::fusion::pop_back(mpl_vec()) == make_vector(1, 2, 3, 4)));
+    }
+
+    {
+        list<int, int> l(1, 2);
+        std::cout << pop_back(l) << std::endl;
+        BOOST_TEST((pop_back(l) == make_list(1)));
     }
 
     return boost::report_errors();
