1360a99be9ac78f7e1686b5898b7fa914f8130da
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-13 16:21:39 +0000

    move proto tags into adl-blocking namespace
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74368 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/proto/debug.hpp
+++ boost/proto/debug.hpp
@@ -22,7 +22,7 @@
 
 namespace boost { namespace proto
 {
-    namespace tag
+    namespace tagns_ { namespace tag
     {
     #define BOOST_PROTO_DEFINE_TAG_INSERTION(Tag)                               \
         /** \brief INTERNAL ONLY */                                             \
@@ -80,7 +80,7 @@ namespace boost { namespace proto
         BOOST_PROTO_DEFINE_TAG_INSERTION(function)
 
     #undef BOOST_PROTO_DEFINE_TAG_INSERTION
-    }
+    }}
 
     namespace hidden_detail_
     {
--- boost/proto/proto_fwd.hpp
+++ boost/proto/proto_fwd.hpp
@@ -207,62 +207,67 @@ namespace boost { namespace proto
 
     ///////////////////////////////////////////////////////////////////////////////
     // Operator tags
-    namespace tag
+    namespace tagns_
     {
-        struct terminal;
-        struct unary_plus;
-        struct negate;
-        struct dereference;
-        struct complement;
-        struct address_of;
-        struct logical_not;
-        struct pre_inc;
-        struct pre_dec;
-        struct post_inc;
-        struct post_dec;
+        namespace tag
+        {
+            struct terminal;
+            struct unary_plus;
+            struct negate;
+            struct dereference;
+            struct complement;
+            struct address_of;
+            struct logical_not;
+            struct pre_inc;
+            struct pre_dec;
+            struct post_inc;
+            struct post_dec;
 
-        struct shift_left;
-        struct shift_right;
-        struct multiplies;
-        struct divides;
-        struct modulus;
-        struct plus;
-        struct minus;
-        struct less;
-        struct greater;
-        struct less_equal;
-        struct greater_equal;
-        struct equal_to;
-        struct not_equal_to;
-        struct logical_or;
-        struct logical_and;
-        struct bitwise_and;
-        struct bitwise_or;
-        struct bitwise_xor;
-        struct comma;
-        struct mem_ptr;
+            struct shift_left;
+            struct shift_right;
+            struct multiplies;
+            struct divides;
+            struct modulus;
+            struct plus;
+            struct minus;
+            struct less;
+            struct greater;
+            struct less_equal;
+            struct greater_equal;
+            struct equal_to;
+            struct not_equal_to;
+            struct logical_or;
+            struct logical_and;
+            struct bitwise_and;
+            struct bitwise_or;
+            struct bitwise_xor;
+            struct comma;
+            struct mem_ptr;
 
-        struct assign;
-        struct shift_left_assign;
-        struct shift_right_assign;
-        struct multiplies_assign;
-        struct divides_assign;
-        struct modulus_assign;
-        struct plus_assign;
-        struct minus_assign;
-        struct bitwise_and_assign;
-        struct bitwise_or_assign;
-        struct bitwise_xor_assign;
-        struct subscript;
-        struct member;
-        struct if_else_;
-        struct function;
+            struct assign;
+            struct shift_left_assign;
+            struct shift_right_assign;
+            struct multiplies_assign;
+            struct divides_assign;
+            struct modulus_assign;
+            struct plus_assign;
+            struct minus_assign;
+            struct bitwise_and_assign;
+            struct bitwise_or_assign;
+            struct bitwise_xor_assign;
+            struct subscript;
+            struct member;
+            struct if_else_;
+            struct function;
 
-        // Fusion tags
-        struct proto_expr;
-        struct proto_expr_iterator;
-        struct proto_flat_view;
+            // Fusion tags
+            struct proto_expr;
+            struct proto_expr_iterator;
+            struct proto_flat_view;
+        }
     }
+
+    using namespace tagns_;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     struct _;
