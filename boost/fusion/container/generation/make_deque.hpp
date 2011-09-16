704f4704e6ca91aa71a434332df2b04f6a13e1b5
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-16 05:30:23 +0000

    Bumped copyright date
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74407 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/container/generation/make_deque.hpp
+++ boost/fusion/container/generation/make_deque.hpp
@@ -1,12 +1,12 @@
 /*=============================================================================
-    Copyright (c) 2001-2006 Joel de Guzman
+    Copyright (c) 2001-2011 Joel de Guzman
     Copyright (c) 2006 Dan Marsden
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying 
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 /*=============================================================================
-    Copyright (c) 2001-2006 Joel de Guzman
+    Copyright (c) 2001-2011 Joel de Guzman
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying 
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- boost/fusion/container/vector/vector_fwd.hpp
+++ boost/fusion/container/vector/vector_fwd.hpp
@@ -1,8 +1,8 @@
 /*=============================================================================
     Copyright (c) 1999-2003 Jaakko Jarvi
-    Copyright (c) 2001-2006 Joel de Guzman
+    Copyright (c) 2001-2011 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #if !defined(FUSION_VECTOR_FORWARD_07072005_0125)
@@ -10,11 +10,20 @@
 
 #include <boost/fusion/container/vector/limits.hpp>
 #include <boost/preprocessor/repetition/enum_params_with_a_default.hpp>
-#include <boost/fusion/container/vector/vector10_fwd.hpp>
-#include <boost/fusion/container/vector/vector20_fwd.hpp>
-#include <boost/fusion/container/vector/vector30_fwd.hpp>
-#include <boost/fusion/container/vector/vector40_fwd.hpp>
-#include <boost/fusion/container/vector/vector50_fwd.hpp>
+
+#include <boost/fusion/container/vector/vector10.hpp>
+#if (FUSION_MAX_VECTOR_SIZE > 10)
+#include <boost/fusion/container/vector/vector20.hpp>
+#endif
+#if (FUSION_MAX_VECTOR_SIZE > 20)
+#include <boost/fusion/container/vector/vector30.hpp>
+#endif
+#if (FUSION_MAX_VECTOR_SIZE > 30)
+#include <boost/fusion/container/vector/vector40.hpp>
+#endif
+#if (FUSION_MAX_VECTOR_SIZE > 40)
+#include <boost/fusion/container/vector/vector50.hpp>
+#endif
 
 namespace boost { namespace fusion
 {
