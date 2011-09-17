17e46280cd33554adf019de6681c9087922b55fd
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-17 02:41:58 +0000

    Added preprocessed files to speed compilation
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74426 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/fusion/preprocess/preprocess.cpp
+++ libs/fusion/preprocess/preprocess.cpp
@@ -5,4 +5,6 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #include <boost/fusion/container/vector.hpp>
+#include <boost/fusion/container/list.hpp>
+#include <boost/fusion/tuple.hpp>
 
