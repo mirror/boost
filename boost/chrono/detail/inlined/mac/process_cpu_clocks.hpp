b80b202cb937e7388f7e7e9b1cfcddd10dfd02b0
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-18 08:42:33 +0000

    Chrono: Fix typo on Mac while adding noexceptl
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74445 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/chrono/detail/inlined/mac/process_cpu_clocks.hpp
+++ boost/chrono/detail/inlined/mac/process_cpu_clocks.hpp
@@ -117,28 +117,6 @@ process_real_cpu_clock::time_point process_real_cpu_clock::now(
     }
 }
 
-process_real_cpu_clock::time_point process_real_cpu_clock::now() BOOST_CHRONO_NOEXCEPT
-{
-    tms tm;
-    clock_t c = ::times( &tm );
-    if ( c == clock_t(-1) ) // error
-    {
-      BOOST_ASSERT(0 && "Boost::Chrono - Internal Error");
-    }
-    else
-    {
-        if ( chrono_detail::tick_factor() != -1 )
-        {
-            return time_point(
-                    microseconds(c)*chrono_detail::tick_factor());
-        }
-        else
-        {
-          BOOST_ASSERT(0 && "Boost::Chrono - Internal Error");
-        }
-    }
-    return time_point();
-}
 
 process_user_cpu_clock::time_point process_user_cpu_clock::now(
         system::error_code & ec)
@@ -191,6 +169,28 @@ process_user_cpu_clock::time_point process_user_cpu_clock::now(
     }
 }
 
+process_user_cpu_clock::time_point process_user_cpu_clock::now() BOOST_CHRONO_NOEXCEPT
+{
+    tms tm;
+    clock_t c = ::times( &tm );
+    if ( c == clock_t(-1) ) // error
+    {
+      BOOST_ASSERT(0 && "Boost::Chrono - Internal Error");
+    }
+    else
+    {
+        if ( chrono_detail::tick_factor() != -1 )
+        {
+          return time_point(
+                  microseconds(tm.tms_utime + tm.tms_cutime)*chrono_detail::tick_factor());
+        }
+        else
+        {
+          BOOST_ASSERT(0 && "Boost::Chrono - Internal Error");
+        }
+    }
+    return time_point();
+}
 process_system_cpu_clock::time_point process_system_cpu_clock::now() BOOST_CHRONO_NOEXCEPT
 {
     tms tm;
