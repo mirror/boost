07ece7fa7bf390b00890246a9f1282e9f21ae78a
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-18 14:12:53 +0000

    Adapt test to definition of BOOST_CHRONO_HAS_THREAD_CLOCK+BOOST_CHRONO_HAS_PROCESS_CLOCK
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74451 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/chrono/test/stopwatch/simple_stopwatch_pass.cpp
@@ -0,0 +1,128 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//  Adaptation to Boost of the libcxx
+//  Copyright 2010 Vicente J. Botet Escriba
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+#include <iostream>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/chrono/stopwatches/simple_stopwatch.hpp>
+#include <boost/chrono/process_cpu_clocks.hpp>
+#include <boost/chrono/thread_clock.hpp>
+#include <boost/chrono/chrono_io.hpp>
+#include <boost/system/system_error.hpp>
+#include <boost/detail/lightweight_test.hpp>
+
+#if !defined(BOOST_NO_STATIC_ASSERT)
+#define NOTHING ""
+#endif
+
+namespace ex
+{
+    template<class Rep, class Period>
+    void sleep_for(const boost::chrono::duration<Rep, Period>& d)
+    {
+      typedef boost::chrono::high_resolution_clock Clock;
+      typename Clock::time_point go =
+          Clock::now() + d;
+      while (Clock::now() < go)
+      {
+      }
+    }
+}
+
+template <typename Clock>
+void check_invariants()
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+    BOOST_CHRONO_STATIC_ASSERT((boost::is_same<typename Stopwatch::rep, typename Stopwatch::clock::duration::rep>::value), NOTHING, ());
+    BOOST_CHRONO_STATIC_ASSERT((boost::is_same<typename Stopwatch::period, typename Stopwatch::clock::duration::period>::value), NOTHING, ());
+    BOOST_CHRONO_STATIC_ASSERT((boost::is_same<typename Stopwatch::duration, typename Stopwatch::clock::time_point::duration>::value), NOTHING, ());
+    BOOST_CHRONO_STATIC_ASSERT(Stopwatch::is_steady == Stopwatch::clock::is_steady, NOTHING, ());
+    // to be replaced by has static member bool is_steady
+}
+
+template <typename Clock>
+void check_default_constructor()
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+  Stopwatch _;
+}
+
+template <typename Clock>
+void check_constructor_ec()
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+  boost::system::error_code ec;
+  Stopwatch _(ec);
+  BOOST_TEST(ec.value()==0);
+}
+
+template <typename Clock>
+void check_constructor_throws()
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+  Stopwatch _(boost::throws());
+}
+
+
+template <typename Clock>
+void check_elapsed(bool check=true)
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+  Stopwatch sw;
+  ex::sleep_for(boost::chrono::milliseconds(10));
+  typename Stopwatch::duration d=sw.elapsed();
+  //std::cout << d << std::endl;
+  //std::cout << boost::chrono::duration_cast<boost::chrono::milliseconds>(d) << std::endl;
+  if (check)
+  BOOST_TEST(boost::chrono::duration_cast<boost::chrono::milliseconds>(d)>=boost::chrono::milliseconds(10));
+}
+template <typename Clock>
+void check_elapsed2()
+{
+  typedef boost::chrono::simple_stopwatch<Clock> Stopwatch;
+  Stopwatch sw;
+  ex::sleep_for(boost::chrono::milliseconds(10));
+  typename Stopwatch::duration d=sw.elapsed();
+}
+
+
+template <typename Clock>
+void check_all(bool check=true)
+{
+  check_invariants<Clock>();
+  check_default_constructor<Clock>();
+  check_constructor_ec<Clock>();
+  check_constructor_throws<Clock>();
+  check_elapsed<Clock>(check);
+}
+
+int main()
+{
+  check_all<boost::chrono::high_resolution_clock>();
+#ifdef BOOST_CHRONO_HAS_CLOCK_STEADY
+  check_all<boost::chrono::steady_clock>();
+#endif
+  check_all<boost::chrono::system_clock>();
+
+#if defined(BOOST_CHRONO_HAS_THREAD_CLOCK)
+  check_all<boost::chrono::thread_clock>();
+#endif
+    
+#if defined(BOOST_CHRONO_HAS_PROCESS_CLOCKS)
+  check_all<boost::chrono::process_real_cpu_clock>();
+  check_all<boost::chrono::process_user_cpu_clock>(false);
+  check_all<boost::chrono::process_system_cpu_clock>(false);
+  check_all<boost::chrono::process_cpu_clock>();
+#endif
+    
+  return boost::report_errors();
+}
