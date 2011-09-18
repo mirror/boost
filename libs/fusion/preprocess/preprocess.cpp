0f7af1f06564c4f6894af1c9d18f5fafaec3ae56
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-18 10:45:06 +0000

    wave preprocessor driver update
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74448 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/fusion/preprocess/preprocess.cpp
+++ libs/fusion/preprocess/preprocess.cpp
@@ -4,7 +4,7 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#include <boost/fusion/container/vector.hpp>
-#include <boost/fusion/container/list.hpp>
+#include <boost/fusion/container.hpp>
+#include <boost/fusion/algorithm.hpp>
 #include <boost/fusion/tuple.hpp>
 
