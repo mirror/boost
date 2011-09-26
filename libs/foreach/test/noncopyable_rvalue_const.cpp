1c783a6f0f1af2826ac6e7c56db764d159f94cb9
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-09-26 18:10:45 +0000

    support for noncopyable rvalue ranges using C++0x
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74584 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/foreach/test/noncopyable_rvalue_const.cpp
@@ -0,0 +1,70 @@
+//  (C) Copyright Eric Niebler 2005.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/*
+  Revision history:
+  25 August 2005 : Initial version.
+*/
+
+#include <boost/test/minimal.hpp>
+#include <boost/foreach.hpp>
+
+#ifndef BOOST_FOREACH_USE_RVALUE_REFERENCE_BINDING
+# error Expected failure : non-copyable rvalues disallowed
+#else
+
+class my_container
+{
+public:
+    my_container()
+    {
+        array_[0] = 1;
+        array_[1] = 2;
+        array_[2] = 3;
+        array_[3] = 4;
+        array_[4] = 5;
+    }
+
+    typedef int* iterator;
+    typedef int const* const_iterator;
+
+    iterator begin() { return array_; }
+    const_iterator begin() const { return array_; }
+
+    iterator end() { return array_ + 5; }
+    const_iterator end() const { return array_ + 5; }
+
+private:
+    int array_[5];
+
+    // non-copyable
+    my_container(my_container const &);
+    my_container &operator =(my_container const &);
+
+    // non-movable
+    my_container(my_container &&);
+    my_container &operator =(my_container &&);
+};
+
+typedef my_container const const_my_container;
+
+///////////////////////////////////////////////////////////////////////////////
+// test_main
+//   
+int test_main( int, char*[] )
+{
+    int counter = 0;
+
+    BOOST_FOREACH(int i, const_my_container())
+    {
+        counter += i;
+    }
+
+    BOOST_CHECK(15 == counter);
+
+    return 0;
+}
+
+#endif
