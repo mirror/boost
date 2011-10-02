caf00357d93c9799f696422bcad46b177cd56f7a
viboes <viboes@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-10-02 18:08:24 +0000

    Chrono: update perf with times() and clock()
    git-svn-id: http://svn.boost.org/svn/boost/trunk@74653 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/chrono/perf/store_now_in_vector.cpp
+++ libs/chrono/perf/store_now_in_vector.cpp
@@ -6,7 +6,11 @@
 #include <boost/chrono/chrono.hpp>
 #include <boost/chrono/stopwatches/reporters/stopwatch_reporter.hpp>
 #include <boost/chrono/stopwatches/simple_stopwatch.hpp>
+#include <boost/chrono/process_cpu_clocks.hpp>
 #include <vector>
+#include <sys/time.h> //for gettimeofday and timeval
+#include <sys/times.h> //for times
+#include <unistd.h>
 
 static const std::size_t size = 1000000;
 
@@ -22,6 +26,30 @@ void perf(std::vector<typename Clock::time_point>& vec)
   }
 }
 
+void perf2(std::vector<clock_t>& vec)
+{
+  typedef boost::chrono::stopwatch_reporter<boost::chrono::simple_stopwatch<boost::chrono::high_resolution_clock> > Reporter;
+  Reporter rp;
+  for (int i=size; i>0; --i)
+  {
+    tms tm;
+    vec[i]=::times(&tm);
+    //vec[i]=typename Clock::time_point(typename Clock::time_point::duration(i));
+  }
+}
+
+void perf3(std::vector<clock_t>& vec)
+{
+  typedef boost::chrono::stopwatch_reporter<boost::chrono::simple_stopwatch<boost::chrono::high_resolution_clock> > Reporter;
+  Reporter rp;
+  for (int i=size; i>0; --i)
+  {
+    vec[i]=::clock();
+    //vec[i]=typename Clock::time_point(typename Clock::time_point::duration(i));
+  }
+}
+
+
 template <typename Clock>
 void test()
 {
@@ -32,6 +60,33 @@ void test()
   {
     if (vec[i]==vec[i-1]) ++cnt;
   }
+  std::cout << cnt << std::endl;
+}
+void test2()
+{
+  std::vector<clock_t> vec(size);
+  perf2(vec);
+  std::size_t cnt=0;
+  for (int i=10; i>1; --i)
+  {
+    if (vec[i]==vec[i-1]) ++cnt;
+    std::cout << vec[i] << " " ;
+  }
+  std::cout<< std::endl;
+  std::cout << cnt << std::endl;
+}
+
+void test3()
+{
+  std::vector<clock_t> vec(size);
+  perf3(vec);
+  std::size_t cnt=0;
+  for (int i=10; i>1; --i)
+  {
+    if (vec[i]==vec[i-1]) ++cnt;
+    std::cout << vec[i] << " " ;
+  }
+  std::cout<< std::endl;
   std::cout << cnt << std::endl;
 }
 
@@ -45,5 +100,7 @@ int main() {
   test<boost::chrono::process_real_cpu_clock>();
 #endif
   test<boost::chrono::high_resolution_clock>();
+  test2();
+  test3();
   return 1;
 }
