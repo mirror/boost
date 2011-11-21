874531f9750bb75646ed5983cb2ade8c5b9ff3bd
david.bellot <david.bellot@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-11-21 00:38:29 +0000

    added licenses where boost.inspect said they were missing
    git-svn-id: http://svn.boost.org/svn/boost/trunk@75583 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/numeric/ublas/test/comp_mat_erase.cpp
+++ libs/numeric/ublas/test/comp_mat_erase.cpp
@@ -1,3 +1,9 @@
+//  Copyright (c) 2011 David Bellot
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #include "boost/numeric/ublas/matrix_sparse.hpp"
 
 #define BOOST_TEST_MODULE SparseMatrixErasureTest
--- libs/numeric/ublas/test/utils.hpp
+++ libs/numeric/ublas/test/utils.hpp
@@ -1,4 +1,8 @@
-/** -*- c++ -*- \file utils.hpp \brief Test utilities. */
+//  Copyright (c) 2011 David Bellot
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef TEST_UTILS_HPP
 #define TEST_UTILS_HPP
