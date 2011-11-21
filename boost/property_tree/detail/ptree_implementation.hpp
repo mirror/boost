ef2a29192ec37fd3340d0d34282136f33aa3379a
cornedbee <cornedbee@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-11-21 13:11:18 +0000

    Improve pair member bug workaround: should now hopefully work with Intel; also improve code clarity. See bug #5307.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@75593 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/property_tree/detail/ptree_implementation.hpp
+++ boost/property_tree/detail/ptree_implementation.hpp
@@ -17,8 +17,10 @@
 #include <boost/utility/swap.hpp>
 #include <memory>
 
-#if defined(BOOST_MSVC) && \
-    (_MSC_FULL_VER >= 160000000 && _MSC_FULL_VER < 170000000)
+#if (defined(BOOST_MSVC) && \
+     (_MSC_FULL_VER >= 160000000 && _MSC_FULL_VER < 170000000)) || \
+    (defined(BOOST_INTEL_WIN) && \
+     defined(BOOST_DINKUMWARE_STDLIB))
 #define BOOST_PROPERTY_TREE_PAIR_BUG
 #endif
 
@@ -34,28 +36,23 @@ namespace boost { namespace property_tree
         // class. Unfortunately this does break the interface.
         BOOST_STATIC_CONSTANT(unsigned,
             first_offset = offsetof(value_type, first));
+#endif
         typedef multi_index_container<value_type,
             multi_index::indexed_by<
                 multi_index::sequenced<>,
                 multi_index::ordered_non_unique<multi_index::tag<by_name>,
+#if defined(BOOST_PROPERTY_TREE_PAIR_BUG)
                     multi_index::member_offset<value_type, const key_type,
                                         first_offset>,
-                    key_compare
-                >
-            >
-        > base_container;
 #else
-        typedef multi_index_container<value_type,
-            multi_index::indexed_by<
-                multi_index::sequenced<>,
-                multi_index::ordered_non_unique<multi_index::tag<by_name>,
                     multi_index::member<value_type, const key_type,
                                         &value_type::first>,
+#endif
                     key_compare
                 >
             >
         > base_container;
-#endif
+
         // The by-name lookup index.
         typedef typename base_container::template index<by_name>::type
             by_name_index;
