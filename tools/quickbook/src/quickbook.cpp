abd7aa34ed4d420398114618e15e69e4f252743a
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-11-27 21:48:00 +0000

    Quickbook: Update version.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@75698 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/quickbook/src/quickbook.cpp
+++ tools/quickbook/src/quickbook.cpp
@@ -36,7 +36,7 @@
 #pragma warning(disable:4355)
 #endif
 
-#define QUICKBOOK_VERSION "Quickbook Version 1.5.7 (dev)"
+#define QUICKBOOK_VERSION "Quickbook Version 1.5.7 (trunk)"
 
 namespace quickbook
 {
