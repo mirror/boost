d29bd32eae1859fb606f3115af78a52e6e627df9
jofaber <jofaber@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-12-30 20:53:12 +0000

    Added move semantics for T operators(...) according to the Dave-Abrahams-Transformation (http://cpp-next.com/archive/2009/09/making-your-next-move/). Worked like a charm ;)
    git-svn-id: http://svn.boost.org/svn/boost/trunk@76235 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/icl/test/test_casual_/test_casual.cpp
+++ libs/icl/test/test_casual_/test_casual.cpp
@@ -39,17 +39,6 @@ using namespace boost;
 using namespace unit_test;
 using namespace boost::icl;
 
-void pass_it(shared_ptr<int> pi)
-{
-    *pi = 41;
-    cout << "uses: " << pi.use_count() << " cont: " << *pi << endl;
-}
-
-void pass_it_ref(shared_ptr<int>& pi)
-{
-    *pi = 43;
-    cout << "uses: " << pi.use_count() << " cont: " << *pi << endl;
-}
 
 BOOST_AUTO_TEST_CASE(casual)
 {
@@ -59,45 +48,10 @@ BOOST_AUTO_TEST_CASE(casual)
     typedef interval_set<T>                     IntervalSetT;
     typedef IntervalMapT::interval_type         IntervalT;
 
-    shared_ptr<int> pi(new int(42));
-    cout << "uses: " << pi.use_count() << " cont: " << *pi << endl;
-    pass_it(pi);
-    pass_it_ref(pi);
+    IntervalSetT is = IntervalSetT(I_I(1,1)) + IntervalSetT(I_I(3,3));
+
+    cout << is << endl;
 
     BOOST_CHECK_EQUAL(true, true);
 }
 
-/*
-BOOST_AUTO_TEST_CASE(isEmptyTest)
-{
-    typedef int                         Value;
-    typedef boost::icl::interval<Value> Interval;
-    typedef std::numeric_limits<Value>  Limits;
-
-    Value const max(Limits::max());
-
-    Interval::interval_type piff = Interval::open(max,     max);
-
-    BOOST_CHECK(!icl::is_empty(Interval::open(max - 2, max)));
-    BOOST_CHECK( icl::is_empty(Interval::open(max - 1, max)));
-    BOOST_CHECK( icl::is_empty(Interval::open(max,     max)));
-}
-*/
-
-BOOST_AUTO_TEST_CASE(totalRangeTest)
-{
-    typedef int                                                              Value;
-    typedef boost::icl::interval<Value>                                      Interval;
-    typedef std::numeric_limits<Value>                                       Limits;
-    typedef boost::icl::interval_map<Value, int, boost::icl::total_enricher> Container;
-
-    Value const min(Limits::min());
-    Value const max(Limits::max());
-
-    boost::icl::interval_map<Value, int, boost::icl::total_enricher> intervals;
-
-    intervals += std::make_pair(Interval::closed(min, max), 0);
-    intervals += std::make_pair(Interval::right_open(0, 10),  3);
-
-    BOOST_CHECK_EQUAL(intervals.iterative_size(), 3);
-}
