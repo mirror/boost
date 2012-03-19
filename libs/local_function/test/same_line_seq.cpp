2f2894c7fd86e2c48528746886f5b9085f935e2f
lcaminiti <lcaminiti@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-03-19 18:09:12 +0000

    Added tests for ScopeExit and LocalFunction _ID macros.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@77407 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/local_function/test/same_line_seq.cpp
@@ -0,0 +1,34 @@
+
+// Copyright (C) 2009-2012 Lorenzo Caminiti
+// Distributed under the Boost Software License, Version 1.0
+// (see accompanying file LICENSE_1_0.txt or a copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// Home at http://www.boost.org/libs/local_function
+
+#include <boost/local_function.hpp>
+#include <boost/preprocessor/cat.hpp>
+#define BOOST_TEST_MODULE TestSameLineSeq
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+    
+#define LOCAL_INC_DEC(offset) \
+    int BOOST_LOCAL_FUNCTION_ID( \
+            BOOST_PP_CAT(inc, __LINE__) /* unique ID */, 0 /* no TPL */, \
+            (const bind offset) (const int x) ) { \
+        return x + offset; \
+    } BOOST_LOCAL_FUNCTION_NAME(inc) \
+    \
+    int BOOST_LOCAL_FUNCTION_ID( \
+            BOOST_PP_CAT(dec, __LINE__) /* unique ID */, 0 /* no TPL */, \
+            (const bind offset) (const int x) ) { \
+        return x - offset; \
+    } BOOST_LOCAL_FUNCTION_NAME(dec)
+
+BOOST_AUTO_TEST_CASE(test_same_line_seq)
+{
+    int delta = 10;
+    LOCAL_INC_DEC(delta) // Declare local functions on same line using `_ID`.
+    
+    BOOST_CHECK(dec(inc(123)) == 123);
+}
+
---
+++ libs/scope_exit/test/same_line.cpp
@@ -0,0 +1,63 @@
+
+// Copyright (C) 2006-2009, 2012 Alexander Nasonov
+// Copyright (C) 2012 Lorenzo Caminiti
+// Distributed under the Boost Software License, Version 1.0
+// (see accompanying file LICENSE_1_0.txt or a copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// Home at http://www.boost.org/libs/scope_exit
+
+#include <boost/config.hpp>
+#ifndef BOOST_NO_VARIADIC_MACROS
+
+#include <boost/scope_exit.hpp>
+#include <boost/preprocessor/cat.hpp>
+#define BOOST_TEST_MODULE TestSameLine
+#include <boost/test/unit_test.hpp>
+
+//[same_line
+#define SCOPE_EXIT_INC_DEC(variable, offset) \
+    BOOST_SCOPE_EXIT_ID( \
+            BOOST_PP_CAT(inc, __LINE__) /* unique ID */, 0 /* no TPL */, \
+            &variable, offset) { \
+        variable += offset; \
+    } BOOST_SCOPE_EXIT_END_ID(BOOST_PP_CAT(inc, __LINE__)) \
+    \
+    BOOST_SCOPE_EXIT_ID( \
+            BOOST_PP_CAT(dec, __LINE__) /* unique ID */, 0 /* no TPL */, \
+            &variable, offset) { \
+        variable -= offset; \
+    } BOOST_SCOPE_EXIT_END_ID(BOOST_PP_CAT(dec, __LINE__))
+
+#define SCOPE_EXIT_ALL_INC_DEC(variable, offset) \
+    BOOST_SCOPE_EXIT_ALL_ID(BOOST_PP_CAT(inc, __LINE__) /* unique ID */, \
+            =, &variable) { \
+        variable += offset; \
+    }; \
+    BOOST_SCOPE_EXIT_ALL_ID(BOOST_PP_CAT(dec, __LINE__) /* unique ID */, \
+            =, &variable) { \
+        variable -= offset; \
+    };
+
+BOOST_AUTO_TEST_CASE(test_same_line) {
+    int x = 0, delta = 10;
+
+    {
+        SCOPE_EXIT_INC_DEC(x, delta)
+    }
+    BOOST_CHECK(x == 0);
+
+#ifndef BOOST_NO_LAMBDAS
+    {
+        SCOPE_EXIT_ALL_INC_DEC(x, delta)
+    }
+    BOOST_CHECK(x == 0);
+#endif
+}
+//]
+
+#else
+
+int main(void) { return 0; } // Trivial test.
+
+#endif
+
