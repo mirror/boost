8b6b8800b5edd19af4fc42b9abecb9adc162f2db
djowel <djowel@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-04-07 07:43:05 +0000

    c++11 version start
    git-svn-id: http://svn.boost.org/svn/boost/trunk@77803 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/container/deque/cpp03_deque_fwd.hpp
+++ boost/fusion/container/deque/cpp03_deque_fwd.hpp
@@ -5,8 +5,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#if !defined(FUSION_DEQUE_FORWARD_02092007_0749)
-#define FUSION_DEQUE_FORWARD_02092007_0749
+#if !defined(FUSION_CPP03_DEQUE_FORWARD_02092007_0749)
+#define FUSION_CPP03_DEQUE_FORWARD_02092007_0749
 
 #include <boost/fusion/container/deque/limits.hpp>
 #include <boost/preprocessor/repetition/enum_params_with_a_default.hpp>
