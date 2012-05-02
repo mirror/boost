9a1e4cb2dbf342caf0396f509f5e621152ce065d
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-05-02 07:04:56 +0000

    merge [78286] from trunk, fixes #6846
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@78303 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/xpressive/traits/cpp_regex_traits.hpp
+++ boost/xpressive/traits/cpp_regex_traits.hpp
@@ -24,6 +24,7 @@
 #include <boost/assert.hpp>
 #include <boost/integer.hpp>
 #include <boost/mpl/assert.hpp>
+#include <boost/static_assert.hpp>
 #include <boost/detail/workaround.hpp>
 #include <boost/type_traits/is_same.hpp>
 #include <boost/xpressive/detail/detail_fwd.hpp>
@@ -89,7 +90,7 @@ namespace detail
     template<umaskex_t In, umaskex_t Out = highest_bit, bool Done = (0 == (Out & In))>
     struct unused_mask
     {
-        BOOST_MPL_ASSERT_RELATION(1, !=, Out);
+        BOOST_STATIC_ASSERT(1 != Out);
         BOOST_STATIC_CONSTANT(umaskex_t, value = (unused_mask<In, (Out >> 1)>::value));
     };
 
