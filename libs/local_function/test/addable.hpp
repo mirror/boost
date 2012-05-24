b50cb4dde65f07470d4b10d5b6c10770c3eefffe
lcaminiti <lcaminiti@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-05-24 01:35:04 +0000

    Merged ScopeExit (improved), LocalFunction (new), Functional/OverloadedFunction (new), and Utility/IdentityType (new) from trunk into release branch.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@78564 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/local_function/test/addable.hpp
@@ -0,0 +1,28 @@
+
+// Copyright (C) 2009-2012 Lorenzo Caminiti
+// Distributed under the Boost Software License, Version 1.0
+// (see accompanying file LICENSE_1_0.txt or a copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// Home at http://www.boost.org/libs/local_function
+
+#ifndef ADDABLE_HPP_
+#define ADDABLE_HPP_
+
+#include <boost/concept_check.hpp>
+
+template<typename T>
+struct Addable { // User-defined concept.
+    BOOST_CONCEPT_USAGE(Addable) {
+        return_type(x + y); // Check addition `T operator+(T x, T y)`.
+    }
+
+private:
+    T const& x;
+    T const& y;
+
+    void return_type(T const&) // Used to check addition returns type `T`.
+        {} // Do nothing implementation (required for some linkers).
+};
+
+#endif // #include guard
+
