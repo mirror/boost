1b7eca3e97df6a77edafca57bce6375ea4633568
emildotchevski <emildotchevski@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-06-20 05:08:25 +0000

    Removing tabs.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@79014 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/exception/detail/exception_ptr.hpp
+++ boost/exception/detail/exception_ptr.hpp
@@ -457,12 +457,12 @@ boost
         BOOST_ASSERT(p);
         p.ptr_->rethrow();
         BOOST_ASSERT(0);
-		#if defined(UNDER_CE)
-			// some CE platforms don't define ::abort(), let alone std::abort()
-			exit(-1);
-		#else
-			abort();
-		#endif
+        #if defined(UNDER_CE)
+            // some CE platforms don't define ::abort()
+            exit(-1);
+        #else
+            abort();
+        #endif
         }
 
     inline
