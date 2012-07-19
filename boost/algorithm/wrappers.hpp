19cf9944ededbdce7e0cab2491c902a07c540941
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-07-19 00:28:16 +0000

    Tabs. I likes them; the inspect tool, not so much
    git-svn-id: http://svn.boost.org/svn/boost/trunk@79590 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/algorithm/wrappers.hpp
+++ boost/algorithm/wrappers.hpp
@@ -7,43 +7,43 @@
    Alternate interfaces (aka "wrappers") for algorithms.
 */
 
-#ifndef	BOOST_ALGORITHM_WRAPPERS_HPP
-#define	BOOST_ALGORITHM_WRAPPERS_HPP
+#ifndef BOOST_ALGORITHM_WRAPPERS_HPP
+#define BOOST_ALGORITHM_WRAPPERS_HPP
 
 namespace boost { namespace algorithm {
 
 /// \fn find_ptr ( Container &c, Key k )
 /// \return a pointer to the value matching the key in the container, 
-///	or NULL if the key does not exist in the container.
+/// or NULL if the key does not exist in the container.
 ///
-///	\note: This is a wrapper around Container::find, with a useful interface.
-///	Suggested by Olaf van der Spek 
+/// \note: This is a wrapper around Container::find, with a useful interface.
+/// Suggested by Olaf van der Spek 
 /// 
 /// \param c    The container to be searched
-/// \param k	The key value to search with
+/// \param k    The key value to search with
 template <class Container, class Key>
 typename Container::value_type::second_type* 
 find_ptr ( Container &c, Key k )
 {
-	typename Container::iterator iter = c.find ( k );
-	return iter == c.end() ? NULL : &iter->second;
+    typename Container::iterator iter = c.find ( k );
+    return iter == c.end() ? NULL : &iter->second;
 }
 
 /// \fn find_ptr ( const Container &c, Key k )
 /// \return a pointer to the value matching the key in the container, 
-///	or NULL if the key does not exist in the container.
+/// or NULL if the key does not exist in the container.
 ///
-///	\note: This is a wrapper around Container::find, with a useful interface.
-///	Suggested by Olaf van der Spek 
+/// \note: This is a wrapper around Container::find, with a useful interface.
+/// Suggested by Olaf van der Spek 
 /// 
 /// \param c    The container to be searched
-/// \param k	The key value to search with
+/// \param k    The key value to search with
 template <class Container, class Key>
 const typename Container::value_type::second_type* 
 find_ptr ( const Container &c, Key k )
 {
-	typename Container::const_iterator iter = c.find ( k );
-	return iter == c.end() ? NULL : &iter->second;
+    typename Container::const_iterator iter = c.find ( k );
+    return iter == c.end() ? NULL : &iter->second;
 }
 
 
--- libs/algorithm/test/wrapper_test1.cpp
+++ libs/algorithm/test/wrapper_test1.cpp
@@ -19,52 +19,52 @@ namespace ba = boost::algorithm;
 
 void test_int ()
 {
-	std::map<int, int> m;
-	std::multimap<int, int> mm;
-	
-	int *ptr;
-	
-//	try with an empty map
-	BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
-	BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
-	
-	m.insert  ( std::make_pair <int, int> ( 5, 5 ));
-	mm.insert ( std::make_pair <int, int> ( 9, 9 ));
-	BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
-	BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
+    std::map<int, int> m;
+    std::multimap<int, int> mm;
+    
+    int *ptr;
+    
+//  try with an empty map
+    BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
+    BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
+    
+    m.insert  ( std::make_pair <int, int> ( 5, 5 ));
+    mm.insert ( std::make_pair <int, int> ( 9, 9 ));
+    BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
+    BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
 
-	ptr = ba::find_ptr ( m, 5 );
-	BOOST_CHECK ( ptr != NULL && *ptr == 5 );
-	BOOST_CHECK ( ba::find_ptr ( m , 9 ) == NULL );
+    ptr = ba::find_ptr ( m, 5 );
+    BOOST_CHECK ( ptr != NULL && *ptr == 5 );
+    BOOST_CHECK ( ba::find_ptr ( m , 9 ) == NULL );
 
-	ptr = ba::find_ptr ( mm, 9 );
-	BOOST_CHECK ( ptr != NULL && *ptr == 9 );
-	BOOST_CHECK ( ba::find_ptr ( mm, 5 ) == NULL );
-	
+    ptr = ba::find_ptr ( mm, 9 );
+    BOOST_CHECK ( ptr != NULL && *ptr == 9 );
+    BOOST_CHECK ( ba::find_ptr ( mm, 5 ) == NULL );
+    
 }
 
 void test_str ()
 {
-	std::map<int, std::string> m;
-	std::multimap<int, std::string> mm;
-	std::string *ptr;
+    std::map<int, std::string> m;
+    std::multimap<int, std::string> mm;
+    std::string *ptr;
 
-//	try with an empty map
-	BOOST_CHECK ( ba::find_ptr ( m , 31 ) == NULL );
-	BOOST_CHECK ( ba::find_ptr ( mm, 31 ) == NULL );
-	
-	m.insert  ( std::make_pair <int, std::string> ( 55, "fifty-five" ));
-	mm.insert ( std::make_pair <int, std::string> ( 66, "sixty-six" ));
-	BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
-	BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
+//  try with an empty map
+    BOOST_CHECK ( ba::find_ptr ( m , 31 ) == NULL );
+    BOOST_CHECK ( ba::find_ptr ( mm, 31 ) == NULL );
+    
+    m.insert  ( std::make_pair <int, std::string> ( 55, "fifty-five" ));
+    mm.insert ( std::make_pair <int, std::string> ( 66, "sixty-six" ));
+    BOOST_CHECK ( ba::find_ptr ( m , 3 ) == NULL );
+    BOOST_CHECK ( ba::find_ptr ( mm, 3 ) == NULL );
 
-	ptr = ba::find_ptr ( m, 55 );
-	BOOST_CHECK ( ptr != NULL && *ptr == "fifty-five" );
-	BOOST_CHECK ( ba::find_ptr ( m , 66 ) == NULL );
+    ptr = ba::find_ptr ( m, 55 );
+    BOOST_CHECK ( ptr != NULL && *ptr == "fifty-five" );
+    BOOST_CHECK ( ba::find_ptr ( m , 66 ) == NULL );
 
-	ptr = ba::find_ptr ( mm, 66 );
-	BOOST_CHECK ( ptr != NULL && *ptr == "sixty-six" );
-	BOOST_CHECK ( ba::find_ptr ( mm, 55 ) == NULL );
+    ptr = ba::find_ptr ( mm, 66 );
+    BOOST_CHECK ( ptr != NULL && *ptr == "sixty-six" );
+    BOOST_CHECK ( ba::find_ptr ( mm, 55 ) == NULL );
 }
 
 int test_main( int , char* [] )
