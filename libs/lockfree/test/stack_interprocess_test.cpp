4c0b0cfe40616b1f4125114508afe8df8bf34d88
timblechmann <timblechmann@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-09-09 13:12:43 +0000

    lockfree: fix some inspect warnings
    git-svn-id: http://svn.boost.org/svn/boost/trunk@80465 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/lockfree/test/stack_interprocess_test.cpp
+++ libs/lockfree/test/stack_interprocess_test.cpp
@@ -1,3 +1,9 @@
+//  Copyright (C) 2011 Tim Blechmann
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #include <cstdlib> //std::system
 #include <sstream>
 
--- libs/lockfree/test/test_helpers.hpp
+++ libs/lockfree/test/test_helpers.hpp
@@ -1,3 +1,9 @@
+//  Copyright (C) 2011 Tim Blechmann
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_LOCKFREE_TEST_HELPERS
 #define BOOST_LOCKFREE_TEST_HELPERS
 
