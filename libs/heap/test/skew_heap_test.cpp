3d4cf54ae1c21d81b6ded992f6bbb26a33ab4d06
timblechmann <timblechmann@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-09-09 13:07:24 +0000

    heap: fix some inspection warnings
    git-svn-id: http://svn.boost.org/svn/boost/trunk@80462 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/heap/test/skew_heap_test.cpp
+++ libs/heap/test/skew_heap_test.cpp
@@ -1,3 +1,11 @@
+/*=============================================================================
+    Copyright (c) 2010 Tim Blechmann
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
 #define BOOST_TEST_MAIN
 #ifdef BOOST_HEAP_INCLUDE_TESTS
 #include <boost/test/included/unit_test.hpp>
