652cad3e58a840310359932b026eb66d40d8a842
guwi17 <guwi17@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-09-10 20:50:10 +0000

    boost/numeric/ublas/traits.hpp, libs/numeric/ublas/test/test_assignment.cpp, libs/numeric/ublas/test/begin_end.cpp: see #7296, fix test issues with unsigned integer types
    git-svn-id: http://svn.boost.org/svn/boost/trunk@80483 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/numeric/ublas/traits.hpp
+++ boost/numeric/ublas/traits.hpp
@@ -41,6 +41,14 @@ namespace {
     // we'll find either std::abs or else another version via ADL:
     return abs (t);
   }
+  // unsigned types are always non-negative
+  template<> unsigned int boost_numeric_ublas_abs (const unsigned int& t) {
+    return t;
+  }
+  // unsigned types are always non-negative
+  template<> unsigned long boost_numeric_ublas_abs (const unsigned long& t) {
+    return t;
+  }
 }
 
 namespace boost { namespace numeric { namespace ublas {
