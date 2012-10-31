cb18e72318303a1c63e667c45151a7eaafb52e78
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-10-31 17:14:43 +0000

    Narrower 'using'; Refs #7470
    git-svn-id: http://svn.boost.org/svn/boost/trunk@81122 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/functional/hash/detail/hash_float.hpp
+++ boost/functional/hash/detail/hash_float.hpp
@@ -210,7 +210,7 @@ namespace boost
         template <class T>
         inline std::size_t float_hash_value(T v)
         {
-            using namespace std;
+            using namespace std::fpclassify;
             switch (fpclassify(v)) {
             case FP_ZERO:
                 return 0;
