7a20f0fb2432ad57adbe697a347db520e5367fcb
olli <olli@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-12-08 11:40:02 +0000

    coroutine: fix min/max problem triggered by Windows min/max macros
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@81790 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/coroutine/detail/stack_allocator_windows.hpp
+++ boost/coroutine/detail/stack_allocator_windows.hpp
@@ -89,16 +89,14 @@ public:
 
     static std::size_t default_stacksize()
     {
-        using namespace std;
-
         std::size_t size = 64 * 1024; // 64 kB
         if ( is_stack_unbound() )
-            return max( size, minimum_stacksize() );
+            return (std::max)( size, minimum_stacksize() );
 
         BOOST_ASSERT( maximum_stacksize() >= minimum_stacksize() );
         return maximum_stacksize() == minimum_stacksize()
             ? minimum_stacksize()
-            : min( size, maximum_stacksize() );
+            : ( std::min)( size, maximum_stacksize() );
     }
 
     // because Windows seams not to provide a limit for minimum stacksize
