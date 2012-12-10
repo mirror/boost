b844687ce1997c058d04ce596399647803f51960
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2012-12-10 04:39:12 +0000

    Added BOOST_TTI_HAS_DATA macro and functionality.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@81813 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/tti/has_member_data.hpp
+++ boost/tti/has_member_data.hpp
@@ -8,11 +8,11 @@
 #define BOOST_TTI_HAS_MEMBER_DATA_HPP
 
 #include <boost/preprocessor/cat.hpp>
-#include <boost/type_traits/remove_const.hpp>
+#include <boost/tti/detail/ddeftype.hpp>
+#include <boost/tti/detail/dmem_data.hpp>
 #include <boost/tti/gen/has_member_data_gen.hpp>
 #include <boost/tti/gen/namespace_gen.hpp>
-#include <boost/tti/detail/dmem_data.hpp>
-#include <boost/tti/detail/ddeftype.hpp>
+#include <boost/type_traits/remove_const.hpp>
 
 /*
 
@@ -32,7 +32,7 @@
 
     generates a metafunction called "trait" where 'trait' is the macro parameter.
     
-              template<class TTI_T,class TTI_Type>
+              template<class BOOST_TTI_TP_ET,class BOOST_TTI_TP_TYPE>
               struct trait
                 {
                 static const value = unspecified;
@@ -41,13 +41,13 @@
 
               The metafunction types and return:
     
-                TTI_T    = the enclosing type in which to look for our 'name'
-                                            OR
-                           The type of the member data in the form of a pointer
-                           to member data.
+                BOOST_TTI_TP_ET   = the enclosing type in which to look for our 'name'
+                                                         OR
+                                    The type of the member data in the form of a pointer
+                                    to member data.
                 
-                TTI_Type = (optional) The type of the member data if the first
-                           parameter is the enclosing type.
+                BOOST_TTI_TP_TYPE = (optional) The type of the member data if the first
+                                    parameter is the enclosing type.
                 
                 returns  = 'value' is true if the 'name' exists, with the correct data type,
                            otherwise 'value' is false.
@@ -55,14 +55,14 @@
 */
 #define BOOST_TTI_TRAIT_HAS_MEMBER_DATA(trait,name) \
   BOOST_TTI_DETAIL_TRAIT_HAS_MEMBER_DATA(trait,name) \
-  template<class TTI_T,class TTI_Type = BOOST_TTI_NAMESPACE::detail::deftype> \
+  template<class BOOST_TTI_TP_ET,class BOOST_TTI_TP_TYPE = BOOST_TTI_NAMESPACE::detail::deftype> \
   struct trait : \
     BOOST_PP_CAT(trait,_detail_hmd) \
       < \
-      typename BOOST_TTI_NAMESPACE::detail::dmem_get_type<TTI_T,TTI_Type>::type, \
+      typename BOOST_TTI_NAMESPACE::detail::dmem_get_type<BOOST_TTI_TP_ET,BOOST_TTI_TP_TYPE>::type, \
       typename boost::remove_const \
         < \
-        typename BOOST_TTI_NAMESPACE::detail::dmem_get_enclosing<TTI_T,TTI_Type>::type \
+        typename BOOST_TTI_NAMESPACE::detail::dmem_get_enclosing<BOOST_TTI_TP_ET,BOOST_TTI_TP_TYPE>::type \
         >::type \
       > \
     { \
@@ -76,7 +76,7 @@
 
     generates a metafunction called "has_member_data_name" where 'name' is the macro parameter.
     
-              template<class TTI_T,class TTI_Type>
+              template<class BOOST_TTI_TP_ET,class BOOST_TTI_TP_TYPE>
               struct has_member_data_name
                 {
                 static const value = unspecified;
@@ -85,13 +85,13 @@
 
               The metafunction types and return:
     
-                TTI_T    = the enclosing type in which to look for our 'name'
-                                            OR
-                           The type of the member data in the form of a pointer
-                           to member data.
+                BOOST_TTI_TP_ET   = the enclosing type in which to look for our 'name'
+                                                         OR
+                                    The type of the member data in the form of a pointer
+                                    to member data.
                 
-                TTI_Type = (optional) The type of the member data if the first
-                           parameter is the enclosing type.
+                BOOST_TTI_TP_TYPE = (optional) The type of the member data if the first
+                                    parameter is the enclosing type.
                 
                 returns  = 'value' is true if the 'name' exists, with the correct data type,
                            otherwise 'value' is false.
