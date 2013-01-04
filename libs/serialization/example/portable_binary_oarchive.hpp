d0e301a456a939f5751dd1f38de3b6527b928355
ramey <ramey@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-01-04 16:39:43 +0000

    merge to release
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@82352 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/serialization/example/portable_binary_oarchive.hpp
+++ libs/serialization/example/portable_binary_oarchive.hpp
@@ -34,7 +34,7 @@
 // exception to be thrown if integer read from archive doesn't fit
 // variable being loaded
 class portable_binary_oarchive_exception : 
-    public virtual boost::archive::archive_exception
+    public boost::archive::archive_exception
 {
 public:
     typedef enum {
