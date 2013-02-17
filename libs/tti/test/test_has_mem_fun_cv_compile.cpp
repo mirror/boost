685948e44c94d817c69881ed5b447c506c12cf5b
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-02-17 22:49:31 +0000

    Further tests for cv-qualification with member functions.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@82959 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/tti/test/test_has_mem_fun_cv_compile.cpp
+++ libs/tti/test/test_has_mem_fun_cv_compile.cpp
@@ -10,9 +10,9 @@
 int main()
   {
   
-  // You can always instantiate without compiler errors
+  // You can always instantiate without compiler errors even if the member function does not exist
   
-//  BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(someFunctionMember)<AnotherType,double,boost::mpl::vector<short,short,long,int> > aVar3;
+  BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(someFunctionMember)<AnotherType,double,boost::mpl::vector<short,short,long,int>,boost::function_types::const_qualified> aVar1;
   
   // Use const enclosing type
   
@@ -23,6 +23,26 @@ int main()
   
   BOOST_MPL_ASSERT((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(AConstFunction)<AType,double,boost::mpl::vector<long,char>,boost::function_types::const_qualified>));
   BOOST_MPL_ASSERT((StillTest<AnotherType,AType,boost::mpl::vector<int>,boost::function_types::const_qualified>));
+  
+  // Use volatile enclosing type
+  
+  BOOST_MPL_ASSERT((AnVol<volatile AnotherType,int,boost::mpl::vector<AType *,short> >));
+  BOOST_MPL_ASSERT((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(AVolatileFunction)<volatile AType,double,boost::mpl::vector<long,char> >));
+  
+  // Use volatile_qualified
+  
+  BOOST_MPL_ASSERT((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(StillVolatile)<AnotherType,bool,boost::mpl::vector<int>,boost::function_types::volatile_qualified>));
+  BOOST_MPL_ASSERT((Volly<AType,void,boost::mpl::vector<float,double>,boost::function_types::volatile_qualified>));
+  
+  // Use const volatile enclosing type
+  
+  BOOST_MPL_ASSERT((CVAnother<const volatile AnotherType,int,boost::mpl::vector<AType *,short> >));
+  BOOST_MPL_ASSERT((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(StillCV)<const volatile AnotherType,short,boost::mpl::vector<int> >));
+  
+  // Use cv_qualified
+  
+  BOOST_MPL_ASSERT((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(ConstVolFunction)<AType,void,boost::mpl::vector<float,double>,boost::function_types::cv_qualified>));
+  BOOST_MPL_ASSERT((CVBoth<AType,double,boost::mpl::vector<long,char>,boost::function_types::cv_qualified>));
   
   return 0;
 
--- libs/tti/test/test_has_member_cv.cpp
+++ libs/tti/test/test_has_member_cv.cpp
@@ -10,10 +10,26 @@
 int main()
   {
   
+  // Use const
+  
   BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(AConstFunction)<double (AType::*)(long,char) const>::value));
   BOOST_TEST((StillTest<AType (AnotherType::*)(int) const>::value));
   BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(AnotherConstFunction)<int (AnotherType::*)(AType *, short) const>::value));
   BOOST_TEST((AskIfConst<void (AType::*)(float,double) const>::value));
+  
+  // Use volatile
+  
+  BOOST_TEST((AnVol<int (AnotherType::*)(AType *,short) volatile>::value));
+  BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(AVolatileFunction)<double (AType::*)(long,char) volatile>::value));
+  BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(StillVolatile)<bool (AnotherType::*)(int) volatile>::value));
+  BOOST_TEST((Volly<void (AType::*)(float,double) volatile>::value));
+  
+  // Use const volatile
+  
+  BOOST_TEST((CVAnother<int (AnotherType::*)(AType *,short) const volatile>::value));
+  BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(StillCV)<short (AnotherType::*)(int) const volatile>::value));
+  BOOST_TEST((BOOST_TTI_HAS_MEMBER_FUNCTION_GEN(ConstVolFunction)<void (AType::*)(float,double) const volatile>::value));
+  BOOST_TEST((CVBoth<double (AType::*)(long,char) const volatile>::value));
   
   return boost::report_errors();
 
