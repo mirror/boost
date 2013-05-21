860248b7572988bd3db5e3e9a9ba7ea25accb59a
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-05-21 22:50:19 +0000

    Remove 'private' definition that I used for debugging.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@84408 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/unordered/test/unordered/noexcept_tests.cpp
+++ libs/unordered/test/unordered/noexcept_tests.cpp
@@ -2,7 +2,7 @@
 // Copyright 2013 Daniel James.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#define private public
+
 #include "../helpers/prefix.hpp"
 #include <boost/unordered_set.hpp>
 #include <boost/unordered_map.hpp>
