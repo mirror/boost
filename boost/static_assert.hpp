8e376ed33c07ce45f1d1d59fafc946a26ec3e023
johnmaddock <johnmaddock@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-06-21 12:27:49 +0000

    Merge static_assert fix from Trunk. Fixes #8644.
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@84858 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/static_assert.hpp
+++ boost/static_assert.hpp
@@ -30,7 +30,7 @@
 #  ifndef BOOST_NO_CXX11_VARIADIC_MACROS
 #     define BOOST_STATIC_ASSERT_MSG( ... ) static_assert(__VA_ARGS__)
 #  else
-#     define BOOST_STATIC_ASSERT_MSG( ... ) static_assert(__VA_ARGS__)
+#     define BOOST_STATIC_ASSERT_MSG( B, Msg ) BOOST_STATIC_ASSERT( B )
 #  endif
 #else
 #     define BOOST_STATIC_ASSERT_MSG( B, Msg ) BOOST_STATIC_ASSERT( B )
