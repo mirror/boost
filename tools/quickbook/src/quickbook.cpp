bb1aaa8759f3cd89530be8d610d7b78928e6776c
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-07-01 19:33:13 +0000

    1.6.0 alpha
    git-svn-id: http://svn.boost.org/svn/boost/trunk@84930 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- tools/quickbook/src/quickbook.cpp
+++ tools/quickbook/src/quickbook.cpp
@@ -40,7 +40,7 @@
 #pragma warning(disable:4355)
 #endif
 
-#define QUICKBOOK_VERSION "Quickbook Version 1.5.9"
+#define QUICKBOOK_VERSION "Quickbook Version 1.6.0 alpha (dev)"
 
 namespace quickbook
 {
