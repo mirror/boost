0aa7efd87b6795c3529bfbbd4aade6c0f1425b6a
eric_niebler <eric_niebler@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-07-02 02:10:04 +0000

    specialization of convert_impl for map_tag, refs #8759
    git-svn-id: http://svn.boost.org/svn/boost/trunk@84937 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/fusion/container/map/convert.hpp
+++ boost/fusion/container/map/convert.hpp
@@ -80,6 +80,30 @@ namespace boost { namespace fusion
         typedef result_of::as_map<Sequence const> gen;
         return gen::call(fusion::begin(seq), fusion::end(seq));
     }
+
+    namespace extension
+    {
+        template <typename T>
+        struct convert_impl;
+
+        template <>
+        struct convert_impl<map_tag>
+        {
+            template <typename Sequence>
+            struct apply
+            {
+                typedef typename
+                    result_of::as_map<Sequence>::type
+                type;
+
+                static type call(Sequence& seq)
+                {
+                    typedef result_of::as_map<Sequence> gen;
+                    return gen::call(fusion::begin(seq), fusion::end(seq));
+                }
+            };
+        };
+    }
 }}
 
 #endif
