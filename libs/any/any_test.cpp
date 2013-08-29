2840c430628c09e6e70e90ccf44c962ed4cb0328
apolukhin <apolukhin@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-08-29 11:30:58 +0000

    Merge tests of Boost.Any from trunk (refs #6999)
    git-svn-id: http://svn.boost.org/svn/boost/branches/release@85511 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/any/any_test.cpp
+++ libs/any/any_test.cpp
@@ -1,6 +1,6 @@
 // what:  unit tests for variant type boost::any
 // who:   contributed by Kevlin Henney
-// when:  July 2001
+// when:  July 2001, 2013
 // where: tested with BCC 5.5, MSVC 6.0, and g++ 2.95
 
 #include <cstdlib>
@@ -38,6 +38,7 @@ namespace any_tests // test suite
     void test_cast_to_reference();
     void test_with_array();
     void test_with_func();
+    void test_clear();
 
     const test_case test_cases[] =
     {
@@ -51,7 +52,8 @@ namespace any_tests // test suite
         { "copying operations on a null",   test_null_copying      },
         { "cast to reference types",        test_cast_to_reference },
         { "storing an array inside",        test_with_array        },
-        { "implicit cast of returned value",test_with_func         }
+        { "implicit cast of returned value",test_with_func         },
+        { "clear() methods",                test_clear             }
     };
 
     const test_case_iterator begin = test_cases;
@@ -286,26 +288,46 @@ namespace any_tests // test definitions
         std::string s;
         s = any_cast<std::string>(returning_string1());
         s = any_cast<const std::string&>(returning_string1());
-        //s = any_cast<std::string&>(returning_string1());
 
         s = any_cast<std::string>(returning_string2());
         s = any_cast<const std::string&>(returning_string2());
-        //s = any_cast<std::string&>(returning_string2());
 
-#if !defined(_MSC_VER) || _MSC_VER != 1600
-#ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
-        //s = static_cast<std::string&&>(any_cast<std::string&>(returning_string1()));
+#if (!defined(_MSC_VER) || _MSC_VER != 1600) && !defined(BOOST_NO_CXX11_RVALUE_REFERENCES) 
+#if !defined(__INTEL_COMPILER) && !defined(__ICL)
+        // Intel compiler thinks that it must choose the `any_cast(const any&)` function 
+        // instead of the `any_cast(const any&&)`.
+        // Bug was not reported because of missing premier support account + annoying 
+        // registrations requirements.
         s = any_cast<std::string&&>(returning_string1());
-
-        //s = static_cast<std::string&&>(any_cast<std::string&>(returning_string2()));
-        s = any_cast<std::string&&>(returning_string2());
 #endif
+        s = any_cast<std::string&&>(returning_string2());
 #endif
     }
 
+    
+    void test_clear()
+    {
+        std::string text = "test message";
+        any value = text;
+
+        check_false(value.empty(), "empty");
+        
+        value.clear();
+        check_true(value.empty(), "non-empty after clear");
+
+        value.clear();
+        check_true(value.empty(), "non-empty after second clear");
+
+        value = text;
+        check_false(value.empty(), "empty");
+        
+        value.clear();
+        check_true(value.empty(), "non-empty after clear");
+    }
 }
 
 // Copyright Kevlin Henney, 2000, 2001. All rights reserved.
+// Copyright Antony Polukhin, 2013.
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
