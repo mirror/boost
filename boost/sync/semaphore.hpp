7d6ddb879045528ecfd1df85080b98f54f10ef87
timblechmann <timblechmann@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-09-15 11:05:46 +0000

    sync: add doxygen documentation for semaphore
    git-svn-id: http://svn.boost.org/svn/boost/trunk@85674 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/sync/semaphore.hpp
+++ boost/sync/semaphore.hpp
@@ -9,6 +9,72 @@
 #ifndef BOOST_SYNC_SEMAPHORE_HPP
 #define BOOST_SYNC_SEMAPHORE_HPP
 
+#ifdef BOOST_SYNC_DETAIL_DOXYGEN
+
+namespace boost {
+namespace sync  {
+
+class semaphore
+{
+    /**
+     * \b Effects: Constructs a semaphore object. The semaphore is initialized to `initial_count`, which is expected to be non-negative.
+     *
+     * \b Throws: if an error occurs.
+     *
+     * */
+    semaphore(int initial_count = 0);
+
+    /**
+     * \b Precondition: No thread is waiting on the semaphore
+     *
+     * \b Effects: Destroys the object
+     *
+     * */
+    ~semaphore();
+
+    /**
+     * \b Effects: Increments the semaphore count. If a thread is waiting for this semaphore, it will be unblocked.
+     *
+     * \b Throws: if an error occurs.
+     * */
+    void post();
+
+    /**
+     * \b Effects: If the semaphore count is positive, it atomically decrements it and returns. Otherwise blocks the current thread, until it can successfully decrement a positive semaphore count.
+     *
+     * \b Throws: if an error occurs.
+     * */
+    void wait();
+
+    /**
+     * \b Effects: If the semaphore count is positive, it atomically decrements it and returns `true`. Otherwise `false`.
+     *
+     * \b Throws: if an error occurs.
+     * */
+    bool try_wait();
+
+    /**
+     * \b Effects: If the semaphore count is positive, it atomically decrements it and returns `true`. Otherwise it waits for the semaphore for `duration`.
+     *
+     * \b Throws: if an error occurs.
+     * */
+    template <class Duration>
+    bool try_wait_for(const Duration & duration);
+
+    /**
+     * \b Effects: If the semaphore count is positive, it atomically decrements it and returns `true`. Otherwise it waits for the semaphore until `timeout`.
+     *
+     * \b Throws: if an error occurs.
+     * */
+    template <class TimePoint>
+    bool try_wait_until(const TimePoint & timeout);
+};
+
+
+}}
+
+#else
+
 #include <boost/sync/detail/config.hpp>
 
 #ifdef BOOST_HAS_PRAGMA_ONCE
@@ -34,5 +100,7 @@
 #endif
 
 #include <boost/sync/detail/footer.hpp>
+
+#endif
 
 #endif // BOOST_THREAD_SEMAPHORE_HPP
