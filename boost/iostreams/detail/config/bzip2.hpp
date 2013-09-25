78a8460c98105298df84f0478113d78c393c8cc2
skelly <skelly@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-09-25 13:23:57 +0000

    IOstreams: Remove obsolete MSVC version check
    git-svn-id: http://svn.boost.org/svn/boost/trunk@85900 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/iostreams/detail/config/bzip2.hpp
+++ boost/iostreams/detail/config/bzip2.hpp
@@ -19,7 +19,7 @@
 # if defined(BOOST_MSVC) || \
      defined(__BORLANDC__) || \
      (defined(__MWERKS__) && defined(_WIN32) && (__MWERKS__ >= 0x3000)) || \
-     (defined(__ICL) && defined(_MSC_EXTENSIONS) && (_MSC_VER >= 1200)) \
+     (defined(__ICL) && defined(_MSC_EXTENSIONS)) \
      /**/
 
 // Specify the name of the .lib file.
