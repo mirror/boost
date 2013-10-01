57223fa623f6e9a846ea713bff94ebab46e929e8
skelly <skelly@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-10-01 08:43:30 +0000

    Container: Remove obsolete GCC version check.
    git-svn-id: http://svn.boost.org/svn/boost/trunk@86109 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/detail/container_fwd.hpp
+++ boost/detail/container_fwd.hpp
@@ -119,12 +119,7 @@ namespace std
     template <class T> class allocator;
     template <class charT, class traits, class Allocator> class basic_string;
 
-#if BOOST_WORKAROUND(__GNUC__, < 3) && !defined(__SGI_STL_PORT) && !defined(_STLPORT_VERSION)
-
-    template <class charT> struct string_char_traits;
-#else
     template <class charT> struct char_traits;
-#endif
 
 #if defined(BOOST_CONTAINER_FWD_COMPLEX_STRUCT)
     template <class T> struct complex;
