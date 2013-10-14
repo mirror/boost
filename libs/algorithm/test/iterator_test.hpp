a97066c51b1cb9e748993debeb183c1973a24cc8
marshall <marshall@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-10-14 21:35:20 +0000

    Updated license and copyright
    git-svn-id: http://svn.boost.org/svn/boost/trunk@86311 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/algorithm/test/iterator_test.hpp
+++ libs/algorithm/test/iterator_test.hpp
@@ -1,3 +1,12 @@
+/* 
+   Copyright (c) Marshall Clow 2013.
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+    For more information, see http://www.boost.org
+*/
+
 #ifndef ITERATOR_TEST_H
 #define ITERATOR_TEST_H
 
