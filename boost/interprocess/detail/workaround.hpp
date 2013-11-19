9d51b54dffe63d56b5c70f289b2195a7a810ccb2
igaztanaga <igaztanaga@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2013-11-19 11:53:51 +0000

    Fixes #9408 ("Android does not support XSI_SHARED_MEMORY_OBJECTS")
    git-svn-id: http://svn.boost.org/svn/boost/trunk@86765 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/interprocess/detail/workaround.hpp
+++ boost/interprocess/detail/workaround.hpp
@@ -73,7 +73,7 @@
    #endif
 
    //Check for XSI shared memory objects. They are available in nearly all UNIX platforms
-   #if !defined(__QNXNTO__)
+   #if !defined(__QNXNTO__) && !defined(__ANDROID__)
       #define BOOST_INTERPROCESS_XSI_SHARED_MEMORY_OBJECTS
    #endif
 
